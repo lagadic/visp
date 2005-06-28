faea987d18d67890c89c1336cd4e1297b6f9add9
Eric Marchand <Eric.Marchand@irisa.fr>
2005-06-28 12:55:19 +0000

    modif des exceptions

--- src/robot/robot/vpRobotException.h
+++ src/robot/robot/vpRobotException.h
@@ -11,7 +11,7 @@
  * Version control
  * ===============
  *
- *  $Id: vpRobotException.h,v 1.1.1.1 2005-06-08 07:08:09 fspindle Exp $
+ *  $Id: vpRobotException.h,v 1.2 2005-06-28 12:55:19 marchand Exp $
  *
  * Description
  * ============
@@ -55,46 +55,46 @@ public:
     {
 
 	    /** Erreur lancee par le constructor. */
-	    ERRConstruction,
+	    constructionError,
 
 	    /** Erreur lancee lors de la construction d'un objet CRobot
 	     * correspondant a un robot reel si l'objet de la classe en
 	     * question doit etre unique. */
-	    ERRUniqueRobot,
+	    uniqueRobotError,
 
 	    /** Erreur lancee par les fonctions de commande si le
 	     * robot n'est pas dans le bon etat au moment du passage
 	     * d'ordre. */
-	    ERRWrongState,
+	    wrongStateError,
 
 	    /** Erreur lancee par les fonctions de changement d'etat
 	     * si le changement demandee n'est pas possible. */
-	    ERRStateModification,
+	    stateModificationError,
 
 	    /** Erreur generee lors d'un retour non nulle d'une fonction
 	     * de communication de la lib Afma6. */
-	    ERRCommunication,
+	    communicationError,
 
 	    /** Erreur lancee apres un appel a une fonction de la lib
 	     * bas-niveau de control de l'afma6 ayant renvoye une erreur. */
-	    ERRLowLevel,
+	    lowLevelError,
 
 	    /** Erreur lancee par la fonction de parsing des parametres du
 	     * robot, si le fichier donne en entree n'est pas valide.
 	     */
-	    ERRReadingParameters,
+	    readingParametersError,
 
 	    /** Erreur lancee par les methodes d'une classe qui necessite
 	     * un appel a une fonction d'initialisation apres la
 	     * construction si l'init n'a pas ete fait. */
-	    ERRNotInitialized,
+	    notInitializedError,
 
 	    /** Erreur lancee par les fonctions decrites dans lAPI mais
 	     * pas completement implementee. Dans ce cas, la fonction
 	     * affiche simplement un message d'erreur avant de sortir
 	     * par le 'throw'.
 	     */
-	    ERRNotImplemented
+	    notImplementedError
     } ;
 
 public:
