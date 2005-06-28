6d448d4e8a05f7d26b8d91f176ec1c355649b4dc
Eric Marchand <Eric.Marchand@irisa.fr>
2005-06-28 08:35:43 +0000

    generateur de bruit gaussien et blanc

---
+++ ViSP/src/math/misc/vpNoise.cpp
@@ -0,0 +1,124 @@
+#include <visp/vpNoise.h>
+#include <math.h>
+
+/*!
+  \file noise.cpp
+  \brief Classe for generating random number
+  with uniform and normal probability density
+
+  The algorithms and notations used are described in
+
+  James E. Gentle, Random Number Generation and Monte Carlo Methods,
+  Springer 1998
+*/
+
+
+#define a  (unsigned long)16807
+#define m  (unsigned long)2147483647
+#define q  (unsigned long)127773
+#define r  (unsigned long)2836
+#define normalizer  (double)(2147484721.0)
+
+/*!  \brief Minimal random number generator of Park and Miller. Returns a
+  uniform random deviate between 0.0 and 1.0.
+
+  S.K. Park and K.W. Miller, " Random Number Generators: Good Ones Are Hard To
+  Find", Communications of the ACM, October 1988, pp. 1192-1201.
+
+
+*/
+inline void
+vpUniRand::draw0()
+//minimal standard
+//Park and Miller random number generator
+{
+  unsigned long k= x/q;//temp value for computing without overflow
+  x = a*(x-k*q)-k*r;
+  if (x < 0) x += m; //compute x without overflow
+}
+
+/*!
+  \brief Bays-Durham Shuffling of Park-Miller generator
+
+  Minimal random number generator of Park and Miller with Bays-Durham
+  shuffle. Returns a uniform random deviate between 0.0 and 1.0 (exclusive of
+  the endpoint values).
+*/
+double
+vpUniRand::draw1()
+{
+  const unsigned char ntab = 33;  //we work on a 32 elements array.
+                                  //the 33rd one is actually the first value of y.
+  const unsigned char modulo = ntab-2;
+
+  static long y = 0;
+  static long T[ntab];
+
+  unsigned char j; //index of T
+
+  //step 0
+  if (!y) { //first time
+    for(j = 0; j < ntab; j++) {
+      draw0();
+      T[j]=x;
+    } //compute table T
+    y=T[ntab-1];
+  }
+
+  //step 1
+  j = y & modulo; //compute modulo ntab+1 (the first element is the 0th)
+
+  //step 3
+  y=T[j];
+  double ans = (double)y/normalizer;
+
+  //step 4
+  //generate x(k+i) and set y=x(k+i)
+  draw0();
+
+  //refresh T[j];
+  T[j]=x;
+
+  return ans;
+}
+
+/*!
+  \brief Generate a normal random variable using the Box-Muller generator
+
+  Generate a normal random variable with mean 0 and standard deviation of 1.
+   To adjust to some other distribution, multiply by the standard deviation and
+   add the mean.  Box-Muller method
+*/
+double
+vpGaussRand::gaussianDraw()
+{
+  double v1, v2, rsq;
+  static bool AlreadyDone = false;
+  static double x2;
+
+  if (AlreadyDone) {
+    AlreadyDone=false;
+    return x2;
+  }
+
+  else {
+
+    do {
+      v1=2*draw1()-1;
+      v2=2*draw1()-1;
+      rsq=v1*v1+v2*v2;
+    } while (rsq >= 1);
+
+  double fac=sqrt(-2*log(rsq)/rsq);
+  x2=v2*fac;
+  AlreadyDone=true;
+  return v1*fac;
+  }
+}
+#undef a
+#undef m
+#undef q
+#undef r
+#undef normalizer
+
+
