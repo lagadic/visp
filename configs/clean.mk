9057f016a4cdec88d1c158f056eef4d80c43660e
Fabien Spindler <Fabien.Spindler@inria.fr>
2005-07-13 07:42:14 +0000

    clean also other files (tca*, pchdir, *.d, .inslog2)

--- configs/clean.mk
+++ configs/clean.mk
@@ -2,3 +2,19 @@ clean:
 	rm -f *%
 	rm -f *~
 	rm -f *.o
+	@for i in `find -type f -name tca.* -print | sort` ; do\
+		rm -f $$i;\
+		echo "rm -f " $$i;\
+	done
+	@for i in `find -type d -name pchdir -print | sort` ; do\
+		rm -fr $$i;\
+		echo "rm -fr " $$i;\
+	done
+	@for i in `find -type f -name *.d -print | sort` ; do\
+		rm -f $$i;\
+		echo "rm -f " $$i;\
+	done
+	@for i in `find -type f -name .inslog2 -print | sort` ; do\
+		rm -f $$i;\
+		echo "rm -f " $$i;\
+	done
