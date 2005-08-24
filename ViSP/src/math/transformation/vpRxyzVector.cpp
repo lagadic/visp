cd3d177932d14213b8979bde729bc562d874fcb7
Francois Chaumette <Francois.Chaumette@inria.fr>
2005-08-24 15:08:22 +0000

    vpRxyzVector::buildFrom(const vpRotationMatrix& R)
    - mise au point d'une nouvelle version plus robuste
    - mise en place d'une verification de la nouvelle version (A activer
      avec DEBUG_LEVEL1)

--- ViSP/src/math/transformation/vpRxyzVector.cpp
+++ ViSP/src/math/transformation/vpRxyzVector.cpp
@@ -12,7 +12,7 @@
  * Version control
  * ===============
  *
- *  $Id: vpRxyzVector.cpp,v 1.3 2005-07-19 15:18:42 obourqua Exp $
+ *  $Id: vpRxyzVector.cpp,v 1.4 2005-08-24 15:08:22 chaumett Exp $
  *
  * Description
  * ============
@@ -25,6 +25,7 @@
 #include <math.h>
 #include <vpRxyzVector.h>
 
+#define DEBUG_LEVEL1 0
 /*!
   \file vpRxyzVector.cpp
   \brief class that consider the case of the  Rxyz angle parameterization for the rotation :
@@ -70,27 +71,67 @@ vpRxyzVector::vpRxyzVector(const vpThetaUVector& tu)
 }
 
 //! convert a rotation matrix into Rxyz vector
+
+#ifdef COEF_MIN_ROT
+#undef COEF_MIN_ROT
+#endif
+#define COEF_MIN_ROT 1e-6
 vpRxyzVector
 vpRxyzVector::buildFrom(const vpRotationMatrix& R)
 {
 
+  double phi ;
+
+  if ((fabs(R[1][2]) < COEF_MIN_ROT) && (fabs(R[2][2]) < COEF_MIN_ROT)) phi = 0 ;
+  else phi = atan2(-R[1][2], R[2][2]) ;
+
+  double si = sin(phi) ;
+  double co = cos(phi) ;
+  double theta = atan2(R[0][2], -si*R[1][2] + co*R[2][2]) ;
+  double psi = atan2(co*R[1][0] + si*R[2][0], co*R[1][1] + si*R[2][1]);
+
+  r[0] = phi ;
+  r[1] = theta ;
+  r[2] = psi ;
+
+  if (DEBUG_LEVEL1)  // test new version wrt old version
+  {
+    // old version
+
     double  v1;
+    double r2[3];  // has to be replaced by r below if good version
+
     v1 = R[0][2];
     if (v1 > 1.0 ) v1 = 1.0;
     if (v1 < -1.0 ) v1 = -1.0;
-    r[1] = asin(v1);
-    if ( fabs(fabs(r[1]) - M_PI_2) < 0.00001)
+    r2[1] = asin(v1);
+    if ( fabs(fabs(r2[1]) - M_PI_2) < 0.00001)
     {
-	r[0] = 0.0;
-	r[2] = atan2(R[1][0],R[1][1]);
+	r2[0] = 0.0;
+	r2[2] = atan2(R[1][0],R[1][1]);
     }
     else
     {
-	r[0] = atan2(-R[1][2],R[2][2]);
-	r[2] = atan2(-R[0][1],R[0][0]);
+	r2[0] = atan2(-R[1][2],R[2][2]);
+	r2[2] = atan2(-R[0][1],R[0][0]);
     }
+    // verification of the new version
+    int pb = 0;
+    int i;
+    for (i=0;i<3;i++)
+    {
+      if (fabs(r[i] - r2[i]) > 1e-5) pb = 1;
+    }
+    if (pb == 1)
+    {
+      printf("vpRxyzVector::buildFrom(const vpRotationMatrix& R)\n");
+      printf(" r      : %lf %lf %lf\n",r[0],r[1],r[2]);
+      printf(" r2     : %lf %lf %lf\n",r2[0],r2[1],r2[2]);
+      printf(" r - r2 : %lf %lf %lf\n",r[0]-r2[0],r[1]-r2[1],r[2]-r2[2]);
+    }
+  }
 
-
+    // What is below corresponds to another representation, but which one???
   /* double phi ;
   if ((fabs(R[1][2]) < 1e-6)&&(fabs(R[2][2]) < 1e-6)) phi = 0 ;
   else phi = atan2(R[1][2], R[2][2]) ;
@@ -106,7 +147,7 @@ vpRxyzVector::buildFrom(const vpRotationMatrix& R)
 
   return *this ;
 }
-
+#undef COEF_MIN_ROT
 
 //! convert a rotation matrix into Theta U vector
 vpRxyzVector
@@ -120,7 +161,7 @@ vpRxyzVector::buildFrom(const vpThetaUVector& tu)
     return *this ;
 }
 
-
+#undef DEBUG_LEVEL1
 /*
  * Local variables:
  * c-basic-offset: 2
