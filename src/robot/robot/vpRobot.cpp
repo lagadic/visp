a6c85b49bf8ecb8e45ac645b072af3781bd71644
Nicolas Mansard <Nicolas Mansard@inria.fr>
2005-09-07 13:44:16 +0000

    Add the two fields vpRobot:max{Rot|Transl}ationVelocity, and the corresponding accessor/modifiors.

--- src/robot/robot/vpRobot.cpp
+++ src/robot/robot/vpRobot.cpp
@@ -1,6 +1,24 @@
 #include <visp/vpRobot.h>
 #include <visp/vpDebug.h>
 
+/* -------------------------------------------------------------------------- */
+/* --- CONSTRUCTEUR --------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+
+vpRobot::
+vpRobot (void)
+  :
+  maxTranslationVelocity (maxTranslationVelocityDefault),
+  maxRotationVelocity (maxRotationVelocityDefault)
+{
+  return ;
+}
+
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+
 /*!
   \file vpRobot.cpp
   \brief class that defines a generic virtual robot
@@ -35,4 +53,47 @@ getPosition (vpRobot::ControlFrameType repere)
   this ->getPosition (repere, r);
 
   return r;
+}
+
+/* -------------------------------------------------------------------------- */
+/* --- VELOCITY CONTROL ----------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+
+/*! Set the maximal velocity that can be sent to the robot (in 
+ *translation).
+ */
+void vpRobot::
+setMaxTranslationVelocity (const double maxVt)
+{
+  this ->maxTranslationVelocity = maxVt;
+  return;
+}
+
+/*! Get the maximal velocity that can be sent to the robot (in 
+ * translation).
+ */
+
+double vpRobot::
+getMaxTranslationVelocity (void) const 
+{
+  return this ->maxTranslationVelocity;
+}
+/*! Set the maximal velocity that can be sent to the robot (in 
+ * rotation).
+ */
+
+void vpRobot::
+setMaxRotationVelocity (const double maxVr)
+{
+  this ->maxRotationVelocity = maxVr;
+  return;
+}
+
+/*! Get the maximal velocity that can be sent to the robot (in 
+ * rotation).
+ */
+double vpRobot::
+getMaxRotationVelocity (void) const
+{
+  return this ->maxRotationVelocity;
 }
