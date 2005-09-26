2510500ebdbb5e68ca80a9b5af922b167f4cc7c9
Fabien Spindler <Fabien.Spindler@inria.fr>
2005-09-26 16:06:20 +0000

    Test de l'afma6

---
+++ test/servo-afma6/testRobotAfma6.cpp
@@ -0,0 +1,47 @@
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * Copyright Projet Lagadic / IRISA-INRIA Rennes, 2005
+ * www  : http://www.irisa.fr/lagadic
+ *+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * File:      $RCSfile: testRobotAfma6.cpp,v $
+ * Author:    Fabien Spindler
+ *
+ * Version control
+ * ===============
+ *
+ *  $Id: testRobotAfma6.cpp,v 1.1 2005-09-26 16:06:20 fspindle Exp $
+ *
+ * Description
+ * ============
+ *  Tests the control law
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/*!
+  \example testRobotAfma6.cpp
+
+  Example of a real robot control, the Afma6 robot (cylindrical robot, with 4
+  degrees of freedom).
+*/
+
+#include <iostream>
+using namespace std;
+
+#include <visp/vpConfig.h>
+#include <visp/vpRobotAfma6.h>
+#include <visp/vpDebug.h>
+
+#ifdef HAVE_LIBGSL
+int gsl_warnings_off;
+#endif
+
+int main()
+{
+  cout << "a test..." << endl;
+
+  vpAfma6 afma6;
+  CTRACE << afma6;
+
+  vpRobotAfma6 robotAfma6;
+  CTRACE << robotAfma6;
+  return 1;
+}
