fb081e4614b79b83c24d23940a568895c3766b3b
Nicolas Mansard <Nicolas Mansard@inria.fr>
2006-01-12 17:29:02 +0000

    Simplification of some rules in the Makefile. Add the dynamic library path.

--- ViSP/configs/directory.mk
+++ ViSP/configs/directory.mk
@@ -1,43 +1,17 @@
-directory:
-	@if [ ! -d $(VISP_DIR_OBJ) ] ;\
-	then \
-		echo "*" ; \
-		echo "* Creation of the $(VISP_DIR_OBJ) directory..." ; \
-		echo "*" ; \
-		mkdir -p  $(VISP_DIR_OBJ) ; \
-	fi ;
-	@if [ ! -d $(VISP_OBJ_PATH) ] ;\
-	then \
-		echo "*" ; \
-		echo "* Creation of the $(VISP_OBJ_PATH) directory..." ; \
-		echo "*" ; \
-		mkdir -p  $(VISP_OBJ_PATH) ; \
-	fi ;
-	@if [ ! -d $(VISP_DIR_DEP) ] ;\
-	then \
-		echo "*" ; \
-		echo "* Creation of the $(VISP_DIR_DEP) directory..." ; \
-		echo "*" ; \
-		mkdir -p  $(VISP_DIR_DEP) ; \
-	fi ;
-	@if [ ! -d $(VISP_DEP_PATH) ] ;\
-	then \
-		echo "*" ; \
-		echo "* Creation of the $(VISP_DEP_PATH) directory..." ; \
-		echo "*" ; \
-		mkdir -p  $(VISP_DEP_PATH) ; \
-	fi ;
-#	@if [ ! -d $(VISP_DIR_LIB)/$(OS) ] ;\
-#	then \
-#		echo "*" ; \
-#		echo "* Creation of the $(VISP_DIR_LIB)/$(OS) directory..." ; \
-#		echo "*" ; \
-#		mkdir -p  $(VISP_DIR_LIB)/$(OS) ; \
-#	fi ;
-#	@if [ ! -d $(VISP_LIB_PATH) ] ;\
-#	then \
-#		echo "*" ; \
-#		echo "* Creation of the $(VISP_LIB_PATH) directory..." ; \
-#		echo "*" ; \
-#		mkdir -p  $(VISP_LIB_PATH) ; \
-#	fi ;
+%_MKDIR:
+	@if [ ! -d $* ] ;\
+        then \
+                echo "*" ; \
+                echo "* Creation of the $* directory..." ; \
+                echo "*" ; \
+                mkdir -p $* ; \
+        fi ;
+
+BASE_DIRS = \
+	$(VISP_OBJ_PATH)_MKDIR \
+	$(VISP_DEP_PATH)_MKDIR \
+	$(VISP_LIB_PATH)_MKDIR \
+	$(VISP_BIN_PATH)_MKDIR 
+
+directory: $(BASE_DIRS)
+
