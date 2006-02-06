0ea62de5d8b2101d439be3ee2f9cf2a113248161
Odile Bourquardez <Odile.Bourquardez@inria.fr>
2006-02-06 14:25:52 +0000

    Initial Version

---
+++ src/visual-feature/vpFeatureVanishingPoint.h
@@ -0,0 +1,119 @@
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * Copyright Projet Lagadic / IRISA-INRIA Rennes, 2006
+ * www  : http://www.irisa.fr/lagadic
+ *+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * File:      vpFeatureVanishingPoint.h
+ * Project:   ViSP 2.0
+ * Author:    Odile Bourquardez
+ *
+ * Version control
+ * ===============
+ *
+ *  $Id: vpFeatureVanishingPoint.h,v 1.1 2006-02-06 14:25:52 obourqua Exp $
+ *
+ * Description
+ * ============
+ *     class that defines 2D vanishing point visual feature (Z coordinate in 3D space is infinity)
+ *
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+#ifndef vpFeatureVanishingPoint_H
+#define vpFeatureVanishingPoint_H
+
+
+
+/*!  \file vpFeatureVanishingPoint.h \brief Class that defines 2D vanishing
+  point visual feature (Z coordinate in 3D space is infinity)
+*/
+
+#include <visp/vpMatrix.h>
+#include <visp/vpBasicFeature.h>
+#include <visp/vpPoint.h>
+
+#include <visp/vpHomogeneousMatrix.h>
+
+
+
+/*!  \class vpFeatureVanishingPoint \brief Class that defines 2D vanishing
+  point visual feature (Z coordinate in 3D space is infinity)
+*/
+class vpFeatureVanishingPoint : public vpBasicFeature
+{
+public:
+  enum pointFeatureEnum
+    {
+      X,   // x coordinates
+      Y    // y coordinates
+    };
+
+  /*
+    attributes and members directly related to the vpBasicFeature needs
+    other functionalities ar usefull but not mandatory
+  */
+  //no Z required
+
+public:
+  //! basic construction
+  void init() ;
+  //! basic constructor
+  vpFeatureVanishingPoint() ;
+  //! destructor
+  ~vpFeatureVanishingPoint() { ; }
+
+
+public:
+  /*
+    /section Set coordinates
+  */
+
+
+   //! set the point x-coordinates
+  void set_x(const double _x) ;
+  //! get the point x-coordinates
+  double get_x()  const ;
+  //! set the point y-coordinates
+  void set_y(const double _y) ;
+  //! get the point y-coordinates
+  double get_y()   const ;
+  //! set the point xy coordinates
+  void set_xy(const double _x, const double _y) ;
+   // void buildFrom(const vpPoint &p) ;
+  void buildFrom(const double _x, const double _y) ;
+
+
+
+  public:
+  /*
+    vpBasicFeature method instantiation
+  */
+  // feature selection
+  inline static int selectX()  { return FEATURE_LINE[0] ; }
+  inline static int selectY()  { return FEATURE_LINE[1] ; }
+
+
+    //! compute the interaction matrix from a subset a the possible features
+  vpMatrix  interaction(const int select = FEATURE_ALL) const;
+  //! compute the error between two visual features from a subset
+  //! a the possible features
+  vpColVector error(const vpBasicFeature &s_star,
+		    const int select = FEATURE_ALL)  ;
+  //! compute the error between a visual features and zero
+  vpColVector error(const int select = FEATURE_ALL)  ;
+  //! print the name of the feature
+  void print(const int select = FEATURE_ALL ) const ;
+
+  //! feature duplication
+  vpFeatureVanishingPoint *duplicate() const ;
+
+public:
+  void display(const vpCameraParameters &cam,
+	       vpImage<unsigned char> &I,
+	       int color=vpColor::green) const ;
+
+} ;
+
+
+
+#endif
