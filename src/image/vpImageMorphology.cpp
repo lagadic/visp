2281029612de79949cf2511d15a3f5372e80385a
Fabien Spindler <Fabien.Spindler@inria.fr>
2006-02-07 14:45:28 +0000

    New morphology tools (erosion, dilatation)

---
+++ src/image/vpImageMorphology.cpp
@@ -0,0 +1,29 @@
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * Copyright Projet Lagadic / IRISA-INRIA Rennes, 2006
+ * www  : http://www.irisa.fr/lagadic
+ *+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * File:      vpImageMorphology.h
+ * Project:   ViSP2
+ *
+ * Version control
+ * ===============
+ *
+ *  $Id: vpImageMorphology.cpp,v 1.1 2006-02-07 14:45:28 fspindle Exp $
+ *
+ * Description
+ * ============
+ *   Morphology tools
+ *
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+#include<visp/vpImageMorphology.h>
+
+
+
+/*
+ * Local variables:
+ * c-basic-offset: 2
+ * End:
+ */
