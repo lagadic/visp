14847ae5e04dfda37ff9c600a67b1258630ef567
Fabien Spindler <Fabien.Spindler@inria.fr>
2006-05-30 08:38:36 +0000

    - Add new header with copyright and QPL licence - Improvement for dynamic library generation under windows (import and export   are considered)

--- ViSP/include/vpConfig.h.cmake
+++ ViSP/include/vpConfig.h.cmake
@@ -1,3 +1,40 @@
+/****************************************************************************
+ *
+ * $Id: vpConfig.h.cmake,v 1.5 2006-05-30 08:38:36 fspindle Exp $
+ *
+ * Copyright (C) 1998-2006 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit.
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * ViSP configuration.
+ *
+ * Authors:
+ * Fabien Spindler
+ *
+ *****************************************************************************/
+
 #ifndef vpConfig_h
 #define vpConfig_h
 
@@ -12,6 +49,10 @@
 
 // ViSP version.
 #cmakedefine VISP_VERSION "${VISP_VERSION}"
+
+// ViSP library is either compiled static or shared
+// Used to set declspec(import, export) in headers if required under Windows
+#cmakedefine VISP_BUILD_SHARED_LIBS
 
 // Defined if X11 library available.
 #cmakedefine VISP_HAVE_X11
@@ -58,5 +99,20 @@
 // Defined if Irisa's Ptu-46 pan-tilt head available.
 #cmakedefine VISP_HAVE_PTU46
 
+// Under Windows, for shared libraries (DLL) we need to define export on
+// compilation or import on use (like a third party project).
+// We exploit here the fact that cmake auto set xxx_EXPORTS (with S) on 
+// compilation.
+#if defined (WIN32) && defined(VISP_BUILD_SHARED_LIBS) 
+#  ifdef visp_2_EXPORTS 
+#    define VISP_EXPORT __declspec(dllexport)
+#  else  
+#    define VISP_EXPORT __declspec(dllimport)
+#  endif 
+#else
+#  define VISP_EXPORT
+#endif
 
 #endif
+
+
