dcb8f24db0b388e76ea642cff0fdcd4c7ef231cf
Fabien Spindler <Fabien.Spindler@inria.fr>
2006-05-30 08:43:28 +0000

    Add new header with copyright and QPL licence

--- ViSP/example/image/vpImageIoExample.cpp
+++ ViSP/example/image/vpImageIoExample.cpp
@@ -1,25 +1,39 @@
-
-/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * Copyright Projet Lagadic / IRISA-INRIA Rennes, 2005
- * www  : http://www.irisa.fr/lagadic
- *+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/****************************************************************************
  *
- * File:      vpImageIoExample.cpp
- * Project:   ViSP2
- * Author:    Eric Marchand
+ * $Id: vpImageIoExample.cpp,v 1.2 2006-05-30 08:42:16 fspindle Exp $
  *
- * Version control
- * ===============
+ * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
- *  $Id: vpImageIoExample.cpp,v 1.1 2005-11-30 11:15:59 marchand Exp $
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
  *
- * Description
- * ============
- *   test reading and writting of PPM color image
- *   read an image that does not exist
- *   write in a directory that does no exist
+ * This file is part of the ViSP toolkit.
  *
- * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Reading and writting images on the disk.
+ *
+ * Authors:
+ * Eric Marchand
+ *
+ *****************************************************************************/
 
 
 #include <visp/vpImage.h>
@@ -67,7 +81,7 @@ main()
     ERROR_TRACE("at main level");
     cout << e << endl ;
   }
-  
+
   // same thing if you to write in a directory that does not exist
   // or where you are not allowd to write.
   try
@@ -81,7 +95,7 @@ main()
   }
 
   cout << "----------------------------------------------------" << endl ;
- 
+
  // Let's consider that the image is now a color image (32 bits RGBa)
   vpImage<vpRGBa> Irgba ;
 
