8780c726cb1497b5912b2b81bb439d38a5ae4616
Bruno Renier <Bruno Renier>
2006-08-24 08:29:52 +0000

    Added Direct3D9 based Display for windows.

---
+++ CMakeModules/FindDIRECT3D.cmake
@@ -0,0 +1,111 @@
+#############################################################################
+#
+# $Id: FindDIRECT3D.cmake,v 1.1 2006-08-24 08:29:51 brenier Exp $
+#
+# Copyright (C) 1998-2006 Inria. All rights reserved.
+#
+# This software was developed at:
+# IRISA/INRIA Rennes
+# Projet Lagadic
+# Campus Universitaire de Beaulieu
+# 35042 Rennes Cedex
+# http://www.irisa.fr/lagadic
+#
+# This file is part of the ViSP toolkit
+#
+# This file may be distributed under the terms of the Q Public License
+# as defined by Trolltech AS of Norway and appearing in the file
+# LICENSE included in the packaging of this file.
+#
+# Licensees holding valid ViSP Professional Edition licenses may
+# use this file in accordance with the ViSP Commercial License
+# Agreement provided with the Software.
+#
+# This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+# WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Contact visp@irisa.fr if any conditions of this licensing are
+# not clear to you.
+#
+# Description:
+# Try to find Direct3D on Windows. This code comes originaly
+# from http://www.cmake.org/Wiki/CMakeUserFindDirectShow .
+# Once loaded this will define:
+#
+# DIRECT3D_FOUND        - system has DirectShow
+# DIRECT3D_INCLUDE_DIR  - include directory for DirectShow
+# DIRECT3D_LIBRARIES    - libraries you need to link to
+#
+# Authors:
+# Bruno Renier
+#
+#############################################################################
+
+SET(DIRECT3D_FOUND "NO")
+
+# Direct3D is only available on Windows platforms
+IF(WIN32)
+  # Find Direct3D Include Directory
+  FIND_PATH(DIRECT3D_INCLUDE_DIR d3dx9.h
+    "C:/Program Files/Microsoft Visual Studio .NET 2003/Vc7/PlatformSDK/Include"
+    "C:/Program Files/Microsoft DirectX SDK (February 2006)/Include"
+    "C:/Program Files/Microsoft DirectX 9.0 SDK (June 2005)/Include"
+    "C:/Program Files/Microsoft DirectX SDK (June 2006)/Include"
+    "C:/DXSDK/Include"
+    DOC "What is the path where the file d3dx9.h can be found"
+  )
+
+    # if Direct3D include dir found, then find Direct3D libraries
+    IF(DIRECT3D_INCLUDE_DIR)
+      FIND_LIBRARY(DIRECT3D_d3d9_LIBRARY d3d9
+        "C:/Program Files/Microsoft DirectX SDK (February 2006)/Lib/x86"
+        "C:/Program Files/Microsoft DirectX 9.0 SDK (June 2005)/Lib/x86"
+        "C:/Program Files/Microsoft DirectX SDK (June 2006)/Lib/x86"
+        "C:/DXSDK/Include/Lib"
+        DOC "Where can the Direct3D d3d9 library be found"
+      )
+      FIND_LIBRARY(DIRECT3D_d3dx9_LIBRARY d3dx9
+        "C:/Program Files/Microsoft DirectX SDK (February 2006)/Lib/x86"
+        "C:/Program Files/Microsoft DirectX 9.0 SDK (June 2005)/Lib/x86"
+        "C:/Program Files/Microsoft DirectX SDK (June 2006)/Lib/x86"
+        "C:/DXSDK/Include/Lib"
+        DOC "Where can the Direct3D d3dx9 library be found"
+      )
+
+      # if Direct3D libraries found, then we're ok
+      IF(DIRECT3D_d3d9_LIBRARY)
+      IF(DIRECT3D_d3dx9_LIBRARY)
+        # everything found
+        SET(DIRECT3D_FOUND "YES")
+      ENDIF(DIRECT3D_d3dx9_LIBRARY)
+      ENDIF(DIRECT3D_d3d9_LIBRARY)
+    ENDIF(DIRECT3D_INCLUDE_DIR)
+
+  MARK_AS_ADVANCED(
+    DIRECT3D_INCLUDE_DIR
+    DIRECT3D_d3d9_LIBRARY
+    DIRECT3D_d3dx9_LIBRARY
+  )
+
+
+ENDIF(WIN32)
+
+
+#---------------------------------------------------------------------
+IF(DIRECT3D_FOUND)
+
+  SET(DIRECT3D_LIBRARIES
+    ${DIRECT3D_d3d9_LIBRARY}
+    ${DIRECT3D_d3dx9_LIBRARY}
+  )
+ELSE(DIRECT3D_FOUND)
+  # make FIND_PACKAGE friendly
+  IF(NOT DIRECT3D_FIND_QUIETLY)
+    IF(DIRECT3D_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR
+              "Direct3D required, please specify it's location.")
+    ELSE(DIRECT3D_FIND_REQUIRED)
+      MESSAGE(STATUS "Direct3D was not found.")
+    ENDIF(DIRECT3D_FIND_REQUIRED)
+  ENDIF(NOT DIRECT3D_FIND_QUIETLY)
+ENDIF(DIRECT3D_FOUND)
---
+++ src/video-device/windows/vpDisplayD3D.h
@@ -0,0 +1,72 @@
+/****************************************************************************
+ *
+ * $Id: vpDisplayD3D.h,v 1.1 2006-08-24 08:29:52 brenier Exp $
+ *
+ * Copyright (C) 1998-2006 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Windows 32 display using D3D
+ *
+ * Authors:
+ * Bruno Renier
+ *
+ *****************************************************************************/
+
+#include <visp/vpConfig.h>
+#if ( defined(VISP_HAVE_D3D9) ) 
+
+#ifndef VPDISPLAYD3D_HH
+#define VPDISPLAYD3D_HH
+
+
+#include <visp/vpDisplayWin32.h>
+
+/*!
+  \class vpDisplayD3D
+
+  \brief Display for windows using Direct3D.
+  Direct3D is part of the DirectX API available under Windows operating systems.
+
+  \warning Requires DirectX9 SDK to compile and DirectX9 DLLs to run.
+*/
+class VISP_EXPORT vpDisplayD3D : public vpDisplayWin32
+{
+public:
+  vpDisplayD3D();
+  
+  vpDisplayD3D(vpImage<vpRGBa> &I,
+		    int winx=-1, int winy=-1,
+		    char *_title=NULL);
+  
+  vpDisplayD3D(vpImage<unsigned char> &I,
+		    int winx=-1, int winy=-1,
+		    char *_title=NULL);
+  
+  ~vpDisplayD3D();
+  
+};
+#endif
+#endif
