1dbf88d94fc08d6f72ad20916a96b8bb0fb78168
Fabien Spindler <Fabien.Spindler@inria.fr>
2006-09-07 10:06:32 +0000

    Replace exit(-1) by return(-1) so that if no V4L2 framegrabber card is installed, we don't have an error on the Dart server.

--- test/framegrabber/testV4l2Grey.cpp
+++ test/framegrabber/testV4l2Grey.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * $Id: testV4l2Grey.cpp,v 1.11 2006-09-01 14:05:44 fspindle Exp $
+ * $Id: testV4l2Grey.cpp,v 1.12 2006-09-07 10:06:32 fspindle Exp $
  *
  * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
@@ -182,7 +182,7 @@ main(int argc, char ** argv)
   catch(...)
   {
     vpERROR_TRACE("Cannot acquire an image...") ;
-    exit(-1);
+    return(-1);
   }
 
   cout << "Image size: " << I.getCols() << "  " << I.getRows() <<endl  ;
