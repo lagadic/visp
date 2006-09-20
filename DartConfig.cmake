1f555f87aacbcaf705764f1b1d7a2aa10fc54eaa
Fabien Spindler <Fabien.Spindler@inria.fr>
2006-09-20 15:57:34 +0000

    NIGHTLY_START_TIME set to "9:00PM"

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -1,6 +1,6 @@
 #############################################################################
 #
-# $Id: DartConfig.cmake,v 1.5 2006-09-11 16:18:14 fspindle Exp $
+# $Id: DartConfig.cmake,v 1.6 2006-09-20 15:57:34 fspindle Exp $
 #
 # Copyright (C) 1998-2006 Inria. All rights reserved.
 #
@@ -39,4 +39,4 @@ SET (DROP_METHOD "xmlrpc")
 SET (DROP_SITE "http://dart.irisa.fr")
 SET (DROP_LOCATION "ViSP2")
 SET (COMPRESS_SUBMISSION ON)
-SET (NIGHTLY_START_TIME "9:00AM")
+SET (NIGHTLY_START_TIME "9:00PM")
