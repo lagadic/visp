df6725e10d4542a7c6de81c9eeb67eb2aad4a324
Fabien Spindler <Fabien.Spindler@inria.fr>
2006-11-08 08:53:51 +0000

    Minor changes

--- test/servo-afma4/moveAfma4.cpp
+++ test/servo-afma4/moveAfma4.cpp
@@ -1,22 +1,39 @@
-
-
-/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * Copyright Projet Lagadic / IRISA-INRIA Rennes, 2005
- * www  : http://www.irisa.fr/lagadic
- *+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/****************************************************************************
  *
- * File:      $RCSfile: moveAfma4.cpp,v $
- * Author:    Eric Marchand
+ * $Id: moveAfma4.cpp,v 1.6 2006-11-08 08:53:51 fspindle Exp $
  *
- * Version control
- * ===============
+ * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
- *  $Id: moveAfma4.cpp,v 1.5 2006-10-20 14:04:39 mtallur Exp $
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
  *
- * Description
- * ============
- *  Tests the control law
- * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ * This file is part of the ViSP toolkit
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Test for Afma 4 dof robot.
+ *
+ * Authors:
+ * Fabien Spindler
+ *
+ *****************************************************************************/
 
 /*!
   \example moveAfma4.cpp
@@ -40,7 +57,7 @@ int gsl_warnings_off;
 int
 main()
 {
-  try 
+  try
   {
     vpRobotAfma4 robot ;
 
@@ -82,7 +99,7 @@ main()
     robot.setVelocity(vpRobot::ARTICULAR_FRAME, q) ;
     sleep(5) ;
 
-    vp  TRACE("Velocity control: vertical translation...") ;
+    vpTRACE("Velocity control: vertical translation...") ;
     q = 0 ;
     q[1] = -0.2 ; // Vertical translation
     robot.setVelocity(vpRobot::ARTICULAR_FRAME, q) ;
@@ -98,7 +115,7 @@ main()
     q[3] = vpMath::rad(2) ; // tilt
     robot.setVelocity(vpRobot::ARTICULAR_FRAME, q) ;
     sleep(5) ;
- 
+
     //
     // Velocity control in camera frame
     //
@@ -162,7 +179,7 @@ main()
   {
     vpERROR_TRACE(" Test failed") ;
     return 0;
-  }  
+  }
 
 }
 #else
