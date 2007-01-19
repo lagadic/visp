7d01194b268d306794a24084ef9ef138344ca9fe
Fabien Spindler <Fabien.Spindler@inria.fr>
2007-01-19 17:28:35 +0000

    Add -o option to allow to save images on the disk

--- test/framegrabber/testDirectShow.cpp
+++ test/framegrabber/testDirectShow.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * $Id: testDirectShow.cpp,v 1.4 2006-08-30 15:57:20 fspindle Exp $
+ * $Id: testDirectShow.cpp,v 1.5 2007-01-19 17:28:35 fspindle Exp $
  *
  * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
@@ -51,19 +51,21 @@
 
 #include <visp/vpDirectShowGrabber.h>
 #include <visp/vpImage.h>
+#include <visp/vpImageIo.h>
 #include <visp/vpDisplayGTK.h>
 #include <visp/vpDisplayGDI.h>
 #include <visp/vpParseArgv.h>
+#include <visp/vpTime.h>
 
 // List of allowed command line options
-#define GETOPTARGS	"dh"
+#define GETOPTARGS	"dhn:o:"
 
 /*!
 
   Print the program options.
 
  */
-void usage(char *name, char *badparam)
+void usage(char *name, char *badparam, unsigned &nframes, string &opath)
 {
   fprintf(stdout, "\n\
 Acquire images using DirectShow (under Windows only) and display\n\
@@ -77,9 +79,14 @@ OPTIONS:                                               Default\n\
   -d \n\
      Turn off the display.\n\
 \n\
+  -n [%%u] : Number of frames to acquire.                   %u\n\
+\n\
+  -o [%%s] : Filename for image saving.                     %s\n\
+            The %%d is for the image numbering.\n\
+\n\
   -h \n\
      Print the help.\n\
-\n");
+\n", nframes, opath.c_str());
 
 }
 /*!
@@ -89,7 +96,8 @@ OPTIONS:                                               Default\n\
   \return false if the program has to be stopped, true otherwise.
 
 */
-bool getOptions(int argc, char **argv, bool &display)
+bool getOptions(int argc, char **argv, bool &display,
+		unsigned &nframes, bool &save, string &opath)
 {
   char *optarg;
   int	c;
@@ -97,17 +105,22 @@ bool getOptions(int argc, char **argv, bool &display)
 
     switch (c) {
     case 'd': display = false; break;
-    case 'h': usage(argv[0], NULL); return false; break;
+    case 'n':
+      nframes = atoi(optarg); break;
+    case 'o':
+      save = true;
+      opath = optarg; break;
+    case 'h': usage(argv[0], NULL, nframes, opath); return false; break;
 
     default:
-      usage(argv[0], optarg);
+      usage(argv[0], optarg, nframes, opath);
       return false; break;
     }
   }
 
   if ((c == 1) || (c == -1)) {
     // standalone param or error
-    usage(argv[0], NULL);
+    usage(argv[0], NULL, nframes, opath);
     cerr << "ERROR: " << endl;
     cerr << "  Bad argument " << optarg << endl << endl;
     return false;
@@ -121,16 +134,24 @@ int
 main(int argc, char ** argv)
 {
   bool opt_display = true;
+  unsigned nframes = 50;
+  bool save = false;
 
   // Read the command line options
-  if (getOptions(argc, argv, opt_display) == false) {
+  if (getOptions(argc, argv, opt_display, nframes, save, opath) == false) {
     exit (-1);
   }
 
   // Declare an image, this is a color image (in RGBa format). It
   // size is not defined yet. It will be defined when the image will
   // acquired the first time.
+#ifdef GRAB_COLOR
   vpImage<vpRGBa> I ;
+  string opath = "C:/temp/I%04d.ppm";
+#else
+  vpImage<unsigned char> I ;
+  string opath = "C:/temp/I%04d.pgm";
+#endif
 
   // Create the grabber
   vpDirectShowGrabber grabber;
@@ -162,9 +183,12 @@ main(int argc, char ** argv)
   }
 
   try {
+    double tbegin=0, tend=0, tloop=0, ttotal=0;
+
+    ttotal = 0;
+    tbegin = vpTime::measureTimeMs();
     // Loop for image acquisition and display
-    for(int i=0 ; i<100 ; i++)
-    {
+    for (unsigned i = 0; i < nframes; i++) {
       //Acquires an RGBa image
       grabber.acquire(I);
 
@@ -172,15 +196,33 @@ main(int argc, char ** argv)
 	//Displays the grabbed rgba image
 	vpDisplay::display(I);
       }
+
+      if (save) {
+	char buf[FILENAME_MAX];
+	sprintf(buf, opath.c_str(), i);
+	string filename(buf);
+	cout << "Write: " << filename << endl;
+#ifdef GRAB_COLOR
+	vpImageIo::writePPM(I, filename);
+#else
+	vpImageIo::writePGM(I, filename);
+#endif
+      }
+      tend = vpTime::measureTimeMs();
+      tloop = tend - tbegin;
+      tbegin = tend;
+      cout << "loop time: " << tloop << " ms" << endl;
+      ttotal += tloop;
     }
+    cout << "Mean loop time: " << ttotal / nframes << " ms" << endl;
+    cout << "Mean frequency: " << 1000./(ttotal / nframes) << " fps" << endl;
+
+    grabber.close();
   }
   catch(...)
   {
-    exit(-1);
+    vpCERROR << "Failure: exit" << endl;
   }
-
-  grabber.close();
-
 }
 #else
 int
