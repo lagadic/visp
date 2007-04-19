e21ecdef56da95cef0cc774a33dc299c553c6100
Anthony Saunier <Anthony.Saunier@inria.fr>
2007-04-19 07:33:27 +0000

    Add missing ";"

--- src/robot/real-robot/afma4/vpAfma4.cpp
+++ src/robot/real-robot/afma4/vpAfma4.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * $Id: vpAfma4.cpp,v 1.6 2007-04-18 16:14:28 asaunier Exp $
+ * $Id: vpAfma4.cpp,v 1.7 2007-04-19 07:33:27 asaunier Exp $
  *
  * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
@@ -942,7 +942,7 @@ vpAfma4::get_fJe(const vpColVector &q, vpMatrix &fJe)
   }
   catch(...)
   {
-  	vpERROR_TRACE("Error caught")
+    vpERROR_TRACE("Error caught");
     throw ;
   }
 
