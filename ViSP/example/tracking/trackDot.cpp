6c7549189cc2b1a3b040a57943e92a84cb3fc5c2
Fabien Spindler <Fabien.Spindler@inria.fr>
2007-06-06 08:58:16 +0000

    Add [-p <personal image path>] [-f <first image>] [-n <number of images>] [-s <step>] command line options to allow image sequence specifying

--- ViSP/example/tracking/trackDot.cpp
+++ ViSP/example/tracking/trackDot.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * $Id: trackDot.cpp,v 1.9 2007-05-03 16:00:17 fspindle Exp $
+ * $Id: trackDot.cpp,v 1.10 2007-06-06 08:58:16 fspindle Exp $
  *
  * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
@@ -68,7 +68,7 @@
 #include <visp/vpIoTools.h>
 
 // List of allowed command line options
-#define GETOPTARGS	"cdi:h"
+#define GETOPTARGS	"cdf:i:n:p:s:h"
 
 /*!
 
@@ -77,25 +77,53 @@ Print the program options.
   \param name : Program name.
   \param badparam : Bad parameter name.
   \param ipath : Input image path.
+  \param ppath : Personal image path.
+  \param first : First image.
+  \param nimages : Number of images to manipulate.
+  \param step : Step between two images.
 
 */
-void usage(char *name, char *badparam, std::string ipath)
+void usage(char *name, char *badparam, std::string ipath, std::string ppath,
+	   unsigned first, unsigned nimages, unsigned step)
 {
   fprintf(stdout, "\n\
 Test dot tracking.\n\
 \n\
 SYNOPSIS\n\
-  %s [-i <input image path>] [-c] [-d] [-h]\n", name);
+  %s [-i <test image path>] [-p <personal image path>]\n\
+     [-f <first image>] [-n <number of images>] [-s <step>]\n\
+     [-c] [-d] [-h]\n", name);
 
   fprintf(stdout, "\n\
 OPTIONS:                                               Default\n\
   -i <input image path>                                %s\n\
      Set image input path.\n\
      From this path read images \n\
-     \"ViSP-images/mire-2/image.%%04d.pgm\"\n\
+     \"ViSP-images/mire-2/image.%%04d.pgm\". These \n\
+     images come from ViSP-images-x.y.z.tar.gz available \n\
+     on the ViSP website.\n\
      Setting the VISP_INPUT_IMAGE_PATH environment\n\
      variable produces the same behaviour than using\n\
      this option.\n\
+ \n\
+ -p <personal image path>                             %s\n\
+     Specify a personal sequence containing images \n\
+     to process.\n\
+     By image sequence, we mean one file per image.\n\
+     The following image file formats PNM (PGM P5, PPM P6)\n\
+     are supported. The format is selected by analysing \n\
+     the filename extension.\n\
+     Example : \"/Temp/ViSP-images/cube/image.%%04d.pgm\"\n\
+     %%04d is for the image numbering.\n\
+ \n\
+  -f <first image>                                     %u\n\
+     First image number of the sequence.\n\
+ \n\
+  -n <number of images>                                %u\n\
+     Number of images to load from the sequence.\n\
+ \n\
+  -s <step>                                            %u\n\
+     Step between two images.\n\
 \n\
   -c\n\
      Disable the mouse click. Usefull to automaze the \n\
@@ -106,25 +134,30 @@ OPTIONS:                                               Default\n\
 \n\
   -h\n\
      Print the help.\n",
-	  ipath.c_str());
+	  ipath.c_str(), ppath.c_str(), first, nimages, step);
 
   if (badparam)
     fprintf(stdout, "\nERROR: Bad parameter [%s]\n", badparam);
 }
 /*!
 
-Set the program options.
+  Set the program options.
 
   \param argc : Command line number of parameters.
   \param argv : Array of command line parameters.
   \param ipath : Input image path.
   \param click_allowed : Mouse click activation.
+  \param ppath : Personal image path.
+  \param first : First image.
+  \param nimages : Number of images to display.
+  \param step : Step between two images.
   \param display : Display activation.
 
   \return false if the program has to be stopped, true otherwise.
 
 */
-bool getOptions(int argc, char **argv, std::string &ipath,
+bool getOptions(int argc, char **argv, std::string &ipath, std::string &ppath,
+		unsigned &first, unsigned &nimages, unsigned &step, 
 		bool &click_allowed, bool &display)
 {
   char *optarg;
@@ -135,17 +168,22 @@ bool getOptions(int argc, char **argv, std::string &ipath,
     case 'c': click_allowed = false; break;
     case 'd': display = false; break;
     case 'i': ipath = optarg; break;
-    case 'h': usage(argv[0], NULL, ipath); return false; break;
+    case 'p': ppath = optarg; break;
+    case 'f': first = (unsigned) atoi(optarg); break;
+    case 'n': nimages = (unsigned) atoi(optarg); break;
+    case 's': step = (unsigned) atoi(optarg); break;
+    case 'h': usage(argv[0], NULL, ipath, ppath, first, nimages, step);
+      return false; break;
 
     default:
-      usage(argv[0], optarg, ipath);
+      usage(argv[0], optarg, ipath, ppath, first, nimages, step);
       return false; break;
     }
   }
 
   if ((c == 1) || (c == -1)) {
     // standalone param or error
-    usage(argv[0], NULL, ipath);
+    usage(argv[0], NULL, ipath, ppath, first, nimages, step);
     std::cerr << "ERROR: " << std::endl;
     std::cerr << "  Bad argument " << optarg << std::endl << std::endl;
     return false;
@@ -161,8 +199,12 @@ main(int argc, char ** argv)
   std::string env_ipath;
   std::string opt_ipath;
   std::string ipath;
+  std::string opt_ppath;
   std::string dirname;
   std::string filename;
+  unsigned opt_first = 1;
+  unsigned opt_nimages = 1500;
+  unsigned opt_step = 1;
   bool opt_click_allowed = true;
   bool opt_display = true;
 
@@ -175,10 +217,9 @@ main(int argc, char ** argv)
   if (! env_ipath.empty())
     ipath = env_ipath;
 
-
   // Read the command line options
-  if (getOptions(argc, argv, opt_ipath, opt_click_allowed,
-		 opt_display) == false) {
+  if (getOptions(argc, argv, opt_ipath, opt_ppath,opt_first, opt_nimages,
+		 opt_step, opt_click_allowed, opt_display) == false) {
     exit (-1);
   }
 
@@ -188,7 +229,7 @@ main(int argc, char ** argv)
 
   // Compare ipath and env_ipath. If they differ, we take into account
   // the input path comming from the command line option
-  if (opt_ipath.empty()) {
+  if (opt_ipath.empty() && opt_ppath.empty()) {
     if (ipath != env_ipath) {
       std::cout << std::endl
 	   << "WARNING: " << std::endl;
@@ -199,32 +240,59 @@ main(int argc, char ** argv)
   }
 
   // Test if an input path is set
-  if (opt_ipath.empty() && env_ipath.empty()){
-    usage(argv[0], NULL, ipath);
+  if (opt_ipath.empty() && env_ipath.empty() && opt_ppath.empty() ){
+    usage(argv[0], NULL, ipath, opt_ppath, opt_first, opt_nimages, opt_step);
     std::cerr << std::endl
 	 << "ERROR:" << std::endl;
     std::cerr << "  Use -i <visp image path> option or set VISP_INPUT_IMAGE_PATH "
 	 << std::endl
 	 << "  environment variable to specify the location of the " << std::endl
-	 << "  image path where test images are located." << std::endl << std::endl;
+	 << "  image path where test images are located." << std::endl
+	 << "  Use -p <personal image path> option if you want to "<<std::endl
+	 << "  use personal images." << std::endl
+         << std::endl;
+
     exit(-1);
   }
-
 
   // Declare an image, this is a gray level image (unsigned char)
   // it size is not defined yet, it will be defined when the image will
   // read on the disk
   vpImage<unsigned char> I ;
 
-  // Set the path location of the image sequence
-  dirname = ipath +  vpIoTools::path("/ViSP-images/mire-2/");
-
-  // Build the name of the image file
-  unsigned iter = 1; // Image number
+  unsigned iter = opt_first;
   std::ostringstream s;
-  s.setf(std::ios::right, std::ios::adjustfield);
-  s << "image." << std::setw(4) << std::setfill('0') << iter << ".pgm";
-  filename = dirname + s.str();
+  char cfilename[FILENAME_MAX];
+
+  if (opt_ppath.empty()){
+  
+  
+    // Warning :
+    // the image sequence is not provided with the ViSP package
+    // therefore the program will return you an error :
+    //  !!    vpImageIoPnm.cpp: readPGM(#210) :couldn't read file
+    //        ViSP-images/mire-2/image.0001.pgm
+    //  !!    vpDotExample.cpp: main(#95) :Error while reading the image
+    //  terminate called after throwing an instance of 'vpImageException'
+    //
+    //  The sequence is available on the visp www site
+    //  http://www.irisa.fr/lagadic/visp/visp.html
+    //  in the download section. It is named "ViSP-images.tar.gz"
+  
+    // Set the path location of the image sequence
+    dirname = ipath + vpIoTools::path("/ViSP-images/mire-2/");
+  
+    // Build the name of the image file
+  
+    s.setf(std::ios::right, std::ios::adjustfield);
+    s << "image." << std::setw(4) << std::setfill('0') << iter << ".pgm";
+    filename = dirname + s.str();
+  }
+  else {
+
+    sprintf(cfilename,opt_ppath.c_str(), iter) ;
+    filename = cfilename;
+  }
 
   // Read the PGM image named "filename" on the disk, and put the
   // bitmap into the image structure I.  I is initialized to the
@@ -317,13 +385,19 @@ main(int argc, char ** argv)
     }
 
   try {
-    while (iter < 1200)
-      {
+    while (iter < opt_first + opt_nimages*opt_step) {
 	// set the new image name
-	s.str("");
-	s << "image." << std::setw(4) << std::setfill('0') << iter << ".pgm";
-	filename = dirname + s.str();
+	if (opt_ppath.empty()){
+	  s.str("");
+	  s << "image." << std::setw(4) << std::setfill('0') << iter << ".pgm";
+	  filename = dirname + s.str();
+	}
+	else {
+	  sprintf(cfilename, opt_ppath.c_str(), iter) ;
+	  filename = cfilename;
+	}
 	// read the image
+	std::cout << "read : " << filename << std::endl;
 	vpImageIo::readPGM(I, filename);
 
 	if (opt_display) {
