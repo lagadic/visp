fe1d5435784de4b8a033bd50520c5e650bab7fbb
Anthony Saunier <Anthony.Saunier@inria.fr>
2007-11-07 14:50:05 +0000

    Change the condition to print a warning when a conflict appear between images sources.

--- ViSP/test/video-device/testClick.cpp
+++ ViSP/test/video-device/testClick.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * $Id: testClick.cpp,v 1.7 2007-09-28 14:56:28 asaunier Exp $
+ * $Id: testClick.cpp,v 1.8 2007-11-07 14:50:05 asaunier Exp $
  *
  * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
@@ -275,7 +275,7 @@ main(int argc, char ** argv)
 
     // Compare ipath and env_ipath. If they differ, we take into account
     // the input path comming from the command line option
-    if (opt_ipath.empty()) {
+    if (!opt_ipath.empty() && !env_ipath.empty()) {
       if (ipath != env_ipath) {
         std::cout << std::endl
 	     << "WARNING: " << std::endl;
