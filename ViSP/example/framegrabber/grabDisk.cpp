4d06c1d83f9847610252a62e85bcb608e66cb76b
Anthony Saunier <Anthony.Saunier@inria.fr>
2007-11-09 15:44:25 +0000

    Change a condition to print a warning when a conflict occured between images sources.

--- ViSP/example/framegrabber/grabDisk.cpp
+++ ViSP/example/framegrabber/grabDisk.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * $Id: grabDisk.cpp,v 1.6 2007-09-28 14:45:01 asaunier Exp $
+ * $Id: grabDisk.cpp,v 1.7 2007-11-09 15:44:25 asaunier Exp $
  *
  * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
@@ -239,7 +239,7 @@ main(int argc, char ** argv)
 
   // Compare ipath and env_ipath. If they differ, we take into account
   // the input path comming from the command line option
-  if (opt_ipath.empty()) {
+  if (!opt_ipath.empty() && !env_ipath.empty()) {
     if (ipath != env_ipath) {
       std::cout << std::endl
 	   << "WARNING: " << std::endl;
