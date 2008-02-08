612ed388edd21529a26086362a76508aadd64e58
Anthony Saunier <Anthony.Saunier@inria.fr>
2008-02-08 14:01:59 +0000

    Add comments in the 2nd version replace the 3rd by a new one tacking (x,y,t,thetaUz) features

--- ViSP/example/servo-simu/servoSimuPoint2DhalfCamVelocity2.cpp
+++ ViSP/example/servo-simu/servoSimuPoint2DhalfCamVelocity2.cpp
@@ -1,7 +1,7 @@
 
 /****************************************************************************
  *
- * $Id: servoSimuPoint2DhalfCamVelocity2.cpp,v 1.6 2007-11-28 10:54:19 fspindle Exp $
+ * $Id: servoSimuPoint2DhalfCamVelocity2.cpp,v 1.7 2008-02-08 14:01:59 asaunier Exp $
  *
  * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
@@ -39,7 +39,8 @@
 
 /*!
   \file servoSimuPoint2DhalfCamVelocity2.cpp
-  \brief Simulation of a 2 1/2 D visual servoing (x,y,log Z, theta U)
+  \brief Simulation of a 2 1/2 D visual servoing (x,y,logZ, theta U)
+  - (x,y,logZ, theta U) features
   - eye-in-hand control law,
   - velocity computed in the camera frame,
   - no display.
@@ -48,6 +49,7 @@
 /*!
   \example servoSimuPoint2DhalfCamVelocity2.cpp
   Simulation of a 2 1/2 D visual servoing (x,y,log Z, theta U)
+  - (x,y,logZ, theta U) features
   - eye-in-hand control law,
   - velocity computed in the camera frame,
   - no display.
@@ -139,42 +141,104 @@ main(int argc, char ** argv)
   if (getOptions(argc, argv) == false) {
     exit (-1);
   }
+  
+  std::cout << std::endl ;
+  std::cout << "-------------------------------------------------------" << std::endl ;
+  std::cout << " simulation of a 2 1/2 D visual servoing " << std::endl ;
+  std::cout << "-------------------------------------------------------" << std::endl ;
+  std::cout << std::endl ;
+
+  // In this example we will simulate a visual servoing task.
+  // In simulation, we have to define the scene frane Ro and the
+  // camera frame Rc.
+  // The camera location is given by an homogenous matrix cMo that
+  // describes the position of the camera in the scene frame.
+
 
   vpServo task ;
-  vpRobotCamera robot ;
+  
 
   vpTRACE("sets the initial camera location " ) ;
+  // we give the camera location as a size 6 vector (3 translations in meter
+  // and 3 rotation (theta U representation) )
   vpPoseVector c_r_o(0.1,0.2,2,
 		     vpMath::rad(20), vpMath::rad(10),  vpMath::rad(50)
 		     ) ;
 
-  vpCTRACE ; std::cout << std::endl ;
+  // this pose vector is then transformed in a 4x4 homogeneous matrix
   vpHomogeneousMatrix cMo(c_r_o) ;
-  vpCTRACE ; std::cout << std::endl ;
+  
+  // We define a robot
+  // The vpRobotCamera implements a simple moving that is juste defined
+  // by its location cMo
+  vpRobotCamera robot ;
+ 
+  // the robot position is set to the defined cMo position
   robot.setPosition(cMo) ;
-  vpCTRACE ; std::cout << std::endl ;
 
+  // Now that the current camera position has been defined,
+  // let us defined the defined camera location.
+  // It is defined by cdMo
   vpTRACE("sets the desired camera location " ) ;
   vpPoseVector cd_r_o(0,0,1,
 		      vpMath::rad(0),vpMath::rad(0),vpMath::rad(0)) ;
   vpHomogeneousMatrix cdMo(cd_r_o) ;
 
 
+
+  //----------------------------------------------------------------------
+  // A 2 1/2 D visual servoing can be defined by
+  // - the position of a point x,y
+  // - the difference between this point depth and a desire depth
+  //   modeled by log Z/Zd to be regulated to 0
+  // - the rotation that the camera has to realized cdMc
+
+  // Let us now defined the current value of these features
+
+
+  // since we simulate we have to define a 3D point that will
+  // forward-projected to define the current position x,y of the
+  // reference point
+
+  //------------------------------------------------------------------
+  // First feature (x,y)
+  //------------------------------------------------------------------
+  vpTRACE("1st feature (x,y)");
+  // Let oP be this ... point,
+  // a vpPoint class has three main member
+  // .oP : 3D coordinates in scene frame
+  // .cP : 3D coordinates in camera frame
+  // .p : 2D
+   
   //------------------------------------------------------------------
   vpTRACE("\tsets the point coordinates in the world frame "  ) ;
   vpPoint point ;
+  // defined point coordinates in the scene frame : oP
   point.setWorldCoordinates(0,0,0) ;
+  // computes  the point coordinates in the camera frame and its
+  // 2D coordinates cP and then p
   vpTRACE("\tproject : computes  the point coordinates in the camera frame and its 2D coordinates"  ) ;
   point.track(cMo) ;
 
+  // We also defined (again by forward projection) the desired position
+  // of this point according to the desired camera position
   vpPoint pointd ;
   pointd.setWorldCoordinates(0,0,0) ;
   pointd.track(cdMo) ;
+  
+   // Nevertheless, a vpPoint is not a feature, this is just a "tracker"
+  // from which the feature are built
+  // a feature is juste defined by a vector s, a way to compute the
+  // interaction matrix and the error, and if required a (or a vector of)
+  // 3D information
 
-  //------------------------------------------------------------------
-  vpTRACE("1st feature (x,y)");
+  // for a point (x,y) Visp implements the vpFeaturePoint class.
+  // we no defined a feature for x,y (and for (x*,y*))
   vpFeaturePoint p,pd ;
-
+  
+  // and we initialized the vector s=(x,y) of p from the tracker P
+  // Z coordinates in p is also initialized, it will be used to compute
+  // the interaction matrix
   vpFeatureBuilder::create(p,point)  ;
   vpFeatureBuilder::create(pd,pointd)  ;
 
@@ -182,33 +246,69 @@ main(int argc, char ** argv)
   //------------------------------------------------------------------
   vpTRACE("2nd feature (logZ)") ;
   vpTRACE("\tnot necessary to project twice (reuse p)") ;
+  //------------------------------------------------------------------
+  // Second feature log (Z/Zd)
+  //
+
+  // This case in intersting since this visual feature has not
+  // been predefined in VisP
+  // In such case we have a generic feature class vpGenericFeature
+  // We will have to defined
+  // the vector s : .set_s(...)
+  // the interaction matrix Ls : .setInteractionMatrix(...)
+
+  // log(Z/Zd) is then a size 1 vector logZ
   vpGenericFeature logZ(1) ;
+  // initialized to s = log(Z/Zd)
+  // Let us note that here we use the point P and Pd, it's not necessary
+  // to forward project twice (it's already done)
   logZ.set_s(log(point.get_Z()/pointd.get_Z())) ;
 
 
-  //------------------------------------------------------------------
+  // This visual has to be regulated to zero
   vpTRACE("3rd feature ThetaU") ;
-  vpTRACE("\tcompute the rotation that the camera has to realize "  ) ;
-  vpHomogeneousMatrix cdMc ;
-  cdMc = cdMo*cMo.inverse() ;
+ 
+  //------------------------------------------------------------------
+  // 3rd feature ThetaU
+  // The thetaU feature is defined, tu represents the rotation that the camera
+  // has to realized.
+  // the complete displacement is then defined by:
+  //------------------------------------------------------------------
+ vpHomogeneousMatrix cdMc ;
+ vpTRACE("\tcompute the rotation that the camera has to realize "  ) ;
+ cdMc = cdMo*cMo.inverse() ;
 
+  // from this displacement, we extract the rotation cdRc represented by
+  // the angle theta and the rotation axis u
   vpFeatureThetaU tu ;
   tu.buildFrom(cdMc) ;
+  // This visual has to be regulated to zero
 
   vpTRACE("\tsets the desired rotation (always zero !) ") ;
   vpTRACE("\tsince s is the rotation that the camera has to realize ") ;
 
 
   //------------------------------------------------------------------
+  // Let us now the task itself
+  //------------------------------------------------------------------
 
   vpTRACE("define the task") ;
   vpTRACE("\t we want an eye-in-hand control law") ;
   vpTRACE("\t robot is controlled in the camera frame") ;
+  //  we choose to control the robot in the camera frame
   task.setServo(vpServo::EYEINHAND_CAMERA) ;
+  // Interaction matrix is computed with the current value of s
   task.setInteractionMatrixType(vpServo::CURRENT) ;
+  
+  // we build the task by "stacking" the visual feature
+  // previously defined
   task.addFeature(p,pd) ;
   task.addFeature(logZ) ;
   task.addFeature(tu) ;
+  // addFeature(X,Xd) means X should be regulated to Xd
+  // addFeature(X) means that X should be regulated to 0
+  // some features such as vpFeatureThetaU MUST be regulated to zero
+  // (otherwise, it will results in an error at exectution level)
 
   vpTRACE("\t set the gain") ;
   task.setLambda(1) ;
@@ -216,6 +316,8 @@ main(int argc, char ** argv)
 
   vpTRACE("Display task information " ) ;
   task.print() ;
+  //------------------------------------------------------------------
+  // An now the closed loop
 
   int iter=0 ;
   vpTRACE("\t loop") ;
@@ -253,6 +355,8 @@ main(int argc, char ** argv)
 
       if (iter==1) vpTRACE("\t\t send the camera velocity to the controller ") ;
       robot.setVelocity(vpRobot::CAMERA_FRAME, v) ;
+    // Note that for vpRobotCamera, camera position cMo, is updated using the
+    // exponential map.
 
 
       std::cout << task.error.sumSquare() <<std::endl ; ;
