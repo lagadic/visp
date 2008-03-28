1a0ea254d82d88a7bb9666eb207fbe28c03cb889
Eric Marchand <Eric.Marchand@irisa.fr>
2008-03-28 16:49:38 +0000

    correction bug du precedente commit mea culpa

--- ViSP/src/tracking/forward-projection/vpPoint.cpp
+++ ViSP/src/tracking/forward-projection/vpPoint.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * $Id: vpPoint.cpp,v 1.13 2008-03-28 13:36:09 marchand Exp $
+ * $Id: vpPoint.cpp,v 1.14 2008-03-28 16:49:38 marchand Exp $
  *
  * Copyright (C) 1998-2006 Inria. All rights reserved.
  *
@@ -179,37 +179,38 @@ vpPoint::changeFrame(const vpHomogeneousMatrix &cMo)
 /*! \brief change frame
  */
 const vpPoint
-operator*(const vpHomogeneousMatrix &cMo, const vpPoint& oP)
+operator*(const vpHomogeneousMatrix &aMb, const vpPoint& bP)
 {
-  vpPoint cP ;
+  vpPoint aP ;
 
 
   vpColVector v(4),v1(4) ;
 
-  v[0] = oP.get_X() ;
-  v[1] = oP.get_Y() ;
-  v[2] = oP.get_Z() ;
-  v[3] = oP.get_W() ;
+  v[0] = bP.get_X() ;
+  v[1] = bP.get_Y() ;
+  v[2] = bP.get_Z() ;
+  v[3] = bP.get_W() ;
 
-  v1[0] = cMo[0][0]*v[0] + cMo[0][1]*v[1]+ cMo[0][2]*v[2]+ cMo[0][3]*v[3] ;
-  v1[1] = cMo[1][0]*v[0] + cMo[1][1]*v[1]+ cMo[1][2]*v[2]+ cMo[1][3]*v[3] ;
-  v1[2] = cMo[2][0]*v[0] + cMo[2][1]*v[1]+ cMo[2][2]*v[2]+ cMo[2][3]*v[3] ;
-  v1[3] = cMo[3][0]*v[0] + cMo[3][1]*v[1]+ cMo[3][2]*v[2]+ cMo[3][3]*v[3] ;
+  v1[0] = aMb[0][0]*v[0] + aMb[0][1]*v[1]+ aMb[0][2]*v[2]+ aMb[0][3]*v[3] ;
+  v1[1] = aMb[1][0]*v[0] + aMb[1][1]*v[1]+ aMb[1][2]*v[2]+ aMb[1][3]*v[3] ;
+  v1[2] = aMb[2][0]*v[0] + aMb[2][1]*v[1]+ aMb[2][2]*v[2]+ aMb[2][3]*v[3] ;
+  v1[3] = aMb[3][0]*v[0] + aMb[3][1]*v[1]+ aMb[3][2]*v[2]+ aMb[3][3]*v[3] ;
 
   v1 /= v1[3] ;
 
   //  v1 = M*v ;
-  cP.set_X(v1[0]) ;
-  cP.set_Y(v1[1]) ;
-  cP.set_Z(v1[2]) ;
-  cP.set_W(v1[3]) ;
-  oP.set_X(v1[0]) ;
-  oP.set_Y(v1[1]) ;
-  oP.set_Z(v1[2]) ;
-  oP.set_W(v1[3]) ;
+  aP.set_X(v1[0]) ;
+  aP.set_Y(v1[1]) ;
+  aP.set_Z(v1[2]) ;
+  aP.set_W(v1[3]) ;
+ 
+  aP.set_oX(v1[0]) ;
+  aP.set_oY(v1[1]) ;
+  aP.set_oZ(v1[2]) ;
+  aP.set_oW(v1[3]) ;
+ 
 
-
-  return cP ;
+  return aP ;
 }
 
 /*! \brief change frame
