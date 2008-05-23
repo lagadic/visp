0c4ce3e91d52f687c552066aaae14a8b2c837f16
Fabien Spindler <Fabien.Spindler@inria.fr>
2008-05-23 13:25:47 +0000

    Rename manual examples with prefix man

--- example/manual/geometric-features/manGeometricFeatures.cpp
+++ example/manual/geometric-features/manGeometricFeatures.cpp
@@ -1,7 +1,56 @@
-#include <visp/vpConfig.h>
+/****************************************************************************
+ *
+ * $Id: manGeometricFeatures.cpp,v 1.1 2008-05-23 13:25:47 fspindle Exp $
+ *
+ * Copyright (C) 1998-2008 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Geometric features example.
+ *
+ * Authors:
+ * Anthony Saunier
+ * Fabien Spindler
+ *
+ *****************************************************************************/
+/*!
+  \file manGeometricFeatures.cpp
+
+  \brief Geometric features projection example.
+
+*/
+/*!
+  \example manGeometricFeatures.cpp
+
+  \brief Geometric features projection example.
+ 
+*/
 
 #include <iostream>
 
+#include <visp/vpConfig.h>
 #include <visp/vpDebug.h>
 #include <visp/vpImageIo.h>
 // For 2D image 
@@ -23,7 +72,7 @@
 
 int main()
 {
-  #ifdef VISP_HAVE_GTK
+#ifdef VISP_HAVE_GTK
   
   std::cout << "ViSP geometric features display example" <<std::endl;
   unsigned int height = 288;
@@ -83,6 +132,6 @@ int main()
   std::cout << "ViSP creates \"./geometricFeatures.ppm\" B&W image "<< std::endl;
   vpImageIo::writePPM(Ic, "./geometricFeatures.ppm");
 
-  #endif
+#endif
   return 0;
 }
