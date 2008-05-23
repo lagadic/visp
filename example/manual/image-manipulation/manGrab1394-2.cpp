b497d981c138a1b38d218505584bb32040cb2fb3
Anthony Saunier <Anthony.Saunier@inria.fr>
2008-05-23 15:22:01 +0000

    add doxygen documentation

--- example/manual/image-manipulation/manGrab1394-2.cpp
+++ example/manual/image-manipulation/manGrab1394-2.cpp
@@ -1,3 +1,53 @@
+/****************************************************************************
+ *
+ * $Id: manGrab1394-2.cpp,v 1.2 2008-05-23 15:22:01 asaunier Exp $
+ *
+ * Copyright (C) 1998-2008 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * images grabbing example.
+ *
+ * Authors:
+ * Anthony Saunier
+ * Fabien Spindler
+ *
+ *****************************************************************************/
+/*!
+  \file manGrab1394-2.cpp
+
+  \brief images grabbing example with the vp1394TwoGrabber class.
+
+ */
+/*!
+  \example manGrab1394-2.cpp
+
+  \brief images grabbing example with the vp1394TwoGrabber class.
+ 
+ */
+
 #include <visp/vpImage.h>
 #include <visp/vp1394TwoGrabber.h>
 
--- example/manual/image-manipulation/manGrabDisk.cpp
+++ example/manual/image-manipulation/manGrabDisk.cpp
@@ -1,3 +1,53 @@
+/****************************************************************************
+ *
+ * $Id: manGrabDisk.cpp,v 1.2 2008-05-23 15:22:01 asaunier Exp $
+ *
+ * Copyright (C) 1998-2008 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * images grabbing example.
+ *
+ * Authors:
+ * Anthony Saunier
+ * Fabien Spindler
+ *
+ *****************************************************************************/
+/*!
+  \file manGrabDisk.cpp
+
+  \brief images grabbing example with the vpDiskGrabber class.
+
+ */
+/*!
+  \example manGrabDisk.cpp
+
+  \brief images grabbing example with the vpDiskGrabber class.
+ 
+ */
+
 #include <visp/vpConfig.h>
 
 #include <visp/vpImage.h>
