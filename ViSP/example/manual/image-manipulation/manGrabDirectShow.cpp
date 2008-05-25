86cf5279f2aae8164ae7d68ba74d0e9d4c5c35a5
Fabien Spindler <Fabien.Spindler@inria.fr>
2008-05-25 07:29:17 +0000

    Compilation error suppression

--- ViSP/example/manual/image-manipulation/manGrabDirectShow.cpp
+++ ViSP/example/manual/image-manipulation/manGrabDirectShow.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * $Id: manGrabDirectShow.cpp,v 1.3 2008-05-23 15:44:42 asaunier Exp $
+ * $Id: manGrabDirectShow.cpp,v 1.4 2008-05-25 07:29:17 fspindle Exp $
  *
  * Copyright (C) 1998-2008 Inria. All rights reserved.
  *
@@ -53,25 +53,26 @@
 #include <visp/vpImage.h>
 #include <visp/vpDirectShowGrabber.h>
 
-int main(){
-#ifdef VISP_HAVE_DIRECTSHOW
-
+int main()
+{
   vpImage<unsigned char> I; // Grey level image
   
+#ifdef VISP_HAVE_DIRECTSHOW
   vpDirectShowGrabber g; // Create the grabber
   if(g.getDeviceNumber() == 0) //test if a camera is connected
   {
     g.close();
-    exit(0);
+    return -1;
   }
   
   g.open(); // Initialize the grabber
   
-  setImageSize(640,480); // If the camera supports 640x480 image size
-  setFramerate(30); // If the camera supports 30fps framerate
+  g.setImageSize(640,480); // If the camera supports 640x480 image size
+  g.setFramerate(30); // If the camera supports 30fps framerate
   
   while(1)
     g.acquire(I); // Acquire an image
-
 #endif
+
+  return 0;
 }
