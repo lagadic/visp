67cc53f24a47af1b0b8e8c354d57531e029be873
Fabien Spindler <Fabien.Spindler@inria.fr>
2008-11-27 12:57:49 +0000

    Add new test testAfma6.cpp

---
+++ ViSP/test/servo-afma6/testAfma6.cpp
@@ -0,0 +1,93 @@
+/****************************************************************************
+ *
+ * $Id: testAfma6.cpp,v 1.1 2008-11-27 12:57:49 fspindle Exp $
+ *
+ * Copyright (C) 1998-2006 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Test for Afma 6 dof robot.
+ *
+ * Authors:
+ * Fabien Spindler
+ *
+ *****************************************************************************/
+
+/*!
+  \example testAfma6.cpp
+
+  Example of a real robot control, the Afma6 robot (cartesian robot, with 6
+  degrees of freedom).
+*/
+
+#include <iostream>
+
+
+#include <visp/vpConfig.h>
+#include <visp/vpAfma6.h>
+#include <visp/vpDebug.h>
+#include <visp/vpCameraParameters.h>
+
+
+int main()
+{
+  try {
+
+    std::cout << "a test for vpAfma6 class..." << std::endl;
+
+    vpAfma6 afma6;
+    vpCameraParameters cam;
+
+    std::cout << "-- Default settings for Afma6  ---" 
+	      << std::endl;
+    std::cout << afma6 << std::endl;
+    afma6.getCameraParameters(cam, 640, 480);
+    std::cout << cam << std::endl;
+
+    std::cout << "-- Settings associated to camera Dragonfly2 without distortion ---" 
+	      << std::endl;
+    afma6.init( vpAfma6::CAMERA_DRAGONFLY2_8MM);
+
+    std::cout << afma6 << std::endl;
+    afma6.getCameraParameters(cam, 640, 480);
+    std::cout << cam << std::endl;
+
+    std::cout << "-- Settings associated to camera Dragonfly2 with distortion ------" 
+	      << std::endl;
+    afma6.init( vpAfma6::CAMERA_DRAGONFLY2_8MM,
+		vpCameraParameters::perspectiveProjWithDistortion);
+    std::cout << afma6 << std::endl;
+    afma6.getCameraParameters(cam, 640, 480);
+    std::cout << cam << std::endl;
+
+    return 0;
+  }
+  catch(...) {
+    vpERROR_TRACE(" Test failed");
+    return 0;
+  }
+
+}
+
