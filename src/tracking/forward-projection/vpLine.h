4fc5f71f14024bd98c3498eb1e38a0d731607a2b
Nicolas Melchior <Nicolas.Melchior@inria.fr>
2009-02-02 15:17:19 +0000

    Improve the html documentation.

--- src/tracking/forward-projection/vpLine.h
+++ src/tracking/forward-projection/vpLine.h
@@ -53,24 +53,31 @@
 /*!
   \class vpLine
   \ingroup TrackingFeature GeometryFeature
-  \brief Class that defines what is a line.
+  \brief Class that defines a line in the three different frames : the object frame, the camera frame and the image frame. All the features are given  and must be set in meter.
 
-  Line equation in 2D
+  In the 3D frames (ie the object frame and the camera frame), the line is defined as the intersection between two plans. Thus, the parameters which define the line are the parameters which define the two equations of the two plans. Each point which belongs to the line is a solution of those two equations :
+  \f[ A1 X + B1 Y + C1 Z +D1 = 0 \f]
+  \f[ A2 X + B2 Y + C2 Z +D2 = 0 \f]
+  Here \f$ (X, Y, Z) \f$ are the 3D coordinates in one of the two 3D frames.
 
-  x cos(theta) + y sin(theta) - rho = 0
+  In this class it is easily possible to compute the "equation" of the line in the camera frame thanks to this in the object frame. But you have to notes that four constraints are added in the planes equations.
+  \f[ D1 = 0 \f]
+  \f[ D2 > 0 \f]
+  \f[ A1 A2 + B1 B2 + C1 C2 = 0 \f]
+  \f[ || A2 || = 1 \f]
 
-  in 3D the line is represented using the intersection of two planes
+  In the 2D frame (ie the image frame), the line is defined thanks to its equation.
+  \f[ x \times cos(\theta) + y \times sin(\theta) -\rho = 0 \f]
+  Here \f$ x \f$ and \f$ y \f$ are the coordinates in the image frame, \f$ \rho \f$ and \f$ \theta \f$ are the parameters used to define the line. The value of \f$ \theta \f$ is between \f$ -\pi/2 \f$ and \f$ \pi/2 \f$ and the value of \f$ \rho \f$ can be positive or negative. The conventions used to choose the sign of \f$ \rho \f$ and the value of \f$ \theta \f$ are illustrated by the following image.
 
-  a1 x + b1 y + c1 z + d1 = 0
-  a2 x + b2 y + c2 z + d2 = 0
+  \image html vpFeatureLine.gif
+  \image latex vpFeatureLine.ps  width=10cm
 
-  when converted in the camera frame, four constraints are added in the planes
-  equation :
-  d1 = 0
-  d2 > 0
-  a1 a2 + b1 b2 + c1 c2 = 0
-  || a2 || = 1
+  In this class the line features corresponding to the object frame are located in the oP vector \f[ oP = \left[\begin{array}{c}A1_o \\ B1_o \\ C1_o \\ D1_o \\ A2_o \\ B2_o \\ C2_o \\ D2_o \end{array}\right] \f]
 
+  The line features corresponding to the camera frame are located in the cP vector \f[ cP = \left[\begin{array}{c}A1_c \\ B1_c \\ C1_c \\ D1_c \\ A2_c \\ B2_c \\ C2_c \\ D2_c \end{array}\right] \f]
+
+  The line features corresponding to the camera frame are located in the p vector \f[ p = \left[\begin{array}{c} \rho \\ \theta \end{array}\right] \f]
 */
 class VISP_EXPORT vpLine : public vpForwardProjection
 {
@@ -89,36 +96,58 @@ public:
   vpColVector oP2 ;
   */
 public:
-  //! basic construction
+
   void init() ;
-  //! basic constructor
+
   vpLine() ;
-  //! destructor
+  //! Destructor
   virtual ~vpLine() { ; }
 
 public:
 
-  //! set the line coordinates
+  /*!
+    Sets the \f$ \rho \f$ value.
+
+    \param _rho : The desired value for \f$ \rho \f$.
+  */
   void setRho(const double _rho) {  p[0] = _rho ; };
+
+  /*!
+    Sets the \f$ \theta \f$ value.
+
+    \param _theta : The desired value for \f$ \theta \f$.
+  */
   void setTheta(const double _theta) {  p[1] = _theta ;};
 
-  //! get the line -coordinates
+  /*!
+    Gets the \f$ \theta \f$ value.
+
+    \return Returns the current value of \f$ \theta \f$.
+  */
   double getTheta()   const {  return p[1] ; }
+
+  /*!
+    Gets the \f$ \rho \f$ value.
+
+    \return Returns the current value of \f$ \rho \f$.
+  */
   double getRho()  const  {  return p[0] ; }
 
 
-  //! set the line world coordinates
+
   void setWorldCoordinates(const double &A1, const double &B1,
 			   const double &C1, const double &D1,
 			   const double &A2, const double &B2,
 			   const double &C2, const double &D2) ;
-  //! set the line world coordinates from two planes
+
+
   void setWorldCoordinates(const vpColVector &_oP1,
 			   const vpColVector &_oP2) ;
-  //! set the line world coordinates
+
+
   void setWorldCoordinates(const vpColVector &_oP1) ;
 
-  //! projection
+
   void projection() ;
   void projection(const vpColVector &_cP, vpColVector &p) ;
   void changeFrame(const vpHomogeneousMatrix &cMo, vpColVector &_cP) ;
