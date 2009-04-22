eec21058b24f5380ab68583ee8772106944af238
Nicolas Melchior <Nicolas.Melchior@inria.fr>
2009-04-22 13:48:42 +0000

    Add of the class.

---
+++ src/image/vpImagePoint.cpp
@@ -0,0 +1,65 @@
+/****************************************************************************
+ *
+ * $Id: vpBasicFeature.h,v 1.10 2008-02-26 10:32:10 asaunier Exp $
+ *
+ * Copyright (C) 1998-2006 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit.
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * 2D point usefull for image processing
+ *
+ * Author:
+ * Nicolas Melchior
+ *
+ *****************************************************************************/
+
+#include <visp/vpImagePoint.h>
+#include <visp/vpDebug.h>
+
+
+/*!
+  \file vpImagePoint.h
+  \brief class that defines a 2D point in an image. This class is usefull for image processing.
+*/
+
+
+void
+vpImagePoint::init()
+{
+  iP.resize(2) ;
+  iP = 0;
+}
+
+vpImagePoint::vpImagePoint()
+{
+  init();
+}
+
+
+/*
+ * Local variables:
+ * c-basic-offset: 2
+ * End:
+ */
