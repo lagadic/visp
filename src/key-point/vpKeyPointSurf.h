dc46dd0cb1810cfff346416293e7e68872418a97
Fabien Spindler <Fabien.Spindler@inria.fr>
2009-05-04 08:25:35 +0000

    Fix a compilation error if OpenCV is not detected

--- src/key-point/vpKeyPointSurf.h
+++ src/key-point/vpKeyPointSurf.h
@@ -41,7 +41,9 @@
 
 /*!
   \file vpKeyPointSurf.h
-  \brief Class that implements the SURF key points and technics thanks to the OpenCV library.
+
+  \brief Class that implements the SURF key points and technics thanks
+  to the OpenCV library.
 */
 
 #include <visp/vpConfig.h>
@@ -57,10 +59,31 @@
 
 /*!
   \class vpKeyPointSurf
-  \brief Class that implements the SURF key points and technics thanks to the OpenCV library. The goal of this class is to provide a tool to match points from a model and points belonging to an image in which the model appears.
-  The coordinates of the different reference points and matched points are given in pixel thanks to the class vpImagePoint. In this documentation we do not explain the SURF technics. So if you want to learn more about it you can refere to the following article : Herbert Bay, Tinne Tuytelaars and Luc Van Gool "SURF: Speeded Up Robust Features", Proceedings of the 9th European Conference on Computer Vision, Springer LNCS volume 3951, part 1, pp 404--417, 2006.
+  
+  \brief Class that implements the SURF key points and technics thanks
+  to the OpenCV library.
 
-  If you use this class the first things you have to do is to create the reference thanks to a refrence image which contains the interesting object to detect. Then you have to grab other images containing the object. After calling the specific method to match points you can access to the lists of matched points thanks to the methods getMatchedPointsInReferenceImage() and getMatchedPointsInCurrentImage(). These two methods return a list of matched points. The nth element of the first list is matched with the nth element of the second list. The following small example show how to use the class.
+  The goal of this class is to provide a tool to match points from a
+  model and points belonging to an image in which the model appears.
+  The coordinates of the different reference points and matched points
+  are given in pixel thanks to the class vpImagePoint. In this
+  documentation we do not explain the SURF technics. So if you want to
+  learn more about it you can refere to the following article :
+  Herbert Bay, Tinne Tuytelaars and Luc Van Gool "SURF: Speeded Up
+  Robust Features", Proceedings of the 9th European Conference on
+  Computer Vision, Springer LNCS volume 3951, part 1, pp 404--417,
+  2006.
+
+  If you use this class the first things you have to do is to create
+  the reference thanks to a refrence image which contains the
+  interesting object to detect. Then you have to grab other images
+  containing the object. After calling the specific method to match
+  points you can access to the lists of matched points thanks to the
+  methods getMatchedPointsInReferenceImage() and
+  getMatchedPointsInCurrentImage(). These two methods return a list of
+  matched points. The nth element of the first list is matched with
+  the nth element of the second list. The following small example show
+  how to use the class.
 
   \code
 #include <visp/vpImage.h>
@@ -86,7 +109,9 @@ int main()
   surf.display(Irefrence, Icurrent);
   \endcode
 
-  It is also possible to create the refernece thanks to only a part of the reference image (not the whole image) and find points to match in only a part of the current image. The small following example shows how to this
+  It is also possible to create the refernece thanks to only a part of the 
+  reference image (not the whole image) and find points to match in only a 
+  part of the current image. The small following example shows how to this
 
   \code
 #include <visp/vpImage.h>
@@ -136,15 +161,16 @@ int main()
 class VISP_EXPORT vpKeyPointSurf : public vpBasicKeyPoint
 {
   public:
-    /*!
-      This enumerate enable to set the detail level of the descriptors
-      basicDescriptor means that the descriptors are composed by 64 elements floating-point vector.
-      extendedDescriptor means that the descriptors are composed by 128 elements floating-point vector.
+    /*!  
+      This enumerate enables to set the detail level of the
+      descriptors.
     */
     typedef enum 
     {
-      basicDescriptor,
-      extendedDescriptor
+      basicDescriptor,   /*<! basicDescriptor means that the descriptors are
+			   composed by 64 elements floating-point vector. */
+      extendedDescriptor /*<! Means that the descriptors are composed by
+			   128 elements floating-point vector. */
     } vpDescriptorType;
 
   public:
@@ -154,31 +180,47 @@ class VISP_EXPORT vpKeyPointSurf : public vpBasicKeyPoint
     virtual ~vpKeyPointSurf() {cvReleaseMemStorage(&storage);} ;
 
     int buildReference(const vpImage<unsigned char> &I);
-    int buildReference(const vpImage<unsigned char> &I, vpImagePoint &iP, unsigned int height, unsigned int width);
-    int buildReference(const vpImage<unsigned char> &I, const vpRect rectangle);
+    int buildReference(const vpImage<unsigned char> &I, 
+		       vpImagePoint &iP,
+		       unsigned int height, unsigned int width);
+    int buildReference(const vpImage<unsigned char> &I,
+		       const vpRect rectangle);
     int matchPoint(const vpImage<unsigned char> &I);
-    int matchPoint(const vpImage<unsigned char> &I, vpImagePoint &iP, unsigned int height, unsigned int width);
+    int matchPoint(const vpImage<unsigned char> &I, 
+		   vpImagePoint &iP, unsigned int height, unsigned int width);
     int matchPoint(const vpImage<unsigned char> &I, const vpRect rectangle);
-    void display(vpImage<unsigned char> &Iref, vpImage<unsigned char> &Icurrent);
+    void display(vpImage<unsigned char> &Iref, 
+		 vpImage<unsigned char> &Icurrent);
     void display(vpImage<unsigned char> &Icurrent);
 
     /*!
-      Sets the value of the hessian threhold.
-      Notes that during the computation of the hessian for each potential points, only the points which have a hessian value higher than the threshold are keeped.
-      Fore more details about the threshold see the article Herbert Bay, Tinne Tuytelaars and Luc Van Gool "SURF: Speeded Up Robust Features", Proceedings of the 9th European Conference on Computer Vision, Springer LNCS volume 3951, part 1, pp 404--417, 2006.
+
+      Sets the value of the hessian threhold.  Note that during the
+      computation of the hessian for each potential points, only the
+      points which have a hessian value higher than the threshold are
+      keeped.  Fore more details about the threshold see the article
+      Herbert Bay, Tinne Tuytelaars and Luc Van Gool "SURF: Speeded Up
+      Robust Features", Proceedings of the 9th European Conference on
+      Computer Vision, Springer LNCS volume 3951, part 1, pp 404--417,
+      2006.
 
       \param hessianThreshold : Desired hessian threshold value.
     */
-    void setHessianThreshold (double hessianThreshold) {this->hessianThreshold = hessianThreshold; params = cvSURFParams(this->hessianThreshold, this->descriptorType);} ;
+    void setHessianThreshold (double hessianThreshold) {
+	this->hessianThreshold = hessianThreshold;
+	params = cvSURFParams(this->hessianThreshold, this->descriptorType);
+    } ;
 
     /*!
-      Sets the type of descriptors to use.
-      basicDescriptor means that the descriptors are composed by 64 elements floating-point vector.
-      extendedDescriptor means that the descriptors are composed by 128 elements floating-point vector.
 
-      \param descriptorType : type of descriptor to use.
+      Sets the type of descriptors to use.
+
+      \param descriptorType : Type of descriptor to use.
     */
-    void setDescriptorType (vpDescriptorType descriptorType) {this->descriptorType = descriptorType; params = cvSURFParams(this->hessianThreshold, this->descriptorType);} ;
+    void setDescriptorType (vpDescriptorType descriptorType) {
+	this->descriptorType = descriptorType; 
+	params = cvSURFParams(this->hessianThreshold, this->descriptorType);
+    } ;
 
     /*!
       Gets the value of the hessian threhold.
@@ -211,19 +253,16 @@ class VISP_EXPORT vpKeyPointSurf : public vpBasicKeyPoint
 
     /*!
       only features with keypoint.hessian larger than that are extracted.
-                  // good default value is ~300-500 (can depend on the average
-                  // local contrast and sharpness of the image).
-                  // user can further filter out some features based on their hessian values
-                  // and other characteristics
+      Good default value is ~300-500 (can depend on the average
+      local contrast and sharpness of the image).
+      User can further filter out some features based on their hessian values
+      and other characteristics.
     */
     double hessianThreshold;
     vpDescriptorType descriptorType;
 
 };
 
-#else
-
-std::cout << "Only available with opencv 1.1.0 or more recent versions..." << std::endl;
 
 #endif 
 
