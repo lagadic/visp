9103426033a9c098d775fb9a7552aef03f7a8a96
Fabien Spindler <Fabien.Spindler@inria.fr>
2009-07-08 17:02:40 +0000

    Compilation fix if libplotter is not detected or if platform in not UNIX

--- ViSP/example/tools/plot.cpp
+++ ViSP/example/tools/plot.cpp
@@ -111,5 +111,20 @@ int main ()
   plot.saveData(0,"dataSin.txt");
 }
 
+#else
+int
+main()
+{
+  vpTRACE("Sorry, since libplotter was not detected, plotting functionalities are not available...");
+  return 0;
+}
 #endif
+
+#else
+int
+main()
+{
+  vpTRACE("Sorry, plotting functionalities are only available on unix...");
+  return 0;
+}
 #endif
