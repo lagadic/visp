b21c1950bd44478dc1256c28813f792b99414e5b
Fabien Spindler <Fabien.Spindler@inria.fr>
2009-09-29 16:24:54 +0000

    New vpFeatureTranslation ctc* New example without vpServo

--- ViSP/example/servo-simu/servoSimu3D_cMcd_CamVelocity.cpp
+++ ViSP/example/servo-simu/servoSimu3D_cMcd_CamVelocity.cpp
@@ -36,21 +36,24 @@
  *
  *****************************************************************************/
 /*!
-  \file servoSimu3DCamVelocity2.cpp
-  \brief Simulation of a 3D visual servoing:
-  - eye-in-hand control law,
-  - velocity computed in the camera frame,
-  - no display.
+  \example servoSimu3D_cMcd_CamVelocity.cpp
+
+  Simulation of a 3D visual servoing where the current visual feature
+  is given by \f$s=({^{c}}{\bf t}_{c^*}, \theta U_{{^{c}}{\bf
+  R}_{c^*}})^T\f$ and the desired one \f$s^*=(0,0)^T\f$.
+
+  The control law is set as:
+  - an eye-in-hand control law,
+  - where velocities are computed in the camera frame.
+
+  To compute the camera velocities, we use here the vpServo class. 
+  
+  This example is to make into relation with
+  servoSimu3D_cdMc_CamVelocity.cpp where the current visual feature is
+  \f$s=({^{c^*}}{\bf t}_c, \theta U_{{^{c^*}}{\bf R}_c})^T\f$.
+
 */
 
-
-/*!
-  \example servoSimu3DCamVelocity2.cpp
-  Simulation of a 3D visual servoing:
-  - eye-in-hand control law,
-  - velocity computed in the camera frame,
-  - no display.
-*/
 
 
 #include <stdlib.h>
@@ -152,95 +155,94 @@ main(int argc, const char ** argv)
   std::cout << "-------------------------------------------------------" << std::endl ;
   std::cout << std::endl ;
 
+  // Sets the initial camera location
+  vpPoseVector c_r_o(// Translation tx,ty,tz
+		     0.1, 0.2, 2, 
+		     // ThetaU rotation 
+		     vpMath::rad(20), vpMath::rad(10),  vpMath::rad(50) ) ;
 
-  vpTRACE("sets the initial camera location " ) ;
-  vpPoseVector c_r_o(0.1,0.2,2,
-		     vpMath::rad(20), vpMath::rad(10),  vpMath::rad(50)
-		     ) ;
-
-  vpCTRACE ; std::cout << std::endl ;
+  // From the camera pose build the corresponding homogeneous matrix
   vpHomogeneousMatrix cMo(c_r_o) ;
-  vpCTRACE ; std::cout << std::endl ;
-  robot.setPosition(cMo) ;
-  vpCTRACE ; std::cout << std::endl ;
 
-  vpTRACE("sets the desired camera location " ) ;
-  vpPoseVector cd_r_o(0,0,1,
-		      vpMath::rad(0),vpMath::rad(0),vpMath::rad(0)) ;
+  // Set the robot initial position
+  robot.setPosition(cMo) ;
+
+  // Sets the desired camera location
+  vpPoseVector cd_r_o(// Translation tx,ty,tz
+		      0, 0, 1, 
+		      // ThetaU rotation 
+		      vpMath::rad(0),vpMath::rad(0),vpMath::rad(0)) ; 
+
+  // From the camera desired pose build the corresponding homogeneous matrix
   vpHomogeneousMatrix cdMo(cd_r_o) ;
 
-
-
-  vpTRACE("compute the rotation that the camera has to realize "  ) ;
-  vpHomogeneousMatrix cdMc ;
-  cdMc = cdMo*cMo.inverse() ;
-  
-  // build the translation feature
-  vpFeatureTranslation t(vpFeatureTranslation::cdMc) ;
-  t.buildFrom(cdMc) ;
-
-  // build the theta u feature
+  // Compute the transformation from the initial camera position to the desired one
   vpHomogeneousMatrix cMcd ;
-  cMcd = cdMc.inverse() ; 
+  cMcd = cMo*cdMo.inverse() ;
+  
+  // Build the 3D translation feature: ctc*
+  vpFeatureTranslation t(vpFeatureTranslation::cMcd) ;
+  t.buildFrom(cMcd) ;
+
+  // Build the 3D rotation feature: thetaU_cRc*
   vpFeatureThetaU tu(vpFeatureThetaU::cRcd); // current feature
   tu.buildFrom(cMcd) ;
 
-  vpTRACE("sets the desired rotation (always zero !) ") ;
-  vpTRACE("since s is the rotation that the camera has to realize ") ;
-
-  vpFeatureTranslation td(vpFeatureTranslation::cdMc) ;
+  // Sets the desired rotation (always zero !)  since s is the
+  // rotation that the camera has to achieve. Here s* = (0, 0)^T
+  vpFeatureTranslation td(vpFeatureTranslation::cMcd) ;
   vpFeatureThetaU tud(vpFeatureThetaU::cRcd); // desired feature
 
-  vpTRACE("define the task") ;
-  vpTRACE("\t we want an eye-in-hand control law") ;
-  vpTRACE("\t robot is controlled in the camera frame") ;
+  // Define the task
+  // - we want an eye-in-hand control law
+  // - the robot is controlled in the camera frame
   task.setServo(vpServo::EYEINHAND_CAMERA) ;
-  // We use here the interaction matrix computed with the current
-  // features
+  // - we use here the interaction matrix computed with the current
+  //   features
   task.setInteractionMatrixType(vpServo::CURRENT);
 
-
-  task.addFeature(t,td) ;    // translation
-  task.addFeature(tu,tud) ;  // rotation theta u
+  // Add the current and desired visual features
+  task.addFeature(t,td) ;    // 3D translation
+  task.addFeature(tu,tud) ;  // 3D rotation theta u
   
-  vpTRACE("\t set the gain") ;
+  // - set the constant gain to 1.0
   task.setLambda(1) ;
 
-
-  vpTRACE("Display task information " ) ;
+  // Display task information
   task.print() ;
 
   int iter=0 ;
   vpTRACE("\t loop") ;
-  while(iter++<200)
-    {
-      std::cout << "------------------------------------" << iter <<std::endl ;
-      vpColVector v ;
+  while(iter++ < 200) {
+    std::cout << "------------------------------------" << iter <<std::endl ;
+    vpColVector v ;
 
-      if (iter==1) vpTRACE("\t\t get the robot position ") ;
-      robot.getPosition(cMo) ;
+    // get the robot position
+    robot.getPosition(cMo) ;
 
-      if (iter==1) vpTRACE("\t\t new rotation to realize ") ;
-      cdMc = cdMo*cMo.inverse() ;
-      t.buildFrom(cdMc) ;
-      cMcd = cdMc.inverse() ; 
-      tu.buildFrom(cMcd) ;
+    // new displacement to achieve
+    cMcd = cMo*cdMo.inverse() ;
+      
+    // Update the current visual features
+    t.buildFrom(cMcd) ;
+    tu.buildFrom(cMcd) ;
 
-      if (iter==1) 
-	vpTRACE("\t\t compute the control law ") ;
-      v = task.computeControlLaw() ;
-      if (iter==1) task.print() ;
+    // Compute the control law
+    v = task.computeControlLaw() ;
 
-      if (iter==1) 
-	vpTRACE("\t\t send the camera velocity to the controller ") ;
-      robot.setVelocity(vpRobot::CAMERA_FRAME, v) ;
+    // Display task information
+    if (iter==1) task.print() ;
 
+    // Send the camera velocity to the controller
+    robot.setVelocity(vpRobot::CAMERA_FRAME, v) ;
 
-      std::cout << task.error.sumSquare() <<std::endl ; ;
-    }
-
-  vpTRACE("Display task information " ) ;
+    // Retrieve the error 
+    std::cout << task.error.sumSquare() <<std::endl ; ;
+  }
+  // Display task information
   task.print() ;
+
+  // Kill the task
   task.kill();
 }
 
