1d9146fc7c85a4a4c8b342c39447f3e17893be4a
Nicolas Melchior <Nicolas.Melchior@inria.fr>
2009-12-01 10:48:57 +0000

    Add two examples/tests to explain the use of the vpVideoReader class.

---
+++ ViSP/example/video/videoReader.cpp
@@ -0,0 +1,294 @@
+/****************************************************************************
+ *
+ * $Id: imageDiskRW.cpp 2158 2009-05-07 07:24:51Z fspindle $
+ *
+ * Copyright (C) 1998-2006 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit.
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Reading a video file.
+ *
+ * Authors:
+ * Eric Marchand
+ *
+ *****************************************************************************/
+
+/*!
+  \file videoReader.cpp
+  \brief   reading a video file using vpVideoReader class.
+ */
+
+/*!
+  \example videoReader.cpp
+  Reading a video file using vpVideoReader class.
+ */
+
+#include <visp/vpConfig.h>
+#include <visp/vpImage.h>
+#include <visp/vpImageIo.h>
+#include <visp/vpParseArgv.h>
+#include <visp/vpIoTools.h>
+#include <visp/vpDebug.h>
+#include <visp/vpVideoReader.h>
+#include <visp/vpDisplayOpenCV.h>
+#include <visp/vpDisplayX.h>
+#include <visp/vpDisplayGTK.h>
+
+// List of allowed command line options
+#define GETOPTARGS	"cdi:h"
+
+/*!
+
+Print the program options.
+
+\param name : Program name.
+\param badparam : Bad parameter name.
+\param ipath : Input video path.
+
+ */
+void usage(const char *name, const char *badparam, std::string ipath)
+{
+  fprintf(stdout, "\n\
+Read a video file on the disk.\n\
+\n\
+SYNOPSIS\n\
+  %s [-i <input video path>] \n\
+     [-h]\n						      \
+", name);
+
+  fprintf(stdout, "\n\
+OPTIONS:                                               Default\n\
+  -i <input video path>                                %s\n\
+     Set video input path.\n\
+     From this path read \"ViSP-images/video/video.mpeg\"\n\
+     video.\n\
+     Setting the VISP_INPUT_IMAGE_PATH environment\n\
+     variable produces the same behaviour than using\n\
+     this option.\n\
+\n\
+  -c\n\
+     Disable the mouse click. Usefull to automaze the \n\
+     execution of this program without humain intervention.\n\
+\n\
+  -d \n\
+     Turn off the display.\n\
+\n\
+  -h\n\
+     Print the help.\n\n",
+	  ipath.c_str());
+
+  if (badparam) {
+    fprintf(stderr, "ERROR: \n" );
+    fprintf(stderr, "\nBad parameter [%s]\n", badparam);
+  }
+}
+/*!
+
+  Set the program options.
+
+  \param argc : Command line number of parameters.
+  \param argv : Array of command line parameters.
+  \param ipath : Input video path.
+  \param click_allowed : Mouse click activation.
+  \param display : Display activation.
+  \return false if the program has to be stopped, true otherwise.
+
+*/
+bool getOptions(int argc, const char **argv,
+		std::string &ipath, bool &click_allowed, bool &display)
+{
+  const char *optarg;
+  int	c;
+  while ((c = vpParseArgv::parse(argc, argv, GETOPTARGS, &optarg)) > 1) {
+
+    switch (c) {
+    case 'c': click_allowed = false; break;
+    case 'd': display = false; break;
+    case 'i': ipath = optarg; break;
+    case 'h': usage(argv[0], NULL, ipath); return false; break;
+
+    default:
+      usage(argv[0], optarg, ipath); return false; break;
+    }
+  }
+
+  if ((c == 1) || (c == -1)) {
+    // standalone param or error
+    usage(argv[0], NULL, ipath);
+    std::cerr << "ERROR: " << std::endl;
+    std::cerr << "  Bad argument " << optarg << std::endl << std::endl;
+    return false;
+  }
+
+  return true;
+}
+
+
+
+int
+main(int argc, const char ** argv)
+{
+
+  std::string env_ipath;
+  std::string opt_ipath;
+  std::string ipath;
+  std::string filename;
+  bool opt_click_allowed = true;
+  bool opt_display = true;
+
+  std::cout <<  "-------------------------------------------------------" << std::endl ;
+  std::cout <<  "  videoReader.cpp" <<std::endl << std::endl ;
+
+  std::cout <<  "  reading a video file" << std::endl ;
+  std::cout <<  "-------------------------------------------------------" << std::endl ;
+  std::cout << std::endl ;
+
+
+// Get the VISP_IMAGE_PATH environment variable value
+  char *ptenv = getenv("VISP_INPUT_IMAGE_PATH");
+  if (ptenv != NULL)
+    env_ipath = ptenv;
+
+  // Set the default input path
+  if (! env_ipath.empty())
+    ipath = env_ipath;
+
+  // Read the command line options
+  if (getOptions(argc, argv, opt_ipath, opt_click_allowed,
+		 opt_display) == false) {
+    exit (-1);
+  }
+
+  // Get the option values
+  if (!opt_ipath.empty())
+    ipath = opt_ipath;
+
+  // Compare ipath and env_ipath. If they differ, we take into account
+  // the input path comming from the command line option
+  if (!opt_ipath.empty() && !env_ipath.empty()) {
+    if (ipath != env_ipath) {
+      std::cout << std::endl
+	   << "WARNING: " << std::endl;
+      std::cout << "  Since -i <visp image path=" << ipath << "> "
+	   << "  is different from VISP_IMAGE_PATH=" << env_ipath << std::endl
+	   << "  we skip the environment variable." << std::endl;
+    }
+  }
+
+  // Test if an input path is set
+  if (opt_ipath.empty() && env_ipath.empty()){
+    usage(argv[0], NULL, ipath);
+    std::cerr << std::endl
+	 << "ERROR:" << std::endl;
+    std::cerr << "  Use -i <visp image path> option or set VISP_INPUT_IMAGE_PATH "
+	 << std::endl
+	 << "  environment variable to specify the location of the " << std::endl
+	 << "  video path where test images are located." << std::endl << std::endl;
+    exit(-1);
+  }
+
+
+/////////////////////////////////////////////////////////////////////
+
+
+  // vpImage is a template class you can declare vpImage of ... everything...
+  vpImage<vpRGBa> I ;
+  
+  //Create the video Reader
+  vpVideoReader reader;
+
+  filename = ipath +  vpIoTools::path("/ViSP-images/video/video.mpeg");
+  
+  //Initialize the reader and get the first frame.
+  reader.setFileName(filename.c_str());
+  reader.open(I);
+
+  // We open a window using either X11, GTK or GDI.
+#if defined VISP_HAVE_X11
+  vpDisplayX display;
+#elif defined VISP_HAVE_GTK
+  vpDisplayGTK display;
+#elif defined VISP_HAVE_GDI
+  vpDisplayOpenCV display;
+#endif
+
+  if (opt_display) 
+  {
+    try
+    {
+      // Display size is automatically defined by the image (I) size
+      display.init(I, 100, 100,"Display video frame") ;
+      vpDisplay::display(I) ;
+      vpDisplay::flush(I) ;
+    }
+    catch(...)
+    {
+      vpERROR_TRACE("Error while displaying the image") ;
+      exit(-1);
+    }
+  }
+  
+  if (opt_display && opt_click_allowed)
+  {
+    std::cout << "Click on the image to read and display the second frame" << std::endl;
+    vpDisplay::getClick(I);
+  }
+  
+  reader.getFrame(I,1);
+  
+  if (opt_display)
+  {
+    vpDisplay::display(I) ;
+    vpDisplay::flush(I);
+  }
+  
+  if (opt_display && opt_click_allowed)
+  {
+    std::cout << "Click on the image to read and display the 22th frame" << std::endl;
+    vpDisplay::getClick(I);
+  }
+  
+  reader.getFrame(I,21);
+  
+  if (opt_display)
+  {
+    vpDisplay::display(I) ;
+    vpDisplay::flush(I);
+  }
+  
+  if (opt_display && opt_click_allowed)
+  {
+    std::cout << "Click to leave the example" << std::endl;
+    vpDisplay::getClick(I);
+  }
+  
+  return 0;
+}
+
+/*
+ * Local variables:
+ * c-basic-offset: 2
+ * End:
+ */
