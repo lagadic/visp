cdd74259571564162e54e5b5d83bdf442122f500
Nicolas Melchior <Nicolas.Melchior@inria.fr>
2010-06-07 13:51:31 +0000

    Rename the folder simulator in coin-simulator.

---
+++ ViSP/src/coin-simulator/vpAR.cpp
@@ -0,0 +1,188 @@
+/****************************************************************************
+ *
+ * $Id$
+ *
+ * Copyright (C) 1998-2010 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit.
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Use to display an image behind the internal view of the simulator
+ * used for augmented reality application
+ *
+ * Authors:
+ * Eric Marchand
+ * Fabien Spindler
+ *
+ *****************************************************************************/
+
+/*!
+  \file vpRA.cpp
+  \brief class used to display an image behind the internal view of the simulator. Used for augmented reality applications.
+*/
+
+#include <visp/vpConfig.h>
+
+#ifdef VISP_HAVE_COIN
+
+#include <visp/vpAR.h>
+#include <visp/vpTime.h>
+
+
+/* Objets OIV. */
+#include <Inventor/nodes/SoCone.h> /* Objet cone.                            */
+#include <Inventor/nodes/SoCylinder.h> /* Objet cylindre.                    */
+#include <Inventor/nodes/SoPointLight.h> /* Objet lumiere ponctuelle.        */
+#include <Inventor/nodes/SoCoordinate3.h> /* Liste de points.                */
+#include <Inventor/nodes/SoIndexedFaceSet.h> /* Liste de face.               */
+#include <Inventor/nodes/SoTranslation.h> /* Trasnfo translation.            */
+#include <Inventor/nodes/SoScale.h> /* Trasnfo mise a l'echelle.             */
+#include <Inventor/nodes/SoRotationXYZ.h> /* Transfo rotation simple.       */
+
+#include <Inventor/nodes/SoDirectionalLight.h> /* Objet lumiere directionnelle*/
+#include <Inventor/nodes/SoMaterial.h> /* Matiere (couleur) des objets.     */
+#include <Inventor/nodes/SoDrawStyle.h> /* Style de rendu.                  */
+#include <Inventor/nodes/SoEnvironment.h> /* Eclairage ambiant.              */
+#include <Inventor/nodes/SoGroup.h> /* Groupement de noeuds (sans separation)*/
+#include <Inventor/actions/SoWriteAction.h>
+
+
+
+/*!
+	Basic Destructor that calls the kill() method of the vpSimulator class.
+*/
+vpAR::~vpAR()
+{
+  kill() ;
+}
+
+
+/*!
+	Initialisation of the internal view of the simulator.
+
+	\param width : Width of the internal view.
+	\param height : Height of the internal view.
+	\param type : Type of background image ie gray scaled or color.
+*/
+void
+vpAR::initInternalViewer(int width, int height,  vpImageType type)
+{
+
+  vpSimulator::initInternalViewer(width,height) ;
+
+
+  // no image is loaded
+  background = false ;
+
+  if ( image_background != NULL)
+  {
+    free(image_background) ;
+    image_background = NULL ;
+  }
+
+  typeImage = type;
+  if (typeImage == grayImage)
+    image_background =(GLubyte *)
+      malloc(internal_width*internal_height*sizeof(GLubyte)) ;
+  else
+    image_background =(GLubyte *)
+      malloc(3*internal_width*internal_height*sizeof(GLubyte)) ;
+
+}
+
+
+
+/*!
+	Set the background image and turn it to deal with the frame of OpenGL.
+
+	\param I : Gray scaled image for the background.
+*/
+// Grey pictures SetBackGroundImage
+void
+vpAR::setImage(vpImage<unsigned char> &I)
+{
+
+  if ((internal_width != (int)I.getWidth()) ||
+      (internal_height != (int)I.getHeight()))
+	{
+	  vpERROR_TRACE("The image size is different from the view size ");
+	  throw(vpException(vpException::dimensionError),"The image size is different from the view size") ;
+	}
+
+
+  background = true ;
+
+
+  for (unsigned int i=0 ; i < I.getHeight() ; i++)
+    for (unsigned int j=0 ; j < I.getWidth() ; j++)
+      //le repere image open GL est en bas a gauche donc l'image serait inverse
+      image_background[i*I.getWidth()+j] = I[I.getHeight()-i-1][j] ;
+
+}
+
+
+
+/*!
+	Set the background image and turn it to deal with the frame of OpenGL.
+
+	\param I : Color image for the background.
+*/
+// Color pictures SetBackGroundImage
+void
+vpAR::setImage(vpImage<vpRGBa> &I)
+{
+
+  if ((internal_width != (int)I.getWidth()) ||
+      (internal_height != (int)I.getHeight()))
+	{
+	  vpERROR_TRACE("The image size is different from the view size ");
+	  throw(vpException(vpException::dimensionError),"The image size is different from the view size") ;
+	}
+
+
+  background = true ;
+
+  unsigned int k =0 ;
+  for (unsigned int i=0 ; i <I.getHeight()  ; i++)
+    {
+      k=0;
+      for (unsigned int j=0 ; j <I.getWidth()   ; j++)
+	//le repere image open GL est en bas a gauche donc l'image serait inverse
+	{
+	  image_background[i*I.getWidth()*3+k+0]=I[I.getHeight()-i-1][j].R ;
+	  image_background[i*I.getWidth()*3+k+1]=I[I.getHeight()-i-1][j].G ;
+	  image_background[i*I.getWidth()*3+k+2]=I[I.getHeight()-i-1][j].B ;
+	  k+=3;
+      }
+    }
+
+}
+
+#endif
+
+/*
+ * Local variables:
+ * c-basic-offset: 2
+ * End:
+ */
---
+++ ViSP/src/coin-simulator/vpViewer.cpp
@@ -0,0 +1,262 @@
+/****************************************************************************
+ *
+ * $Id$
+ *
+ * Copyright (C) 1998-2010 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit.
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Simulator based on Coin3d.
+ *
+ * Authors:
+ * Eric Marchand
+ *
+ *****************************************************************************/
+/*!
+  \file vpViewer.cpp
+  Viewer used by the simulator. Under Windows, the viewer is
+  based either on SoWin or SoQt. Under Unix, the viewer is based on SoQt or SoXt.
+*/
+
+#include <visp/vpConfig.h>
+
+#ifdef VISP_HAVE_COIN
+#include <visp/vpViewer.h>
+#include <visp/vpSimulator.h>
+
+
+#include <Inventor/events/SoKeyboardEvent.h>
+#include <Inventor/nodes/SoEventCallback.h>
+
+
+#if defined(VISP_HAVE_SOWIN)
+vpViewer::vpViewer(HWND parent,  vpSimulator *_simu, vpViewerType viewerType):
+  SoWinExaminerViewer(parent,(char *)NULL,false)
+#elif defined(VISP_HAVE_SOQT)
+vpViewer::vpViewer(QWidget * parent,  vpSimulator *_simu, vpViewerType viewerType) :
+  SoQtExaminerViewer(parent,(char *)NULL,false)
+#elif defined(VISP_HAVE_SOXT)
+vpViewer::vpViewer(Widget parent,  vpSimulator *_simu, vpViewerType viewerType):
+  SoXtExaminerViewer(parent,(char *)NULL,false)
+#endif
+{
+  this->simu = _simu ;
+  this->viewerType = viewerType;
+  // Coin should not clear the pixel-buffer, so the background image
+  // is not removed.
+
+  this->setClearBeforeRender(FALSE, TRUE);
+  //  this->setAntialiasing(true, 2) ;
+  setAutoRedraw(false);
+}
+
+vpViewer::~vpViewer()
+{
+
+}
+
+void
+vpViewer::actualRedraw(void)
+{
+
+  {
+   const SbViewportRegion vp = this->getViewportRegion();
+   SbVec2s origin = vp.getViewportOriginPixels();
+   SbVec2s size = vp.getViewportSizePixels();
+   glViewport(origin[0], origin[1], size[0], size[1]);
+
+   const SbColor col = this->getBackgroundColor();
+   glClearColor(col[0], col[1], col[2], 0.0f);
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+   // this should be used only with the vpAR:vpSimulator
+   // to diplay an image background
+   if (simu->image_background != NULL)
+   {
+     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+     if (simu->typeImage == vpSimulator::grayImage)
+       glDrawPixels(simu->getInternalWidth(), simu->getInternalHeight(),
+		    (GLenum)GL_LUMINANCE,
+		    GL_UNSIGNED_BYTE,
+		    simu->image_background );
+     else
+       glDrawPixels(simu->getInternalWidth(), simu->getInternalHeight(),
+		    (GLenum)GL_RGB,
+		    GL_UNSIGNED_BYTE,
+		    simu->image_background );
+
+     glEnable(GL_DEPTH_TEST);
+     glClear(GL_DEPTH_BUFFER_BIT);     // clear the z-buffer
+     glClearDepth(100.0);              // Profondeur du Z-Buf
+   }
+   // Render normal scenegraph.
+#if defined(VISP_HAVE_SOWIN)
+    SoWinExaminerViewer::actualRedraw();
+#elif defined(VISP_HAVE_SOQT)
+    SoQtExaminerViewer::actualRedraw();
+#elif defined(VISP_HAVE_SOXT)
+    SoXtExaminerViewer::actualRedraw();
+#endif
+   glSwapBuffers() ;
+    if(viewerType == vpViewer::internalView){
+       simu->get = 0 ;
+       glReadPixels(0,0,simu->getInternalWidth(), simu->getInternalHeight(),
+		      (GLenum)GL_RGB,
+		      GL_UNSIGNED_BYTE,
+		      simu->bufferView ) ;
+       simu->get =1 ;
+    }
+ }
+
+}
+
+/*!
+
+  \param x : width
+  \param y : height
+  \param fixed : set as true to disable mouse window resizing
+
+*/
+void
+#if defined(VISP_HAVE_SOWIN) || defined (VISP_HAVE_SOQT)
+vpViewer::resize(int x, int y, bool fixed)
+#else
+vpViewer::resize(int x, int y, bool /*fixed*/)
+#endif
+{
+  SbVec2s size(x,y) ;
+  setSize(size);
+  setGLSize(size) ;
+
+#if defined(VISP_HAVE_SOWIN)
+  HWND parent = getParentWidget();
+
+  RECT rcClient, rcWindow;
+  POINT ptDiff;
+  GetClientRect(parent, &rcClient);
+  GetWindowRect(parent, &rcWindow);
+  ptDiff.x = (rcWindow.right - rcWindow.left) - rcClient.right;
+  ptDiff.y = (rcWindow.bottom - rcWindow.top) - rcClient.bottom;
+  MoveWindow(parent,rcWindow.left, rcWindow.top, 
+	     x + ptDiff.x, y + ptDiff.y, TRUE);
+  if(fixed){
+    DWORD dwStyle = GetWindowLong(parent, GWL_STYLE);
+    dwStyle &= ~(WS_SIZEBOX);
+    SetWindowLong(parent, GWL_STYLE,dwStyle);
+  }
+#elif defined(VISP_HAVE_SOQT)
+  if(fixed){
+    QWidget * parent = getParentWidget();
+    parent->setFixedSize(x, y);
+  }
+#endif 
+}
+
+/*!
+\param left : left coordinate
+\param bottom : bottom coordinate
+\param x : width
+\param y : height
+
+*/
+
+SbBool
+vpViewer::processSoEvent(const SoEvent * const event)
+{
+  if ( this->isViewing() &&
+       event->getTypeId() == SoKeyboardEvent::getClassTypeId() )
+  {
+    SoKeyboardEvent * kbevent = (SoKeyboardEvent *) event;
+    switch ( kbevent->getKey() ) {
+    case SoKeyboardEvent::H:
+      if ( kbevent->getState() == SoButtonEvent::DOWN )
+      {
+	      std::cout << "H : this help "<<std::endl ;
+	      std::cout << "M : get and save the external camera location (matrix)"<<std::endl;
+	      std::cout << "V : get and save the external camera location (vector)"<<std::endl;
+	      std::cout << "M : load camera location (vector)"<<std::endl;
+	      std::cout << "P : get external camera location and set the internal one"<<std::endl;
+      }
+      return TRUE;
+
+    case SoKeyboardEvent::M:
+      if ( kbevent->getState() == SoButtonEvent::DOWN )
+      {
+	      vpHomogeneousMatrix cMf ;
+	      simu->getExternalCameraPosition(cMf) ;
+	      std::ofstream f("cMf.dat") ;
+	      cMf.save(f) ;
+	      f.close() ;
+      }
+      return TRUE;
+    case SoKeyboardEvent::V:
+      if ( kbevent->getState() == SoButtonEvent::DOWN )
+      {
+	      vpHomogeneousMatrix cMf ;
+	      simu->getExternalCameraPosition(cMf) ;
+	      vpPoseVector vcMf(cMf) ;
+	      std::ofstream f("vcMf.dat") ;
+	      vcMf.save(f) ;
+	      f.close() ;
+      }
+      return TRUE;
+    case SoKeyboardEvent::L:
+      if ( kbevent->getState() == SoButtonEvent::DOWN )
+      {
+	      vpPoseVector vcMf;
+	      std::ifstream f("vcMf.dat") ;
+	      vcMf.load(f) ;
+	      f.close() ;
+	      vpHomogeneousMatrix cMf(vcMf) ;
+	      simu->setCameraPosition(cMf) ;
+	      simu->moveInternalCamera(cMf) ;
+      }
+      return TRUE;
+    case SoKeyboardEvent::P:
+      if ( kbevent->getState() == SoButtonEvent::DOWN )
+      {
+	      vpHomogeneousMatrix cMf ;
+	      simu->getExternalCameraPosition(cMf) ;
+	      vpPoseVector vcMf(cMf) ;
+	      vcMf.print() ;
+	      simu->setCameraPosition(cMf) ;
+	      simu->moveInternalCamera(cMf) ;
+      }
+      return TRUE;
+    default:
+      break;
+    }
+  }
+#if defined(VISP_HAVE_SOWIN)
+  return  SoWinExaminerViewer::processSoEvent(event);
+#elif defined(VISP_HAVE_SOQT)
+  return  SoQtExaminerViewer::processSoEvent(event);
+#elif defined(VISP_HAVE_SOXT)
+  return  SoXtExaminerViewer::processSoEvent(event);
+#endif
+
+}
+
+#endif
