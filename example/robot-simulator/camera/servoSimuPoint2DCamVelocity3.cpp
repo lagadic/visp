4992e89ef4c89f634408fd84edf48a6289075d04
Nicolas Melchior <Nicolas.Melchior@inria.fr>
2010-06-07 14:58:43 +0000

    Add the examples which were in the servo-simu folder.

---
+++ example/robot-simulator/camera/servoSimuPoint2DCamVelocity3.cpp
@@ -0,0 +1,269 @@
+/****************************************************************************
+ *
+ * $Id: servoSimuPoint2DCamVelocity3.cpp 2503 2010-02-16 18:55:01Z fspindle $
+ *
+ * Copyright (C) 1998-2010 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Simulation of a 2D visual servoing on a point.
+ *
+ * Authors:
+ * Eric Marchand
+ * Fabien Spindler
+ *
+ *****************************************************************************/
+
+
+/*!
+  \file servoSimuPoint2DCamVelocity3.cpp
+  \brief Servo a point:
+  - eye-in-hand control law,
+  - velocity computed in the camera frame,
+  - no display.
+
+  wrt. servoSimuPoint2DCamVelocity1.cpp only the type of control law is modified. This
+  illustrates the need for Jacobian update and Twist transformation matrix
+  initialization, only the X coordinate is selected.
+
+  Only the X coordinate is selected (test the selection process).
+*/
+
+/*!
+  \example servoSimuPoint2DCamVelocity3.cpp
+  Servo a point:
+  - eye-in-hand control law,
+  - velocity computed in the camera frame,
+  - no display.
+
+  wrt. servoSimuPoint2DCamVelocity1.cpp only the type of control law is modified. This
+  illustrates the need for Jacobian update and Twist transformation matrix
+  initialization, only the X coordinate is selected.
+
+  Only the X coordinate is selected (test the selection process).
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <visp/vpMath.h>
+#include <visp/vpHomogeneousMatrix.h>
+#include <visp/vpFeaturePoint.h>
+#include <visp/vpServo.h>
+#include <visp/vpRobotCamera.h>
+#include <visp/vpDebug.h>
+#include <visp/vpFeatureBuilder.h>
+#include <visp/vpParseArgv.h>
+
+// List of allowed command line options
+#define GETOPTARGS	"h"
+
+/*!
+
+Print the program options.
+
+  \param name : Program name.
+  \param badparam : Bad parameter name.
+
+*/
+void usage(const char *name, const char *badparam)
+{
+  fprintf(stdout, "\n\
+Simulation of a 2D visual servoing on a point:\n\
+- eye-in-hand control law,\n\
+- articular velocity are computed,\n\
+- without display,\n\
+- only the X coordinate of the point is selected.\n\
+\n\
+SYNOPSIS\n\
+  %s [-h]\n", name);
+
+  fprintf(stdout, "\n\
+OPTIONS:                                               Default\n\
+\n\
+  -h\n\
+     Print the help.\n");
+
+  if (badparam)
+    fprintf(stdout, "\nERROR: Bad parameter [%s]\n", badparam);
+}
+
+/*!
+
+Set the program options.
+
+  \param argc : Command line number of parameters.
+  \param argv : Array of command line parameters.
+
+  \return false if the program has to be stopped, true otherwise.
+
+*/
+bool getOptions(int argc, const char **argv)
+{
+  const char *optarg;
+  int	c;
+  while ((c = vpParseArgv::parse(argc, argv, GETOPTARGS, &optarg)) > 1) {
+
+    switch (c) {
+    case 'h': usage(argv[0], NULL); return false; break;
+
+    default:
+      usage(argv[0], optarg);
+      return false; break;
+    }
+  }
+
+  if ((c == 1) || (c == -1)) {
+    // standalone param or error
+    usage(argv[0], NULL);
+    std::cerr << "ERROR: " << std::endl;
+    std::cerr << "  Bad argument " << optarg << std::endl << std::endl;
+    return false;
+  }
+
+  return true;
+}
+
+int
+main(int argc, const char ** argv)
+{
+  // Read the command line options
+  if (getOptions(argc, argv) == false) {
+    exit (-1);
+  }
+
+  vpServo task ;
+  vpRobotCamera robot ;
+
+
+  std::cout << std::endl ;
+  std::cout << "-------------------------------------------------------" << std::endl ;
+  std::cout << " Test program for vpServo "  <<std::endl ;
+  std::cout << " Eye-in-hand task control,  articular velocity are computed" << std::endl ;
+  std::cout << " Simulation " << std::endl ;
+  std::cout << " task : servo a point " << std::endl ;
+  std::cout << "-------------------------------------------------------" << std::endl ;
+  std::cout << std::endl ;
+
+
+  vpTRACE("sets the initial camera location " ) ;
+  vpHomogeneousMatrix cMo ;
+  cMo[0][3] = 0.1 ;
+  cMo[1][3] = 0.2 ;
+  cMo[2][3] = 2 ;
+  robot.setPosition(cMo) ;
+
+
+  vpTRACE("sets the point coordinates in the world frame "  ) ;
+  vpPoint point ;
+  point.setWorldCoordinates(0,0,0) ;
+
+  vpTRACE("project : computes  the point coordinates in the camera frame and its 2D coordinates"  ) ;
+  point.track(cMo) ;
+
+  vpTRACE("sets the current position of the visual feature ") ;
+  vpFeaturePoint p ;
+  vpFeatureBuilder::create(p,point)  ;  //retrieve x,y and Z of the vpPoint structure
+
+
+  vpTRACE("sets the desired position of the visual feature ") ;
+  vpFeaturePoint pd ;
+  pd.buildFrom(0,0,1) ; // buildFrom(x,y,Z) ;
+
+
+  vpTRACE("define the task") ;
+  vpTRACE("\t we want an eye-in-hand control law") ;
+  vpTRACE("\t articular velocity are computed") ;
+  task.setServo(vpServo::EYEINHAND_L_cVe_eJe) ;
+
+
+  vpTRACE("Set the position of the camera in the end-effector frame ") ;
+  vpHomogeneousMatrix cMe ;
+  vpVelocityTwistMatrix cVe(cMe) ;
+  task.set_cVe(cVe) ;
+
+  vpTRACE("Set the Jacobian (expressed in the end-effector frame)") ;
+  vpMatrix eJe ;
+  robot.get_eJe(eJe) ;
+  task.set_eJe(eJe) ;
+
+  vpTRACE("\t we want to see a point on a point..") ;
+  task.addFeature(p,pd, vpFeaturePoint::selectX()) ;
+
+  vpTRACE("\t set the gain") ;
+  task.setLambda(1) ;
+
+
+  vpTRACE("Display task information " ) ;
+  task.print() ;
+
+  int iter=0 ;
+  vpTRACE("\t loop") ;
+  while(iter++<100)
+    {
+      std::cout << "---------------------------------------------" << iter <<std::endl ;
+      vpColVector v ;
+
+
+      if (iter==1)
+	{
+	  vpTRACE("Set the Jacobian (expressed in the end-effector frame)") ;
+	  vpTRACE("since q is modified eJe is modified") ;
+	}
+      robot.get_eJe(eJe) ;
+      task.set_eJe(eJe) ;
+
+
+      if (iter==1) vpTRACE("\t\t get the robot position ") ;
+      robot.getPosition(cMo) ;
+      if (iter==1) vpTRACE("\t\t new point position ") ;
+      point.track(cMo) ;
+      vpFeatureBuilder::create(p,point)  ;  //retrieve x,y and Z of the vpPoint structure
+
+
+
+
+      if (iter==1) vpTRACE("\t\t compute the control law ") ;
+      v = task.computeControlLaw() ;
+
+      if (iter==1)
+	{
+	  vpTRACE("Display task information " ) ;
+	  task.print() ;
+	}
+
+      if (iter==1) vpTRACE("\t\t send the camera velocity to the controller ") ;
+      robot.setVelocity(vpRobot::CAMERA_FRAME, v) ;
+
+      vpTRACE("\t\t || s - s* || ") ;
+      std::cout << task.error.sumSquare() <<std::endl ; ;
+    }
+
+  vpTRACE("Display task information " ) ;
+  task.print() ;
+  task.kill();
+}
+
---
+++ example/robot-simulator/camera/servoSimuPoint2DhalfCamVelocity1.cpp
@@ -0,0 +1,281 @@
+/****************************************************************************
+ *
+ * $Id: servoSimuPoint2DhalfCamVelocity1.cpp 2457 2010-01-07 10:41:18Z nmelchio $
+ *
+ * Copyright (C) 1998-2010 Inria. All rights reserved.
+ *
+ * This software was developed at:
+ * IRISA/INRIA Rennes
+ * Projet Lagadic
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * http://www.irisa.fr/lagadic
+ *
+ * This file is part of the ViSP toolkit
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined by Trolltech AS of Norway and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * Licensees holding valid ViSP Professional Edition licenses may
+ * use this file in accordance with the ViSP Commercial License
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Contact visp@irisa.fr if any conditions of this licensing are
+ * not clear to you.
+ *
+ * Description:
+ * Simulation of a 2 1/2 D visual servoing.
+ *
+ * Authors:
+ * Eric Marchand
+ * Fabien Spindler
+ *
+ *****************************************************************************/
+
+/*!
+  \file servoSimuPoint2DhalfCamVelocity1.cpp
+  \brief Simulation of a 2 1/2 D visual servoing (x,y,Z,theta U):
+  - (x,y,Z,theta U) features,
+  - eye-in-hand control law,
+  - velocity computed in the camera frame,
+  - no display.
+
+*/
+
+/*!
+  \example servoSimuPoint2DhalfCamVelocity1.cpp
+  Simulation of a 2 1/2 D visual servoing (theta U):
+  - (x,y,Z,theta U) features,
+  - eye-in-hand control law,
+  - velocity computed in the camera frame,
+  - no display.
+
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <visp/vpMath.h>
+#include <visp/vpHomogeneousMatrix.h>
+#include <visp/vpPoint.h>
+#include <visp/vpFeaturePoint.h>
+#include <visp/vpFeatureThetaU.h>
+#include <visp/vpFeaturePoint3D.h>
+#include <visp/vpServo.h>
+#include <visp/vpRobotCamera.h>
+#include <visp/vpDebug.h>
+#include <visp/vpFeatureBuilder.h>
+#include <visp/vpParseArgv.h>
+
+// List of allowed command line options
+#define GETOPTARGS	"h"
+
+/*!
+
+Print the program options.
+
+  \param name : Program name.
+  \param badparam : Bad parameter name.
+
+*/
+void usage(const char *name, const char *badparam)
+{
+  fprintf(stdout, "\n\
+Simulation of a 2 1/2 D visual servoing (x,y,Z,theta U):\n\
+- eye-in-hand control law,\n\
+- velocity computed in the camera frame,\n\
+- without display.\n\
+\n\
+SYNOPSIS\n\
+  %s [-h]\n", name);
+
+  fprintf(stdout, "\n\
+OPTIONS:                                               Default\n\
+\n\
+  -h\n\
+     Print the help.\n");
+
+  if (badparam) {
+    fprintf(stderr, "ERROR: \n" );
+    fprintf(stderr, "\nBad parameter [%s]\n", badparam);
+  }
+}
+
+/*!
+
+Set the program options.
+
+  \param argc : Command line number of parameters.
+  \param argv : Array of command line parameters.
+
+  \return false if the program has to be stopped, true otherwise.
+
+*/
+bool getOptions(int argc, const char **argv)
+{
+  const char *optarg;
+  int	c;
+  while ((c = vpParseArgv::parse(argc, argv, GETOPTARGS, &optarg)) > 1) {
+
+    switch (c) {
+    case 'h': usage(argv[0], NULL); return false; break;
+
+    default:
+      usage(argv[0], optarg);
+      return false; break;
+    }
+  }
+
+  if ((c == 1) || (c == -1)) {
+    // standalone param or error
+    usage(argv[0], NULL);
+    std::cerr << "ERROR: " << std::endl;
+    std::cerr << "  Bad argument " << optarg << std::endl << std::endl;
+    return false;
+  }
+
+  return true;
+}
+
+int
+main(int argc, const char ** argv)
+{
+  // Read the command line options
+  if (getOptions(argc, argv) == false) {
+    exit (-1);
+  }
+
+  vpServo task ;
+  vpRobotCamera robot ;
+
+  std::cout << std::endl ;
+  std::cout << "-------------------------------------------------------" << std::endl ;
+  std::cout << " Test program for vpServo "  <<std::endl ;
+  std::cout << " task :  2 1/2 D visual servoing " << std::endl ;
+  std::cout << "-------------------------------------------------------" << std::endl ;
+  std::cout << std::endl ;
+
+
+  vpTRACE("sets the initial camera location " ) ;
+  vpPoseVector c_r_o(0.1,0.2,2,
+		     vpMath::rad(20), vpMath::rad(10),  vpMath::rad(50)
+		     ) ;
+
+  vpCTRACE ; std::cout << std::endl ;
+  vpHomogeneousMatrix cMo(c_r_o) ;
+  vpCTRACE ; std::cout << std::endl ;
+  robot.setPosition(cMo) ;
+  vpCTRACE ; std::cout << std::endl ;
+
+  vpTRACE("sets the desired camera location " ) ;
+  vpPoseVector cd_r_o(0,0,1,
+		      vpMath::rad(0),vpMath::rad(0),vpMath::rad(0)) ;
+  vpHomogeneousMatrix cdMo(cd_r_o) ;
+
+
+  vpTRACE("\tsets the point coordinates in the world frame "  ) ;
+  vpPoint point ;
+  point.setWorldCoordinates(0,0,0) ;
+  vpTRACE("\tproject : computes  the point coordinates in the camera frame and its 2D coordinates"  ) ;
+  point.track(cMo) ;
+
+  vpPoint pointd ;
+  pointd.setWorldCoordinates(0,0,0) ;
+  pointd.track(cdMo) ;
+  //------------------------------------------------------------------
+  vpTRACE("1st feature (x,y)");
+
+
+  vpTRACE("\t want to it at (0,0)") ;
+  vpFeaturePoint p ;
+  vpFeatureBuilder::create(p,point)  ;
+
+
+  vpFeaturePoint pd ;
+  vpFeatureBuilder::create(pd,pointd)  ;
+
+
+  //------------------------------------------------------------------
+  vpTRACE("2nd feature (Z)") ;
+  vpTRACE("\tnot necessary to project twice (reuse p)") ;
+  vpFeaturePoint3D Z ;
+  vpFeatureBuilder::create(Z,point)  ;  //retrieve x,y and Z of the vpPoint structure
+
+  vpTRACE("\twant to see it one meter away (here again use pd)") ;
+  vpFeaturePoint3D Zd ;
+  vpFeatureBuilder::create(Zd,pointd)  ;  //retrieve x,y and Z of the vpPoint structure
+
+
+  //------------------------------------------------------------------
+  vpTRACE("3rd feature ThetaU") ;
+  vpTRACE("\tcompute the rotation that the camera has to realize "  ) ;
+  vpHomogeneousMatrix cdMc ;
+  cdMc = cdMo*cMo.inverse() ;
+
+  vpFeatureThetaU tu(vpFeatureThetaU::cdRc) ;
+  tu.buildFrom(cdMc) ;
+
+  vpTRACE("\tsets the desired rotation (always zero !) ") ;
+  vpTRACE("\tsince s is the rotation that the camera has to realize ") ;
+
+
+  //------------------------------------------------------------------
+
+  vpTRACE("define the task") ;
+  vpTRACE("\t we want an eye-in-hand control law") ;
+  vpTRACE("\t robot is controlled in the camera frame") ;
+  task.setServo(vpServo::EYEINHAND_CAMERA) ;
+
+  task.addFeature(p,pd) ;
+  task.addFeature(Z,Zd,vpFeaturePoint3D::selectZ()) ;
+  task.addFeature(tu) ;
+
+  vpTRACE("\t set the gain") ;
+  task.setLambda(1) ;
+
+
+  vpTRACE("Display task information " ) ;
+  task.print() ;
+
+  int iter=0 ;
+  vpTRACE("\t loop") ;
+  while(iter++<200)
+    {
+      std::cout << "---------------------------------------------" << iter <<std::endl ;
+      vpColVector v ;
+
+      if (iter==1) vpTRACE("\t\t get the robot position ") ;
+      robot.getPosition(cMo) ;
+
+      if (iter==1) vpTRACE("\t\t update the feature ") ;
+      point.track(cMo) ;
+      vpFeatureBuilder::create(p,point)  ;
+      vpFeatureBuilder::create(Z,point)  ;
+
+      cdMc = cdMo*cMo.inverse() ;
+      tu.buildFrom(cdMc) ;
+
+
+      if (iter==1) vpTRACE("\t\t compute the control law ") ;
+      v = task.computeControlLaw() ;
+      if (iter==1) task.print() ;
+
+      if (iter==1) vpTRACE("\t\t send the camera velocity to the controller ") ;
+      robot.setVelocity(vpRobot::CAMERA_FRAME, v) ;
+
+
+      std::cout << task.error.sumSquare() <<std::endl ; ;
+    }
+
+  vpTRACE("Display task information " ) ;
+  task.print() ;
+  task.kill();
+  vpTRACE("Final camera location " ) ;
+  std::cout << cMo << std::endl ;
+}
+
