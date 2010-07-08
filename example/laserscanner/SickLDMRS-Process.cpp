c8617144d993055fd6fd064c42daafeb7a63adb6
Andrea Cherubini <Andrea.Cherubini@inria.fr>
2010-07-08 15:42:55 +0000

    Changed cout to print actual laser scanner layer.

--- example/laserscanner/SickLDMRS-Process.cpp
+++ example/laserscanner/SickLDMRS-Process.cpp
@@ -138,7 +138,7 @@ void *laser_display_and_save_loop(void *)
     // Parse the four layers
     for (int layer=0; layer<4; layer++) {
       if (! ((0x1<<layer) & layerToDisplay)) {
-	std::cout << "Layer " << layer << " is not to display" << std::endl;
+	std::cout << "Layer " << layer+1 << " is not displayed" << std::endl;
 	continue;
       }
       
@@ -164,7 +164,7 @@ void *laser_display_and_save_loop(void *)
       
      
       vpImagePoint E; // Beam echo
-      double resolution = 50; // 100 pixels = 1 meter
+      double resolution = 5; // 100 pixels = 1 meter
 //       std::cout << "display layer " << layer << " nb points: " 
 // 		<< pointsLayer.size() << std::endl;
       for (unsigned int i=0; i<pointsLayer.size(); i++) {
