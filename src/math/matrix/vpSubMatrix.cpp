7fdb5ac296170f1ce0ca282e688131c43ccef408
Fabien Spindler <Fabien.Spindler@inria.fr>
2010-09-14 10:14:54 +0000

    Rename the license file LICENSE.GPL in LICENSE.txt to be compatible with cpack

--- src/math/matrix/vpSubMatrix.cpp
+++ src/math/matrix/vpSubMatrix.cpp
@@ -10,7 +10,7 @@
  * This software is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * ("GPL") version 2 as published by the Free Software Foundation.
- * See the file LICENSE.GPL at the root directory of this source
+ * See the file LICENSE.txt at the root directory of this source
  * distribution for additional information about the GNU GPL.
  *
  * For using ViSP with software that can not be combined with the GNU
