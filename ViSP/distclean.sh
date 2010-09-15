03eb1b25c066678408549a5f7622f91fe2cd6527
Fabien Spindler <Fabien.Spindler@inria.fr>
2010-09-15 14:46:52 +0000

    Fix bad file locations and improve the Makefile file removing

--- ViSP/distclean.sh
+++ ViSP/distclean.sh
@@ -46,30 +46,30 @@ $RM CMakeCache.txt CMakeOutput.log config.log warning.log cmake.check_cache
 $RM install_manifest.txt DartConfiguration.tcl
 $RM doc/config-doxygen warning.log cmake_uninstall.cmake
 $RM include/visp/vpConfig.h
-$RM lib/VISPBuildSettings.cmake
-$RM lib/VISPConfig.cmake
-$RM lib/VISPLibraryDepends.cmake
-$RM lib/VISPUse.cmake
+$RM VISPBuildSettings.cmake
+$RM VISPConfig.cmake
+$RM VISPLibraryDepends.cmake
+$RM VISPUse.cmake
 $RM bin/visp-config
 
 $RM -r Testing autom4te.cache
 
-for i in `find -type d -name "CMakeFiles" -print | sort`
+for i in `find . -type d -name "CMakeFiles" -print | sort`
     do
     	$RM -r $i;
 	echo "$RM -r $i"
     done
-for i in `find -type d -name "CMakeTmp" -print | sort`
+for i in `find . -type d -name "CMakeTmp" -print | sort`
     do
     	$RM -r $i;
 	echo "$RM -r $i"
     done
-for i in `find -type d -name "*.dir" -print | sort`
+for i in `find . -type d -name "*.dir" -print | sort`
     do
     	$RM -r $i;
 	echo "$RM -r $i"
     done
-for i in `find test -type d -name "debug" -print | sort`
+for i in `find . test -type d -name "debug" -print | sort`
     do
     	$RM -r $i;
 	echo "$RM -r $i"
@@ -87,92 +87,92 @@ for i in `find example -type d -name "debug" -print | sort`
 
 
 
-for i in `find -type f -name "*.rule" -print | sort`
+for i in `find . -type f -name "*.rule" -print | sort`
     do
     	$RM  $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "DartTestfile.txt" -print | sort`
+for i in `find . -type f -name "DartTestfile.txt" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*~" -print | sort`
+for i in `find . -type f -name "*~" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.plg" -print | sort`
+for i in `find . -type f -name "*.plg" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.vcproj*" -print | sort`
+for i in `find . -type f -name "*.vcproj*" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "vc60.idb*" -print | sort`
+for i in `find . -type f -name "vc60.idb*" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.dsp*" -print | sort`
+for i in `find . -type f -name "*.dsp*" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.dsw" -print | sort`
+for i in `find . -type f -name "*.dsw" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "cmake_install.cmake" -print | sort`
+for i in `find . -type f -name "cmake_install.cmake" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.opt" -print | sort`
+for i in `find . -type f -name "*.opt" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.sln" -print | sort`
+for i in `find . -type f -name "*.sln" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.ncb" -print | sort`
+for i in `find . -type f -name "*.ncb" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.suo" -print | sort`
+for i in `find . -type f -name "*.suo" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "Makefile" -print | sort`
+for i in `find . -type d \( -name Makefile \) -prune -o -type f -name "Makefile" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.obj" -print | sort`
+for i in `find . -type f -name "*.obj" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.o" -print | sort`
+for i in `find . -type f -name "*.o" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.bb" -print | sort`
+for i in `find . -type f -name "*.bb" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
     done
-for i in `find -type f -name "*.bbg" -print | sort`
+for i in `find . -type f -name "*.bbg" -print | sort`
     do
     	$RM $i;
 	echo "$RM $i"
