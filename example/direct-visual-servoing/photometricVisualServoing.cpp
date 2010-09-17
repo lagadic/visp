241d5a4d34551be4fe69c736fb5de246eeba615f
Nicolas Melchior <Nicolas.Melchior@inria.fr>
2010-09-17 14:54:39 +0000

    Add the example and the test corresponding to the photometric visual feature.

--- example/direct-visual-servoing/photometricVisualServoing.cpp
+++ example/direct-visual-servoing/photometricVisualServoing.cpp
@@ -51,7 +51,6 @@
 #include <visp/vpConfig.h>
 #include <visp/vpDebug.h>
 
-
 #include <visp/vpImage.h>
 #include <visp/vpImageIo.h>
 #include <visp/vpImageTools.h>
@@ -59,8 +58,6 @@
 #include <visp/vpCameraParameters.h>
 #include <visp/vpTime.h>
 #include <visp/vpRobotCamera.h>
-
-
 
 #include <visp/vpMath.h>
 #include <visp/vpHomogeneousMatrix.h>
@@ -70,29 +67,162 @@
 #include <visp/vpDisplayD3D.h>
 #include <visp/vpDisplayX.h>
 
-
 #include <visp/vpFeatureLuminance.h>
 #include <visp/vpParseArgv.h>
 
-
 #include <visp/vpImageSimulator.h>
 
-
-using namespace std ;
-
-
-
 #define  Z             1
+
+#include <visp/vpParseArgv.h>
+#include <visp/vpIoTools.h>
+
+// List of allowed command line options
+#define GETOPTARGS	"cdi:h"
+
+/*!
+
+  Print the program options.
+
+  \param name : Program name.
+  \param badparam : Bad parameter name.
+  \param ipath : Input image path.
+
+*/
+void usage(const char *name, const char *badparam, std::string ipath)
+{
+  fprintf(stdout, "\n\
+Tracking of Surf key-points.\n\
+\n\
+SYNOPSIS\n\
+  %s [-i <input image path>] [-c] [-d] [-h]\n", name);
+
+  fprintf(stdout, "\n\
+OPTIONS:                                               Default\n\
+  -i <input image path>                                %s\n\
+     Set image input path.\n\
+     From this path read \"ViSP-images/doisneau/doisneau.jpg\"\n\
+     images. \n\
+     Setting the VISP_INPUT_IMAGE_PATH environment\n\
+     variable produces the same behaviour than using\n\
+     this option.\n\
+\n\
+  -c\n\
+     Disable the mouse click. Usefull to automaze the \n\
+     execution of this program without humain intervention.\n\
+\n\
+  -d \n\
+     Turn off the display.\n\
+\n\
+  -h\n\
+     Print the help.\n",
+	  ipath.c_str());
+
+  if (badparam)
+    fprintf(stdout, "\nERROR: Bad parameter [%s]\n", badparam);
+}
+/*!
+
+  Set the program options.
+
+  \param argc : Command line number of parameters.
+  \param argv : Array of command line parameters.
+  \param ipath : Input image path.
+  \param click_allowed : Mouse click activation.
+  \param display : Display activation.
+
+  \return false if the program has to be stopped, true otherwise.
+
+*/
+bool getOptions(int argc, const char **argv, std::string &ipath,
+		bool &click_allowed, bool &display)
+{
+  const char *optarg;
+  int	c;
+  while ((c = vpParseArgv::parse(argc, argv, GETOPTARGS, &optarg)) > 1) {
+
+    switch (c) {
+    case 'c': click_allowed = false; break;
+    case 'd': display = false; break;
+    case 'i': ipath = optarg; break;
+    case 'h': usage(argv[0], NULL, ipath); return false; break;
+
+    default:
+      usage(argv[0], optarg, ipath);
+      return false; break;
+    }
+  }
+
+  if ((c == 1) || (c == -1)) {
+    // standalone param or error
+    usage(argv[0], NULL, ipath);
+    std::cerr << "ERROR: " << std::endl;
+    std::cerr << "  Bad argument " << optarg << std::endl << std::endl;
+    return false;
+  }
+
+  return true;
+}
 
 
 
 int
-main()
+main(int argc, const char ** argv)
 {
+  std::string env_ipath;
+  std::string opt_ipath;
+  std::string ipath;
+  std::string filename;
+  bool opt_click_allowed = true;
+  bool opt_display = true;
   
+  // Get the VISP_IMAGE_PATH environment variable value
+  char *ptenv = getenv("VISP_INPUT_IMAGE_PATH");
+  if (ptenv != NULL)
+    env_ipath = ptenv;
+
+  // Set the default input path
+  if (! env_ipath.empty())
+    ipath = env_ipath;
+
+
+  // Read the command line options
+  if (getOptions(argc, argv, opt_ipath, opt_click_allowed,
+		 opt_display) == false) {
+    exit (-1);
+  }
+
+  // Get the option values
+  if (!opt_ipath.empty())
+    ipath = opt_ipath;
+
+  // Compare ipath and env_ipath. If they differ, we take into account
+  // the input path comming from the command line option
+  if (!opt_ipath.empty() && !env_ipath.empty()) {
+    if (ipath != env_ipath) {
+      std::cout << std::endl
+	   << "WARNING: " << std::endl;
+      std::cout << "  Since -i <visp image path=" << ipath << "> "
+	   << "  is different from VISP_IMAGE_PATH=" << env_ipath << std::endl
+	   << "  we skip the environment variable." << std::endl;
+    }
+  }
+
+  // Test if an input path is set
+  if (opt_ipath.empty() && env_ipath.empty()){
+    usage(argv[0], NULL, ipath);
+    std::cerr << std::endl
+	 << "ERROR:" << std::endl;
+    std::cerr << "  Use -i <visp image path> option or set VISP_INPUT_IMAGE_PATH "
+	 << std::endl
+	 << "  environment variable to specify the location of the " << std::endl
+	 << "  image path where test images are located." << std::endl << std::endl;
+    exit(-1);
+  }
 
   vpImage<unsigned char> Itexture ;
-  vpImageIo::read(Itexture,"/udd/marchand/images/doisneau2.jpeg") ;
+  filename = ipath +  vpIoTools::path("/ViSP-images/Klimt/Klimt.jpeg");
+  vpImageIo::read(Itexture,filename) ;
 
   vpColVector X[4];
   for (int i = 0; i < 4; i++) X[i].resize(3);
@@ -149,11 +279,14 @@ main()
   #elif defined VISP_HAVE_GTK
   vpDisplayGTK d;
   #endif
-  d.init(I, 20, 10, "Photometric visual servoing : s") ;
-
-  vpDisplay::display(I);
-  vpDisplay::flush(I);
-  vpDisplay::getClick(I) ;
+  if (opt_display) {
+    d.init(I, 20, 10, "Photometric visual servoing : s") ;
+    vpDisplay::display(I);
+    vpDisplay::flush(I);
+  }
+  if (opt_display && opt_click_allowed) {
+    vpDisplay::getClick(I) ;
+  }
 
 
   // ----------------------------------------------------------
@@ -169,10 +302,13 @@ main()
   I =0 ;
   sim.getImage(I,cam);  // and aquire the image Id
   
-  
-  vpDisplay::display(I) ;
-  vpDisplay::flush(I) ;
-  vpDisplay::getClick(I) ;
+  if (opt_display) {
+    vpDisplay::display(I) ;
+    vpDisplay::flush(I) ;
+  }
+  if (opt_display && opt_click_allowed) {
+    vpDisplay::getClick(I) ;
+  }
   
  
   vpImage<unsigned char> Idiff ;
@@ -190,12 +326,11 @@ main()
   #elif defined VISP_HAVE_GTK
   vpDisplayGTK d1;
   #endif
-  d1.init(Idiff, 680, 10, "photometric visual servoing : s-s* ") ;
-  vpDisplay::display(Idiff) ;
-  vpDisplay::flush(Idiff) ;
-
-
-
+  if (opt_display) {
+    d1.init(Idiff, 680, 10, "photometric visual servoing : s-s* ") ;
+    vpDisplay::display(Idiff) ;
+    vpDisplay::flush(Idiff) ;
+  }
 
   // create the robot (here the INRIA Rennes Afma6)
   vpRobotCamera robot ;
@@ -278,11 +413,12 @@ main()
   // ----------------------------------------------------------
   int iter   = 1;
   int iterGN = 90 ; // swicth to Gauss Newton after iterGN iterations
-
+  
+  double normeError = 0;
   do
     {
 
-      cout << "--------------------------------------------" << iter++ << endl ;
+      std::cout << "--------------------------------------------" << iter++ << std::endl ;
 
 
       //  Acquire the new image
@@ -292,8 +428,10 @@ main()
       vpDisplay::flush(I) ;
 
       vpImageTools::imageDifference(I,Id,Idiff) ;
+      if (opt_display) {
       vpDisplay::display(Idiff) ;
       vpDisplay::flush(Idiff) ;
+      }
 
       // Compute current visual feature
       sI.buildFrom(I) ;
@@ -304,8 +442,8 @@ main()
 
 
       
-      double normeError = (error.sumSquare());
-      cout << "|e| "<<normeError <<endl ;
+      normeError = (error.sumSquare());
+      std::cout << "|e| "<<normeError <<std::endl ;
       
      
      // double t = vpTime::measureTimeMs() ;
@@ -329,15 +467,15 @@ main()
 	v = - lambda*e;
       }
 
-      cout << "lambda = " << lambda << "  mu = " << mu ;
-      cout << " |Tc| = " << sqrt(v.sumSquare()) << endl;
+      std::cout << "lambda = " << lambda << "  mu = " << mu ;
+      std::cout << " |Tc| = " << sqrt(v.sumSquare()) << std::endl;
       
       // send the robot velocity
       robot.setVelocity(vpRobot::CAMERA_FRAME, v) ;
       robot.getPosition(cMo) ;
 
     }
-  while(iter < 1800);
+  while(normeError > 10000);
 
 
   v = 0 ;
