24a300130b12d2363ba43af667fdf3344293a7a2
Fabien Spindler <Fabien.Spindler@inria.fr>
2011-03-29 18:44:37 +0000

    Minor changes to produce an image where drawings are more visible.

--- example/manual/image-manipulation/manDisplay.cpp
+++ example/manual/image-manipulation/manDisplay.cpp
@@ -39,12 +39,7 @@
  * Fabien Spindler
  *
  *****************************************************************************/
-/*!
-  \file manDisplay.cpp
 
-  \brief Display example.
-
- */
 /*!
   \example manDisplay.cpp
 
@@ -52,7 +47,6 @@
  
  */
 #include <visp/vpConfig.h>
-
 #include <visp/vpImage.h>
 #include <visp/vpImageIo.h>
 #include <visp/vpColor.h>
@@ -62,13 +56,14 @@
 int main()
 {
   // Create a grey level image
-  vpImage<unsigned char> I ;
+  vpImage<vpRGBa> I ;
 
   // Create image points for pixel coordinates
   vpImagePoint ip, ip1, ip2;
 
   // Load a grey image from the disk
-  std::string filename = "/tmp/ViSP-images/Klimt/Klimt.pgm";
+  //  std::string filename = "/tmp/ViSP-images/Klimt/Klimt.ppm";
+  std::string filename = "Klimt.ppm";
   vpImageIo::read(I, filename) ;
 
 #ifdef VISP_HAVE_GTK
@@ -84,52 +79,52 @@ int main()
 
   // Display in overlay a red cross at position 100,10 in the
   // image. The lines are 20 pixels long
-  ip.set_i( 100 );
-  ip.set_j( 10 );
-  vpDisplay::displayCross(I, ip, 20, vpColor::red) ;
+  ip.set_i( 200 );
+  ip.set_j( 200 );
+  vpDisplay::displayCross(I, ip, 20, vpColor::red, 3) ;
 
   // Display in overlay a horizontal red line
   ip1.set_i( 10 );
   ip1.set_j( 0 );
   ip2.set_i( 10 );
   ip2.set_j( I.getWidth() );
-  vpDisplay::displayLine(I, ip1, ip2, vpColor::red) ;
+  vpDisplay::displayLine(I, ip1, ip2, vpColor::red, 3) ;
 
   // Display in overlay a vertical green dot line
   ip1.set_i( 0 );
   ip1.set_j( 20 );
   ip2.set_i( I.getWidth() );
   ip2.set_j( 20 );
-  vpDisplay::displayDotLine(I, ip1, ip2, vpColor::green) ;
+  vpDisplay::displayDotLine(I, ip1, ip2, vpColor::green, 3) ;
 
   // Display in overlay a blue arrow
   ip1.set_i( 0 );
   ip1.set_j( 0 );
   ip2.set_i( 100 );
   ip2.set_j( 100 );
-  vpDisplay::displayArrow(I, ip1, ip2, vpColor::blue) ;
+  vpDisplay::displayArrow(I, ip1, ip2, vpColor::blue, 8, 4, 3) ;
 
   // Display in overlay some circles. The position of the center is 200, 200
   // the radius is increased by 20 pixels for each circle
   for (unsigned i=0 ; i < 5 ; i++) {
     ip.set_i( 200 );
     ip.set_j( 200 );
-    vpDisplay::displayCircle(I, ip, 20*i, vpColor::yellow) ;
+    vpDisplay::displayCircle(I, ip, 20*i, vpColor::white, false, 3) ;
   }
 
   // Display in overlay a rectangle. 
   // The position of the top left corner is 300, 200.
   // The width is 200. The height is 100.
-  ip.set_i( 300 );
-  ip.set_j( 200 );  
-  vpDisplay::displayRectangle(I, ip, 200, 100,vpColor::orange) ;
+  ip.set_i( 280 );
+  ip.set_j( 150 );  
+  vpDisplay::displayRectangle(I, ip, 270, 30,vpColor::purple, false, 3) ;
 
   // Display in overlay a yellow string
-  ip.set_i( 85 );
-  ip.set_j( 100 );  
+  ip.set_i( 300 );
+  ip.set_j( 160 );  
   vpDisplay::displayCharString(I, ip,
 			       "ViSP is a marvelous software",
-			       vpColor::yellow) ;
+			       vpColor::black) ;
   //Flush the display : without this line nothing will appear on the screen
   vpDisplay::flush(I);
 
@@ -139,7 +134,7 @@ int main()
   vpDisplay::getImage(I, Ioverlay) ;
 
   // Write the color image on the disk
-  filename = "/tmp/Klimt_grey.overlay.ppm";
+  filename = "./Klimt.overlay.ppm";
   vpImageIo::write(Ioverlay, filename) ;
 
   // If click is allowed, wait for a mouse click to close the display
