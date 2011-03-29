7a4dd8a1cfb30f216f93d29116fe23c393e9b907
Romain Tallonneau <Romain Tallonneau@inria.fr>
2011-03-29 14:08:12 +0000

    Replace code in robotSimulator by a calls to wireFrameSimulator. This also allows to load vrml model when using a robot simulator.

--- src/robot/simulator-robot/vpRobotSimulator.cpp
+++ src/robot/simulator-robot/vpRobotSimulator.cpp
@@ -117,55 +117,8 @@ vpRobotSimulator::~vpRobotSimulator()
 void
 vpRobotSimulator::initScene(vpSceneObject obj, vpSceneDesiredObject desiredObject)
 {
-  char name[FILENAME_MAX];
-
-  object = obj;
-  this->desiredObject = desiredObject;
-
-  strcpy(name,VISP_SCENES_DIR);
-  switch (obj)
-  {
-    case THREE_PTS : {strcat(name,"/3pts.bnd"); break; }
-    case CUBE : { strcat(name, "/cube.bnd"); break; }
-    case PLATE : { strcat(name, "/plate.bnd"); break; }
-    case SMALL_PLATE : { strcat(name, "/plate_6cm.bnd"); break; }
-    case RECTANGLE : { strcat(name, "/rectangle.bnd"); break; }
-    case SQUARE_10CM : { strcat(name, "/square10cm.bnd"); break; }
-    case DIAMOND : { strcat(name, "/diamond.bnd"); break; }
-    case TRAPEZOID : { strcat(name, "/trapezoid.bnd"); break; }
-    case THREE_LINES : { strcat(name, "/line.bnd"); break; }
-    case ROAD : { strcat(name, "/road.bnd"); break; }
-    case TIRE : { strcat(name, "/circles2.bnd"); break; }
-    case PIPE : { strcat(name, "/pipe.bnd"); break; }
-    case CIRCLE : { strcat(name, "/circle.bnd"); break; }
-    case SPHERE : { strcat(name, "/sphere.bnd"); break; }
-    case CYLINDER : { strcat(name, "/cylinder.bnd"); break; }
-    case PLAN: { strcat(name, "/plan.bnd"); break; }
-  }
-  set_scene(name,&(this->scene),1.0);
-
-  switch (desiredObject)
-  {
-    case D_STANDARD : { break; }
-    case D_CIRCLE : { 
-      strcpy(name,VISP_SCENES_DIR);
-      strcat(name, "/cercle_sq2.bnd");
-      break; }
-    case D_TOOL : { break; }
-  }
-  set_scene(name,&(this->desiredScene),1.0);
-
-  if (obj == PIPE) load_rfstack(IS_INSIDE);
-  else add_rfstack(IS_BACK);
-
-  add_vwstack ("start","depth", 0.0, 100.0);
-  add_vwstack ("start","window", -0.1,0.1,-0.1,0.1);
-  add_vwstack ("start","type", PERSPECTIVE);
-
-  sceneInitialized = true;
-  displayObject = true;
-  displayDesiredObject = true;
- // displayCamera = true;
+  vpWireFrameSimulator::initScene(obj, desiredObject);
+  displayCamera = false;
 }
 
 /*!
@@ -173,6 +126,7 @@ vpRobotSimulator::initScene(vpSceneObject obj, vpSceneDesiredObject desiredObjec
   at the current position and at the desired position.
   
   Here you can use the scene you want. You have to set the path to the .bnd file which is a scene file.
+  It is also possible to use a vrml (.wrl) file.
 
   \param obj : Path to the scene file you want to use.
   \param desiredObject : Path to the scene file you want to use.
@@ -180,27 +134,8 @@ vpRobotSimulator::initScene(vpSceneObject obj, vpSceneDesiredObject desiredObjec
 void
 vpRobotSimulator::initScene(const char* obj, const char* desiredObject)
 {
-  char name[FILENAME_MAX];
-
-  object = THREE_PTS;
-  this->desiredObject = D_STANDARD;
-
-  strcpy(name,obj);
-  set_scene(name,&(this->scene),1.0);
-
-  strcpy(name,desiredObject);
-  set_scene(name,&(this->desiredScene),1.0);
-
-  add_rfstack(IS_BACK);
-
-  add_vwstack ("start","depth", 0.0, 100.0);
-  add_vwstack ("start","window", -0.1,0.1,-0.1,0.1);
-  add_vwstack ("start","type", PERSPECTIVE);
-
-  sceneInitialized = true;
-  displayObject = true;
-  displayDesiredObject = true;
-//  displayCamera = true;
+  vpWireFrameSimulator::initScene(obj, desiredObject);
+  displayCamera = false;
 }
 
 /*!
@@ -214,68 +149,23 @@ vpRobotSimulator::initScene(const char* obj, const char* desiredObject)
 void
 vpRobotSimulator::initScene(vpSceneObject obj)
 {
-  char name[FILENAME_MAX];
-
-  object = obj;
-
-  strcpy(name,VISP_SCENES_DIR);
-  switch (obj)
-  {
-    case THREE_PTS : {strcat(name,"/3pts.bnd"); break; }
-    case CUBE : { strcat(name, "/cube.bnd"); break; }
-    case PLATE : { strcat(name, "/plate.bnd"); break; }
-    case SMALL_PLATE : { strcat(name, "/plate_6cm.bnd"); break; }
-    case RECTANGLE : { strcat(name, "/rectangle.bnd"); break; }
-    case SQUARE_10CM : { strcat(name, "/square10cm.bnd"); break; }
-    case DIAMOND : { strcat(name, "/diamond.bnd"); break; }
-    case TRAPEZOID : { strcat(name, "/trapezoid.bnd"); break; }
-    case THREE_LINES : { strcat(name, "/line.bnd"); break; }
-    case ROAD : { strcat(name, "/road.bnd"); break; }
-    case TIRE : { strcat(name, "/circles2.bnd"); break; }
-    case PIPE : { strcat(name, "/pipe.bnd"); break; }
-    case CIRCLE : { strcat(name, "/circle.bnd"); break; }
-    case SPHERE : { strcat(name, "/sphere.bnd"); break; }
-    case CYLINDER : { strcat(name, "/cylinder.bnd"); break; }
-    case PLAN: { strcat(name, "/plan.bnd"); break; }
-  }
-  set_scene(name,&(this->scene),1.0);
-  
-  add_rfstack(IS_BACK);
-
-  add_vwstack ("start","depth", 0.0, 100.0);
-  add_vwstack ("start","window", -0.1,0.1,-0.1,0.1);
-  add_vwstack ("start","type", PERSPECTIVE);
-
-  sceneInitialized = true;
-  displayObject = true;
+  vpWireFrameSimulator::initScene(obj);
+  displayCamera = false;
 }
 
 /*!
   Initialize the display. It enables to choose the type of scene which will be used to display the object
   at the current position. The object at the desired position is not displayed.
   
-  Here you can use the scene you want. You have to set the path to the .bnd file which is a scene file.
+  Here you can use the scene you want. You have to set the path to the .bnd file which is a scene file, or the vrml file.
 
   \param obj : Path to the scene file you want to use.
 */
 void
 vpRobotSimulator::initScene(const char* obj)
 {
-  char name[FILENAME_MAX];
-
-  object = THREE_PTS;
-
-  strcpy(name,obj);
-  set_scene(name,&(this->scene),1.0);
-
-  add_rfstack(IS_BACK);
-
-  add_vwstack ("start","depth", 0.0, 100.0);
-  add_vwstack ("start","window", -0.1,0.1,-0.1,0.1);
-  add_vwstack ("start","type", PERSPECTIVE);
-
-  sceneInitialized = true;
-  displayObject = true;
+  vpWireFrameSimulator::initScene(obj);
+  displayCamera = false;
 }
 
 /*!
