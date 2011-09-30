0117dd3c45aa24fa306116aae460366ce4a164cb
Fabien Spindler <Fabien.Spindler@inria.fr>
2011-09-30 17:48:22 +0000

    Update the headers Fix when the libraries are not detected

--- ViSP/CMakeModules/FindLIBFREENECT.cmake
+++ ViSP/CMakeModules/FindLIBFREENECT.cmake
@@ -1,8 +1,45 @@
-##
-## Copyright Projet Lagadic / IRISA-INRIA Rennes, 2011
-## www: http://www.irisa.fr/lagadic
-##
-## Author: Celine Teuliere
+#############################################################################
+#
+# $Id: FindIRISA.cmake 3057 2011-02-11 13:17:26Z fspindle $
+#
+# This file is part of the ViSP software.
+# Copyright (C) 2005 - 2011 by INRIA. All rights reserved.
+# 
+# This software is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# ("GPL") version 2 as published by the Free Software Foundation.
+# See the file LICENSE.txt at the root directory of this source
+# distribution for additional information about the GNU GPL.
+#
+# For using ViSP with software that can not be combined with the GNU
+# GPL, please contact INRIA about acquiring a ViSP Professional 
+# Edition License.
+#
+# See http://www.irisa.fr/lagadic/visp/visp.html for more information.
+# 
+# This software was developed at:
+# INRIA Rennes - Bretagne Atlantique
+# Campus Universitaire de Beaulieu
+# 35042 Rennes Cedex
+# France
+# http://www.irisa.fr/lagadic
+#
+# If you have questions regarding the use of this file, please contact
+# INRIA at visp@inria.fr
+# 
+# This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+# WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Description:
+# Try to find Cerebellum Irisa library API to dial with the robot Afma6
+#
+# LIBFREENECT_FOUND
+# LIBFREENECT_INCLUDE_DIR
+# LIBFREENECT_LIBRARIES
+#
+# Authors:
+# Celine Teuliere
+# Fabien Spindler
 
 IF(WIN32)
   FIND_PATH(LIBFREENECT_INCLUDE_DIR libfreenect.h
@@ -15,6 +52,8 @@ IF(WIN32)
 ELSE(WIN32) 
   FIND_PATH(LIBFREENECT_INCLUDE_DIR libfreenect.h
     $ENV{LIBFREENECT_HOME}/include/
+    /usr/include
+    /usr/local/include
     )
   FIND_LIBRARY(LIBFREENECT_LIBRARY libfreenect.a
     $ENV{LIBFREENECT_HOME}/lib
@@ -26,16 +65,9 @@ ENDIF(WIN32)
 
 ## --------------------------------
 
-IF(NOT LIBFREENECT_INCLUDE_DIR)
-  MESSAGE(SEND_ERROR "libfreenect include dir not found.")
-ENDIF(NOT LIBFREENECT_INCLUDE_DIR)
-
 IF(LIBFREENECT_LIBRARY)
   SET(LIBFREENECT_LIBRARIES ${LIBFREENECT_LIBRARY})
-ELSE(LIBFREENECT_LIBRARY)
-  MESSAGE(SEND_ERROR "libfreenect library not found.")
 ENDIF(LIBFREENECT_LIBRARY)
-
 
 IF(LIBFREENECT_LIBRARIES AND LIBFREENECT_INCLUDE_DIR)
   SET(LIBFREENECT_INCLUDE_DIR ${LIBFREENECT_INCLUDE_DIR})
--- ViSP/CMakeModules/FindLIBUSB-1.cmake
+++ ViSP/CMakeModules/FindLIBUSB-1.cmake
@@ -1,8 +1,45 @@
-##
-## Copyright Projet Lagadic / IRISA-INRIA Rennes, 2011
-## www: http://www.irisa.fr/lagadic
-##
-## Author: Celine Teuliere
+#############################################################################
+#
+# $Id: FindIRISA.cmake 3057 2011-02-11 13:17:26Z fspindle $
+#
+# This file is part of the ViSP software.
+# Copyright (C) 2005 - 2011 by INRIA. All rights reserved.
+# 
+# This software is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# ("GPL") version 2 as published by the Free Software Foundation.
+# See the file LICENSE.txt at the root directory of this source
+# distribution for additional information about the GNU GPL.
+#
+# For using ViSP with software that can not be combined with the GNU
+# GPL, please contact INRIA about acquiring a ViSP Professional 
+# Edition License.
+#
+# See http://www.irisa.fr/lagadic/visp/visp.html for more information.
+# 
+# This software was developed at:
+# INRIA Rennes - Bretagne Atlantique
+# Campus Universitaire de Beaulieu
+# 35042 Rennes Cedex
+# France
+# http://www.irisa.fr/lagadic
+#
+# If you have questions regarding the use of this file, please contact
+# INRIA at visp@inria.fr
+# 
+# This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+# WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Description:
+# Try to find Cerebellum Irisa library API to dial with the robot Afma6
+#
+# LIBUSB_1_FOUND
+# LIBUSB_1_INCLUDE_DIR
+# LIBUSB_1_LIBRARIES
+#
+# Authors:
+# Celine Teuliere
+# Fabien Spindler
 
 IF(WIN32)
   FIND_PATH(LIBUSB_1_INCLUDE_DIR libusb.h
@@ -30,16 +67,9 @@ ENDIF(WIN32)
 
 ## --------------------------------
 
-IF(NOT LIBUSB_1_INCLUDE_DIR)
-  MESSAGE(SEND_ERROR "libusb include dir not found.")
-ENDIF(NOT LIBUSB_1_INCLUDE_DIR)
-
 IF(LIBUSB_1_LIBRARY)
   SET(LIBUSB_1_LIBRARIES ${LIBUSB_1_LIBRARY})
-ELSE(LIBUSB_1_LIBRARY)
-  MESSAGE(SEND_ERROR "libusb library not found.")
 ENDIF(LIBUSB_1_LIBRARY)
-
 
 IF(LIBUSB_1_LIBRARIES AND LIBUSB_1_INCLUDE_DIR)
   SET(LIBUSB_1_INCLUDE_DIR ${LIBUSB_1_INCLUDE_DIR})
