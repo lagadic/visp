26a4264eff152f74e410fc10b991f2631a54c211
Celine Teuliere <Celine.Teuliere@inria.fr>
2011-10-11 13:52:15 +0000

    add vpDMResolution handling in vpKinect and minimize computations while mutex are locked

--- src/kinectdevice/vpKinect.cpp
+++ src/kinectdevice/vpKinect.cpp
@@ -51,15 +51,12 @@
 vpKinect::vpKinect(freenect_context *ctx, int index)
 		: Freenect::FreenectDevice(ctx, index),
 		  m_new_rgb_frame(false), 
-		  m_new_depth_frame(false), 
+		  m_new_depth_map(false),
+		  m_new_depth_image(false),
 		  height(480), width(640)
 {
 	dmap.resize(height, width);
-	Idmap.resize(height, width);
 	IRGB.resize(height, width);
-	//IRcam.initPersProjWithoutDistortion(606.12,595.78,321.5,235.8);
-	IRcam.initPersProjWithDistortion(606.12, 595.78, 321.5, 235.8, -0.27, -0.27);
-	RGBcam.initPersProjWithoutDistortion(606, 595, 321, 235);// to calibrate
 }
 
 /*!
@@ -69,8 +66,24 @@ vpKinect::~vpKinect()
 {
 }
 
-void vpKinect::start()
+void vpKinect::start(vpDMResolution res)
 {
+	DMres = res;
+	RGBcam.initPersProjWithoutDistortion(606, 595, 321, 235);// to calibrate
+	if (DMres == LOW){
+		std::cout << "vpKinect::start LOW depth map resolution 240x320" << std::endl;
+//		IRcam.setparameters(IRcam.get_px()/2, IRcam.get_py()/2, IRcam.get_u0()/2, IRcam.get_v0()/2);
+		//IRcam.initPersProjWithoutDistortion(303.06,297.89,160.75,117.9);
+		IRcam.initPersProjWithDistortion(303.06, 297.89, 160.75, 117.9, -0.27, -0.27);
+	}
+	else
+	{
+		std::cout << "vpKinect::start MEDIUM depth map resolution 480x640" << std::endl;
+		//IRcam.initPersProjWithoutDistortion(606.12,595.78,321.5,235.8);
+		IRcam.initPersProjWithDistortion(606.12, 595.78, 321.5, 235.8, -0.27, -0.27);
+//		Idmap.resize(height, width);
+
+	}
 	this->startVideo();
 	this->startDepth();
 }
@@ -87,7 +100,7 @@ void vpKinect::stop()
  */
 void vpKinect::VideoCallback(void* rgb, uint32_t /* timestamp */)
 {
-	std::cout << "vpKinect Video callback" << std::endl;
+//	std::cout << "vpKinect Video callback" << std::endl;
 	vpMutex::vpScopedLock lock(m_rgb_mutex);
 	uint8_t* rgb_ = static_cast<uint8_t*>(rgb);
 	for (unsigned i = 0; i< height;i++){
@@ -111,41 +124,24 @@ void vpKinect::VideoCallback(void* rgb, uint32_t /* timestamp */)
   In this function this value is converted into a metric depth map and
   stored in dmap.  (range : 0.3 - 5m).
 
-  The corresponding gray level image Idmap is also updated.
 */
 void vpKinect::DepthCallback(void* depth, uint32_t /* timestamp */)
 {
-	std::cout << "vpKinect Depth callback" << std::endl;
 	vpMutex::vpScopedLock lock(m_depth_mutex);
 	uint16_t* depth_ = static_cast<uint16_t*>(depth);
-
 	for (unsigned i = 0; i< height;i++){
 		for (unsigned j = 0 ; j < width ; j++)
 		{
 			dmap[i][j] = 0.1236 * tan(depth_[width*i +j] / 2842.5 + 1.1863);//formula from http://openkinect.org/wiki/Imaging_Information
-			if(depth_[width*i +j]>1023){
-				dmap[i][j] = -1;//Depth cannot be computed
+			if(depth_[width*i +j]>1023){//Depth cannot be computed
+				dmap[i][j] = -1;
 			}
-
-			Idmap[i][j] = (unsigned char)(255*depth_[width*i +j]/(float) 1024);
 		}
 	}
-	m_new_depth_frame = true;
+	m_new_depth_map = true;
+	m_new_depth_image = true;
 }
 
-/*!
-  Get metric depth map (float) and corresponding image.
- */
-bool vpKinect::getDepthMap(vpImage<float>& map,vpImage<unsigned char>& Imap)
-{
-  vpMutex::vpScopedLock lock(m_depth_mutex);
-  if (!m_new_depth_frame)
-    return false;
-  map = dmap;
-  Imap = Idmap;
-  m_new_depth_frame = false;
-  return true;
-}
 
 /*!
   Get metric depth map (float).
@@ -153,11 +149,61 @@ bool vpKinect::getDepthMap(vpImage<float>& map,vpImage<unsigned char>& Imap)
 bool vpKinect::getDepthMap(vpImage<float>& map)
 {
   vpMutex::vpScopedLock lock(m_depth_mutex);
-  if (!m_new_depth_frame)
+  if (!m_new_depth_map)
     return false;
   map = dmap;
-  m_new_depth_frame = false;
+  m_new_depth_map = false;
   return true;
+}
+
+
+/*!
+ *   Get metric depth map (float) and corresponding image.
+ */
+bool vpKinect::getDepthMap(vpImage<float>& map,vpImage<unsigned char>& Imap)
+{
+	vpImage<float> tempMap;
+	m_depth_mutex.lock();
+	if (!m_new_depth_map && !m_new_depth_image)
+	{
+		m_depth_mutex.unlock();
+		return false;
+	}
+	tempMap = dmap;
+
+	m_new_depth_map = false;
+	m_new_depth_image = false;
+	m_depth_mutex.unlock();
+
+	if (DMres == LOW){
+	  unsigned int h = height/2;
+	  unsigned int w = width/2;
+	  if ((Imap.getHeight()!=320 )||(map.getHeight()!=320))
+		  vpERROR_TRACE(1, "Image size does not match vpKinect DM resolution");
+	  for(unsigned int i = 0; i < h; i++)
+	    for(unsigned int j = 0; j < w; j++){
+	    	map[i][j] = tempMap[i<<1][j<<1];
+	    	if (map[i][j] != -1)
+	    		Imap[i][j] = (unsigned char)(255*map[i][j]/5);
+	    	else
+	    		Imap[i][j] = 255;
+	    }
+	}
+	else
+	{
+		if ((Imap.getHeight()!=480 )||(map.getHeight()!=480))
+				  vpERROR_TRACE(1, "Image size does not match vpKinect DM resolution");
+		for (unsigned i = 0; i< height;i++)
+			for (unsigned j = 0 ; j < width ; j++){
+				map[i][j] = tempMap[i][j];
+				if (map[i][j] != -1)
+					Imap[i][j] = (unsigned char)(255*map[i][j]/5);
+				else
+					Imap[i][j] = 255;
+			}
+	}
+
+	return true;
 }
 
 
@@ -176,3 +222,4 @@ bool vpKinect::getRGB(vpImage<vpRGBa>& IRGB)
 
 
 #endif // VISP_HAVE_LIBFREENECT
+
