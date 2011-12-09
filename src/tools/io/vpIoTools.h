4278b71dc86aafae136a3117e7d192a4603e661b
Fabien Spindler <Fabien.Spindler@inria.fr>
2011-12-09 17:05:08 +0000

    Improve doc

--- src/tools/io/vpIoTools.h
+++ src/tools/io/vpIoTools.h
@@ -113,40 +113,40 @@ int main()
   The example below shows how to read a configuration file and how to create a name for experiment files.
   We assume the following file "/home/user/demo/config.txt" :
   \code
-  expNumber 2
-  save 0
-  lambda 0.4
-  use2D 0
-  use3D 1
+expNumber 2
+save 0
+lambda 0.4
+use2D 0
+use3D 1
   \endcode
 
   \code
-	#include <iostream>
-	#include <string>
-	#include <visp/vpIoTools.h>
+#include <iostream>
+#include <string>
+#include <visp/vpIoTools.h>
 
 int main()
 {
-	// reading configuration file
-	vpIoTools::loadConfigFile("/home/user/demo/config.txt");
-	std::string nExp;vpIoTools::readConfigVar("expNumber", nExp);	// nExp <- "2"
-	double lambda;vpIoTools::readConfigVar("lambda", lambda);	// lambda <- 0.4
-	bool use2D;vpIoTools::readConfigVar("use2D", use2D);	// use2D <- false
-	bool use3D;vpIoTools::readConfigVar("use3D", use3D);	// use3D <- true
-	bool doSave;vpIoTools::readConfigVar("save", doSave);	//  doSave <- false
+  // reading configuration file
+  vpIoTools::loadConfigFile("/home/user/demo/config.txt");
+  std::string nExp;vpIoTools::readConfigVar("expNumber", nExp); // nExp <- "2"
+  double lambda;vpIoTools::readConfigVar("lambda", lambda);     // lambda <- 0.4
+  bool use2D;vpIoTools::readConfigVar("use2D", use2D);          // use2D <- false
+  bool use3D;vpIoTools::readConfigVar("use3D", use3D);          // use3D <- true
+  bool doSave;vpIoTools::readConfigVar("save", doSave);         //  doSave <- false
 
-	// creating name for experiment files
-	vpIoTools::setBaseDir("/home/user/data");
-  	vpIoTools::setBaseName("exp" + nExp);				// full name <- "/home/user/data/exp2"
-  	vpIoTools::addNameElement("2D", use2D);				// full name <- "/home/user/data/exp2" since use2D==false
-  	vpIoTools::addNameElement("3D", use3D);				// full name <- "/home/user/data/exp2_3D"
-  	vpIoTools::addNameElement("lambda", lambda);		// full name <- "/home/user/data/exp2_3D_lambda0.4"
+  // creating name for experiment files
+  vpIoTools::setBaseDir("/home/user/data");
+  vpIoTools::setBaseName("exp" + nExp);         // full name <- "/home/user/data/exp2"
+  vpIoTools::addNameElement("2D", use2D);       // full name <- "/home/user/data/exp2" since use2D==false
+  vpIoTools::addNameElement("3D", use3D);       // full name <- "/home/user/data/exp2_3D"
+  vpIoTools::addNameElement("lambda", lambda);  // full name <- "/home/user/data/exp2_3D_lambda0.4"
 
-  	// saving file
-  	vpIoTools::saveConfigFile(doSave);			// would copy "/home/user/demo/config.txt" to "/home/user/data/exp2_3D_lambda0.4_config.txt" if doSave was true
+  // saving file
+  vpIoTools::saveConfigFile(doSave); // would copy "/home/user/demo/config.txt" to "/home/user/data/exp2_3D_lambda0.4_config.txt" if doSave was true
 
-  	// create sub directory
-  	vpIoTools::createBaseNamePath();			// creates "/home/user/data/exp2_3D_lambda0.4/"
+  // create sub directory
+  vpIoTools::createBaseNamePath();  // creates "/home/user/data/exp2_3D_lambda0.4/"
 }
   \endcode
 
@@ -162,11 +162,11 @@ public:
 	static std::string getenv(std::string &env);
 	static bool checkDirectory(const char *dirname);
 	static bool checkDirectory(const std::string &dirname);
-	static void makeDirectory(const char *dirname);
-	static void makeDirectory(const std::string &dirname);
 	static bool checkFilename(const char *filename);
 	static bool checkFilename(const std::string &filename);
-	static bool remove(const char *filename);
+  static void makeDirectory(const char *dirname);
+  static void makeDirectory(const std::string &dirname);
+  static bool remove(const char *filename);
 	static bool remove(const std::string &filename);
 	static bool rename(const char *oldfilename, const char *newfilename);
 	static bool rename(const std::string &oldfilename, const std::string &newfilename);
