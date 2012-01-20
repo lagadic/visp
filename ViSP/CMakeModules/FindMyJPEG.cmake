f05b11fa9213ae461f285c37901d4b96fe511232
Fabien Spindler <Fabien.Spindler@inria.fr>
2012-01-20 17:43:08 +0000

    - Remove GTK1 detectetion and support - Remove FindDoxygen.cmake since it is provided by CMake - Use CMake native package finder for zlib, png, jpeg, coin3d, gtk2 3rd party libraries.   If detection fails, we use our own package finder. - Improve sowin, lapack, soqt finder to handle optimized versus debug libraries under   windows in the Find...cmake instead in the CMakeLists.txt file

--- ViSP/CMakeModules/FindMyJPEG.cmake
+++ ViSP/CMakeModules/FindMyJPEG.cmake
@@ -34,9 +34,9 @@
 # Try to find libjpeg library.
 # Once run this will define: 
 #
-# LIBJPEG_FOUND
-# LIBJPEG_INCLUDE_DIRS
-# LIBJPEG_LIBRARIES
+# JPEG_FOUND
+# JPEG_INCLUDE_DIRS
+# JPEG_LIBRARIES
 #
 # Authors:
 # Nicolas Melchior
@@ -45,7 +45,7 @@
 
 
 # detection of the Libjpeg headers location
-  FIND_PATH(LIBJPEG_INCLUDE_DIR 
+  FIND_PATH(JPEG_INCLUDE_DIR 
     NAMES
     jpeglib.h
     PATHS
@@ -55,10 +55,10 @@
     $ENV{LIBJPEG_DIR}
     "C:/Program Files/GnuWin32/include"
     )
-  #MESSAGE("LIBJPEG_INCLUDE_DIR=${LIBJPEG_INCLUDE_DIR}")
+  #MESSAGE("JPEG_INCLUDE_DIR=${JPEG_INCLUDE_DIR}")
 
   # Detection of the Libjpeg library on Unix
-  FIND_LIBRARY(LIBJPEG_LIBRARY
+  FIND_LIBRARY(JPEG_LIBRARY
     NAMES
     jpeg libjpeg
     PATHS
@@ -70,22 +70,22 @@
     $ENV{LIBJPEG_DIR}
     "C:/Program Files/GnuWin32/lib"
     )
-  #MESSAGE("LIBJPEG_LIBRARY=${LIBJPEG_LIBRARY}")
+  #MESSAGE("JPEG_LIBRARY=${JPEG_LIBRARY}")
 
 
   MARK_AS_ADVANCED(
-    LIBJPEG_LIBRARY
-    LIBJPEG_INCLUDE_DIR
+    JPEG_LIBRARY
+    JPEG_INCLUDE_DIR
   )
   
 ## --------------------------------
   
-IF(LIBJPEG_LIBRARY AND LIBJPEG_INCLUDE_DIR)
+IF(JPEG_LIBRARY AND JPEG_INCLUDE_DIR)
     # The material is found. Check if it works on the requested architecture
     include(CheckCXXSourceCompiles)
 	
-    SET(CMAKE_REQUIRED_LIBRARIES ${LIBJPEG_LIBRARY})
-    SET(CMAKE_REQUIRED_INCLUDES ${LIBJPEG_INCLUDE_DIR})
+    SET(CMAKE_REQUIRED_LIBRARIES ${JPEG_LIBRARY})
+    SET(CMAKE_REQUIRED_INCLUDES ${JPEG_INCLUDE_DIR})
     CHECK_CXX_SOURCE_COMPILES("
       #include <stdio.h>  
       #include <jpeglib.h> // Contrib for jpeg image io
@@ -96,17 +96,17 @@ IF(LIBJPEG_LIBRARY AND LIBJPEG_INCLUDE_DIR)
         struct jpeg_error_mgr jerr;
         cinfo.err = jpeg_std_error(&jerr);
       }
-      " LIBJPEG_BUILD_TEST) 
-    #MESSAGE("LIBJPEG_BUILD_TEST: ${LIBJPEG_BUILD_TEST}")
-    IF(LIBJPEG_BUILD_TEST)
-      SET(LIBJPEG_INCLUDE_DIRS ${LIBJPEG_INCLUDE_DIR})
-      SET(LIBJPEG_LIBRARIES  ${LIBJPEG_LIBRARY})
-      SET(LIBJPEG_FOUND TRUE)
+      " JPEG_BUILD_TEST) 
+    #MESSAGE("JPEG_BUILD_TEST: ${JPEG_BUILD_TEST}")
+    IF(JPEG_BUILD_TEST)
+      SET(JPEG_INCLUDE_DIRS ${JPEG_INCLUDE_DIR})
+      SET(JPEG_LIBRARIES  ${JPEG_LIBRARY})
+      SET(JPEG_FOUND TRUE)
     ELSE()
-      SET(LIBJPEG_FOUND FALSE)
+      SET(JPEG_FOUND FALSE)
       #MESSAGE("libjpeg library found but not compatible with architecture.")
     ENDIF() 
 
-ELSE(LIBJPEG_LIBRARY AND LIBJPEG_INCLUDE_DIR)
-  SET(LIBJPEG_FOUND FALSE)
-ENDIF(LIBJPEG_LIBRARY AND LIBJPEG_INCLUDE_DIR)
+ELSE(JPEG_LIBRARY AND JPEG_INCLUDE_DIR)
+  SET(JPEG_FOUND FALSE)
+ENDIF(JPEG_LIBRARY AND JPEG_INCLUDE_DIR)
