db35f2e169e56dbccbe74d71935bacff4f02982e
Fabien Spindler <Fabien.Spindler@inria.fr>
2012-03-01 09:57:29 +0000

    Fix a warning in the sample code

--- ViSP/src/device/framegrabber/1394/vp1394TwoGrabber.h
+++ ViSP/src/device/framegrabber/1394/vp1394TwoGrabber.h
@@ -126,21 +126,20 @@ int main()
   ncameras = g.getNumCameras();
   std::cout << ncameras << " cameras found:" << std::endl;
 
-  for(int i=0; i< ncameras; i++)
+  for(unsigned int i=0; i< ncameras; i++)
   {
     g.setCamera(i);
     uint64_t guid = g.getGuid();
-    printf("camera %d with guid 0x%lx\n", i, guid);
+    printf("camera %d with guid 0x%lx\n", i, (long unsigned int)guid);
   }
 
   // produce:
   // 2 cameras found:
   // camera 0 with guid 0xb09d01009b329c
   // camera 1 with guid 0xb09d01007e0ee7
+  g.setCamera( (uint64_t)0xb09d01009b329cULL );
 
-  g.setCamera( 0xb09d01009b329cULL );
-
-  printf("Use camera with GUID: 0x%lx\n", g.getGuid());
+  printf("Use camera with GUID: 0x%lx\n", (long unsigned int)g.getGuid());
   g.acquire(I); // Acquire an image from the camera with GUID 0xb09d01009b329c
   
   vpImageIo::writePGM(I, "image.pgm"); // Write image on the disk
