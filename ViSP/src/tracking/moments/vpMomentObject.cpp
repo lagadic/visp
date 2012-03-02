d2fdac692be497544afcd08b3c83273ea540a66b
Filipp Novotny <Filipp Novotny@inria.fr>
2012-03-02 13:43:42 +0000

    removed obvious shared variables (const) from openmp shared directive

--- ViSP/src/tracking/moments/vpMomentObject.cpp
+++ ViSP/src/tracking/moments/vpMomentObject.cpp
@@ -262,58 +262,55 @@ int main()
 */
 
 void vpMomentObject::fromImage(const vpImage<unsigned char>& image, unsigned char threshold, const vpCameraParameters& cam){
-#ifdef VISP_HAVE_OPENMP	
-  //int th_id=0;
-	
-  #pragma omp parallel /* private(th_id) */ shared(cam,image,threshold)
-	{		
-    // th_id = omp_get_thread_num();
-		std::vector<double> curvals(order*order); 
-		curvals.assign(order*order,0.);
+#ifdef VISP_HAVE_OPENMP    
+  #pragma omp parallel shared(threshold)
+  {        
+    std::vector<double> curvals(order*order); 
+    curvals.assign(order*order,0.);
     int i_, j_;
 
-		#pragma omp for nowait//automatically organize loop counter between threads
+    #pragma omp for nowait//automatically organize loop counter between threads
     for(int i=0;i<(int)image.getCols();i++){
       for(int j=0;j<(int)image.getRows();j++){
         i_ = static_cast<unsigned int>(i);
         j_ = static_cast<unsigned int>(j);
         if(image[j_][i_]>threshold){
-					double x=0;
-					double y=0;
+          double x=0;
+          double y=0;
           vpPixelMeterConversion::convertPoint(cam,i_,j_,x,y);
 
-					double xval=1.;
-					double yval=1.;
-					for(register unsigned int k=0;k<order;k++){						
-						xval=1.;
-						for(register unsigned int l=0;l<order-k;l++){
-							curvals[(k*order+l)]+=(xval*yval);
-							xval*=x;
-						}
-						yval*=y;
-					}					
-				}
-			}
-		}		
-		
-		#pragma omp master //only set this variable in master thread
-		{			
-			values.assign(order*order, 0.);			
-		}
+          double xval=1.;
+          double yval=1.;
+          for(register unsigned int k=0;k<order;k++){            
+            xval=1.;
+            for(register unsigned int l=0;l<order-k;l++){
+              curvals[(k*order+l)]+=(xval*yval);
+              xval*=x;
+            }
+            yval*=y;
+          }          
+        }
+      }
+    }    
+    
+    #pragma omp master //only set this variable in master thread
+    {      
+      values.assign(order*order, 0.);      
+    }
 
-		#pragma omp barrier
-		
-		for(register unsigned int k=0;k<order;k++){		
-			for(register unsigned int l=0;l<order-k;l++){
-				#pragma omp atomic
-				values[k*order+l]+= curvals[k*order+l];
-			}
-		}
-		
-	}		
-	
+    #pragma omp barrier
+    
+    for(register unsigned int k=0;k<order;k++){    
+      for(register unsigned int l=0;l<order-k;l++){
+        #pragma omp atomic
+        values[k*order+l]+= curvals[k*order+l];
+      }
+    }
+    
+  }    
+  
 #else
-	std::vector<double> cache(order*order,0.);
+  std::vector<double> cache(order*order,0.);
     values.assign(order*order,0);
     for(register unsigned int i=0;i<image.getCols();i++){
         for(register unsigned int j=0;j<image.getRows();j++){
