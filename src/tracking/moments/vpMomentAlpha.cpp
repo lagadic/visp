b263f95356e9a597b96bb5b79c24fd76a69892ea
Manikandan Bakthavatchalam <manikandan.bakthavatchalam@inria.fr>
2012-03-13 10:13:23 +0000

    Alpha disambiguation prevented for symmetric objects losing their 3rd order moments

--- src/tracking/moments/vpMomentAlpha.cpp
+++ src/tracking/moments/vpMomentAlpha.cpp
@@ -49,7 +49,7 @@
   A reference alpha is class harbouring an alpha value computed for a \f$[-\pi/2..\pi/2]\f$ portion of the circle. Not setting a reference alpha will prevent you
   from doing more than 180deg rotation with moments.
 */
-vpMomentAlpha::vpMomentAlpha() : vpMoment(),isRef(true){
+vpMomentAlpha::vpMomentAlpha() : vpMoment(),isRef(true), symmetric(false){
     values.resize(1);
 }
 
@@ -61,7 +61,11 @@ vpMomentAlpha::vpMomentAlpha() : vpMoment(),isRef(true){
   \param ref : vector of 3rd order centered moments corresponding to the reference alpha in the following order: \f$\mu_{03},\mu_{12},\mu_{21},\mu_{30}\f$.
   \param alphaRef : value of the reference alpha.
 */
-vpMomentAlpha::vpMomentAlpha(std::vector<double>& ref,double alphaRef) : vpMoment(),isRef(false),ref(ref),alphaRef(alphaRef){
+vpMomentAlpha::vpMomentAlpha(std::vector<double>& ref,double alphaRef) : vpMoment(),isRef(false),ref(ref),alphaRef(alphaRef), symmetric(false){
+	for (std::vector<double>::iterator it = ref.begin(); it!=ref.end(); it++)
+		if (*it<=1e-4)
+			symmetric = true;
+
     values.resize(1);
 }
 
@@ -70,6 +74,7 @@ vpMomentAlpha::vpMomentAlpha(std::vector<double>& ref,double alphaRef) : vpMomen
   Depends on vpMomentCentered.
 */
 void vpMomentAlpha::compute(){
+	//symmetric = symmetric | this->getObject().isSymmetric();
     bool found_moment_centered;    
     
     vpMomentCentered& momentCentered = (static_cast<vpMomentCentered&>(getMoments().get("vpMomentCentered",found_moment_centered)));
@@ -82,51 +87,53 @@ void vpMomentAlpha::compute(){
     unsigned int order = 4;
     std::vector<double> rotMu(4);
     std::vector<double> realMu(4);
-    if(isRef){
-        alphaRef = alpha;
-    }else{
-        double r11 = cos(alpha-alphaRef);
-        double r12 = sin(alpha-alphaRef);
-        double r21 = -sin(alpha-alphaRef);
-        double r22 = cos(alpha-alphaRef);
-        unsigned int idx=0;
-        for(register unsigned int c=0;c<(order)*(order);c++){
-            unsigned int i = c%order;
-            unsigned int j = c/order;
 
-            if(i+j==3){
-                double r11_k = 1.;
-                for(register unsigned int k=0;k<=i;k++){
-                    double r12_i_k = pow(r12,(int)(i-k));
-                    double comb_i_k = static_cast<double>( vpMath::comb(i,k) );
-                    for(register unsigned int l=0;l<=j;l++){
-                        rotMu[idx]+= static_cast<double>( 
-                          comb_i_k*vpMath::comb(j,l)
-                          *r11_k*pow(r21,(int)l)
-                          *r12_i_k*pow(r22,(int)(j-l))
-                          *momentCentered.get(k+l,(unsigned int)(int)(i+j-k-l)) );
-                    }
-                    r11_k*=r11;
-                }
-                realMu[idx] = momentCentered.get(i,j);
-                idx++;
-            }
-        }
-        double sum=0.;
-        bool signChange=true;
-        for(register unsigned int i=0;i<4;i++){
-	  if(std::abs(rotMu[i])>1e10 * std::numeric_limits<double>::epsilon() && std::abs(ref[i])>1e10 * std::numeric_limits<double>::epsilon() && rotMu[i]*ref[i]>0) signChange=false;
-	  sum+=std::abs(rotMu[i]*ref[i]);
-        }
 
-        if(sum<1e4 * std::numeric_limits<double>::epsilon()) signChange=false;
-        if(signChange)
-            alpha=alpha+3.14;
+		if(isRef){
+			alphaRef = alpha;
+		}else {
+			if (!symmetric) {
+				double r11 = cos(alpha-alphaRef);
+				double r12 = sin(alpha-alphaRef);
+				double r21 = -sin(alpha-alphaRef);
+				double r22 = cos(alpha-alphaRef);
+				unsigned int idx=0;
+				for(register unsigned int c=0;c<(order)*(order);c++){
+					unsigned int i = c%order;
+					unsigned int j = c/order;
 
+					if(i+j==3){
+						double r11_k = 1.;
+						for(register unsigned int k=0;k<=i;k++){
+							double r12_i_k = pow(r12,(int)(i-k));
+							double comb_i_k = static_cast<double>( vpMath::comb(i,k) );
+							for(register unsigned int l=0;l<=j;l++){
+								rotMu[idx]+= static_cast<double>(
+								  comb_i_k*vpMath::comb(j,l)
+								  *r11_k*pow(r21,(int)l)
+								  *r12_i_k*pow(r22,(int)(j-l))
+								  *momentCentered.get(k+l,(unsigned int)(int)(i+j-k-l)) );
+							}
+							r11_k*=r11;
+						}
+						realMu[idx] = momentCentered.get(i,j);
+						idx++;
+					}
+				}
+
+				double sum=0.;
+				bool signChange=true;
+				for(register unsigned int i=0;i<4;i++){
+					if(std::abs(rotMu[i])>1e10 * std::numeric_limits<double>::epsilon() && std::abs(ref[i])>1e10 * std::numeric_limits<double>::epsilon() && rotMu[i]*ref[i]>0) signChange=false;
+					sum+=std::abs(rotMu[i]*ref[i]);
+				}
+
+				if(sum<1e4 * std::numeric_limits<double>::epsilon()) signChange=false;
+				if(signChange)
+					alpha=alpha+3.14;
+		}
     }
-
     values[0] = alpha;
-
 }
 
 /*!
