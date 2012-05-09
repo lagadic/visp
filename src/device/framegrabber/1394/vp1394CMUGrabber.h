7d6d5ab4c32764aea59e898b08f596850258f09c
Fabien Spindler <Fabien.Spindler@inria.fr>
2012-05-09 08:04:13 +0000

    Set getter as const methods Set vpColorCodingType as public Doc improvement

--- src/device/framegrabber/1394/vp1394CMUGrabber.h
+++ src/device/framegrabber/1394/vp1394CMUGrabber.h
@@ -4,7 +4,7 @@
  *
  * This file is part of the ViSP software.
  * Copyright (C) 2005 - 2012 by INRIA. All rights reserved.
- * 
+ *
  * This software is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * ("GPL") version 2 as published by the Free Software Foundation.
@@ -12,11 +12,11 @@
  * distribution for additional information about the GNU GPL.
  *
  * For using ViSP with software that can not be combined with the GNU
- * GPL, please contact INRIA about acquiring a ViSP Professional 
+ * GPL, please contact INRIA about acquiring a ViSP Professional
  * Edition License.
  *
  * See http://www.irisa.fr/lagadic/visp/visp.html for more information.
- * 
+ *
  * This software was developed at:
  * INRIA Rennes - Bretagne Atlantique
  * Campus Universitaire de Beaulieu
@@ -26,7 +26,7 @@
  *
  * If you have questions regarding the use of this file, please contact
  * INRIA at visp@inria.fr
- * 
+ *
  * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
  * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
@@ -35,12 +35,12 @@
  * Firewire cameras video capture based on CMU 1394 Digital Camera SDK.
  *
  * Authors:
- * Lucas Lopes Lemos FEMTO-ST, AS2M departement, Besançon
- * Guillaume Laurent FEMTO-ST, AS2M departement, Besançon
+ * Lucas Lopes Lemos FEMTO-ST, AS2M departement, Besancon
+ * Guillaume Laurent FEMTO-ST, AS2M departement, Besancon
  * Fabien Spindler
  *
  *****************************************************************************/
- 
+
 #ifndef vp1394CMUGrabber_h
 #define vp1394CMUGrabber_h
 
@@ -69,16 +69,16 @@
 
   \brief Firewire cameras video capture based on CMU 1394 Digital Camera SDK.
 
-   This block is based on the CMU 1394 Digital Camera SDK. The CMU 1394 Digital 
-   Camera Driver must be installed (go to http://www.cs.cmu.edu/~iwan/1394/index.html 
+   This block is based on the CMU 1394 Digital Camera SDK. The CMU 1394 Digital
+   Camera Driver must be installed (go to http://www.cs.cmu.edu/~iwan/1394/index.html
    to download it).
-   - Parameters: 
-		- Camera index (0, 1, 2, ... or 10). First camera has index 0.
-		- Image format
-		- Frame rate. Real frame rate depends on device capacities.
-		- Control : shutter speed and gain can be internally set, but it is possible to set manually them. 
-					* Exposure time register value. Real exposure time depends on device capacities.
-					* Gain register value. Real gain depends on device capacities.
+   - Parameters:
+    - Camera index (0, 1, 2, ... or 10). First camera has index 0.
+    - Image format
+    - Frame rate. Real frame rate depends on device capacities.
+    - Control : shutter speed and gain can be internally set, but it is possible to set manually them.
+          * Exposure time register value. Real exposure time depends on device capacities.
+          * Gain register value. Real gain depends on device capacities.
 
   The examples below show how to use this class.
 
@@ -90,34 +90,34 @@
 #include <visp/vpImage.h>
 #include <visp/vpDisplayOpenCV.h>
 #include <visp/vp1394CMUGrabber.h>
-         
+
 int main()
 {
 #if defined(VISP_HAVE_CMU1394)
-	std::cout << "ViSP Image acquisition example" <<std::endl;
+  std::cout << "ViSP Image acquisition example" <<std::endl;
 
-	vpImage<unsigned char> I;
-	vp1394CMUGrabber g;
+  vpImage<unsigned char> I;
+  vp1394CMUGrabber g;
 
-	g.open(I);
-	g.acquire(I);
+  g.open(I);
+  g.acquire(I);
 
-	vpDisplayOpenCV d(I);
+  vpDisplayOpenCV d(I);
     vpDisplay::display(I);
 
-	for(;;)
-	{
-		g.acquire(I);
-		vpDisplay::display(I);
-		vpDisplay::flush(I);
-		if (vpDisplay::getClick(I, false)) // a click to exit
-				break;
-	}
+  for(;;)
+  {
+    g.acquire(I);
+    vpDisplay::display(I);
+    vpDisplay::flush(I);
+    if (vpDisplay::getClick(I, false)) // a click to exit
+        break;
+  }
 
-	g.close();
+  g.close();
 #endif
-	std::cout << "ViSP exiting..." <<std::endl;
-	return 0;
+  std::cout << "ViSP exiting..." <<std::endl;
+  return 0;
 }
   \endcode
 
@@ -129,61 +129,61 @@ int main()
 #include <visp/vpImage.h>
 #include <visp/vpDisplayOpenCV.h>
 #include <visp/vp1394CMUGrabber.h>
-         
+
 int main()
 {
 #if defined(VISP_HAVE_CMU1394)
-	std::cout << "ViSP Image acquisition example" << std::endl;
+  std::cout << "ViSP Image acquisition example" << std::endl;
 
-	vpImage<unsigned char> I;
-	vp1394CMUGrabber g;
+  vpImage<unsigned char> I;
+  vp1394CMUGrabber g;
 
-	if( g.getNumberOfConnectedCameras() > 1 )
-		std::cout << "There are " << g.getNumberOfConnectedCameras() << " connected cameras." << std::endl;
-	if( g.getNumberOfConnectedCameras() == 1 )
-		std::cout << "There is " << g.getNumberOfConnectedCameras() << " connected camera." << std::endl;
-	else
-		std::cout << "There is no connected camera." << std::endl;
-			
-	// Setting camera parameters manually
-	g.selectCamera(0);
-	g.setControl(0, 2000);
-	g.setFPS(3);          // 15 FPS
-	g.setVideoMode(0, 5); // 640x480 - MONO
+  if( g.getNumberOfConnectedCameras() > 1 )
+    std::cout << "There are " << g.getNumberOfConnectedCameras() << " connected cameras." << std::endl;
+  if( g.getNumberOfConnectedCameras() == 1 )
+    std::cout << "There is " << g.getNumberOfConnectedCameras() << " connected camera." << std::endl;
+  else
+    std::cout << "There is no connected camera." << std::endl;
 
-	g.open(I);
-	g.acquire(I);
+  // Setting camera parameters manually
+  g.selectCamera(0);
+  g.setControl(0, 2000);
+  g.setFPS(3);          // 15 FPS
+  g.setVideoMode(0, 5); // 640x480 - MONO
 
-	// Display camera description
-	g.displayCameraDescription(0);
-	g.displayCameraModel();
-	std::cout << "Height: " << g.getHeight() << " Width: " << g.getWidth() << std::endl;
+  g.open(I);
+  g.acquire(I);
 
-	vpDisplayOpenCV d(I);
-	vpDisplay::display(I);
+  // Display camera description
+  g.displayCameraDescription(0);
+  g.displayCameraModel();
+  std::cout << "Height: " << g.getHeight() << " Width: " << g.getWidth() << std::endl;
 
-	for(;;)
-	{
-		g.acquire(I);
-		vpDisplay::display(I);
-		vpDisplay::flush(I);
-		if (vpDisplay::getClick(I, false)) // a click to exit
-				break;
-	}
+  vpDisplayOpenCV d(I);
+  vpDisplay::display(I);
 
-	g.close();
+  for(;;)
+  {
+    g.acquire(I);
+    vpDisplay::display(I);
+    vpDisplay::flush(I);
+    if (vpDisplay::getClick(I, false)) // a click to exit
+        break;
+  }
+
+  g.close();
 #endif
-	std::cout << "ViSP exiting..." <<std::endl;
-	return 0;
+  std::cout << "ViSP exiting..." <<std::endl;
+  return 0;
 }
   \endcode
 */
 
 class VISP_EXPORT vp1394CMUGrabber : public vpFrameGrabber
 {
- private :
-    /*!
-    Enumeration of color codings. 
+public:
+  /*!
+    Enumeration of color codings.
   */
   typedef enum {
     YUV444,
@@ -195,167 +195,123 @@ class VISP_EXPORT vp1394CMUGrabber : public vpFrameGrabber
     UNKNOWN
   } vpColorCodingType;
 
+private :
   //! Current camera handle
-	C1394Camera *camera;
-    //! Camera index on the bus
-    int index;
-    //! Current video format
-    unsigned long _format;
-	//! Current video mode
-	unsigned long _mode ;
-	//! Current video frame rate
-	unsigned long _fps ;
-	//! Current auto mode
-	bool _modeauto;
-    //! Current gain
-	unsigned short _gain;
-	//! Current shutter
-	unsigned short _shutter;
+  C1394Camera *camera;
+  //! Camera index on the bus
+  int index;
+  //! Current video format
+  unsigned long _format;
+  //! Current video mode
+  unsigned long _mode ;
+  //! Current video frame rate
+  unsigned long _fps ;
+  //! Current auto mode
+  bool _modeauto;
+  //! Current gain
+  unsigned short _gain;
+  //! Current shutter
+  unsigned short _shutter;
   //! Color coding
   vpColorCodingType _color;
-   
-       
- public:
-	
-   //! Constructor.
-   vp1394CMUGrabber();
-   //! Destructor.
-   virtual ~vp1394CMUGrabber();
 
-   //! Initialization of the grabber using a greyscale image.
-   void open(vpImage<unsigned char> &I);
-   
-   //! Initialization of the grabber using a color image.
-   void open(vpImage<vpRGBa> &I);
+public:
 
-   //! Acquire one frame in a greyscale image.
-   void acquire(vpImage<unsigned char> &I);
+  // Constructor.
+  vp1394CMUGrabber();
+  // Destructor.
+  virtual ~vp1394CMUGrabber();
 
-   //! Acquire one frame in a color image.
-   void acquire(vpImage<vpRGBa> &I);
+  // Initialization of the grabber using a greyscale image.
+  void open(vpImage<unsigned char> &I);
 
-   //! Set the gain and the shutter values. Call it before open the grabber
-   void setControl(unsigned short gain , unsigned short shutter) ;
+  // Initialization of the grabber using a color image.
+  void open(vpImage<vpRGBa> &I);
 
-   //! Select the camera on the bus. Call it before open the grabber.
-   void selectCamera(int cam_id);
+  // Acquire one frame in a greyscale image.
+  void acquire(vpImage<unsigned char> &I);
 
-   //! Get the video color coding format.
-   vpColorCodingType getVideoColorCoding()
-   {
-     vpColorCodingType color = vp1394CMUGrabber::UNKNOWN;
-     if (_format == 0) 
-     {
-       switch(_mode)
-       {
-       case 0: color = vp1394CMUGrabber::YUV444; break;
-       case 1: color = vp1394CMUGrabber::YUV422; break;
-       case 2: color = vp1394CMUGrabber::YUV411; break;
-       case 3: color = vp1394CMUGrabber::YUV422; break;
-       case 4: color = vp1394CMUGrabber::RGB8; break;
-       case 5: color = vp1394CMUGrabber::MONO8; break;
-       case 6: color = vp1394CMUGrabber::MONO16; break;
-       }
-     }
-     else if (_format == 1)
-     {
-       switch(_mode)
-       {
-       case 0: color = vp1394CMUGrabber::YUV422; break;
-       case 1: color = vp1394CMUGrabber::RGB8; break;
-       case 2: color = vp1394CMUGrabber::MONO8; break;
-       case 3: color = vp1394CMUGrabber::YUV422; break;
-       case 4: color = vp1394CMUGrabber::RGB8; break;
-       case 5: color = vp1394CMUGrabber::MONO8; break;
-       case 6: color = vp1394CMUGrabber::MONO16; break;
-       case 7: color = vp1394CMUGrabber::MONO16; break;
-       }
+  // Acquire one frame in a color image.
+  void acquire(vpImage<vpRGBa> &I);
+
+  // Stop the acquisition.
+  void close();
+
+  // Display information about the camera on the standard output.
+  void displayCameraDescription(int cam_id);
+
+  // Display camera model on the standard output. Call it after open the grabber.
+  void displayCameraModel();
+
+  // Get the number of connected cameras.
+  int getNumberOfConnectedCameras() const ;
+
+  //! Get the video color coding format.
+  vpColorCodingType getVideoColorCoding() const
+  {
+    vpColorCodingType color = vp1394CMUGrabber::UNKNOWN;
+    if (_format == 0)
+    {
+      switch(_mode)
+      {
+        case 0: color = vp1394CMUGrabber::YUV444; break;
+        case 1: color = vp1394CMUGrabber::YUV422; break;
+        case 2: color = vp1394CMUGrabber::YUV411; break;
+        case 3: color = vp1394CMUGrabber::YUV422; break;
+        case 4: color = vp1394CMUGrabber::RGB8; break;
+        case 5: color = vp1394CMUGrabber::MONO8; break;
+        case 6: color = vp1394CMUGrabber::MONO16; break;
+      }
     }
-     else if (_format == 2)
-     {
-       switch(_mode)
-       {
-       case 0: color = vp1394CMUGrabber::YUV422; break;
-       case 1: color = vp1394CMUGrabber::RGB8; break;
-       case 2: color = vp1394CMUGrabber::MONO8; break;
-       case 3: color = vp1394CMUGrabber::YUV422; break;
-       case 4: color = vp1394CMUGrabber::RGB8; break;
-       case 5: color = vp1394CMUGrabber::MONO8; break;
-       case 6: color = vp1394CMUGrabber::MONO16; break;
-       case 7: color = vp1394CMUGrabber::MONO16; break;
-       }
-     }
+    else if (_format == 1)
+    {
+      switch(_mode)
+      {
+        case 0: color = vp1394CMUGrabber::YUV422; break;
+        case 1: color = vp1394CMUGrabber::RGB8; break;
+        case 2: color = vp1394CMUGrabber::MONO8; break;
+        case 3: color = vp1394CMUGrabber::YUV422; break;
+        case 4: color = vp1394CMUGrabber::RGB8; break;
+        case 5: color = vp1394CMUGrabber::MONO8; break;
+        case 6: color = vp1394CMUGrabber::MONO16; break;
+        case 7: color = vp1394CMUGrabber::MONO16; break;
+      }
+    }
+    else if (_format == 2)
+    {
+      switch(_mode)
+      {
+        case 0: color = vp1394CMUGrabber::YUV422; break;
+        case 1: color = vp1394CMUGrabber::RGB8; break;
+        case 2: color = vp1394CMUGrabber::MONO8; break;
+        case 3: color = vp1394CMUGrabber::YUV422; break;
+        case 4: color = vp1394CMUGrabber::RGB8; break;
+        case 5: color = vp1394CMUGrabber::MONO8; break;
+        case 6: color = vp1394CMUGrabber::MONO16; break;
+        case 7: color = vp1394CMUGrabber::MONO16; break;
+      }
+    }
 
-     return color;
+    return color;
 
-   }
-   /*! Set the video format and mode. Call it before open the grabber.
-       See the follow table for futher information.
-  
-    <TABLE BORDER="1">
-	<TR><TH> Format </TH><TH> Mode </TH><TH> (H) x (W) </TH><TH> Color  </TH></TR>
-	<TR><TD>   0    </TD><TD>  0   </TD><TD> 160 x 120 </TD><TD> YUV444 </TD></TR>
-	<TR><TD>   0    </TD><TD>  1   </TD><TD> 320 x 240 </TD><TD> YUV422 </TD></TR>
-	<TR><TD>   0    </TD><TD>  2   </TD><TD> 640 x 480 </TD><TD> YUV411 </TD></TR>
-	<TR><TD>   0    </TD><TD>  3   </TD><TD> 640 x 480 </TD><TD> YUV422 </TD></TR>
-	<TR><TD>   0    </TD><TD>  4   </TD><TD> 640 x 480 </TD><TD>  RGB   </TD></TR>
-	<TR><TD>   0    </TD><TD>  5   </TD><TD> 640 x 480 </TD><TD>  MONO  </TD></TR>
-	<TR><TD>   0    </TD><TD>  6   </TD><TD> 640 x 480 </TD><TD> MONO16 </TD></TR>
-	<TR><TD>   1    </TD><TD>  0   </TD><TD> 800 x 600 </TD><TD> YUV422 </TD></TR>
-	<TR><TD>   1    </TD><TD>  1   </TD><TD> 800 x 600 </TD><TD>  RGB   </TD></TR>
-	<TR><TD>   1    </TD><TD>  2   </TD><TD> 800 x 600 </TD><TD>  MONO  </TD></TR>
-	<TR><TD>   1    </TD><TD>  3   </TD><TD>1024 x 768 </TD><TD> YUV422 </TD></TR>
-	<TR><TD>   1    </TD><TD>  4   </TD><TD>1024 x 768 </TD><TD>  RGB   </TD></TR>
-	<TR><TD>   1    </TD><TD>  5   </TD><TD>1024 x 768 </TD><TD>  MONO  </TD></TR>
-	<TR><TD>   1    </TD><TD>  6   </TD><TD> 800 x 600 </TD><TD> MONO16 </TD></TR>
-	<TR><TD>   1    </TD><TD>  7   </TD><TD>1024 x 768 </TD><TD> MONO16 </TD></TR>
-	<TR><TD>   2    </TD><TD>  0   </TD><TD>1280 x 960 </TD><TD> YUV422 </TD></TR>
-	<TR><TD>   2    </TD><TD>  1   </TD><TD>1280 x 960 </TD><TD>  RGB   </TD></TR>
-	<TR><TD>   2    </TD><TD>  2   </TD><TD>1280 x 960 </TD><TD>  MONO  </TD></TR>
-	<TR><TD>   2    </TD><TD>  3   </TD><TD>1600 x 1200</TD><TD> YUV422 </TD></TR>
-	<TR><TD>   2    </TD><TD>  4   </TD><TD>1600 x 1200</TD><TD>  RGB   </TD></TR>
-	<TR><TD>   2    </TD><TD>  5   </TD><TD>1600 x 1200</TD><TD>  MONO  </TD></TR>
-	<TR><TD>   2    </TD><TD>  6   </TD><TD>1280 x 960 </TD><TD> MONO16 </TD></TR>
-	<TR><TD>   2    </TD><TD>  7   </TD><TD>1600 x 1200</TD><TD> MONO16 </TD></TR>
-	</TABLE> 
+  }
 
-   */
-   void setVideoMode(unsigned long format , unsigned long mode );
+  // Select the camera on the bus. Call it before open the grabber.
+  void selectCamera(int cam_id);
 
-   /*! Set the frame rate. Call it before open the grabber. 
-       See the follow table for futher information.
+  // Set the gain and the shutter values. Call it before open the grabber
+  void setControl(unsigned short gain , unsigned short shutter) ;
 
-   <TABLE BORDER="1">
-   <TR><TH> Value </TH>  <TH> Frame rate </TH></TR>
-   <TR><TD>   0   </TD>  <TD>  1.875 fps </TD></TR>
-   <TR><TD>   1   </TD>  <TD>   3.75 fps </TD></TR>
-   <TR><TD>   2   </TD>  <TD>    7.5 fps </TD></TR>
-   <TR><TD>   3   </TD>  <TD>     15 fps </TD></TR>
-   <TR><TD>   4   </TD>  <TD>     30 fps </TD></TR>
-   <TR><TD>   5   </TD>  <TD>     60 fps </TD></TR>
-   <TR><TD>   6   </TD>  <TD>    120 fps </TD></TR>
-   <TR><TD>   7   </TD>  <TD>    240 fps </TD></TR>
-   </TABLE> 
+  // Set the frame rate. Call it before open the grabber.
+  void setFPS ( unsigned long fps );
 
-   */
-   void setFPS ( unsigned long fps );
+  // Set the video format and mode. Call it before open the grabber.
+  void setVideoMode(unsigned long format , unsigned long mode );
 
-   //! Get the number of connected cameras.
-   int getNumberOfConnectedCameras();
+private :
 
-   //! Display information about the camera on the standard output.
-   void displayCameraDescription(int cam_id);
-   
-   //! Display camera model on the standard output. Call it after open the grabber.
-   void displayCameraModel();
+  void initCamera();
 
-   //! Stop the acquisition.
-   void close();
-   
- private : 
-
-   void initCamera();
-   
 };
 
 #endif
