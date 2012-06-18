adddb392f6371481eb7aa2742e2c2e0011a5e1f5
Fabien Spindler <Fabien.Spindler@inria.fr>
2012-06-18 13:32:35 +0000

    Fix compatibility with OpenCV 2.4.1

--- src/key-point/vpFernClassifier.h
+++ src/key-point/vpFernClassifier.h
@@ -48,13 +48,10 @@
 #include <string>
 
 #if (VISP_HAVE_OPENCV_VERSION >= 0x020000) // Require opencv >= 2.0.0
-#if (VISP_HAVE_OPENCV_VERSION >= 0x020400) // Require opencv >= 2.4.0
+#if (VISP_HAVE_OPENCV_VERSION >= 0x020101) // Require opencv >= 2.1.1
 #  include <opencv2/imgproc/imgproc.hpp>
 #  include <opencv2/features2d/features2d.hpp>
 #  include <opencv2/legacy/legacy.hpp>
-#elif (VISP_HAVE_OPENCV_VERSION >= 0x020101) // Require opencv >= 2.1.1
-#  include <opencv2/imgproc/imgproc.hpp>
-#  include <opencv2/features2d/features2d.hpp>
 #elif (VISP_HAVE_OPENCV_VERSION >= 0x020000) // Require opencv >= 2.0.0
 #  include <cv.h>
 #  include <cvaux.hpp>
