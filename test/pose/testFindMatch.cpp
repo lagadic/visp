9647450a355c41810df125ab192b24dab273ee28
Fabien Spindler <Fabien.Spindler@inria.fr>
2012-09-18 06:59:08 +0000

    Returns now 1 if failure, 0 if succeed

--- test/pose/testFindMatch.cpp
+++ test/pose/testFindMatch.cpp
@@ -4,7 +4,7 @@
  *
  * This file is part of the ViSP software.
  * Copyright (C) 2005 - 2012 by INRIA. All rights reserved.
- * 
+ *
  * This software is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * ("GPL") version 2 as published by the Free Software Foundation.
@@ -12,11 +12,11 @@
  * distribution for additional information about the GNU GPL.
  *
  * For using ViSP with software that can not be combined with the GNU
- * GPL, please contact INRIA about acquiring a ViSP Professional 
+ * GPL, please contact INRIA about acquiring a ViSP Professional
  * Edition License.
  *
  * See http://www.irisa.fr/lagadic/visp/visp.html for more information.
- * 
+ *
  * This software was developed at:
  * INRIA Rennes - Bretagne Atlantique
  * Campus Universitaire de Beaulieu
@@ -26,7 +26,7 @@
  *
  * If you have questions regarding the use of this file, please contact
  * INRIA at visp@inria.fr
- * 
+ *
  * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
  * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
@@ -62,58 +62,72 @@
 int
 main()
 {
+
+  std::cout << "Find Matches using Ransac" << std::endl;
+  unsigned int nb3D = 5;
+  unsigned int nb2D = 8;
+  std::vector<vpPoint> P(nb3D);
+  std::vector<vpPoint> p(nb2D);
+
+  P[0].setWorldCoordinates(-L,-L, 0 ) ;
+  P[1].setWorldCoordinates(L,-L, 0 ) ;
+  P[2].setWorldCoordinates(L,L, 0 ) ;
+  P[3].setWorldCoordinates(-L,L, 0 ) ;
+  P[4].setWorldCoordinates(-0,0, L ) ;
+
+  vpHomogeneousMatrix cMo_ref(0,0.2,1,vpMath::rad(0),0,0) ;
+
+  for(unsigned int i=0 ; i < nb3D ; i++)
   {
-    std::cout << "Find Matches using Ransac" << std::endl;
-    unsigned int nb3D = 4;
-    unsigned int nb2D = 8;
-    std::vector<vpPoint> P(nb3D);
-    std::vector<vpPoint> p(nb2D);
-
-    P[0].setWorldCoordinates(-L,-L, 0 ) ;
-    P[1].setWorldCoordinates(L,-L, 0 ) ;
-    P[2].setWorldCoordinates(L,L, 0 ) ;
-    P[3].setWorldCoordinates(-L,L, 0 ) ;
-    //P[4].setWorldCoordinates(-0,0, L ) ; //ERREUR DANS LAGRANGE ET DEMENTHON
-
-    vpHomogeneousMatrix cMo_ref(0,0.2,1,vpMath::rad(0),0,0) ;
-    
-    for(unsigned int i=0 ; i < nb3D ; i++)
-    {
-      vpPoint pt = P[i];
-      pt.project(cMo_ref);
-      p[i].set_x(pt.get_x());
-      p[i].set_y(pt.get_y());
-    }
-
-    p[4].set_x(0.02) ;
-    p[4].set_y(0.05) ;
-    
-    p[5].set_x(0.02) ;
-    p[5].set_y(-0.05) ;
-    
-    p[6].set_x(0.07) ;
-    p[6].set_y(-0.05) ;
-    
-    p[7].set_x(0.24) ;
-    p[7].set_y(0.07) ;
-
-    unsigned int ninliers ;
-    std::vector<vpPoint> inliers;
-    double threshold = 1e-6;
-    unsigned int nbInlierToReachConsensus = 4;
-    
-    vpHomogeneousMatrix cMo ;
-    
-    vpPose::findMatch(p,P,nbInlierToReachConsensus,threshold,ninliers,inliers,cMo);
-    
-    std::cout << "Inliers: " << std::endl;
-    for (unsigned int i = 0; i < inliers.size() ; i++)
-    {
-      inliers[i].print() ;
-      std::cout << std::endl;
-    }
-    
-    std::cout << "cMo :\n" << vpPoseVector(cMo).t() << std::endl << std::endl;
-
+    vpPoint pt = P[i];
+    pt.project(cMo_ref);
+    p[i].set_x(pt.get_x());
+    p[i].set_y(pt.get_y());
   }
+
+  p[4].set_x(0.02) ;
+  p[4].set_y(0.05) ;
+
+  p[5].set_x(0.02) ;
+  p[5].set_y(-0.05) ;
+
+  p[6].set_x(0.07) ;
+  p[6].set_y(-0.05) ;
+
+  p[7].set_x(0.24) ;
+  p[7].set_y(0.07) ;
+
+  unsigned int ninliers ;
+  std::vector<vpPoint> inliers;
+  double threshold = 1e-6;
+  unsigned int nbInlierToReachConsensus = 4;
+
+  vpHomogeneousMatrix cMo ;
+
+  vpPose::findMatch(p,P,nbInlierToReachConsensus,threshold,ninliers,inliers,cMo);
+
+  std::cout << "Inliers: " << std::endl;
+  for (unsigned int i = 0; i < inliers.size() ; i++)
+  {
+    inliers[i].print() ;
+    std::cout << std::endl;
+  }
+
+  std::cout << "cMo :\n" << vpPoseVector(cMo).t() << std::endl << std::endl;
+
+  vpPoseVector pose_ref = vpPoseVector(cMo_ref);
+  vpPoseVector pose_est = vpPoseVector(cMo);
+
+  std::cout << std::endl;
+  std::cout << "reference cMo :\n" << pose_ref.t() << std::endl << std::endl;
+  std::cout << "estimated cMo :\n" << pose_est.t() << std::endl << std::endl;
+
+  int test_fail = 0;
+  for(int i=0; i<6; i++) {
+    if (std::fabs(pose_ref[i]-pose_est[i]) > 0.001)
+      test_fail = 1;
+  }
+
+  std::cout << "Matching is " << (test_fail ? "badly" : "well") << " performed" << std::endl;
+  return test_fail;
 }
