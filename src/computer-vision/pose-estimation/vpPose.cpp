d5f9e4946b74036faabc2b7798ee2e079d662f75
Fabien Spindler <Fabien.Spindler@inria.fr>
2012-10-03 06:23:15 +0000

    Fix warnings with MinGW

--- src/computer-vision/pose-estimation/vpPose.cpp
+++ src/computer-vision/pose-estimation/vpPose.cpp
@@ -194,7 +194,7 @@ vpPose::coplanar(int &coplanar_plane_type)
 
   if (npt ==3) return true ;
 
-  double x1,x2,x3,y1,y2,y3,z1,z2,z3 ;
+  double x1=0,x2=0,x3=0,y1=0,y2=0,y3=0,z1=0,z2=0,z3=0 ;
 
   std::list<vpPoint>::const_iterator it = listP.begin();
 
