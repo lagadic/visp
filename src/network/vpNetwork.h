58ad89abc30fbbe5894550256220ae328250f746
Fabien Spindler <Fabien.Spindler@inria.fr>
2013-10-28 14:23:16 +0000

    Revert the previous commit done on OSX that breaks under Ubuntu.

--- src/network/vpNetwork.h
+++ src/network/vpNetwork.h
@@ -107,7 +107,6 @@ protected:
       emitterAddress.sin_family = AF_INET;
       emitterAddress.sin_addr.s_addr = INADDR_ANY;
       emitterAddress.sin_port = 0;
-      emitterAddress.sin_len = 0;
       socketFileDescriptorEmitter = 0;
     }
   };
