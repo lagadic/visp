0a192435d44762ebbf79f1aca51889cd8c136d1d
Manikandan Bakthavatchalam <manikandan.bakthavatchalam@inria.fr>
2014-03-28 17:41:47 +0000

    In operator <<, VISP_EXPORT added and output modified.printDependencies() introduced

--- src/tracking/moments/vpMomentBasic.cpp
+++ src/tracking/moments/vpMomentBasic.cpp
@@ -81,9 +81,18 @@ void vpMomentBasic::compute(){
   Outputs the moment's values to a stream.
   Same output as in vpMomentObject.
 */
-std::ostream & operator<<(std::ostream & os, vpMomentBasic& m){
-
-    os << m.getObject();
-    
+VISP_EXPORT std::ostream& operator<<(std::ostream & os, const vpMomentBasic& m){
+    os << (__FILE__) << std::endl;
+    vpMomentObject::printWithIndices(m.getObject(), os);
     return os;
+}
+
+/*!
+No dependencies on other vpMoments, since basic moments are computed in vpMomentObject
+Just prints the basic moments in vpMomentObject with indices
+*/
+void vpMomentBasic::printDependencies(std::ostream& os) const{
+    os << (__FILE__) << std::endl;
+    os << "No dependencies on other vpMoments, since basic moments are computed in vpMomentObject" << std::endl;
+    vpMomentObject::printWithIndices(getObject(), os);
 }
