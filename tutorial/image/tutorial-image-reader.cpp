8dccf1540ab86dd0678b1f4a0d223c2802af867d
Fabien Spindler <Fabien.Spindler@inria.fr>
2014-07-08 08:34:38 +0000

    Match the name of the jpeg file that is provided with the tuto

--- tutorial/image/tutorial-image-reader.cpp
+++ tutorial/image/tutorial-image-reader.cpp
@@ -5,7 +5,7 @@ int main()
 {
   try {
     vpImage<vpRGBa> I;
-    vpImageIo::read(I, "lena.jpg");
+    vpImageIo::read(I, "lena.jpeg");
     vpImageIo::write(I, "lena.png");
   }
   catch(vpException e) {
