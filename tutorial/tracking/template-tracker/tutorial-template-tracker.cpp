8d638ab1af2106826d3dd52f78286e510dc1f696
Fabien Spindler <Fabien.Spindler@inria.fr>
2014-12-16 11:06:21 +0000

    Modify the tutorial code to use a video as input Improve the doc

--- tutorial/tracking/template-tracker/tutorial-template-tracker.cpp
+++ tutorial/tracking/template-tracker/tutorial-template-tracker.cpp
@@ -1,51 +1,87 @@
 /*! \example tutorial-template-tracker.cpp */
 #include <visp/vpDisplayGDI.h>
 #include <visp/vpDisplayX.h>
-#include <visp/vpImageIo.h>
+#include <visp/vpVideoReader.h>
+//! [Include]
 #include <visp/vpTemplateTrackerSSDInverseCompositional.h>
 #include <visp/vpTemplateTrackerWarpHomography.h>
+//! [Include]
 
-int main()
+int main(int argc, char** argv)
 {
-#if defined(VISP_HAVE_X11) || defined(VISP_HAVE_GDI)
+#if defined(VISP_HAVE_OPENCV) && (VISP_HAVE_OPENCV_VERSION >= 0x020100) || defined(VISP_HAVE_FFMPEG)
+  std::string videoname = "bruegel.mpg";
+
+  for (int i=0; i<argc; i++) {
+    if (std::string(argv[i]) == "--videoname")
+      videoname = std::string(argv[i+1]);
+    else if (std::string(argv[i]) == "--help") {
+      std::cout << "\nUsage: " << argv[0] << " [--name <video name>] [--help]\n" << std::endl;
+      return 0;
+    }
+  }
+
+  std::cout << "Video name: " << videoname << std::endl;
+
   vpImage<unsigned char> I;
 
-  vpImageIo::readPGM(I, "pattern-template-tracker.pgm");
+  vpVideoReader g;
+  g.setFileName(videoname);
+  g.open(I);
 
-#ifdef VISP_HAVE_X11
+#if defined(VISP_HAVE_X11)
   vpDisplayX display;
-#else
+#elif defined(VISP_HAVE_GDI)
   vpDisplayGDI display;
+#elif defined(VISP_HAVE_OPENCV)
+  vpDisplayOpenCV display;
+#else
+  std::cout << "No image viewer is available..." << std::endl;
 #endif
 
   display.init(I, 100, 100, "Template tracker");
   vpDisplay::display(I);
   vpDisplay::flush(I);
 
+  //! [Construction]
   vpTemplateTrackerWarpHomography warp;
-
   vpTemplateTrackerSSDInverseCompositional tracker(&warp);
+  //! [Construction]
 
-  tracker.setSampling(2,2);
+  tracker.setSampling(2, 2);
   tracker.setLambda(0.001);
   tracker.setIterationMax(200);
   tracker.setPyramidal(2, 1);
 
+  //! [Init]
   tracker.initClick(I);
+  //! [Init]
 
   while(1){
+    g.acquire(I);
     vpDisplay::display(I);
+
+    //! [Track]
     tracker.track(I);
+    //! [Track]
 
+    //! [Homography]
     vpColVector p = tracker.getp();
+    vpHomography H = warp.getHomography(p);
+    std::cout << "Homography: \n" << H << std::endl;
+    //! [Homography]
 
+    //! [Display]
     tracker.display(I, vpColor::red);
+    //! [Display]
 
     if (vpDisplay::getClick(I, false))
       break;
 
     vpDisplay::flush(I);
-    vpTime::wait(40);
   }
+#else
+  (void)argc;
+  (void)argv;
 #endif
 }
