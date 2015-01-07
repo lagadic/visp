b9d113872f52247d7a0ac0ce7edc81745bee1215
Fabien Spindler <Fabien.Spindler@inria.fr>
2015-01-07 15:52:51 +0000

    Introduce usage of v4l2 grabber

--- tutorial/grabber/tutorial-video-recorder.cpp
+++ tutorial/grabber/tutorial-video-recorder.cpp
@@ -1,7 +1,11 @@
 /*! \example tutorial-video-recorder.cpp */
+#include <visp/vpDisplayGDI.h>
+#include <visp/vpDisplayGTK.h>
 #include <visp/vpDisplayOpenCV.h>
+#include <visp/vpDisplayX.h>
 #include <visp/vpTime.h>
 #include <visp/vpVideoWriter.h>
+#include <visp/vpV4l2Grabber.h>
 
 /*!
  This example allows to record a video from a camera.
@@ -9,42 +13,60 @@
 
  Example: ./tutorial-video-recorder --device 0 --name myvideo.mpeg
  */
-int main(int argc, char** argv)
+int main(int argc, const char *argv[])
 {
-#if (VISP_HAVE_OPENCV_VERSION >= 0x020100)
-  std::string videoname = "video-recorded.mpg";
-  int device = 0;
+#if ((defined(VISP_HAVE_V4L2) || (VISP_HAVE_OPENCV_VERSION >= 0x020100)) && (defined(VISP_HAVE_X11) || defined(VISP_HAVE_GDI) || defined(VISP_HAVE_OPENCV) || defined(VISP_HAVE_GTK)))
+  std::string opt_videoname = "video-recorded.mpg";
+  int opt_device = 0;
 
   for (int i=0; i<argc; i++) {
     if (std::string(argv[i]) == "--device")
-      device = atoi(argv[i+1]);
+      opt_device = atoi(argv[i+1]);
     else if (std::string(argv[i]) == "--name")
-      videoname = std::string(argv[i+1]);
+      opt_videoname = std::string(argv[i+1]);
     else if (std::string(argv[i]) == "--help") {
       std::cout << "\nUsage: " << argv[0] << " [--device <device number>] [--name <video name>] [--help]\n" << std::endl;
       return 0;
     }
   }
 
-  std::cout << "Use device: " << device << std::endl;
-  std::cout << "Record video in: " << videoname << std::endl;
+  std::cout << "Use device: " << opt_device << std::endl;
+  std::cout << "Record video in: " << opt_videoname << std::endl;
 
   try {
-    cv::VideoCapture cap(device); // open the default camera
-    if(!cap.isOpened()) { // check if we succeeded
+    //vpImage<vpRGBa> I; // for color images
+    vpImage<unsigned char> I; // for gray images
+
+#if defined(VISP_HAVE_V4L2)
+    vpV4l2Grabber g;
+    std::ostringstream device;
+    device << "/dev/video" << opt_device;
+    g.setDevice(device.str());
+    g.open(I);
+    g.acquire(I);
+#elif defined(VISP_HAVE_OPENCV)
+    cv::VideoCapture g(opt_device);
+    if(!g.isOpened()) { // check if we succeeded
       std::cout << "Failed to open the camera" << std::endl;
       return -1;
     }
     cv::Mat frame;
-    cap >> frame; // get a new frame from camera
-    std::cout << "Image size: " << frame.rows << " " << frame.cols << std::endl;
-
-    //vpImage<vpRGBa> I; // for color images
-    vpImage<unsigned char> I; // for gray images
+    g >> frame; // get a new frame from camera
     vpImageConvert::convert(frame, I);
+#endif
 
-    vpDisplayOpenCV d(I);
+    std::cout << "Image size: " << I.getWidth() << " " << I.getHeight() << std::endl;
 
+#if defined(VISP_HAVE_X11)
+    vpDisplayX d;
+#elif defined(VISP_HAVE_GDI)
+    vpDisplayGDI d;
+#elif defined(VISP_HAVE_OPENCV)
+    vpDisplayOpenCV d;
+#elif defined(VISP_HAVE_GTK)
+    vpDisplayGTK d;
+#endif
+    d.init(I, 0, 0, "Camera view");
     vpVideoWriter writer;
 
 #ifdef VISP_HAVE_FFMPEG
@@ -61,14 +83,17 @@ int main(int argc, char** argv)
 #elif VISP_HAVE_OPENCV_VERSION >= 0x020100
     writer.setCodec( CV_FOURCC('P','I','M','1') );
 #endif
-    writer.setFileName(videoname);
+    writer.setFileName(opt_videoname);
     writer.open(I);
     bool recording = false;
 
     for(;;) {
-      cap >> frame; // get a new frame from camera
-      // Convert the image in ViSP format and display it
+#if defined(VISP_HAVE_V4L2)
+      g.acquire(I);
+#elif defined(VISP_HAVE_OPENCV)
+      g >> frame;
       vpImageConvert::convert(frame, I);
+#endif
       vpDisplay::display(I);
       if (recording == false) {
         vpDisplay::displayText(I, 10, 10, "A click to start recording", vpColor::green);
@@ -84,7 +109,7 @@ int main(int argc, char** argv)
 
       vpDisplay::flush(I);
     }
-    std::cout << "The video was recorded in \"" << videoname << "\"" << std::endl;
+    std::cout << "The video was recorded in \"" << opt_videoname << "\"" << std::endl;
   }
   catch(vpException e) {
     std::cout << "Catch an exception: " << e << std::endl;
