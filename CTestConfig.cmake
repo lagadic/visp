e3ce0188a257dd6047f7939df4c46ab865c97cf6
Fabien Spindler <Fabien.Spindler@inria.fr>
2015-06-26 16:03:04 +0000

    Send test to CDash ViSP-3 new project

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -43,7 +43,7 @@ set(CTEST_NIGHTLY_START_TIME "00:00:00 GMT")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.inria.fr")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=ViSP")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=ViSP-3")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 #--------------------------------------------------------------------
