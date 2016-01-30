bf6d85c7288c9116926cb2e38932a47fd55d2c06
Fabien Spindler <Fabien.Spindler@inria.fr>
2016-01-30 09:16:44 +0100

    Fix build issue in vpMath.h (Closes: #47)

--- modules/core/include/visp3/core/vpMath.h
+++ modules/core/include/visp3/core/vpMath.h
@@ -119,8 +119,7 @@ class VISP_EXPORT vpMath
   static inline int round(const double x) ;
 
   //   return the sign of x (+-1)
-  static inline int sign(double x) ;
-
+  static inline int (sign)(double x) ;
 
   // test if a number equals 0 (with threshold value)
   static inline bool nul(double x, double s=0.001);
@@ -263,7 +262,7 @@ int vpMath::round(const double x)
   \return -1 if x is negative, +1 if positive and 0 if zero.
 
 */
-int vpMath::sign(double x)
+int (vpMath::sign)(double x)
 {
   if (fabs(x) < std::numeric_limits<double>::epsilon())
     return 0;
