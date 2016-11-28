78e9f3249977ea371ab29c52e122b494208dbda4
Fabien Spindler <Fabien.Spindler@inria.fr>
2016-11-28 12:06:20 +0100

    Speed up visual-servo loop

--- modules/vs/test/visual-feature/testFeatureSegment.cpp
+++ modules/vs/test/visual-feature/testFeatureSegment.cpp
@@ -125,7 +125,7 @@ int main(int argc, const char **argv)
 #endif
 
     vpHomogeneousMatrix wMo; // Set to indentity. Robot world frame is equal to object frame
-    vpHomogeneousMatrix cMo (-0.5, 0.5, 4., vpMath::rad(10), vpMath::rad(20), vpMath::rad(90));
+    vpHomogeneousMatrix cMo (-0.5, 0.5, 2., vpMath::rad(10), vpMath::rad(20), vpMath::rad(30));
     vpHomogeneousMatrix cdMo(0., 0., 1., vpMath::rad(0), vpMath::rad(0), vpMath::rad(0));
     vpHomogeneousMatrix wMc; // Camera location in the robot world frame
 
@@ -167,7 +167,7 @@ int main(int argc, const char **argv)
     vpServo task;
     task.setServo(vpServo::EYEINHAND_CAMERA);
     task.setInteractionMatrixType(vpServo::CURRENT);
-    task.setLambda(1) ;
+    task.setLambda(2.) ;
 
     for (int i=0; i <2; i++)
       task.addFeature(seg_cur[i], seg_des[i]);
@@ -200,8 +200,10 @@ int main(int argc, const char **argv)
 
     //param robot
     vpSimulatorCamera robot;
-    float sampling_time = 0.010f; // Sampling period in seconds
+    float sampling_time = 0.02f; // Sampling period in seconds
     robot.setSamplingTime(sampling_time);
+    robot.setMaxTranslationVelocity(5.);
+    robot.setMaxRotationVelocity(vpMath::rad(90.));
     wMc = wMo * cMo.inverse();
     robot.setPosition(wMc);
     int iter=0;
