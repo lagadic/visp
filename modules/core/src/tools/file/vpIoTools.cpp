lagadic/visp

commit 9d6c9e88536997479dd3d0e29e807a92841f55e2

Introduce compatibility with Universal Windows Platform (UWP), Windows Phone and Windows Store

modules/core/src/tools/file/vpIoTools.cpp
@@ -129,19 +129,20 @@ vpIoTools::getUserName(std::string &username)
   }
   username = _username;
 #elif defined(_WIN32)
+#  if ( ! defined(WINAPI_FAMILY) ) || ( defined(WINAPI_FAMILY) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
   unsigned int info_buffer_size = 1024;
   TCHAR  *infoBuf = new TCHAR [info_buffer_size];
   DWORD  bufCharCount = (DWORD) info_buffer_size;
   // Get the user name.
   if( ! GetUserName( infoBuf, &bufCharCount ) ) {
     delete [] infoBuf;
-    vpERROR_TRACE( "Cannot get the username" );
-    throw(vpIoException(vpIoException::cantGetUserName,
-			"Cannot get the username")) ;
-
+    throw(vpIoException(vpIoException::cantGetUserName, "Cannot get the username")) ;
   }
   username = infoBuf;
   delete [] infoBuf;
+#  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
+	throw(vpIoException(vpIoException::cantGetUserName, "Cannot get the username: not implemented on Universal Windows Platform"));
+#  endif
 #endif
 }
 /*!
@@ -176,6 +177,7 @@ vpIoTools::getUserName()
   }
   username = _username;
 #elif defined(_WIN32)
+#  if ( ! defined(WINAPI_FAMILY) ) || ( defined(WINAPI_FAMILY) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
   unsigned int info_buffer_size = 1024;
   TCHAR  *infoBuf = new TCHAR [info_buffer_size];
   DWORD  bufCharCount = (DWORD) info_buffer_size;
@@ -189,6 +191,9 @@ vpIoTools::getUserName()
   }
   username = infoBuf;
   delete [] infoBuf;
+#  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
+  throw(vpIoException(vpIoException::cantGetUserName, "Cannot get the username: not implemented on Universal Windows Platform"));
+#  endif
 #endif
   return username;
 }
@@ -227,18 +232,21 @@ int main()
 std::string
 vpIoTools::getenv(const char *env)
 {
+#if defined(_WIN32) && ( defined(WINAPI_FAMILY) && ! WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
+  throw(vpIoException(vpIoException::cantGetenv, "Cannot get the environment variable value: not implemented on Universal Windows Platform"));
+#else
   std::string value;
   // Get the environment variable value.
   char *_value = NULL;
   _value = ::getenv(env);
   if (_value == NULL) {
-    vpERROR_TRACE( "Cannot get the environment variable value" );
     throw(vpIoException(vpIoException::cantGetenv,
 			"Cannot get the environment variable value")) ;
   }
   value = _value;
 
   return value;
+#endif
 }
 
 /*!
@@ -554,35 +562,54 @@ vpIoTools::checkFilename(const std::string &filename)
 bool
 vpIoTools::copy(const char *src, const char *dst)
 {
-  char cmd[FILENAME_MAX];
-  int ret;
   // Check if we have to consider a file or a directory
   if ( vpIoTools::checkFilename(src) ) {
     //std::cout << "copy file: " << src << " in " << dst << std::endl;
 #if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))) // UNIX
-    sprintf(cmd, "cp -p %s %s", src, dst);
+	char cmd[FILENAME_MAX];
+	int ret;
+	sprintf(cmd, "cp -p %s %s", src, dst);
+	ret = system(cmd);
+	//std::cout << cmd << " return value: " << ret << std::endl;
+	return true;
 #elif defined(_WIN32)
+#  if ( ! defined(WINAPI_FAMILY) ) || ( defined(WINAPI_FAMILY) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
+	char cmd[FILENAME_MAX];
+	int ret;
 	std::string src_ = vpIoTools::path(src);
 	std::string dst_ = vpIoTools::path(dst);
     sprintf(cmd, "copy %s %s", src_.c_str(), dst_.c_str());
+	ret = system(cmd);
+	//std::cout << cmd << " return value: " << ret << std::endl;
+	return true;
+#  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
+	throw(vpIoException(vpException::fatalError, "Cannot copy %s in %s: not implemented on Universal Windows Platform", src, dst));
+#  endif
 #endif
-    ret = system( cmd );
-    //std::cout << cmd << " return value: " << ret << std::endl;
-    return true;
   }
   else if ( vpIoTools::checkDirectory(src) ) {
     //std::cout << "copy directory: " << src << " in " << dst << std::endl;
 #if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))) // UNIX
     sprintf(cmd, "cp -p -r %s %s", src, dst);
+	ret = system(cmd);
+	if (ret) {}; // to avoid a warning
+				 //std::cout << cmd << " return value: " << ret << std::endl;
+	return true;
 #elif defined(_WIN32)
+#  if ( ! defined(WINAPI_FAMILY) ) || ( defined(WINAPI_FAMILY) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
+	char cmd[FILENAME_MAX];
+	int ret;
 	std::string src_ = vpIoTools::path(src);
 	std::string dst_ = vpIoTools::path(dst);
     sprintf(cmd, "copy %s %s", src_.c_str(), dst_.c_str());
+	ret = system(cmd);
+	if (ret) {}; // to avoid a warning
+				 //std::cout << cmd << " return value: " << ret << std::endl;
+	return true;
+#  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
+	throw(vpIoException(vpException::fatalError, "Cannot copy %s in %s: not implemented on Universal Windows Platform", src, dst));
+#  endif
 #endif
-    ret = system( cmd );
-    if(ret) {}; // to avoid a warning
-    //std::cout << cmd << " return value: " << ret << std::endl;
-    return true;
   }
   else {
     std::cout << "Cannot copy: " << src << " in " << dst << std::endl;
@@ -630,17 +657,26 @@ vpIoTools::remove(const char *file_or_dir)
   }
   else if ( vpIoTools::checkDirectory(file_or_dir) ) {
     //std::cout << "remove directory: " << file_or_dir << std::endl;
-    char cmd[FILENAME_MAX];
 #if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))) // UNIX
-    sprintf(cmd, "rm -rf %s", file_or_dir);
+	char cmd[FILENAME_MAX];
+	sprintf(cmd, "rm -rf %s", file_or_dir);
+	int ret = system(cmd);
+	if (ret) {}; // to avoid a warning
+	//std::cout << cmd << " return value: " << ret << std::endl;
+	return true;
 #elif defined(_WIN32)
+#  if ( ! defined(WINAPI_FAMILY) ) || ( defined(WINAPI_FAMILY) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
+	char cmd[FILENAME_MAX];  
 	std::string file_or_dir_ = vpIoTools::path(file_or_dir);
     sprintf(cmd, "rmdir /S /Q %s", file_or_dir_.c_str());
-#endif
-    int ret = system( cmd );
-    if(ret) {}; // to avoid a warning
+	int ret = system(cmd);
+	if (ret) {}; // to avoid a warning
     //std::cout << cmd << " return value: " << ret << std::endl;
-    return true;
+	return true;
+#  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
+	throw(vpIoException(vpException::fatalError, "Cannot remove %s: not implemented on Universal Windows Platform", file_or_dir));
+#  endif
+#endif
   }
   else {
     std::cout << "Cannot remove: " << file_or_dir << std::endl;
@@ -1284,18 +1320,21 @@ std::string vpIoTools::getAbsolutePathname(const std::string &pathname) {
     real_path_str = real_path;
     free(real_path);
   }
+  return real_path_str;
 #elif defined(_WIN32)
-#define BUFSIZE 4096
+#  if ( ! defined(WINAPI_FAMILY) ) || ( defined(WINAPI_FAMILY) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
   DWORD retval = 0;
-  TCHAR buffer[BUFSIZE] = TEXT("");
+  TCHAR buffer[4096] = TEXT("");
 
   retval = GetFullPathName(pathname.c_str(), BUFSIZE, buffer, 0);
   if (retval != 0) {
     real_path_str = buffer;
   }
-#endif
-
   return real_path_str;
+#  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
+  throw(vpIoException(vpException::fatalError, "Cannot get absolute path of %s: not implemented on Universal Windows Platform", pathname.c_str()));
+#  endif
+#endif
 }
 
 /*!

CMakeLists.txt
@@ -46,6 +46,14 @@ else()
   set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/install" CACHE PATH "Installation Directory")
 endif()
 
+if(CMAKE_SYSTEM_NAME MATCHES WindowsPhone OR CMAKE_SYSTEM_NAME MATCHES WindowsStore)
+  set(WINRT TRUE)
+endif(CMAKE_SYSTEM_NAME MATCHES WindowsPhone OR CMAKE_SYSTEM_NAME MATCHES WindowsStore)
+
+if(WINRT)
+  add_definitions(-DWINRT)
+endif()
+
 project(VISP C CXX)
 
 # Detect if the toolchain is for Aldebaran naoqi
@@ -237,33 +245,33 @@ endif()
 
 find_host_program(XRANDR xrandr)
 
-VP_OPTION(USE_AFMA4    "" "" "Include Afma4 robot support"      "" ON IF (RAW1394_FOUND AND RT_FOUND AND CALINUX_FOUND AND IRISA_FOUND))
-VP_OPTION(USE_AFMA6    "" "" "Include Afma6 robot support"      "" ON IF (RAW1394_FOUND AND RT_FOUND AND CALINUX_FOUND AND IRISA_FOUND))
-VP_OPTION(USE_VIPER650 "" "" "Include Viper s650 robot support" "" ON IF (RAW1394_FOUND AND RT_FOUND AND CALINUX_FOUND AND IRISA_FOUND))
-VP_OPTION(USE_VIPER850 "" "" "Include Viper s850 robot support" "" ON IF (RAW1394_FOUND AND RT_FOUND AND CALINUX_FOUND AND IRISA_FOUND))
-VP_OPTION(USE_VIRTUOSE    Virtuose    "" "Include Virtuose SDK support for Haption devices" "" ON)
-VP_OPTION(USE_DC1394      DC1394      "" "Include dc1394 support"          "" ON IF UNIX)
-VP_OPTION(USE_V4L2        V4L2        "" "Include v4l2 support"            "" ON IF UNIX)
-VP_OPTION(USE_FLYCAPTURE  FlyCapture  "" "Include FlyCapture SDK support for PointGrey cameras" "" ON)
-VP_OPTION(USE_COMEDI      Comedi      "" "Include comedi (linux control and measurement device interface) support" "" ON)
-VP_OPTION(USE_BICLOPS     BICLOPS     "" "Include biclops support"         "" ON)
-VP_OPTION(USE_PTU46       PTU46       "" "Include ptu-46 support"          "" ON IF UNIX)
-VP_OPTION(USE_CMU1394     CMU1394     "" "Include cmu1494 support"         "" ON IF WIN32)
-VP_OPTION(USE_GDI         GDI         "" "Include gdi support"             "" ON IF WIN32)
-VP_OPTION(USE_DIRECT3D    DIRECT3D    "" "Include d3d support"             "" ON IF WIN32)
-VP_OPTION(USE_DIRECTSHOW  DIRECTSHOW  "" "Include dshow support"           "" ON IF WIN32)
+VP_OPTION(USE_AFMA4    "" "" "Include Afma4 robot support"      "" ON IF (RAW1394_FOUND AND RT_FOUND AND CALINUX_FOUND AND IRISA_FOUND AND NOT WINRT))
+VP_OPTION(USE_AFMA6    "" "" "Include Afma6 robot support"      "" ON IF (RAW1394_FOUND AND RT_FOUND AND CALINUX_FOUND AND IRISA_FOUND AND NOT WINRT))
+VP_OPTION(USE_VIPER650 "" "" "Include Viper s650 robot support" "" ON IF (RAW1394_FOUND AND RT_FOUND AND CALINUX_FOUND AND IRISA_FOUND AND NOT WINRT))
+VP_OPTION(USE_VIPER850 "" "" "Include Viper s850 robot support" "" ON IF (RAW1394_FOUND AND RT_FOUND AND CALINUX_FOUND AND IRISA_FOUND AND NOT WINRT))
+VP_OPTION(USE_VIRTUOSE    Virtuose    "" "Include Virtuose SDK support for Haption devices" "" ON IF NOT WINRT)
+VP_OPTION(USE_DC1394      DC1394      "" "Include dc1394 support"          "" ON IF UNIX AND NOT WINRT)
+VP_OPTION(USE_V4L2        V4L2        "" "Include v4l2 support"            "" ON IF UNIX AND NOT WINRT)
+VP_OPTION(USE_FLYCAPTURE  FlyCapture  "" "Include FlyCapture SDK support for PointGrey cameras" "" ON IF NOT WINRT)
+VP_OPTION(USE_COMEDI      Comedi      "" "Include comedi (linux control and measurement device interface) support" "" ON IF NOT WINRT)
+VP_OPTION(USE_BICLOPS     BICLOPS     "" "Include biclops support"         "" ON IF NOT WINRT)
+VP_OPTION(USE_PTU46       PTU46       "" "Include ptu-46 support"          "" ON IF UNIX AND NOT WINRT)
+VP_OPTION(USE_CMU1394     CMU1394     "" "Include cmu1494 support"         "" ON IF WIN32 AND NOT WINRT)
+VP_OPTION(USE_GDI         GDI         "" "Include gdi support"             "" ON IF WIN32 AND NOT WINRT)
+VP_OPTION(USE_DIRECT3D    DIRECT3D    "" "Include d3d support"             "" ON IF WIN32 AND NOT WINRT)
+VP_OPTION(USE_DIRECTSHOW  DIRECTSHOW  "" "Include dshow support"           "" ON IF WIN32 AND NOT WINRT)
 VP_OPTION(USE_OPENMP      OpenMP      "" "Include openmp support"          "" ON)
 # Since the FindLAPACK.cmake provided with CMake is for Fortran language,
 # in CMakeModules we have added FindLAPACK_C.cmake for C language
-VP_OPTION(USE_LAPACK      LAPACK_C    "" "Include lapack support"          "" ON)
-VP_OPTION(USE_GSL         GSL         "" "Include gsl support"             "" ON)
-VP_OPTION(USE_COIN3D      "Coin3D;MyCoin3D" "" "Include coin3d support"          "" ON IF OPENGL_FOUND)
-VP_OPTION(USE_YARP        YARP        QUIET "Include yarp support"         "YARP_DIR" ON)
-VP_OPTION(USE_OGRE        OGRE        QUIET "Include Ogre support"         "OGRE_DIR" ON)
-VP_OPTION(USE_OIS         OIS         QUIET "Include Ogre/ois support"     "OIS_DIR"  ON IF USE_OGRE)
-VP_OPTION(USE_LIBFREENECT LIBFREENECT ""    "Include libfreenect support"  "" ON)
-VP_OPTION(USE_LIBUSB_1    LIBUSB_1    ""    "Include libusb-1 support"     "" ON)
-VP_OPTION(USE_REALSENSE   RealSense   ""    "Include RealSense SDK support" "" ON)
+VP_OPTION(USE_LAPACK      LAPACK_C    "" "Include lapack support"          "" ON AND NOT WINRT)
+VP_OPTION(USE_GSL         GSL         "" "Include gsl support"             "" ON AND NOT WINRT)
+VP_OPTION(USE_COIN3D      "Coin3D;MyCoin3D" "" "Include coin3d support"    "" ON IF OPENGL_FOUND AND NOT WINRT)
+VP_OPTION(USE_YARP        YARP        QUIET "Include yarp support"         "YARP_DIR" ON IF NOT WINRT)
+VP_OPTION(USE_OGRE        OGRE        QUIET "Include Ogre support"         "OGRE_DIR" ON IF NOT WINRT)
+VP_OPTION(USE_OIS         OIS         QUIET "Include Ogre/ois support"     "OIS_DIR"  ON IF USE_OGRE AND NOT WINRT)
+VP_OPTION(USE_LIBFREENECT LIBFREENECT ""    "Include libfreenect support"  "" ON IF NOT WINRT)
+VP_OPTION(USE_LIBUSB_1    LIBUSB_1    ""    "Include libusb-1 support"     "" ON IF NOT WINRT)
+VP_OPTION(USE_REALSENSE   RealSense   ""    "Include RealSense SDK support" "" ON IF NOT WINRT)
 if(USE_REALSENSE)
   VP_OPTION(USE_CPP11       CPP11     ""    "Include c++11 support"         "" ON)
   # if option is OFF, force to ON
@@ -273,34 +281,34 @@ if(USE_REALSENSE)
 else()
   VP_OPTION(USE_CPP11       CPP11     ""    "Include c++11 support"        "" OFF)
 endif()
-VP_OPTION(USE_SOWIN       SOWIN       ""    "Include Coin/SoWin support"   "" OFF IF (WIN32 AND USE_COIN3D))
-VP_OPTION(USE_SOQT        SOQT        ""    "Include Coin/SoQt support"    "" OFF IF USE_COIN3D)
-VP_OPTION(USE_QT          Qt          ""    "Include Coin/SoQt/Qt support" "" ON  IF USE_SOQT)
-VP_OPTION(USE_SOXT        SOXT        ""    "Include Coin/SoXt support"    "" OFF IF USE_COIN3D)
+VP_OPTION(USE_SOWIN       SOWIN       ""    "Include Coin/SoWin support"   "" OFF IF (WIN32 AND USE_COIN3D) AND NOT WINRT)
+VP_OPTION(USE_SOQT        SOQT        ""    "Include Coin/SoQt support"    "" OFF IF USE_COIN3D AND NOT WINRT)
+VP_OPTION(USE_QT          Qt          ""    "Include Coin/SoQt/Qt support" "" ON  IF USE_SOQT AND NOT WINRT)
+VP_OPTION(USE_SOXT        SOXT        ""    "Include Coin/SoXt support"    "" OFF IF USE_COIN3D AND NOT WINRT)
 VP_OPTION(USE_PTHREAD     PTHREAD     ""    "Include pthread support"      "" ON)
-VP_OPTION(USE_XML2        XML2        ""    "Include xml support"          "" ON)
+VP_OPTION(USE_XML2        XML2        ""    "Include xml support"          "" ON IF NOT WINRT)
 if(CMAKE_TOOLCHAIN_FILE AND I_AM_A_ROBOT)
   VP_OPTION(USE_OPENCV      "MyOpenCV" QUIET "Include OpenCV support" "OpenCV_DIR;OpenCV_FOUND;OPENCV_FOUND" ON)
 else()
   VP_OPTION(USE_OPENCV      "OpenCV;MyOpenCV" QUIET "Include OpenCV support" "OpenCV_DIR;OpenCV_FOUND;OPENCV_FOUND" ON)
 endif()
-VP_OPTION(USE_ZLIB        "ZLIB;MyZLIB" ""  "Include zlib support"         "" ON)
-VP_OPTION(USE_X11         X11         ""    "Include X11 support"          "" ON)
+VP_OPTION(USE_ZLIB        "ZLIB;MyZLIB" ""  "Include zlib support"         "" ON IF NOT WINRT)
+VP_OPTION(USE_X11         X11         ""    "Include X11 support"          "" ON IF NOT WINRT)
 # The native FindGTK2.cmake doesn't consider libgobject-2.0 that is 
 # requested by ViSP. That's why we use our FindMyGTK2.cmake 
-VP_OPTION(USE_GTK2        MyGTK2       ""    "Include gtk2 support"         "" OFF)
+VP_OPTION(USE_GTK2        MyGTK2       ""    "Include gtk2 support"         "" OFF IF NOT WINRT)
 VP_OPTION(USE_JPEG       "JPEG;MyJPEG" ""    "Include jpeg support"         "" ON)
 VP_OPTION(USE_PNG        "PNG;MyPNG"   ""    "Include png support"          "" ON)
 VP_OPTION(USE_FFMPEG      FFMPEG       ""    "Include ffmpeg support"       "" OFF)
 # To control Pioneer mobile robots, under UNIX we need Aria, pthread, rt and dl 3rd party libraries
-VP_OPTION(USE_ARIA        ARIA         ""    "Include aria support"         "" ON)
+VP_OPTION(USE_ARIA        ARIA         ""    "Include aria support"         "" ON IF NOT WINRT)
 #VP_OPTION(USE_RT          RT           ""    "Include rt support"           "" ON)
 #VP_OPTION(USE_DL          DL           ""    "Include dl support"           "" ON)
 # bar codes
-VP_OPTION(USE_ZBAR        ZBAR         ""    "Include zbar support"         "" ON)
-VP_OPTION(USE_DMTX        DMTX         ""    "Include dmtx support"         "" ON)
-VP_OPTION(USE_PCL         PCL          QUIET "Include Point Cloud Library support" "" ON IF USE_REALSENSE)
-VP_OPTION(BUILD_ATIDAQ    ""           ""    "Include atidaq-c support"     "" ON IF USE_COMEDI)
+VP_OPTION(USE_ZBAR        ZBAR         ""    "Include zbar support"         "" ON IF NOT WINRT)
+VP_OPTION(USE_DMTX        DMTX         ""    "Include dmtx support"         "" ON IF NOT WINRT)
+VP_OPTION(USE_PCL         PCL          QUIET "Include Point Cloud Library support" "" ON IF USE_REALSENSE AND NOT WINRT)
+VP_OPTION(BUILD_ATIDAQ    ""           ""    "Include atidaq-c support"     "" ON IF USE_COMEDI AND NOT WINRT)
 
 # Find IsNaN (should be after USE_CPP11)
 VP_CHECK_PACKAGE(IsNaN)

cmake/AddExtraCompilationFlags.cmake
@@ -134,6 +134,10 @@ if(CMAKE_COMPILER_IS_GNUCXX)
   endif()
 endif()
 
+if(DEFINED WINRT)
+  add_extra_compiler_option(/ZW)
+endif()
+
 # Add user supplied extra options (optimization, etc...)
 vp_list_unique(VISP_EXTRA_C_FLAGS)
 vp_list_unique(VISP_EXTRA_CXX_FLAGS)

cmake/VISPCRTLinkage.cmake
@@ -2,6 +2,42 @@ if(NOT MSVC)
   message(FATAL_ERROR "CRT options are available only for MSVC")
 endif()
 
+if(WINRT)
+  if(CMAKE_SYSTEM_VERSION MATCHES 10)
+    add_definitions(/DWINVER=_WIN32_WINNT_WIN10 /DNTDDI_VERSION=NTDDI_WIN10 /D_WIN32_WINNT=_WIN32_WINNT_WIN10)
+  elseif(CMAKE_SYSTEM_VERSION MATCHES 8.1)
+    add_definitions(/DWINVER=_WIN32_WINNT_WINBLUE /DNTDDI_VERSION=NTDDI_WINBLUE /D_WIN32_WINNT=_WIN32_WINNT_WINBLUE)
+  else()
+    add_definitions(/DWINVER=_WIN32_WINNT_WIN8 /DNTDDI_VERSION=NTDDI_WIN8 /D_WIN32_WINNT=_WIN32_WINNT_WIN8)
+  endif()
+endif()
+
+# Removing LNK4075 warnings for debug WinRT builds
+# "LNK4075: ignoring '/INCREMENTAL' due to '/OPT:ICF' specification"
+# "LNK4075: ignoring '/INCREMENTAL' due to '/OPT:REF' specification"
+if(MSVC AND WINRT)
+  # Optional verification checks since we don't know existing contents of variables below
+  string(REPLACE "/OPT:ICF " "/OPT:NOICF " CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
+  string(REPLACE "/OPT:REF " "/OPT:NOREF " CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
+  string(REPLACE "/INCREMENTAL:YES " "/INCREMENTAL:NO " CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
+  string(REPLACE "/INCREMENTAL " "/INCREMENTAL:NO " CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
+
+  string(REPLACE "/OPT:ICF " "/OPT:NOICF " CMAKE_MODULE_LINKER_FLAGS_DEBUG "${CMAKE_MODULE_LINKER_FLAGS_DEBUG}")
+  string(REPLACE "/OPT:REF " "/OPT:NORE F" CMAKE_MODULE_LINKER_FLAGS_DEBUG "${CMAKE_MODULE_LINKER_FLAGS_DEBUG}")
+  string(REPLACE "/INCREMENTAL:YES " "/INCREMENTAL:NO " CMAKE_MODULE_LINKER_FLAGS_DEBUG "${CMAKE_MODULE_LINKER_FLAGS_DEBUG}")
+  string(REPLACE "/INCREMENTAL " "/INCREMENTAL:NO " CMAKE_MODULE_LINKER_FLAGS_DEBUG "${CMAKE_MODULE_LINKER_FLAGS_DEBUG}")
+
+  string(REPLACE "/OPT:ICF " "/OPT:NOICF " CMAKE_SHARED_LINKER_FLAGS_DEBUG "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
+  string(REPLACE "/OPT:REF " "/OPT:NOREF " CMAKE_SHARED_LINKER_FLAGS_DEBUG "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
+  string(REPLACE "/INCREMENTAL:YES " "/INCREMENTAL:NO " CMAKE_SHARED_LINKER_FLAGS_DEBUG "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
+  string(REPLACE "/INCREMENTAL " "/INCREMENTAL:NO " CMAKE_SHARED_LINKER_FLAGS_DEBUG "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
+
+  # Mandatory
+  set(CMAKE_MODULE_LINKER_FLAGS_DEBUG  "${CMAKE_MODULE_LINKER_FLAGS_DEBUG} /INCREMENTAL:NO /OPT:NOREF /OPT:NOICF")
+  set(CMAKE_EXE_LINKER_FLAGS_DEBUG     "${CMAKE_EXE_LINKER_FLAGS_DEBUG} /INCREMENTAL:NO /OPT:NOREF /OPT:NOICF")
+  set(CMAKE_SHARED_LINKER_FLAGS_DEBUG  "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} /INCREMENTAL:NO /OPT:NOREF /OPT:NOICF")
+endif()
+
 if(NOT BUILD_SHARED_LIBS AND BUILD_WITH_STATIC_CRT)
   foreach(flag_var
           CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE

cmake/templates/vpConfig.h.in
@@ -46,12 +46,16 @@
   #if _MSC_VER > 1500 // 1500 = MSVC 9 2008
     #pragma warning( disable: 4986 )
   #endif
+  #ifdef WINRT
+    #pragma warning(disable:4447)
+  #endif
 
   // 4100 : undocumented ("unreferenced formal parameter")
   // 4127 : conditional expression is constant
   // 4251 : 'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2', ie. disable warnings related to inline functions
   // 4275 : non – DLL-interface classkey 'identifier' used as base for DLL-interface classkey 'identifier'
   // 4351 : new behavior: elements of array will be default initialized
+  // 4447 : Disable warning 'main' signature found without threading model
   // 4514 : 'function' : unreferenced inline function has been removed
   // 4548 : expression before comma has no effect
   // 4668 : 'symbol' is not defined as a preprocessor macro, replacing with '0' for 'directives'

modules/core/src/tools/time/vpTime.cpp
@@ -93,7 +93,8 @@ double getMinTimeForUsleepCall()
 double measureTimeMs()
 {
 #if defined(_WIN32)
-  LARGE_INTEGER time, frequency;
+#if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))) // UNIX
+	LARGE_INTEGER time, frequency;
   QueryPerformanceFrequency(&frequency);
   if(frequency.QuadPart == 0){
     return(timeGetTime());
@@ -102,6 +103,9 @@ double measureTimeMs()
     QueryPerformanceCounter(&time);
     return (double)(1000.0*time.QuadPart/frequency.QuadPart);
   }
+#  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
+	throw(vpException(vpException::fatalError, "Cannot get time: not implemented on Universal Windows Platform"));
+#  endif
 #elif !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))) // UNIX
   struct timeval tp;
   gettimeofday(&tp,0);
@@ -117,7 +121,8 @@ double measureTimeMs()
 double measureTimeMicros()
 {
 #if defined(_WIN32)
-  LARGE_INTEGER time, frequency;
+#if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))) // UNIX
+	LARGE_INTEGER time, frequency;
   QueryPerformanceFrequency(&frequency);
   if(frequency.QuadPart == 0){
     return(timeGetTime());
@@ -126,8 +131,10 @@ double measureTimeMicros()
     QueryPerformanceCounter(&time);
     return (double)(1000000.0*time.QuadPart/frequency.QuadPart);
   }
+#  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
+	throw(vpException(vpException::fatalError, "Cannot get time: not implemented on Universal Windows Platform"));
+#  endif
 #else
-
   struct timeval tp;
   gettimeofday(&tp,0);
   return(1000000.0*tp.tv_sec + tp.tv_usec);

commit 9e42d3650816a8bd57eda58e6d6eed7592ff4679

Fix build issue introduced in commit 9d6c9e88536997479dd3d0e29e807a92841f55e2

modules/core/src/tools/file/vpIoTools.cpp
@@ -232,7 +232,7 @@ int main()
 std::string
 vpIoTools::getenv(const char *env)
 {
-#if defined(_WIN32) && ( defined(WINAPI_FAMILY) && ! WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
+#if defined(_WIN32) && defined(WINRT)
   throw(vpIoException(vpIoException::cantGetenv, "Cannot get the environment variable value: not implemented on Universal Windows Platform"));
 #else
   std::string value;
@@ -566,48 +566,50 @@ vpIoTools::copy(const char *src, const char *dst)
   if ( vpIoTools::checkFilename(src) ) {
     //std::cout << "copy file: " << src << " in " << dst << std::endl;
 #if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))) // UNIX
-	char cmd[FILENAME_MAX];
-	int ret;
-	sprintf(cmd, "cp -p %s %s", src, dst);
-	ret = system(cmd);
-	//std::cout << cmd << " return value: " << ret << std::endl;
-	return true;
+    char cmd[FILENAME_MAX];
+    int ret;
+    sprintf(cmd, "cp -p %s %s", src, dst);
+    ret = system(cmd);
+    //std::cout << cmd << " return value: " << ret << std::endl;
+    return true;
 #elif defined(_WIN32)
 #  if ( ! defined(WINAPI_FAMILY) ) || ( defined(WINAPI_FAMILY) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
-	char cmd[FILENAME_MAX];
-	int ret;
-	std::string src_ = vpIoTools::path(src);
-	std::string dst_ = vpIoTools::path(dst);
+    char cmd[FILENAME_MAX];
+    int ret;
+    std::string src_ = vpIoTools::path(src);
+    std::string dst_ = vpIoTools::path(dst);
     sprintf(cmd, "copy %s %s", src_.c_str(), dst_.c_str());
-	ret = system(cmd);
-	//std::cout << cmd << " return value: " << ret << std::endl;
-	return true;
+    ret = system(cmd);
+    //std::cout << cmd << " return value: " << ret << std::endl;
+    return true;
 #  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
-	throw(vpIoException(vpException::fatalError, "Cannot copy %s in %s: not implemented on Universal Windows Platform", src, dst));
+    throw(vpIoException(vpException::fatalError, "Cannot copy %s in %s: not implemented on Universal Windows Platform", src, dst));
 #  endif
 #endif
   }
   else if ( vpIoTools::checkDirectory(src) ) {
     //std::cout << "copy directory: " << src << " in " << dst << std::endl;
 #if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))) // UNIX
+    char cmd[FILENAME_MAX];
+    int ret;
     sprintf(cmd, "cp -p -r %s %s", src, dst);
-	ret = system(cmd);
-	if (ret) {}; // to avoid a warning
-				 //std::cout << cmd << " return value: " << ret << std::endl;
-	return true;
+    ret = system(cmd);
+    if (ret) {}; // to avoid a warning
+    //std::cout << cmd << " return value: " << ret << std::endl;
+    return true;
 #elif defined(_WIN32)
 #  if ( ! defined(WINAPI_FAMILY) ) || ( defined(WINAPI_FAMILY) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) )
-	char cmd[FILENAME_MAX];
-	int ret;
-	std::string src_ = vpIoTools::path(src);
-	std::string dst_ = vpIoTools::path(dst);
+    char cmd[FILENAME_MAX];
+    int ret;
+    std::string src_ = vpIoTools::path(src);
+    std::string dst_ = vpIoTools::path(dst);
     sprintf(cmd, "copy %s %s", src_.c_str(), dst_.c_str());
-	ret = system(cmd);
-	if (ret) {}; // to avoid a warning
-				 //std::cout << cmd << " return value: " << ret << std::endl;
-	return true;
+    ret = system(cmd);
+    if (ret) {}; // to avoid a warning
+    //std::cout << cmd << " return value: " << ret << std::endl;
+    return true;
 #  elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
-	throw(vpIoException(vpException::fatalError, "Cannot copy %s in %s: not implemented on Universal Windows Platform", src, dst));
+    throw(vpIoException(vpException::fatalError, "Cannot copy %s in %s: not implemented on Universal Windows Platform", src, dst));
 #  endif
 #endif
   }