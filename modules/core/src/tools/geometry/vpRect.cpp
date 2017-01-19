117070043ba77e204eddb3f0781490a57c722270
s-trinh <souriya.trinh@inria.fr>
2017-01-19 16:18:32 +0100

    Add vpRect intersection operator. (#124)

--- modules/core/src/tools/geometry/vpRect.cpp
+++ modules/core/src/tools/geometry/vpRect.cpp
@@ -46,16 +46,13 @@
 #include <visp3/core/vpDebug.h>
 
 /*!
- 
   Constructs a default rectangle with the \e top, \e left corner set to (0,0)
   and \e width and \e height set to 1.
-
 */
 
-vpRect::vpRect() : left(0), top(0), width(0), height(0) {};
+vpRect::vpRect() : left(0), top(0), width(0), height(0) {}
 
 /*!
- 
   Constructs a rectangle with the \e top, \e left corner and \e width
   and \e height.
 
@@ -63,33 +60,28 @@ vpRect::vpRect() : left(0), top(0), width(0), height(0) {};
   \param t : coordinate of the top/left corner along the vertical axis.
   \param w : rectangle width.
   \param h : rectangle height.
-
 */
 vpRect::vpRect(double l, double t, double w, double h)
   : left(l), top(t), width(w), height(h)
 {
-};
+}
 
 /*!
- 
   Constructs a rectangle with \e topLeft the top-left corner location
   and \e width and \e height the rectangle size.
 
   \param topLeft : coordinates of the top/left corner.
   \param w : rectangle width.
   \param h : rectangle height.
-
 */
 vpRect::vpRect(const vpImagePoint &topLeft, double w, double h)
   : left(topLeft.get_u()), top(topLeft.get_v()), width(w), height(h)
 {
-};
+}
 
 /*!
- 
   Constructs a rectangle with \e topLeft the top-left corner location
   and \e bottomRight the bottom-right corner.
-
 */
 vpRect::vpRect(const vpImagePoint &topLeft, const vpImagePoint &bottomRight)
   : left(topLeft.get_u()), top(topLeft.get_v()), width(0), height(0)
@@ -99,18 +91,16 @@ vpRect::vpRect(const vpImagePoint &topLeft, const vpImagePoint &bottomRight)
   
   setBottom( bottomRight.get_v() );
   setRight( bottomRight.get_u() );
-};
+}
 
 /*!
- 
   Constructs a rectangle that is a copy of \e r.
-
 */
 vpRect::vpRect(const vpRect& r)
   : left(0), top(0), width(0), height(0)
 {
   *this = r;
-};
+}
 
 /*!
 
@@ -123,10 +113,9 @@ vpRect &vpRect::operator=(const vpRect& r)
   this->width  = r.width;
   this->height = r.height;
   return *this;
-};
+}
 
 /*!
-
   Create a rectangle as the bounding box of a vector of image points.
   \param ip : Vector of image points. At least 1 points is mandatory,
   otherwise an exception is thrown.
@@ -138,27 +127,23 @@ vpRect::vpRect(const std::vector<vpImagePoint> &ip)
 }
 
 /*!
-
   Check if an image point belongs to the rectangle.
 
   \param ip : the image point.
 
   \return Returns true if the point belongs to the rectangle.
-
 */
 bool vpRect::isInside( const vpImagePoint &ip ) const{
   return ( ip.get_i() <= this->getBottom() && ip.get_i() >= this->getTop() && ip.get_j() <= this->getRight() && ip.get_j() >= this->getLeft());
 }
 
 /*!
-
   Set the rectangle with the \e top, \e left corner and \e width
   and \e height.
   \param l : coordinate of the top/left corner along the horizontal axis.
   \param t : coordinate of the top/left corner along the vertical axis.
   \param w : rectangle width.
   \param h : rectangle height.
-
 */
 void vpRect::set(double l, double t, double w, double h)
 {
@@ -166,10 +151,9 @@ void vpRect::set(double l, double t, double w, double h)
   top = t;
   width = w;
   height = h;
-};
+}
 
 /*!
-
   Set the rectangle with \e topLeft the top-left corner location
   and \e width and \e height the rectangle size.
 
@@ -183,10 +167,9 @@ void vpRect::set(const vpImagePoint &topLeft, double w, double h)
   top = topLeft.get_v();
   width = w;
   height = h;
-};
+}
 
 /*!
-
   Set the rectangle as the bounding box of a vector of image points.
   \param ip : Vector of image points. At least 1 points is mandatory,
   otherwise an exception is thrown.
@@ -214,16 +197,14 @@ void vpRect::set(const std::vector<vpImagePoint> &ip)
   setTop   (minv);
   setRight (maxu);
   setBottom(maxv);
-};
+}
 
 /*!
-
   Set the rectangle with \e topLeft the top-left corner location
   and \e bottomRight the bottom-right corner.
 
   \param topLeft : coordinates of the top-left corner.
   \param bottomRight : coordinates of the bottom-right corner.
-
 */
 void vpRect::set(const vpImagePoint &topLeft, const vpImagePoint &bottomRight)
 {
@@ -232,17 +213,15 @@ void vpRect::set(const vpImagePoint &topLeft, const vpImagePoint &bottomRight)
 
   setBottom( bottomRight.get_v() );
   setRight( bottomRight.get_u() );
-};
+}
 
 /*!
-
   Set the rectangle from \e r.
-
 */
 void vpRect::set(const vpRect& r)
 {
   *this = r;
-};
+}
 
 /*!
  Operator to compare two rectangles.
@@ -264,21 +243,48 @@ bool vpRect::operator==(const vpRect &r) const
 bool vpRect::operator!=(const vpRect &r) const
 {
   //return (top != r.top || left != r.left || width != r.width || height != r.height);
-  return (std::fabs(top-r.top) > std::fabs(top)*std::numeric_limits<double>::epsilon()
-          || std::fabs(left-r.left) > std::fabs(left)*std::numeric_limits<double>::epsilon()
-          || std::fabs(width-r.width) > std::fabs(width)*std::numeric_limits<double>::epsilon()
-          || std::fabs(height-r.height) > std::fabs(height)*std::numeric_limits<double>::epsilon());
+//  return (std::fabs(top-r.top) > std::fabs(top)*std::numeric_limits<double>::epsilon()
+//          || std::fabs(left-r.left) > std::fabs(left)*std::numeric_limits<double>::epsilon()
+//          || std::fabs(width-r.width) > std::fabs(width)*std::numeric_limits<double>::epsilon()
+//          || std::fabs(height-r.height) > std::fabs(height)*std::numeric_limits<double>::epsilon());
+  return !(*this == r);
 }
 
 /*!
+ Intersection operator.
+ \return Intersection rectangle or null rectangle if the two rectangles do not intersect.
+ */
+vpRect& vpRect::operator&=(const vpRect &r) {
+  double x1 = std::max(left, r.left);
+  double y1 = std::max(top, r.top);
+  width  = std::min(left + width, r.left + r.width) - x1;
+  height = std::min(top + height, r.top + r.height) - y1;
+  left = x1;
+  top = y1;
 
+  if (width <= 0 || height <= 0) {
+    *this = vpRect();
+  }
+
+  return *this;
+}
+
+/*!
+ Intersection operator.
+ \return Intersection rectangle or null rectangle if the two rectangles do not intersect.
+ */
+vpRect vpRect::operator&(const vpRect &r) const {
+  vpRect a = *this;
+  return a &= r;
+}
+
+/*!
   Check if an image point belongs to a rectangle.
 
   \param ip : the image point.
   \param rect : the rectangle.
 
   \return Returns true if the point belongs to the rectangle.
-
 */
 VISP_EXPORT bool inRectangle( const vpImagePoint &ip, const vpRect &rect ) {
   return ( ip.get_i() <= rect.getBottom() && ip.get_i() >= rect.getTop() && ip.get_j() <= rect.getRight() && ip.get_j() >= rect.getLeft());
---
+++ modules/core/test/tools/geometry/testRect.cpp
@@ -0,0 +1,89 @@
+/****************************************************************************
+ *
+ * This file is part of the ViSP software.
+ * Copyright (C) 2005 - 2017 by Inria. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * ("GPL") version 2 as published by the Free Software Foundation.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact Inria about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See http://visp.inria.fr for more information.
+ *
+ * This software was developed at:
+ * Inria Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Inria at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Description:
+ * Test vpRect.
+ *
+ *****************************************************************************/
+
+#include <iostream>
+#include <visp3/core/vpConfig.h>
+#include <visp3/core/vpRect.h>
+
+int main() {
+  vpRect c(10.1, 15.05, 19.63, 7.84);
+  vpRect a(c.getLeft() - 12.456, c.getTop() - 7.75, c.getWidth() + 12.456, c.getHeight() + 7.75);
+  vpRect b(c.getLeft(), c.getTop(), c.getWidth() + 8.81, c.getHeight() + 14.57);
+
+  vpRect intersect = a & b;
+
+  std::cout << "Test intersection." << std::endl;
+  std::cout << "a=" << a << std::endl;
+  std::cout << "b=" << b << std::endl;
+  std::cout << "c=" << c << std::endl;
+  std::cout << "intersect=" << intersect << std::endl;
+
+  if (intersect != c) {
+    std::cerr << "Problem with the intersection function!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  a &= b;
+  std::cout << "a=" << a << std::endl;
+
+  if (a != c) {
+    std::cerr << "Problem with the intersection function!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  a = vpRect(11.45, 15.67, 3.5, 7.81);
+  b = vpRect(24.78, 31.46, 7.48, 11.28);
+  intersect = a & b;
+
+  std::cout << "\nTest no intersection." << std::endl;
+  std::cout << "a=" << a << std::endl;
+  std::cout << "b=" << b << std::endl;
+  std::cout << "intersect=" << intersect << std::endl;
+
+  if (intersect != vpRect()) {
+    std::cerr << "Problem with the intersection function!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  a &= b;
+  std::cout << "a=" << a << std::endl;
+
+  if (a != vpRect()) {
+    std::cerr << "Problem with the intersection function!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "vpRect is ok." << std::endl;
+  return EXIT_SUCCESS;
+}
