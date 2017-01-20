2848d614f433309e43b768f59ad0eb6744d427e6
Fabien Spindler <Fabien.Spindler@inria.fr>
2017-01-20 17:26:24 +0100

    Introduce test to get build information

---
+++ modules/core/test/tools/io/testBuildInformation.cpp
@@ -0,0 +1,54 @@
+/****************************************************************************
+ *
+ * This file is part of the ViSP software.
+ * Copyright (C) 2005 - 2017 by Inria. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * ("GPL") version 2 as published by the Free Software Foundation.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact Inria about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See http://visp.inria.fr for more information.
+ *
+ * This software was developed at:
+ * Inria Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Inria at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Description:
+ * Test functions in vpIoTools.
+ *
+ * Authors:
+ * Souriya Trinh
+ *
+ *****************************************************************************/
+
+/*!
+  \example testBuildInformation.cpp
+
+  Test that prints build information.
+*/
+
+#include <iostream>
+#include <visp3/core/vpIoTools.h>
+
+
+int main()
+{
+  std::string info = vpIoTools::getBuildInformation();
+  std::cout << info << std::endl;
+
+  return 0;
+}
