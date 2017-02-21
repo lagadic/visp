lagadic/visp

commit 988759614a78df53436e773427feb356a05bcf16

Fix cppcheck warning introducing log1p() function

modules/vision/src/pose-estimation/vpPoseRansac.cpp
@@ -932,7 +932,11 @@ int vpPose::computeRansacIterations(double probability, double epsilon, const in
 
   double logarg, logval, N;
   logarg = -std::pow(1.0 - epsilon, sampleSize);
+#ifdef VISP_HAVE_FUNC_LOG1P
+  logval = log(logarg);
+#else
   logval = log(1.0 + logarg);
+#endif
   if (vpMath::nul(logval, std::numeric_limits<double>::epsilon())) {
     std::cerr << "vpMath::nul(log(1.0 - std::pow(1.0 - epsilon, sampleSize)), std::numeric_limits<double>::epsilon())"
               << std::endl;

CMakeLists.txt
@@ -393,6 +393,8 @@ VP_CHECK_PACKAGE(Round)
 VP_CHECK_PACKAGE(Erfc)
 # Find Strtof
 VP_CHECK_PACKAGE(Strtof)
+# Find Log1p
+VP_CHECK_PACKAGE(Log1p)
 
 #----------------------------------------------------------------------
 # For Dart server and tests
@@ -728,6 +730,8 @@ VP_SET(VISP_HAVE_FUNC_ERFC TRUE IF HAVE_FUNC_ERFC) # for header vpConfig.h
 VP_SET(VISP_HAVE_FUNC_STD_ERFC TRUE IF HAVE_FUNC_STD_ERFC) # for header vpConfig.h
 # Find strtof function (stdlib.h)
 VP_SET(VISP_HAVE_FUNC_STRTOF TRUE IF HAVE_FUNC_STRTOF) # for header vpConfig.h
+# Find log1p function (math.h)
+VP_SET(VISP_HAVE_FUNC_LOG1P TRUE IF HAVE_FUNC_LOG1P) # for header vpConfig.h
 
 VP_SET(VISP_BUILD_DEPRECATED_FUNCTIONS TRUE IF BUILD_DEPRECATED_FUNCTIONS) # for header vpConfig.h
 VP_SET(VISP_MOMENTS_COMBINE_MATRICES TRUE IF ENABLE_MOMENTS_COMBINE_MATRICES) # for header vpConfig.h

cmake/FindLog1p.cmake
@@ -1,0 +1,56 @@
+#############################################################################
+#
+# This file is part of the ViSP software.
+# Copyright (C) 2005 - 2017 by Inria. All rights reserved.
+#
+# This software is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# ("GPL") version 2 as published by the Free Software Foundation.
+# See the file LICENSE.txt at the root directory of this source
+# distribution for additional information about the GNU GPL.
+#
+# For using ViSP with software that can not be combined with the GNU
+# GPL, please contact Inria about acquiring a ViSP Professional
+# Edition License.
+#
+# See http://visp.inria.fr for more information.
+#
+# This software was developed at:
+# Inria Rennes - Bretagne Atlantique
+# Campus Universitaire de Beaulieu
+# 35042 Rennes Cedex
+# France
+#
+# If you have questions regarding the use of this file, please contact
+# Inria at visp@inria.fr
+#
+# This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+# WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Description:
+# Try to find log1p function.
+#
+# Authors:
+# Fabien Spindler
+#
+#############################################################################
+
+include(CheckCXXSourceCompiles)
+
+macro(check_math_expr _expr _var)
+    unset(${_var} CACHE)
+    if(USE_CPP11)
+      set(CMAKE_REQUIRED_FLAGS ${CPP11_CXX_FLAGS})
+    endif()
+    check_cxx_source_compiles("
+#include <math.h>
+int main(int argc, char ** argv)
+{
+    (void)${_expr};
+    return 0;
+}
+" ${_var})
+endmacro()
+
+check_math_expr("log1p(1.0)"       HAVE_FUNC_LOG1P)
+

cmake/templates/vpConfig.h.in
@@ -391,6 +391,9 @@
 // Defined if strtof function is available
 #cmakedefine VISP_HAVE_FUNC_STRTOF
 
+// Defined if log1p function is available
+#cmakedefine VISP_HAVE_FUNC_LOG1P
+
 // Defined if xrandr program available
 #cmakedefine VISP_HAVE_XRANDR
 

commit b5c579c53b70ebf83530f536b298c809a3a0acca

Fix issue introduced in commit 988759614a78

modules/vision/src/pose-estimation/vpPoseRansac.cpp
@@ -933,7 +933,7 @@ int vpPose::computeRansacIterations(double probability, double epsilon, const in
   double logarg, logval, N;
   logarg = -std::pow(1.0 - epsilon, sampleSize);
 #ifdef VISP_HAVE_FUNC_LOG1P
-  logval = log(logarg);
+  logval = log1p(logarg);
 #else
   logval = log(1.0 + logarg);
 #endif