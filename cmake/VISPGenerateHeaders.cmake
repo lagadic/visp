aca84566c2f9cbc6bf0138adb37274a994b10496
Fabien Spindler <Fabien.Spindler@inria.fr>
2017-02-21 13:19:42 +0100

    Make vpConfig.h independent from <build dir> when installed. Resolves the need of a patch for ArchLinux packaging.

--- cmake/VISPGenerateHeaders.cmake
+++ cmake/VISPGenerateHeaders.cmake
@@ -32,12 +32,66 @@
 #
 #############################################################################
 
-# platform-specific config file
+# ----------------------------------------------------------------------------
+# platform-specific config file vpConfig.h
+# ----------------------------------------------------------------------------
+
+# case 1: when ViSP is build with make; files are used in <binary dir>
+set(data_location_ ${VISP_BINARY_DIR})
+set(VISP_SCENES_DIR ${data_location_}/data/wireframe-simulator)
+set(VISP_ROBOT_ARMS_DIR ${data_location_}/data/robot-simulator)
+# Note that VISP_HAVE_OGRE_RESOURCES_PATH and VISP_HAVE_OGRE_PLUGINS_PATH are set in OgreTools.cmake
+if(UNIX)
+  set(data_location_ "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_DATAROOTDIR}/visp-${VISP_VERSION}")
+else()
+  set(data_location_ "${CMAKE_INSTALL_PREFIX}")
+endif()
+list(APPEND VISP_SCENES_DIR ${data_location_}/data/wireframe-simulator)
+list(APPEND VISP_ROBOT_ARMS_DIR ${data_location_}/data/robot-simulator)
+if(VISP_INSTALL_DIR_OGRE_RESOURCES)
+  list(APPEND VISP_HAVE_OGRE_RESOURCES_PATH "${data_location_}/data/ogre-simulator")
+endif()
+# append to VISP_HAVE_OGRE_PLUGINS_PATH the path of the installed plugins.cfg file
+# to be able to use ViSP from install tree
+if(VISP_INSTALL_DIR_OGRE_RESOURCES)
+  list(APPEND VISP_HAVE_OGRE_PLUGINS_PATH "${CMAKE_INSTALL_PREFIX}/${VISP_LIB_INSTALL_PATH}/visp/data/ogre-simulator")
+endif()
+
 configure_file("${VISP_SOURCE_DIR}/cmake/templates/vpConfig.h.in" "${VISP_INCLUDE_DIR}/visp3/core/vpConfig.h")
-install(FILES "${VISP_INCLUDE_DIR}/visp3/core/vpConfig.h"
-  DESTINATION ${VISP_INC_INSTALL_PATH}/visp3/core
-  COMPONENT dev
-)
+
+# case 2: when ViSP is build with make install; files are used in <install dir>
+if(UNIX)
+  set(data_location_ "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_DATAROOTDIR}/visp-${VISP_VERSION}")
+else()
+  set(data_location_ "${CMAKE_INSTALL_PREFIX}")
+endif()
+set(VISP_SCENES_DIR ${data_location_}/data/wireframe-simulator)
+set(VISP_ROBOT_ARMS_DIR ${data_location_}/data/robot-simulator)
+if(VISP_INSTALL_DIR_OGRE_RESOURCES)
+  set(VISP_HAVE_OGRE_RESOURCES_PATH "${data_location_}/data/ogre-simulator")
+endif()
+# append to VISP_HAVE_OGRE_PLUGINS_PATH the path of the installed plugins.cfg file
+# to be able to use ViSP from install tree
+if(VISP_INSTALL_DIR_OGRE_RESOURCES)
+  set(VISP_HAVE_OGRE_PLUGINS_PATH "${CMAKE_INSTALL_PREFIX}/${VISP_LIB_INSTALL_PATH}/visp/data/ogre-simulator")
+endif()
+
+if(UNIX)
+  configure_file("${VISP_SOURCE_DIR}/cmake/templates/vpConfig.h.in" "${VISP_BINARY_DIR}/unix-install/vpConfig.h")
+
+  install(FILES "${VISP_BINARY_DIR}/unix-install/vpConfig.h"
+    DESTINATION ${VISP_INC_INSTALL_PATH}/visp3/core
+    COMPONENT dev
+  )
+
+else()
+  configure_file("${VISP_SOURCE_DIR}/cmake/templates/vpConfig.h.in" "${VISP_BINARY_DIR}/win-install/vpConfig.h")
+
+  install(FILES "${VISP_BINARY_DIR}/win-install/vpConfig.h"
+    DESTINATION ${VISP_INC_INSTALL_PATH}/visp3/core
+    COMPONENT dev
+  )
+endif()
 
 # ----------------------------------------------------------------------------
 # visp_modules.h that contains all the build modules defines
