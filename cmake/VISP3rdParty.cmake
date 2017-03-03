6213bc6642e61936f17aa3c65ac272edec46f36b
Fabien Spindler <Fabien.Spindler@inria.fr>
2017-03-03 19:39:02 +0100

    Rename the folder containing the atidaq private lib to better match the name of the lib

--- cmake/VISP3rdParty.cmake
+++ cmake/VISP3rdParty.cmake
@@ -4,7 +4,7 @@
 
 if(BUILD_ATIDAQ)
   set(ATIDAQ_LIBRARY atidaq_c)
-  add_subdirectory("${VISP_SOURCE_DIR}/3rdparty/libatidaq-c")
+  add_subdirectory("${VISP_SOURCE_DIR}/3rdparty/atidaq")
   set(ATIDAQ_INCLUDE_DIRS "${${ATIDAQ_LIBRARY}_SOURCE_DIR}" "${${ATIDAQ_LIBRARY}_BINARY_DIR}")
   set(ATIDAQ_LIBRARIES ${ATIDAQ_LIBRARY})
 endif()
