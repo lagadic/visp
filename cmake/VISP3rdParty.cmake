23c496aa36b1c05f2512a74701e53ed90ecdb571
Fabien Spindler <Fabien.Spindler@inria.fr>
2017-03-28 09:14:55 +0200

    Group 3rdparty built-in related vars - rename BUILD_<ATIDAQ, CLIPPER, LAPACK> into WITH_<ATIDAQ, CLIPPER, LAPACK> for better visibility - introduce version of clipper and atidaq in the build log

--- cmake/VISP3rdParty.cmake
+++ cmake/VISP3rdParty.cmake
@@ -2,21 +2,23 @@
 # Add 3rd-party libraries build as static libs
 #----------------------------------------------------------------------
 
-if(BUILD_ATIDAQ)
+if(WITH_ATIDAQ)
   set(ATIDAQ_LIBRARY atidaq_c)
   add_subdirectory("${VISP_SOURCE_DIR}/3rdparty/atidaq")
   set(ATIDAQ_INCLUDE_DIRS "${${ATIDAQ_LIBRARY}_SOURCE_DIR}" "${${ATIDAQ_LIBRARY}_BINARY_DIR}")
   set(ATIDAQ_LIBRARIES ${ATIDAQ_LIBRARY})
+  set(ATIDAQ_VERSION ${ATIDAQ_MAJOR_VERSION}.${ATIDAQ_MINOR_VERSION}.${ATIDAQ_PATCH_VERSION})
 endif()
 
-if(BUILD_CLIPPER)
+if(WITH_CLIPPER)
   set(CLIPPER_LIBRARY clipper)
   add_subdirectory("${VISP_SOURCE_DIR}/3rdparty/clipper")
   set(CLIPPER_INCLUDE_DIRS "${${CLIPPER_LIBRARY}_SOURCE_DIR}" "${${CLIPPER_LIBRARY}_BINARY_DIR}")
   set(CLIPPER_LIBRARIES ${CLIPPER_LIBRARY})
+  set(CLIPPER_VERSION ${CLIPPER_MAJOR_VERSION}.${CLIPPER_MINOR_VERSION}.${CLIPPER_PATCH_VERSION})
 endif()
 
-if(BUILD_LAPACK)
+if(WITH_LAPACK)
   set(LAPACK_LIBRARY visp_lapack)
   add_subdirectory("${VISP_SOURCE_DIR}/3rdparty/clapack")
   set(LAPACK_LIBRARIES ${LAPACK_LIBRARY})
