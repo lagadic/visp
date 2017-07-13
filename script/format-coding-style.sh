9c016f0d66ab455fd2d762d579de7347cfea515f
Fabien Spindler <Fabien.Spindler@inria.fr>
2017-07-13 14:28:47 +0200

    Improve script checking if clang-format exists

--- script/format-coding-style.sh
+++ script/format-coding-style.sh
@@ -3,9 +3,21 @@
 script_path=`dirname $0`
 visp_root=`(cd $script_path/..; pwd)`
 
-echo "Apply coding-style to visp: " $visp_root
 # Change this if your clang-format executable is somewhere else
 clang_format="clang-format"
+
+command_exists () {
+  type "$1" &> /dev/null ;
+}
+
+if ! command_exists $clang_format ; then
+    echo "You have to install clang-format before using this script"
+    echo "  Ubuntu: sudo apt-get install clang-format"
+    echo "  OSX   : brew update; brew install clang-format"
+    exit 0
+fi
+
+echo "Apply coding-style to visp: " $visp_root
 
 read -r -p "Are you sure? [Y/n] " answer
 answer="$(echo ${answer} | tr 'A-Z' 'a-z')"
