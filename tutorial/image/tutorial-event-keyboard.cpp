8c43a612e5b4a8adcf80cab9e1a7b2a19ea7dc90
ashwinpathak20 <ashwinpathak20nov1996@gmail.com>
2018-01-17 21:52:41 +0530

    codes for all the sections added of issue #139

---
+++ tutorial/image/tutorial-event-keyboard.cpp
@@ -0,0 +1,55 @@
+#include <visp/vpConfig.h>
+#include <visp/vpDisplayOpenCV.h>
+#include <visp/vpDisplayX.h>
+#include <visp/vpDisplayGTK.h>
+#include <visp/vpDisplayGDI.h>
+#include <visp/vpDisplayD3D.h>
+int main()
+{
+  vpImage<unsigned char> I(240, 320); // Create a black image
+  vpDisplay *d;
+#if defined(VISP_HAVE_X11)
+  d = new vpDisplayX;
+#elif defined(VISP_HAVE_GTK)
+  d = new vpDisplayGTK;
+#elif defined(VISP_HAVE_GDI)
+  d = new vpDisplayGDI;
+#elif defined(VISP_HAVE_D3D9)
+  d = new vpDisplayD3D;
+#elif defined(VISP_HAVE_OPENCV)
+  d = new vpDisplayOpenCV;
+#else
+  std::cout << "Sorry, no video device is available" << std::endl;
+  return -1;
+#endif
+  // Initialize the display with the image I. Display and image are
+  // now link together.
+#ifdef VISP_HAVE_DISPLAY
+  d->init(I);
+#endif
+  // Set the display background with image I content
+  vpDisplay::display(I);
+  // Flush the foreground and background display
+  vpDisplay::flush(I);
+  // Wait for keyboard event
+  std::cout << "Waiting a keyboard event..." << std::endl;
+  vpDisplay::getKeyboardEvent(I, true);
+  std::cout << "A keyboard event was detected" << std::endl;
+  // Non blocking keyboard event loop
+  int cpt_event = 0;
+  bool event;
+  char key[10];
+  std::cout << "Enter a non blocking keyboard event detection loop..." << std::endl;
+  do {
+    event = vpDisplay::getKeyboardEvent(I, &key[0], false);
+    if (event) {
+      std::cout << "Key detected: " << key << std::endl;
+      cpt_event ++;
+    }
+    vpTime::wait(5); // wait 5 ms
+  } while(cpt_event < 5);
+#ifdef VISP_HAVE_DISPLAY
+  delete d;
+#endif
+}
+  
