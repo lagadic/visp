cc10895ac2c5190d9ca1c916d90c0306a8e14268
Fabien Spindler <Fabien.Spindler@inria.fr>
2018-04-16 09:02:18 +0200

    Add mbot tutorial (#330)
    * Introduce vpSerial class
    * Migrate networking classes in a new group
    * Introduce new setter vp vpMomentAreaNormalized
    * Introduce new option to turn display off
    * Introduce new setter
    * Introduce mBot Ranger educational robot kit visual servoing tutorial
    * Fix build under windows

---
+++ modules/core/include/visp3/core/vpSerial.h
@@ -0,0 +1,196 @@
+/****************************************************************************
+ *
+ * This file is part of the ViSP software.
+ * Copyright (C) 2005 - 2017 by Inria. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact Inria about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See http://visp.inria.fr for more information.
+ *
+ * This software was developed at:
+ * Inria Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Inria at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Description:
+ * Serial communication.
+ *
+ * Authors:
+ * Fabien Spindler
+ *
+ *****************************************************************************/
+
+#ifndef __vpSerial_h_
+#define __vpSerial_h_
+
+#if !defined(_WIN32)
+
+#include <string>
+#include <stdint.h>
+
+#include <visp3/core/vpConfig.h>
+
+/*!
+  \class vpSerial
+  \ingroup group_core_com_serial
+
+  This class allows a serial port communication between 2 devices.
+
+  The following example shows how to write a string on port `/dev/ttyUSB0` using the default constructor:
+  \code
+#include <visp3/core/vpSerial.h>
+
+int main()
+{
+#ifndef WIN32
+  vpSerial serial("/dev/ttyUSB0");
+  serial.write("Hello world");
+#endif
+}
+  \endcode
+
+  \note This class is not implemented on windows-like OS.
+*/
+class VISP_EXPORT vpSerial
+{
+public:
+  /*!
+   * Defines the possible byte sizes for the serial port.
+   */
+  typedef enum {
+    fivebits  = 5, //!< Data is encoded with 5 bits
+    sixbits   = 6, //!< Data is encoded with 6 bits
+    sevenbits = 7, //!< Data is encoded with 7 bits
+    eightbits = 8  //!< Data is encoded with 8 bits
+  } bytesize_t;
+
+  /*!
+   * Defines the possible parity types for the serial port.
+   */
+  typedef enum {
+    parity_none = 0, //!< No parity check
+    parity_odd  = 1, //!< Check for odd parity
+    parity_even = 2  //!< Check for even parity
+  } parity_t;
+
+  /*!
+   * Defines the possible stopbit types for the serial port.
+   */
+  typedef enum {
+    stopbits_one = 1, //!< 1 stop bit is used
+    stopbits_two = 2, //!< 2 stop bits are used
+  } stopbits_t;
+
+  /*!
+   * Defines the possible flowcontrol types for the serial port.
+   */
+  typedef enum {
+    flowcontrol_none = 0, //!< No flow control
+    flowcontrol_software, //!< Software flow control
+    flowcontrol_hardware  //!< Hardware flow control
+  } flowcontrol_t;
+
+  vpSerial(const std::string &port="", unsigned long baudrate = 9600,
+           bytesize_t bytesize = eightbits, parity_t parity = parity_none, stopbits_t stopbits = stopbits_one,
+           flowcontrol_t flowcontrol = flowcontrol_none);
+  ~vpSerial();
+
+  int available();
+  void close();
+
+  /*!
+    Return the baud rate; 9600, 115200...
+    \sa setBaudrate()
+   */
+  unsigned long getBaudrate() {
+    return m_baudrate;
+  }
+
+  /*!
+    Return the byte size.
+    \sa setBytesize()
+   */
+  bytesize_t getBytesize() {
+    return m_bytesize;
+  }
+
+  /*!
+    Return the flow control type.
+    \sa setFlowcontrol()
+   */
+  flowcontrol_t getFlowcontrol() {
+    return m_flowcontrol;
+  }
+
+  /*!
+    Return parity.
+    \sa setParity()
+   */
+  parity_t getParity() {
+    return m_parity;
+  }
+
+  /*!
+    Return the serial port name like `/dev/ttyUSB0`, `/dev/ttySO`, `/dev/ttyAMA0`...
+    \sa setPort()
+   */
+  std::string getPort() {
+    return m_port;
+  }
+
+  /*!
+    Return number of stop bits used.
+    \sa setStopbits()
+   */
+  stopbits_t getStopbits() {
+    return m_stopbits;
+  }
+
+  void open();
+  bool read(char *c, long timeout_s);
+  std::string readline(const std::string &eol);
+  void setBaudrate(const unsigned long baudrate);
+  void setBytesize(const bytesize_t &bytesize);
+  void setFlowcontrol(const flowcontrol_t &flowcontrol);
+  void setParity(const parity_t &parity);
+  void setPort(const std::string &port);
+  void setStopbits(const stopbits_t &stopbits);
+  void write(const std::string &s);
+
+private:
+  void configure();
+
+  std::string m_port;
+  int m_fd;
+
+  bool m_is_open;
+  bool m_xonxoff;
+  bool m_rtscts;
+
+  unsigned long m_baudrate;
+
+  parity_t m_parity;
+  bytesize_t m_bytesize;
+  stopbits_t m_stopbits;
+  flowcontrol_t m_flowcontrol;
+
+};
+
+#endif
+#endif
---
+++ tutorial/robot/mbot/raspberry/visp/test-serial-mbot.cpp
@@ -0,0 +1,85 @@
+#include <visp3/core/vpColVector.h>
+#include <visp3/core/vpSerial.h>
+#include <visp3/core/vpTime.h>
+
+int main(int argc, char *argv[])
+{
+#if !defined(_WIN32)
+  double time = 4;
+  double v_x = 0;
+  double w_z = 0;
+  bool rpm_command = false;
+  int rpm_l = 0;
+  int rpm_r = 0;
+
+  for (int i = 1; i < argc; i++) {
+    if ((std::string(argv[i]) == "--t" || std::string(argv[i]) == "-t") && i+1 < argc) {
+      time = (double)atof(argv[i+1]);
+    } else if ((std::string(argv[i]) == "--vx" || std::string(argv[i]) == "-vx") && i+1 < argc) {
+      v_x = (double)atof(argv[i+1]);
+    } else if ((std::string(argv[i]) == "--wz" || std::string(argv[i]) == "-wz") && i+1 < argc) {
+      w_z = (double)atof(argv[i+1]);
+    } else if ((std::string(argv[i]) == "--rpm_l" || std::string(argv[i]) == "-rpm_l") && i+1 < argc) {
+      rpm_command = true;
+      rpm_l = (double)atoi(argv[i+1]);
+    } else if ((std::string(argv[i]) == "--rpm_r" || std::string(argv[i]) == "-rpm_r") && i+1 < argc) {
+      rpm_command = true;
+      rpm_r = (double)atoi(argv[i+1]);
+    } else if (std::string(argv[i]) == "--help" || std::string(argv[i]) == "-h") {
+      std::cout << "Usage: \n" << argv[0] << " --vx <linear velocity in m/s> --wz <rotational velocity in deg/s> --rpm_l <motor left RPM> --rpm_r <motor right RPM> --t <duration of the command in second> --help" << std::endl;
+      std::cout << "\nExample:\n" << argv[0] << " --vx 0.05 --wz 0 --t 4\n" << std::endl;
+      return 0;
+    }
+  }
+
+  vpSerial serial("/dev/ttyAMA0", 115200);
+
+  {
+    std::stringstream ss;
+    if (rpm_command) {
+      std::cout << "Apply rpm_l=" << rpm_l << " rpm_r=" << rpm_r << " during " << time << " seconds" << std::endl;
+      ss << "MOTOR_RPM=" << rpm_l << "," << rpm_r << "\n";
+    }
+    else {
+      vpColVector v(2);
+      v[0] = v_x;
+      v[1] = vpMath::rad(w_z);
+      std::cout << "Apply v_x=" << v_x << " m/s "<< " w_z=" << w_z << " deg/s during " << time << " seconds" << std::endl;
+      double radius = 0.0325;
+      double L = 0.0725;
+      double motor_left  = -(v[0] + L * v[1]) / radius;
+      double motor_right =  (v[0] - L * v[1]) / radius;
+      std::cout << "Motor left vel: " << motor_left << " motor right vel: " << motor_right << " (rad/s)" << std::endl;
+      double rpm_left  = motor_left  * 30. / M_PI;
+      double rpm_right = motor_right * 30. / M_PI;
+
+      ss << "MOTOR_RPM=" << (int)rpm_left << "," << (int)rpm_right << "\n";
+    }
+    std::cout << "Send: " << ss.str() << std::endl;
+    double t0 =vpTime::measureTimeSecond();
+    while(vpTime::measureTimeSecond() - t0 < time) {
+      serial.write(ss.str());
+      vpTime::wait(100);
+    }
+    return EXIT_SUCCESS;
+  }
+  serial.write("MOTOR_RPM=-100,100\n");
+  vpTime::sleepMs(500);
+  serial.write("MOTOR_RPM=-50,100\n");
+  vpTime::sleepMs(500);
+  serial.write("MOTOR_RPM=50,-50\n");
+  vpTime::sleepMs(500);
+  serial.write("LED_RING=0,0,10,0\n");
+  vpTime::sleepMs(500);
+  serial.write("LED_RING=0,0,0,10\n");
+  vpTime::sleepMs(500);
+  serial.write("LED_RING=0,0,0,0\n");
+  vpTime::sleepMs(500);
+  serial.close();
+#else
+  (void) argc;
+  (void) argv;
+  std::cout << "Serial test is only working on unix-like OS." << std::endl;
+#endif
+  return EXIT_SUCCESS;
+}
