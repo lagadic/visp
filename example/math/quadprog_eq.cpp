65fd0b2355a4913b944d209bd952ff88ef5749d1
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2018-06-19 19:39:49 +0200

    vpLinProg / vpQuadProg

---
+++ example/math/quadprog_eq.cpp
@@ -0,0 +1,149 @@
+/****************************************************************************
+ *
+ * This file is part of the ViSP software.
+ * Copyright (C) 2005 - 2017 by Inria. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact Inria about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See http://visp.inria.fr for more information.
+ *
+ * This software was developed at:
+ * Inria Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Inria at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Description:
+ * Example of sequential calls to QP solver with constant equality constraint
+ *
+ * Authors:
+ * Olivier Kermorgant
+ *
+ *****************************************************************************/
+/*!
+  \file quadprog_eq.cpp
+
+  \brief Example of sequential calls to QP solver with constant equality constraint
+*/
+
+/*!
+  \example quadprog_eq.cpp
+
+  Example of sequential calls to QP solver with constant equality constraint
+*/
+
+#include <visp3/core/vpQuadProg.h>
+#include <visp3/core/vpTime.h>
+#include "qp_plot.h"
+
+using namespace std;
+
+int main ()
+{
+  const int n = 20;    // x dim
+  const int m = 10;    // equality m < n
+  const int p = 30;   // inequality
+  const int o = 16;    // cost function
+
+  std::srand((long) vpTime::measureTimeMs());
+
+  vpMatrix A, Q, C;
+  vpColVector b, d, r;
+
+  A = randM(m,n)*5;
+  b = randV(m)*5;
+  Q = randM(o,n)*5;
+  r = randV(o)*5;
+  C = randM(p,n)*5;
+
+  // make sure Cx <= d has a solution within Ax = b
+  vpColVector x = A.solveBySVD(b);
+  d = C*x;
+  for(int i = 0; i < p; ++i)
+    d[i] += (5.*rand())/RAND_MAX;
+
+  // solver with stored equality and warm start
+  vpQuadProg qp_WS;
+  qp_WS.setEqualityConstraint(A, b);
+
+  vpQuadProg qp_ineq_WS;
+  qp_ineq_WS.setEqualityConstraint(A, b);
+
+  // timing
+  int total = 1000;
+  double t, t_WS(0), t_noWS(0), t_ineq_WS(0), t_ineq_noWS(0);
+  const double eps = 1e-2;
+
+  QPPlot plot(2, total, {"only equalities", "pre-solving", "equalities + inequalities", "pre-solving / warm start"});
+
+  for(int k = 0; k < total; ++k)
+  {
+    // small change on QP data (A and b are constant)
+    Q += eps * randM(o,n);
+    r += eps * randV(o);
+    C += eps * randM(p,n);
+    d += eps * randV(p);
+
+    // solve only equalities
+    // without warm start
+    x = 0;
+    t = vpTime::measureTimeMs();
+    vpQuadProg::solveQPe(Q, r, A, b, x);
+
+    t_noWS += vpTime::measureTimeMs() - t;
+    plot.plot(0,0,k,t);
+
+
+    // with pre-solved Ax = b
+    x = 0;
+    t = vpTime::measureTimeMs();
+    qp_WS.solveQPe(Q, r, x);
+
+    t_WS += vpTime::measureTimeMs() - t;
+    plot.plot(0,1,k,t);
+
+    // with inequalities
+    // without warm start
+    x = 0;
+    vpQuadProg qp;
+    t = vpTime::measureTimeMs();
+    qp.solveQP(Q, r, A, b, C, d, x);
+
+    t_ineq_noWS += vpTime::measureTimeMs() - t;
+    plot.plot(1,0,k,t);
+
+    // with warm start + pre-solving
+    x = 0;
+    t = vpTime::measureTimeMs();
+    qp_ineq_WS.solveQPi(Q, r, C, d, x, true);
+
+    t_ineq_WS += vpTime::measureTimeMs() - t;
+    plot.plot(1,1,k,t);
+  }
+
+  std::cout.precision(3);
+  cout << "With only equality constraints\n";
+  cout << "   pre-solving: t = " << t_WS << " ms (for 1 QP = " << t_WS/total << " ms)\n";
+  cout << "   no pre-solving: t = " << t_noWS << " ms (for 1 QP = " << t_noWS/total << " ms)\n\n";
+
+  cout << "With inequality constraints\n";
+  cout << "   Warm start: t = " << t_ineq_WS << " ms (for 1 QP = " << t_ineq_WS/total << " ms)\n";
+  cout << "   No warm start: t = " << t_ineq_noWS << " ms (for 1 QP = " << t_ineq_noWS/total << " ms)" << endl;
+
+  plot.wait();
+}
---
+++ modules/core/include/visp3/core/vpQuadProg.h
@@ -0,0 +1,175 @@
+/****************************************************************************
+ *
+ * This file is part of the ViSP software.
+ * Copyright (C) 2005 - 2017 by Inria. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact Inria about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See http://visp.inria.fr for more information.
+ *
+ * This software was developed at:
+ * Inria Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Inria at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Description:
+ * Quadratic Programming
+ *
+ * Authors:
+ * Olivier Kermorgant
+ *
+ *****************************************************************************/
+
+#ifndef vpQuadProgh
+#define vpQuadProgh
+
+#include <vector>
+#include <stdlib.h>
+#include <visp3/core/vpConfig.h>
+#include <visp3/core/vpMatrix.h>
+#include <visp3/core/vpMatrixException.h>
+#include <visp3/core/vpLinProg.h>
+
+/*!
+  \file vpQuadProg.h
+  \brief Implementation of Quadratic Program with Active Sets.
+*/
+
+/*!
+  \class vpQuadProg
+  \ingroup group_core_optim
+  \brief This class provides a solver for Quadratic Programs.
+
+  The cost function is written under the form \f$ \min ||\mathbf{Q}\mathbf{x} - \mathbf{r}||^2\f$.
+
+  If a cost function is written under the canonical form \f$\min \frac{1}{2}\mathbf{x}^T\mathbf{H}\mathbf{x} + \mathbf{c}^T\mathbf{x}\f$
+  then fromCanonicalCost() can be used to retrieve Q and r from H and c.
+
+  Equality constraints are solved through projection into the kernel.
+
+  Inequality constraints are solved with active sets.
+
+  In order to be used sequentially, the decomposition of the equality constraint may be stored.
+  The last active set is always stored and used to warm start the next call.
+*/
+class VISP_EXPORT vpQuadProg
+{
+public:
+
+  /** @name Instanciated solvers  */
+  //@{
+  bool solveQPe(const vpMatrix &Q, const vpColVector &r,
+                vpColVector &x) const;
+
+  bool solveQPi(const vpMatrix &Q, const vpColVector &r,
+                const vpMatrix &C, const vpColVector &d,
+                vpColVector &x,
+                const bool use_equality = false,
+                const double &tol = 1e-6);
+
+  bool solveQP(const vpMatrix &Q, const vpColVector &r,
+               vpMatrix A, vpColVector b,
+               const vpMatrix &C, const vpColVector &d,
+               vpColVector &x, const double &tol = 1e-6);
+  //@}
+
+  /** @name Managing sequential calls to solvers  */
+  //@{
+  bool setEqualityConstraint(const vpMatrix &A, const vpColVector &b, const double &tol = 1e-6);
+  /*!
+    Resets the active set that was found by a previous call to solveQP() or solveQPi(), if any.
+  */
+  void resetActiveSet()
+  {
+    active.clear();
+  }
+  //@}
+
+  static void fromCanonicalCost(const vpMatrix &H, const vpColVector &c, vpMatrix &Q, vpColVector &r, const double &tol = 1e-6);
+  static bool solveQPe(const vpMatrix &Q, const vpColVector &r,
+                vpMatrix A, vpColVector b,
+                vpColVector &x, const double &tol = 1e-6);
+
+protected:
+  /*!
+    Active set from the last call to solveQP() or solveQPi(). Used for warm starting the next call.
+  */
+  std::vector<unsigned int> active;
+  /*!
+    Inactive set from the last call to solveQP() or solveQPi(). Used for warm starting the next call.
+  */
+  std::vector<unsigned int> inactive;
+  /*!
+    Stored particular solution from the last call to setEqualityConstraint().
+  */
+  vpColVector x1;
+  /*!
+    Stored projection to the kernel from the last call to setEqualityConstraint().
+  */
+  vpMatrix Z;
+
+  static bool solveByProjection(const vpMatrix &Q, const vpColVector &r,
+                                vpMatrix &A, vpColVector &b,
+                                vpColVector &x, const double &tol = 1e-6);
+
+  /*!
+    Performs a dimension check of passed QP matrices and vectors.
+
+    If any inconsistency is detected, displays a summary and throws an exception.
+
+  \param Q : cost matrix (dimension c x n)
+  \param r : cost vector (dimension c)
+  \param A : pointer to the equality matrix (if any, dimension m x n)
+  \param b : pointer to the equality vector (if any, dimension m)
+  \param C : pointer to the inequality matrix (if any, dimension p x n)
+  \param d : pointer to the inequality vector (if any, dimension p)
+  \param fct : name of the solver that called this function
+
+  \return the dimension of the search space.
+  */
+  static unsigned int checkDimensions(const vpMatrix &Q, const vpColVector &r,
+                                      const vpMatrix* A, const vpColVector* b,
+                                      const vpMatrix* C, const vpColVector* d,
+                                      const std::string fct)
+  {
+    // check data consistency
+    const unsigned int n = Q.getCols();
+    const bool Ab = (A != NULL && b != NULL && A->getRows());
+    const bool Cd = (C != NULL && d != NULL && C->getRows());
+
+    if (  (Ab && n != A->getCols()) ||
+          (Cd && n != C->getCols()) ||
+          (Ab && A->getRows() != b->getRows()) ||
+          (Cd && C->getRows() != d->getRows()) ||
+          Q.getRows() != r.getRows())
+    {
+      std::cout << "vpQuadProg::" << fct << ": wrong dimension\n" <<
+                   "Q: " << Q.getRows() << "x" << Q.getCols() << " - r: " << r.getRows() << std::endl;
+      if(Ab)
+        std::cout << "A: " << A->getRows() << "x" << A->getCols() << " - b: " << b->getRows() << std::endl;
+      if(Cd)
+        std::cout << "C: " << C->getRows() << "x" << C->getCols() << " - d: " << d->getRows() << std::endl;
+      throw vpMatrixException::dimensionError;
+    }
+    return n;
+  }
+};
+
+
+#endif
