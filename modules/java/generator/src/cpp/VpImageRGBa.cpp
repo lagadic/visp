93c8a3387264428eaa7524352fe8bd4a85aa1855
Fabien Spindler <Fabien.Spindler@inria.fr>
2018-06-21 10:54:59 +0200

    Fix build on OSX - remove bindings for deprecated functions - indent code with tab replaced with 2 spaces - change extern "C" usage - remove empty lines

--- modules/java/generator/src/cpp/VpImageRGBa.cpp
+++ modules/java/generator/src/cpp/VpImageRGBa.cpp
@@ -1,64 +1,67 @@
-#include <jni.h>
 #include <cstring>
 #include <sstream>
 #include <visp3/core/vpImage.h>
 #include <visp3/core/vpRGBa.h>
-using namespace std;
 
-extern "C"{
+extern "C" {
+
+#if !defined(__ppc__)
+// to suppress warning from jni.h on OS X
+#  define TARGET_RT_MAC_CFM 0
+#endif
+#include <jni.h>
+
+} // extern "C"
 
 // Java Method:    VpImageRGBa()
 JNIEXPORT jlong JNICALL Java_org_visp_core_VpImageRGBa_n_1VpImageRGBa__
-  (JNIEnv *env, jclass, jstring type){
-	return (jlong) new vpImage<vpRGBa>();
+(JNIEnv *env, jclass, jstring type){
+  return (jlong) new vpImage<vpRGBa>();
 }
-
 
 // Java Method:    VpImageRGBa(int r, int c)
 JNIEXPORT jlong JNICALL Java_org_visp_core_VpImageRGBa_n_1VpImageRGBa__II
-  (JNIEnv *env, jclass, jint r, jint c){
-	return (jlong) new vpImage<vpRGBa>(r,c);
+(JNIEnv *env, jclass, jint r, jint c){
+  return (jlong) new vpImage<vpRGBa>(r,c);
 }
 
 // Java Method:    VpImageRGBa(int r, int c, byte val)
 JNIEXPORT jlong JNICALL Java_org_visp_core_VpImageRGBa_n_1VpImageRGBa__IICCCC
-  (JNIEnv *env, jclass, jint r, jint c, jchar R, jchar G, jchar B, jchar A){
-	vpRGBa val(R,G,B,A);
-	return (jlong) new vpImage<vpRGBa>(r,c,val);
+(JNIEnv *env, jclass, jint r, jint c, jchar R, jchar G, jchar B, jchar A){
+  vpRGBa val(R,G,B,A);
+  return (jlong) new vpImage<vpRGBa>(r,c,val);
 }
 
 // Java Method:    VpImageRGBa(byte[] array, int height, int width, boolean copyData)
 JNIEXPORT jlong JNICALL Java_org_visp_core_VpImageRGBa_n_1VpImageRGBa___3BIIZ
-  (JNIEnv *env, jclass, jbyteArray arr, jint h, jint w, jboolean copyData){
-	jbyte *array = env->GetByteArrayElements(arr, NULL);
-	
-	return (jlong) new vpImage<vpRGBa>((vpRGBa *const) array, (const unsigned int) h, (const unsigned int) w, copyData);
-	
-	// be memory friendly
-	env->ReleaseByteArrayElements(arr, array, 0);
+(JNIEnv *env, jclass, jbyteArray arr, jint h, jint w, jboolean copyData){
+  jbyte *array = env->GetByteArrayElements(arr, NULL);
+
+  return (jlong) new vpImage<vpRGBa>((vpRGBa *const) array, (const unsigned int) h, (const unsigned int) w, copyData);
+
+  // be memory friendly
+  env->ReleaseByteArrayElements(arr, array, 0);
 }
 
 // Java Method:    getCols()
 JNIEXPORT jint JNICALL Java_org_visp_core_VpImageRGBa_n_1cols
-  (JNIEnv *env, jclass, jlong address){
-	vpImage<vpRGBa>* me = (vpImage<vpRGBa>*) address; //TODO: check for NULL
-	return me->getCols();
+(JNIEnv *env, jclass, jlong address){
+  vpImage<vpRGBa>* me = (vpImage<vpRGBa>*) address; //TODO: check for NULL
+  return me->getCols();
 }
 
 // Java Method:    getRows()
 JNIEXPORT jint JNICALL Java_org_visp_core_VpImageRGBa_n_1rows
-  (JNIEnv *env, jclass, jlong address){
-	vpImage<vpRGBa>* me = (vpImage<vpRGBa>*) address; //TODO: check for NULL
-	return me->getRows();
+(JNIEnv *env, jclass, jlong address){
+  vpImage<vpRGBa>* me = (vpImage<vpRGBa>*) address; //TODO: check for NULL
+  return me->getRows();
 }
 
 // Java Method:    dump()
 JNIEXPORT jstring JNICALL Java_org_visp_core_VpImageRGBa_n_1dump
-  (JNIEnv *env, jclass, jlong address){
-	vpImage<vpRGBa>* me = (vpImage<vpRGBa>*) address; //TODO: check for NULL
-	std::stringstream ss;
-	ss << *me;
-	return env->NewStringUTF(ss.str().c_str());
-}
-
+(JNIEnv *env, jclass, jlong address){
+  vpImage<vpRGBa>* me = (vpImage<vpRGBa>*) address; //TODO: check for NULL
+  std::stringstream ss;
+  ss << *me;
+  return env->NewStringUTF(ss.str().c_str());
 }
