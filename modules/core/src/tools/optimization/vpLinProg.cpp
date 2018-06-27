5161ffac831accefce3df254e91862975c72e42d
Fabien Spindler <Fabien.Spindler@inria.fr>
2018-06-27 11:45:34 +0200

    Fix warning

--- modules/core/src/tools/optimization/vpLinProg.cpp
+++ modules/core/src/tools/optimization/vpLinProg.cpp
@@ -128,11 +128,11 @@ bool vpLinProg::colReduction(vpMatrix &A, vpColVector &b, bool full_rank, const 
         IQQt[j][j] += 1;
       // most of the time the first n-m columns are just fine
       A = IQQt.extract(0,0,n,n-m);
-      if((A.t()*A).detByLU() == 0)
+      if (std::fabs((A.t()*A).detByLU()) <= std::numeric_limits<double>::epsilon())
       {
         // rank deficiency, manually find n-m independent columns
         unsigned int j0;
-        for(j0 = 0; j0 < n; ++j0)
+        for (j0 = 0; j0 < n; ++j0)
         {
           if(!vpLinProg::allZero(IQQt.getCol(j0)))
           {
@@ -145,10 +145,10 @@ bool vpLinProg::colReduction(vpMatrix &A, vpColVector &b, bool full_rank, const 
         while(A.getCols() < n-m)
         {
           // add next column and check rank of A^T.A
-          if(!vpLinProg::allZero(IQQt.getCol(j)))
+          if (!vpLinProg::allZero(IQQt.getCol(j)))
           {
             A = vpMatrix::juxtaposeMatrices(A, IQQt.getCol(j));
-            if((A.t()*A).detByLU() == 0)
+            if (std::fabs((A.t()*A).detByLU()) <= std::numeric_limits<double>::epsilon())
               A.resize(n,A.getCols()-1, false);
           }
           j++;
@@ -179,7 +179,7 @@ bool vpLinProg::colReduction(vpMatrix &A, vpColVector &b, bool full_rank, const 
       IQQt[j][j] += 1;
     // most of the time the first n-r columns are just fine
     A = IQQt.extract(0,0,n,n-r);
-    if((A.t()*A).detByLU() == 0)
+    if (std::fabs((A.t()*A).detByLU()) <= std::numeric_limits<double>::epsilon())
     {
       // rank deficiency, manually find n-r independent columns
       unsigned int j0;
@@ -199,7 +199,7 @@ bool vpLinProg::colReduction(vpMatrix &A, vpColVector &b, bool full_rank, const 
         if(!vpLinProg::allZero(IQQt.getCol(j)))
         {
           A = vpMatrix::juxtaposeMatrices(A, IQQt.getCol(j));
-          if((A.t()*A).detByLU() == 0)
+          if (std::fabs((A.t()*A).detByLU()) <= std::numeric_limits<double>::epsilon())
             A.resize(n,A.getCols()-1, false);
         }
         j++;
