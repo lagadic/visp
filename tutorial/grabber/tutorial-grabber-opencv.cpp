c2e4cb5120d6f88ee5ebfbe87f791c83e13e950e
Fabien Spindler <Fabien.Spindler@inria.fr>
2018-11-15 14:45:36 +0100

    Cosmetic improvements

--- tutorial/grabber/tutorial-grabber-opencv.cpp
+++ tutorial/grabber/tutorial-grabber-opencv.cpp
@@ -5,7 +5,7 @@
 #include <visp3/gui/vpDisplayOpenCV.h>
 #include <visp3/io/vpImageIo.h>
 
-// usage: binary <device name>
+// usage: binary -h
 // device name: 0 is the default to dial with the first camera,
 // 1 to dial with a second camera attached to the computer
 int main(int argc, char **argv)
@@ -23,10 +23,12 @@ int main(int argc, char **argv)
       opt_record_mode = std::atoi(argv[i + 1]);
     else if (std::string(argv[i]) == "--help" || std::string(argv[i]) == "-h") {
       std::cout << "\nUsage: " << argv[0]
-                << " [--seqname <sequence name>] [--record <0: continuous (default) | 1: single shot>] "
+                << " [--device <device name (default: 0)>] [--seqname <sequence name>] [--record <0: continuous (default) | 1: single shot>] "
                    "[--help] [-h]\n"
                 << "\nExample to visualize images:\n"
                 << "  " << argv[0] << "\n"
+                << "\nExample to visualize images from a second camera:\n"
+                << "  " << argv[0] << "--device 1\n"
                 << "\nExamples to record a sequence:\n"
                 << "  " << argv[0] << " --seqname I%04d.png \n"
                 << "  " << argv[0] << " --seqname folder/I%04d.png --record 0\n"
@@ -70,7 +72,9 @@ int main(int argc, char **argv)
 
     unsigned int counter = 1;
     bool start_record = false;
+
     for (;;) {
+      double t = vpTime::measureTimeMs();
       cap >> frame; // get a new frame from camera
       // Convert the image in ViSP format and display it
       vpImageConvert::convert(frame, I);
@@ -79,23 +83,23 @@ int main(int argc, char **argv)
       if (! opt_seqname.empty()) {
         if (! opt_record_mode) { // continuous
           if (start_record) {
-            vpDisplay::displayText(I, 10, 10, "Left  click: stop recording", vpColor::red);
+            vpDisplay::displayText(I, 20, 10, "Left  click: stop recording", vpColor::red);
           }
           else {
-            vpDisplay::displayText(I, 10, 10, "Left  click: start recording", vpColor::red);
+            vpDisplay::displayText(I, 20, 10, "Left  click: start recording", vpColor::red);
           }
         }
         else {
-          vpDisplay::displayText(I, 10, 10, "Left  click: record image", vpColor::red);
+          vpDisplay::displayText(I, 20, 10, "Left  click: record image", vpColor::red);
         }
-        vpDisplay::displayText(I, 30, 10, "Right click to quit", vpColor::red);
+        vpDisplay::displayText(I, 40, 10, "Right click: quit", vpColor::red);
       }
       else {
-        vpDisplay::displayText(I, 10, 10, "Click to quit", vpColor::red);
+        vpDisplay::displayText(I, 20, 10, "Click to quit", vpColor::red);
       }
 
       if (! opt_seqname.empty()) {
-        vpDisplay::displayText(I, 50, 10, text_record_mode, vpColor::red);
+        vpDisplay::displayText(I, 60, 10, text_record_mode, vpColor::red);
       }
       vpMouseButton::vpMouseButtonType button;
       if (vpDisplay::getClick(I, button, false)) {
@@ -130,7 +134,7 @@ int main(int argc, char **argv)
 
         counter ++;
         std::string text = std::string("Save: ") + std::string(filename);
-        vpDisplay::displayText(I, 70, 10, text, vpColor::red);
+        vpDisplay::displayText(I, 80, 10, text, vpColor::red);
         std::cout << text << std::endl;
         vpImageIo::write(I, filename);
         if (opt_record_mode == 1) { // single shot mode
@@ -138,6 +142,9 @@ int main(int argc, char **argv)
         }
       }
 
+      std::stringstream ss;
+      ss << "Acquisition time: " << std::setprecision(3) << vpTime::measureTimeMs() - t << " ms";
+      vpDisplay::displayText(I, I.getHeight() - 20, 10, ss.str(), vpColor::red);
       vpDisplay::flush(I);
     }
   } catch (const vpException &e) {
