93315dd8e5bace95b47127050fa93d9539f36db3
Fabien Spindler <Fabien.Spindler@inria.fr>
2018-12-10 18:51:09 +0100

    Introduce pixel-meter and meter-pixel conversion using OpenCV camera parameters Update doc and test accordingly

--- modules/core/include/visp3/core/vpPixelMeterConversion.h
+++ modules/core/include/visp3/core/vpPixelMeterConversion.h
@@ -51,33 +51,45 @@
 #include <visp3/core/vpImagePoint.h>
 #include <visp3/core/vpMath.h>
 
+#if VISP_HAVE_OPENCV_VERSION >= 0x020300
+#  include <opencv2/imgproc/imgproc.hpp>
+#endif
+
 /*!
   \class vpPixelMeterConversion
 
   \ingroup group_core_camera
 
-  \brief Conversion from pixel coordinates \f$(u,v)\f$ to normalized
-  coordinates \f$(x,y)\f$ in meter.
+  Various conversion functions to transform primitives (2D line, moments, 2D point) from pixel to normalized
+  coordinates in meter in the image plane.
 
-  This class relates to vpCameraParameters.
+  Tranformation relies either on ViSP camera parameters implemented in vpCameraParameters or on OpenCV camera parameters
+  that are set from a projection matrix and a distorsion coefficients vector.
 
 */
 class VISP_EXPORT vpPixelMeterConversion
 {
 public:
+  /** @name Using ViSP camera parameters  */
+  //@{
+  static void convertLine(const vpCameraParameters &cam, const double &rho_p, const double &theta_p, double &rho_m,
+                          double &theta_m);
+
+  static void convertMoment(const vpCameraParameters &cam, unsigned int order, const vpMatrix &moment_pixel,
+                            vpMatrix &moment_meter);
   /*!
-    \brief Point coordinates conversion from pixel coordinates
-    \f$(u,v)\f$ to normalized coordinates \f$(x,y)\f$ in meter.
+    Point coordinates conversion from pixel coordinates
+    \f$(u,v)\f$ to normalized coordinates \f$(x,y)\f$ in meter using ViSP camera parameters.
 
     The used formula depends on the projection model of the camera. To
     know the currently used projection model use
     vpCameraParameter::get_projModel()
 
-    \param cam : camera parameters.
-    \param u : input coordinate in pixels along image horizontal axis.
-    \param v : input coordinate in pixels along image vertical axis.
-    \param x : output coordinate in meter along image plane x-axis.
-    \param y : output coordinate in meter along image plane y-axis.
+    \param[in] cam : camera parameters.
+    \param[in] u : input coordinate in pixels along image horizontal axis.
+    \param[in] v : input coordinate in pixels along image vertical axis.
+    \param[out] x : output coordinate in meter along image plane x-axis.
+    \param[out] y : output coordinate in meter along image plane y-axis.
 
     \f$ x = (u-u_0)/p_x \f$ and \f$ y = (v-v_0)/p_y  \f$ in the case of
     perspective projection without distortion.
@@ -87,7 +99,8 @@ public:
     with \f$ r^2=((u - u_0)/p_x)^2+((v-v_0)/p_y)^2 \f$ in the case of
     perspective projection with distortion.
   */
-  inline static void convertPoint(const vpCameraParameters &cam, const double &u, const double &v, double &x, double &y)
+  inline static void convertPoint(const vpCameraParameters &cam,
+                                  const double &u, const double &v, double &x, double &y)
   {
     switch (cam.projModel) {
     case vpCameraParameters::perspectiveProjWithoutDistortion:
@@ -100,17 +113,17 @@ public:
   }
 
   /*!
-    \brief Point coordinates conversion from pixel coordinates
-    Coordinates in pixel to normalized coordinates \f$(x,y)\f$ in meter.
+    Point coordinates conversion from pixel coordinates
+    Coordinates in pixel to normalized coordinates \f$(x,y)\f$ in meter using ViSP camera parameters.
 
     The used formula depends on the projection model of the camera. To
     know the currently used projection model use
     vpCameraParameter::get_projModel()
 
-    \param cam : camera parameters.
-    \param iP : input coordinates in pixels.
-    \param x : output coordinate in meter along image plane x-axis.
-    \param y : output coordinate in meter along image plane y-axis.
+    \param[in] cam : camera parameters.
+    \param[in] iP : input coordinates in pixels.
+    \param[out] x : output coordinate in meter along image plane x-axis.
+    \param[out] y : output coordinate in meter along image plane y-axis.
 
     Thanks to the pixel coordinates in the frame (u,v), the meter coordinates
     are given by :
@@ -123,7 +136,8 @@ public:
     with \f$ r^2=((u - u_0)/p_x)^2+((v-v_0)/p_y)^2 \f$ in the case of
     perspective projection with distortion.
   */
-  inline static void convertPoint(const vpCameraParameters &cam, const vpImagePoint &iP, double &x, double &y)
+  inline static void convertPoint(const vpCameraParameters &cam,
+                                  const vpImagePoint &iP, double &x, double &y)
   {
     switch (cam.projModel) {
     case vpCameraParameters::perspectiveProjWithoutDistortion:
@@ -135,63 +149,64 @@ public:
     }
   }
 
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
   /*!
-    \brief Point coordinates conversion without distortion from pixel
+    Point coordinates conversion without distortion from pixel
     coordinates \f$(u,v)\f$ to normalized coordinates \f$(x,y)\f$ in meter.
 
-    \param cam : camera parameters.
-    \param u : input coordinate in pixels along image horizontal axis.
-    \param v : input coordinate in pixels along image vertical axis.
-    \param x : output coordinate in meter along image plane x-axis.
-    \param y : output coordinate in meter along image plane y-axis.
+    \param[in] cam : camera parameters.
+    \param[in] u : input coordinate in pixels along image horizontal axis.
+    \param[in] v : input coordinate in pixels along image vertical axis.
+    \param[out] x : output coordinate in meter along image plane x-axis.
+    \param[out] y : output coordinate in meter along image plane y-axis.
 
     \f$ x = (u-u_0)/p_x \f$ and  \f$ y = (v-v_0)/p_y  \f$
   */
-  inline static void convertPointWithoutDistortion(const vpCameraParameters &cam, const double &u, const double &v,
-                                                   double &x, double &y)
+  inline static void convertPointWithoutDistortion(const vpCameraParameters &cam,
+                                                   const double &u, const double &v, double &x, double &y)
   {
     x = (u - cam.u0) * cam.inv_px;
     y = (v - cam.v0) * cam.inv_py;
   }
 
   /*!
-    \brief Point coordinates conversion without distortion from pixel
+    Point coordinates conversion without distortion from pixel
     coordinates Coordinates in pixel to normalized coordinates \f$(x,y)\f$ in
     meter.
 
-    \param cam : camera parameters.
-    \param iP : input coordinates in pixels.
-    \param x : output coordinate in meter along image plane x-axis.
-    \param y : output coordinate in meter along image plane y-axis.
+    \param[in] cam : camera parameters.
+    \param[in] iP : input coordinates in pixels.
+    \param[out] x : output coordinate in meter along image plane x-axis.
+    \param[out] y : output coordinate in meter along image plane y-axis.
 
     Thanks to the pixel coordinates in the frame (u,v), the meter coordinates
     are given by :
 
     \f$ x = (u-u_0)/p_x \f$ and  \f$ y = (v-v_0)/p_y  \f$
   */
-  inline static void convertPointWithoutDistortion(const vpCameraParameters &cam, const vpImagePoint &iP, double &x,
-                                                   double &y)
+  inline static void convertPointWithoutDistortion(const vpCameraParameters &cam,
+                                                   const vpImagePoint &iP, double &x, double &y)
   {
     x = (iP.get_u() - cam.u0) * cam.inv_px;
     y = (iP.get_v() - cam.v0) * cam.inv_py;
   }
 
   /*!
-    \brief Point coordinates conversion with distortion from pixel coordinates
+    Point coordinates conversion with distortion from pixel coordinates
     \f$(u,v)\f$ to normalized coordinates \f$(x,y)\f$ in meter.
 
-    \param cam : camera parameters.
-    \param u : input coordinate in pixels along image horizontal axis.
-    \param v : input coordinate in pixels along image vertical axis.
-    \param x : output coordinate in meter along image plane x-axis.
-    \param y : output coordinate in meter along image plane y-axis.
+    \param[in] cam : camera parameters.
+    \param[in] u : input coordinate in pixels along image horizontal axis.
+    \param[in] v : input coordinate in pixels along image vertical axis.
+    \param[out] x : output coordinate in meter along image plane x-axis.
+    \param[out] y : output coordinate in meter along image plane y-axis.
 
     \f$ x = (u-u_0)*(1+k_{du}*r^2)/p_x \f$ and
     \f$ y = (v-v_0)*(1+k_{du}*r^2)/p_y \f$
     with \f$ r^2=((u - u_0)/p_x)^2 + ((v-v_0)/p_y)^2 \f$
   */
-  inline static void convertPointWithDistortion(const vpCameraParameters &cam, const double &u, const double &v,
-                                                double &x, double &y)
+  inline static void convertPointWithDistortion(const vpCameraParameters &cam,
+                                                const double &u, const double &v, double &x, double &y)
   {
     double r2 = 1. + cam.kdu * (vpMath::sqr((u - cam.u0) * cam.inv_px) + vpMath::sqr((v - cam.v0) * cam.inv_py));
     x = (u - cam.u0) * r2 * cam.inv_px;
@@ -199,13 +214,13 @@ public:
   }
 
   /*!
-    \brief Point coordinates conversion with distortion from pixel coordinates
+    Point coordinates conversion with distortion from pixel coordinates
     Coordinates in pixel to normalized coordinates \f$(x,y)\f$ in meter.
 
-    \param cam : camera parameters.
-    \param iP : input coordinates in pixels.
-    \param x : output coordinate in meter along image plane x-axis.
-    \param y : output coordinate in meter along image plane y-axis.
+    \param[in] cam : camera parameters.
+    \param[in] iP : input coordinates in pixels.
+    \param[out] x : output coordinate in meter along image plane x-axis.
+    \param[out] y : output coordinate in meter along image plane y-axis.
 
     Thanks to the pixel coordinates in the frame (u,v), the meter coordinates
     are given by :
@@ -214,26 +229,32 @@ public:
     \f$ y = (v-v_0)*(1+k_{du}*r^2)/p_y \f$
     with \f$ r^2=((u - u_0)/p_x)^2 + ((v-v_0)/p_y)^2 \f$
   */
-  inline static void convertPointWithDistortion(const vpCameraParameters &cam, const vpImagePoint &iP, double &x,
-                                                double &y)
+  inline static void convertPointWithDistortion(const vpCameraParameters &cam,
+                                                const vpImagePoint &iP, double &x, double &y)
   {
     double r2 = 1. + cam.kdu * (vpMath::sqr((iP.get_u() - cam.u0) * cam.inv_px) +
                                 vpMath::sqr((iP.get_v() - cam.v0) * cam.inv_py));
     x = (iP.get_u() - cam.u0) * r2 * cam.inv_px;
     y = (iP.get_v() - cam.v0) * r2 * cam.inv_py;
   }
+#endif // #ifndef DOXYGEN_SHOULD_SKIP_THIS
+  //@}
 
-  //! line coordinates conversion (rho,theta)
-  static void convertLine(const vpCameraParameters &cam, const double &rho_p, const double &theta_p, double &rho_m,
-                          double &theta_m);
-
-  static void convertMoment(const vpCameraParameters &cam, unsigned int order, const vpMatrix &moment_pixel,
+#if VISP_HAVE_OPENCV_VERSION >= 0x020300
+  /** @name Using OpenCV camera parameters  */
+  //@{
+  static void convertLine(const cv::Mat &cameraMatrix,
+                          const double &rho_p, const double &theta_p,
+                          double &rho_m, double &theta_m);
+  static void convertMoment(const cv::Mat &cameraMatrix,
+                            unsigned int order, const vpMatrix &moment_pixel,
                             vpMatrix &moment_meter);
+  static void convertPoint(const cv::Mat &cameraMatrix, const cv::Mat &distCoeffs,
+                           const double &u, const double &v, double &x, double &y);
+  static void convertPoint(const cv::Mat &cameraMatrix, const cv::Mat &distCoeffs,
+                           const vpImagePoint &iP, double &x, double &y);
+  //@}
+#endif
 };
 
 #endif
-/*
- * Local variables:
- * c-basic-offset: 2
- * End:
- */
--- modules/core/test/camera/testCameraParametersConversion.cpp
+++ modules/core/test/camera/testCameraParametersConversion.cpp
@@ -56,6 +56,7 @@ int main()
 {
   try {
     {
+      std::cout << "* Test operator=()" << std::endl;
       vpCameraParameters cam1, cam2;
       cam1.initPersProjWithDistortion(700.0, 700.0, 320.0, 240.0, 0.1, 0.1);
       cam2.initPersProjWithoutDistortion(700.0, 700.0, 320.0, 240.0);
@@ -70,6 +71,7 @@ int main()
         return EXIT_FAILURE;
       }
 
+      std::cout << "* Test computeFov()" << std::endl;
       cam2.computeFov(640u, 480u);
       if (cam1 == cam2) {
         std::cerr << "Issue with vpCameraParameters comparison operator." << std::endl;
@@ -99,33 +101,206 @@ int main()
     double v1 = 240;
     double x1 = 0, y1 = 0;
     double u2 = 0, v2 = 0;
+    std::cout << "* Test point conversion without distorsion" << std::endl;
     vpPixelMeterConversion::convertPoint(cam, u1, v1, x1, y1);
     vpMeterPixelConversion::convertPoint(cam, x1, y1, u2, v2);
     if (!vpMath::equal(u1, u2) || !vpMath::equal(v1, v2)) {
-      vpTRACE("Error in convertPoint without distortion:\n"
-              "u1 = %f, u2 = %f\n"
-              "v1 = %f, v2 = %f\n",
-              u1, u2, v1, v2);
-      return -1;
+      std::cerr << "Error in point conversion without distortion:\n"
+                << "u1 = " << u1 << ", u2 = " << u2 << std::endl
+                << "v1 = " << v1 << ", v2 = " << v2 << std::endl;
+      return EXIT_FAILURE;
     }
-    vpTRACE("convertPoint without distortion :\n"
-            "u1 - u2 = %.20f\n"
-            "v1 - v2 = %.20f\n",
-            u1 - u2, v1 - v2);
 
+    std::cout << "* Test point conversion with distorsion" << std::endl;
     vpPixelMeterConversion::convertPoint(camDist, u1, v1, x1, y1);
     vpMeterPixelConversion::convertPoint(camDist, x1, y1, u2, v2);
     if (!vpMath::equal(u1, u2) || !vpMath::equal(v1, v2)) {
-      vpTRACE("Error in convertPoint with distortion :\n"
-              "u1 = %f, u2 = %f\n"
-              "v1 = %f, v2 = %f\n",
-              u1, u2, v1, v2);
-      return -1;
+      std::cerr << "Error in point conversion without distortion:\n"
+                << "u1 = " << u1 << ", u2 = " << u2 << std::endl
+                << "v1 = " << v1 << ", v2 = " << v2 << std::endl;
+      return EXIT_FAILURE;
     }
-    vpTRACE("convertPoint with distortion :\n"
-            "u1 - u2 = %.20f\n"
-            "v1 - v2 = %.20f\n",
-            u1 - u2, v1 - v2);
+
+    {
+      std::cout << "* Compare ViSP and OpenCV point pixel meter conversion without distorsion" << std::endl;
+      cv::Mat cameraMatrix = (cv::Mat_<double>(3,3) << px,  0, u0,
+                                                        0, py, v0,
+                                                        0,  0,  1);
+      cv::Mat distCoeffs = cv::Mat::zeros(5,1,CV_64FC1);
+      double x2, y2, u2, v2;
+
+      vpPixelMeterConversion::convertPoint(cam, u1, v1, x1, y1);
+      vpPixelMeterConversion::convertPoint(cameraMatrix, distCoeffs, u1, v1, x2, y2);
+      if ( !vpMath::equal(x1, x2, 1e-6) || !vpMath::equal(y1, y2, 1e-6)) {
+        std::cerr << "Error in point pixel meter conversion: visp result (" << x1 << ", " << y1 << ") "
+                  << "differ from OpenCV result (" << x2 << ", " << y2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+
+      vpImagePoint ip(v1, u1);
+      vpPixelMeterConversion::convertPoint(cam, ip, x1, y1);
+      vpPixelMeterConversion::convertPoint(cameraMatrix, distCoeffs, ip, x2, y2);
+      if ( !vpMath::equal(x1, x2, 1e-6) || !vpMath::equal(y1, y2, 1e-6)) {
+        std::cerr << "Error in point pixel meter conversion: visp result (" << x1 << ", " << y1 << ") "
+                  << "differ from OpenCV result (" << x2 << ", " << y2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+
+
+      std::cout << "* Compare ViSP and OpenCV point meter pixel conversion without distorsion" << std::endl;
+      vpMeterPixelConversion::convertPoint(cam, x1, y1, u1, v1);
+      vpMeterPixelConversion::convertPoint(cameraMatrix, distCoeffs, x1, y1, u2, v2);
+      if ( !vpMath::equal(u1, u2, 1e-6) || !vpMath::equal(v1, v2, 1e-6)) {
+        std::cerr << "Error in point meter pixel conversion: visp result (" << u1 << ", " << v1 << ") "
+                  << "differ from OpenCV result (" << u2 << ", " << v2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+
+      vpImagePoint iP1, iP2;
+      vpMeterPixelConversion::convertPoint(cam, x1, y1, iP1);
+      vpMeterPixelConversion::convertPoint(cameraMatrix, distCoeffs, x1, y1, iP2);
+      if ( vpImagePoint::distance(iP1, iP2) > 1e-6) {
+        std::cerr << "Error in point meter pixel conversion: visp result (" << u1 << ", " << v1 << ") "
+                  << "differ from OpenCV result (" << u2 << ", " << v2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      std::cout << "* Compare ViSP and OpenCV point pixel meter conversion with distorsion" << std::endl;
+      cv::Mat cameraMatrix = (cv::Mat_<double>(3,3) << px_dist,       0, u0_dist,
+                                                             0, py_dist, v0_dist,
+                                                             0,       0,       1);
+      cv::Mat distCoeffs = cv::Mat::zeros(5,1,CV_64FC1);
+      distCoeffs.at<double>(0,0) = kdu_dist;
+      double x2, y2;
+
+      vpPixelMeterConversion::convertPoint(camDist, u1, v1, x1, y1);
+      vpPixelMeterConversion::convertPoint(cameraMatrix, distCoeffs, u1, v1, x2, y2);
+      if ( !vpMath::equal(x1, x2, 1e-6) || !vpMath::equal(y1, y2, 1e-6)) {
+        std::cerr << "Error in point conversion: visp result (" << x1 << ", " << y1 << ") "
+                  << "differ from OpenCV result (" << x2 << ", " << y2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+
+      std::cout << "* Compare ViSP and OpenCV point meter pixel conversion with distorsion" << std::endl;
+      distCoeffs.at<double>(0,0) = kud_dist;
+      vpMeterPixelConversion::convertPoint(camDist, x1, y1, u1, v1);
+      vpMeterPixelConversion::convertPoint(cameraMatrix, distCoeffs, x1, y1, u2, v2);
+      if ( !vpMath::equal(u1, u2, 1e-6) || !vpMath::equal(v1, v2, 1e-6)) {
+        std::cerr << "Error in point meter pixel conversion: visp result (" << u1 << ", " << v1 << ") "
+                  << "differ from OpenCV result (" << u2 << ", " << v2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      std::cout << "* Compare ViSP and OpenCV line pixel meter conversion without distorsion" << std::endl;
+      cv::Mat cameraMatrix = (cv::Mat_<double>(3,3) << px,  0, u0,
+                                                        0, py, v0,
+                                                        0,  0,  1);
+      double rho_p = 100, theta_p = vpMath::rad(45);
+      double rho_m1, theta_m1, rho_m2, theta_m2;
+
+      vpPixelMeterConversion::convertLine(cam, rho_p, theta_p, rho_m1, theta_m1);
+      vpPixelMeterConversion::convertLine(cameraMatrix, rho_p, theta_p, rho_m2, theta_m2);
+      if ( !vpMath::equal(rho_m1, rho_m2, 1e-6) || !vpMath::equal(theta_m1, theta_m2, 1e-6)) {
+        std::cerr << "Error in line pixel meter conversion: visp result (" << rho_m1 << ", " << theta_m1 << ") "
+                  << "differ from OpenCV result (" << rho_m2 << ", " << theta_m1 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+
+      std::cout << "* Compare ViSP and OpenCV line meter pixel conversion without distorsion" << std::endl;
+      double rho_p1, theta_p1, rho_p2, theta_p2;
+      vpMeterPixelConversion::convertLine(cam, rho_m1, theta_m1, rho_p1, theta_p1);
+      vpMeterPixelConversion::convertLine(cameraMatrix, rho_m1, theta_m1, rho_p2, theta_p2);
+      if ( !vpMath::equal(rho_p1, rho_p2, 1e-6) || !vpMath::equal(theta_p1, theta_p2, 1e-6)) {
+        std::cerr << "Error in line meter pixel conversion: visp result (" << rho_p1 << ", " << theta_p1 << ") "
+                  << "differ from OpenCV result (" << rho_p2 << ", " << theta_p1 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      std::cout << "* Compare ViSP and OpenCV moments pixel meter conversion without distorsion" << std::endl;
+      cv::Mat cameraMatrix = (cv::Mat_<double>(3,3) << px,  0, u0,
+                                                        0, py, v0,
+                                                        0,  0,  1);
+      unsigned int order = 3;
+      double m00 = 2442, m10 = 414992, m01 = 470311, m11 = 7.99558e+07, m02 = 9.09603e+07, m20 = 7.11158e+07;
+
+      vpMatrix mp(order, order);
+      vpMatrix m1(order, order), m2(order, order);
+
+      mp[0][0] = m00;
+      mp[1][0] = m10;
+      mp[0][1] = m01;
+      mp[2][0] = m20;
+      mp[1][1] = m11;
+      mp[0][2] = m02;
+
+      vpPixelMeterConversion::convertMoment(cam, order, mp, m1);
+      vpPixelMeterConversion::convertMoment(cameraMatrix, order, mp, m2);
+      for (unsigned int i = 0; i < m1.getRows(); i ++) {
+        for (unsigned int j = 0; j < m1.getCols(); j ++) {
+          if ( !vpMath::equal(m1[i][j], m1[i][j], 1e-6) ) {
+            std::cerr << "Error in moments pixel meter conversion: visp result for ["<< i << "]["<< j << "] (" << m1[i][j] << ") "
+                      << "differ from OpenCV result (" << m2[i][j] << ")" << std::endl;
+            return EXIT_FAILURE;
+          }
+        }
+      }
+    }
+
+    {
+      std::cout << "* Compare ViSP and OpenCV ellipse from circle meter pixel conversion without distorsion" << std::endl;
+      cv::Mat cameraMatrix = (cv::Mat_<double>(3,3) << px,  0, u0,
+                                                        0, py, v0,
+                                                        0,  0,  1);
+      vpCircle circle;
+      circle.setWorldCoordinates(0, 0, 1, 0, 0, 0, 0.1); // plane:(Z=0),X0=0,Y0=0,Z=0,R=0.1
+      vpHomogeneousMatrix cMo(0.1, 0.2, 0.5, vpMath::rad(10), vpMath::rad(5), vpMath::rad(45));
+      circle.changeFrame(cMo);
+      circle.projection();
+      vpImagePoint center_p1, center_p2;
+      double mu20_p1, mu11_p1, mu02_p1, mu20_p2, mu11_p2, mu02_p2;
+
+      vpMeterPixelConversion::convertEllipse(cam, circle, center_p1, mu20_p1, mu11_p1, mu02_p1);
+      vpMeterPixelConversion::convertEllipse(cameraMatrix, circle, center_p2, mu20_p2, mu11_p2, mu02_p2);
+
+      if ( !vpMath::equal(mu20_p1, mu20_p2, 1e-6) || !vpMath::equal(mu11_p1, mu11_p2, 1e-6) || !vpMath::equal(mu02_p1, mu02_p2, 1e-6)) {
+        std::cerr << "Error in ellipse from circle meter pixel conversion: visp result (" << mu20_p1 << ", " << mu11_p1 << ", " << mu02_p1 << ") "
+                  << "differ from OpenCV result (" << mu20_p2 << ", " << mu11_p2 << ", " << mu02_p2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+      if ( vpImagePoint::distance(center_p1, center_p2) > 1e-6) {
+        std::cerr << "Error in ellipse from circle meter pixel conversion: visp result (" << center_p1 << ") "
+                  << "differ from OpenCV result (" << center_p2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+
+      std::cout << "* Compare ViSP and OpenCV ellipse from sphere meter pixel conversion without distorsion" << std::endl;
+      vpSphere sphere;
+      sphere.setWorldCoordinates(0, 0, 0, 0.1); // X0=0,Y0=0,Z0=0,R=0.1
+      circle.changeFrame(cMo);
+      circle.projection();
+      vpMeterPixelConversion::convertEllipse(cam, sphere, center_p1, mu20_p1, mu11_p1, mu02_p1);
+      vpMeterPixelConversion::convertEllipse(cameraMatrix, sphere, center_p2, mu20_p2, mu11_p2, mu02_p2);
+
+      if ( !vpMath::equal(mu20_p1, mu20_p2, 1e-6) || !vpMath::equal(mu11_p1, mu11_p2, 1e-6) || !vpMath::equal(mu02_p1, mu02_p2, 1e-6)) {
+        std::cerr << "Error in ellipse from sphere meter pixel conversion: visp result (" << mu20_p1 << ", " << mu11_p1 << ", " << mu02_p1 << ") "
+                  << "differ from OpenCV result (" << mu20_p2 << ", " << mu11_p2 << ", " << mu02_p2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+      if ( vpImagePoint::distance(center_p1, center_p2) > 1e-6) {
+        std::cerr << "Error in ellipse from sphere meter pixel conversion: visp result (" << center_p1 << ") "
+                  << "differ from OpenCV result (" << center_p2 << ")" << std::endl;
+        return EXIT_FAILURE;
+      }
+
+    }
+
+    std::cout << "Test succesful" << std::endl;
     return EXIT_SUCCESS;
   } catch (const vpException &e) {
     std::cout << "Catch an exception: " << e << std::endl;
