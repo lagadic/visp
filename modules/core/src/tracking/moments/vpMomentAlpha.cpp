0658a4ffc7a1e6c4e4ca3ee9c9b6856040ccb4e7
Wenfeng CAI <xoxyzox-m@yahoo.com>
2018-12-19 08:58:13 +0800

    Revert back to atan2() usage in reference mode of vpMomentAlpha
    The issue of out of range [-Pi, Pi] in relative mode (#117) was fixed
    in 64937a5. But 64937a5 also introduced a bug in reference mode
    computation. This commit reverts back to atan2() usage in reference mode
    computation of vpMomentAlpha. Issue #117 was already fixed by the
    updated `if (signChange)` block.

--- modules/core/src/tracking/moments/vpMomentAlpha.cpp
+++ modules/core/src/tracking/moments/vpMomentAlpha.cpp
@@ -82,9 +82,7 @@ void vpMomentAlpha::compute()
   if (!found_moment_centered)
     throw vpException(vpException::notInitialized, "vpMomentCentered not found");
 
-  double t = momentCentered.get(2, 0) - momentCentered.get(0, 2);
-  t = t / sqrt(4.0 * momentCentered.get(1, 1) * momentCentered.get(1, 1) + t * t);
-  double alpha = 0.5 * copysign(1, momentCentered.get(1, 1)) * acos(t);
+  double alpha = 0.5 * atan2(2 * momentCentered.get(1, 1), momentCentered.get(2, 0) - momentCentered.get(0, 2));
 
   std::vector<double> rotMu(4);
   // std::vector<double> realMu(4);
