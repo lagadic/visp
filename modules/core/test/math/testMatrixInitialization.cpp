c64fce7166f79fe9f8f8ae5361154ee2126a2479
Fabien Spindler <Fabien.Spindler@inria.fr>
2019-03-27 17:02:29 +0100

    Update testing

--- modules/core/test/math/testMatrixInitialization.cpp
+++ modules/core/test/math/testMatrixInitialization.cpp
@@ -41,8 +41,54 @@
 
 #include <visp3/core/vpMatrix.h>
 
+bool equal(const vpRotationVector &a1, const vpRotationVector &a2, double epsilon)
+{
+  if (a1.size() != a2.size()) {
+    std::cout << "Rotation vector size differ" << std::endl;
+    return false;
+  }
+  for (unsigned int i=0; i < a1.size(); i++) {
+    if (! vpMath::equal(a1[i], a2[i], epsilon)) {
+      std::cout << "Rotation vector content differ" << std::endl;
+      return false;
+    }
+  }
+  return true;
+}
+
+bool equal(const vpColVector &a1, const vpColVector &a2, double epsilon)
+{
+  if (a1.size() != a2.size()) {
+    std::cout << "Column vector size differ" << std::endl;
+    return false;
+  }
+  for (unsigned int i=0; i < a1.size(); i++) {
+    if (! vpMath::equal(a1[i], a2[i], epsilon)) {
+      std::cout << "Column vector content differ" << std::endl;
+      return false;
+    }
+  }
+  return true;
+}
+
+bool equal(const vpRowVector &a1, const vpRowVector &a2, double epsilon)
+{
+  if (a1.size() != a2.size()) {
+    std::cout << "Row vector size differ" << std::endl;
+    return false;
+  }
+  for (unsigned int i=0; i < a1.size(); i++) {
+    if (! vpMath::equal(a1[i], a2[i], epsilon)) {
+      std::cout << "Row vector content differ" << std::endl;
+      return false;
+    }
+  }
+  return true;
+}
+
 int main()
 {
+  double epsilon = 1e-10;
 #ifdef VISP_HAVE_CXX11
   {
     vpArray2D<float> a{ 1.f, 2.f, 3.f };
@@ -96,15 +142,147 @@ int main()
     m6 = {m2};
     std::cout << "m6:\n" << m6 << std::endl;
 
-    vpColVector c{ 1, 2, 3 };
-    std::cout << "c:\n" << c << std::endl;
-    c = { -1, -2, -3 };
-    std::cout << "c:\n" << c << std::endl;
+    {
+      std::cout << "** Test vpColVector" << std::endl;
+      vpColVector c_ref(6);
+      for (unsigned int i=0; i < 6; i ++) {
+        c_ref[i] = i;
+      }
+      std::cout << "c_ref: " << c_ref.t() << std::endl;
+      vpColVector c{ 0, 1, 2, 3, 4, 5 };
+      std::cout << "c: " << c.t() << std::endl;
+      if (! equal(c_ref, c, epsilon)) {
+        return EXIT_FAILURE;
+      }
+      c_ref.resize(3, false);
+      c_ref *= -1;
+      std::cout << "c_ref: " << c_ref.t() << std::endl;
+      c = { 0, -1, -2 };
+      std::cout << "c: " << c.t() << std::endl;
+      if (! equal(c_ref, c, epsilon)) {
+        return EXIT_FAILURE;
+      }
 
-    vpRowVector r{ 1, 2, 3, 4, 5, 6 };
-    std::cout << "r:\n" << r << std::endl;
-    r = { -1, -2, -3 };
-    std::cout << "r:\n" << r << std::endl;
+      // Test move constructor
+      vpColVector c1(c_ref);
+      std::cout << "c1: " << c1.t() << std::endl;
+      if (! equal(c_ref, c1, epsilon)) {
+        return EXIT_FAILURE;
+      }
+      vpColVector c2 = std::move(c1);    // Move c1 into c2; c1 is now "empty"
+      std::cout << "c1: " << c1.t() << std::endl;
+      if (c1.size()) {
+        return EXIT_FAILURE;
+      }
+      std::cout << "c2: " << c2.t() << std::endl;
+      if (! equal(c_ref, c2, epsilon)) {
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      std::cout << "** Test vpRowVector" << std::endl;
+      vpRowVector r_ref(6);
+      for (unsigned int i=0; i < 6; i ++) {
+        r_ref[i] = i;
+      }
+      std::cout << "r_ref: " << r_ref << std::endl;
+      vpRowVector r{ 0, 1, 2, 3, 4, 5 };
+      std::cout << "r: " << r << std::endl;
+      if (! equal(r_ref, r, epsilon)) {
+        return EXIT_FAILURE;
+      }
+      r_ref.resize(3, false);
+      r_ref *= -1;
+      std::cout << "r_ref: " << r_ref << std::endl;
+      r = { 0, -1, -2 };
+      std::cout << "r: " << r << std::endl;
+      if (! equal(r_ref, r, epsilon)) {
+        return EXIT_FAILURE;
+      }
+
+      // Test move constructor
+      vpRowVector r1(r_ref);
+      std::cout << "r1: " << r1 << std::endl;
+      if (! equal(r_ref, r1, epsilon)) {
+        return EXIT_FAILURE;
+      }
+      vpRowVector r2 = std::move(r1);    // Move r1 into r2; r1 is now "empty"
+      std::cout << "r1: " << r1 << std::endl;
+      if (r1.size()) {
+        return EXIT_FAILURE;
+      }
+      std::cout << "r2: " << r2 << std::endl;
+      if (! equal(r_ref, r2, epsilon)) {
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      std::cout << "** Test vpThetaUVector" << std::endl;
+      vpThetaUVector tu_ref(0, M_PI_2, M_PI);
+      std::cout << "tu_ref: " << tu_ref.t() << std::endl;
+      {
+        vpThetaUVector tu = {0, M_PI_2, M_PI};
+        std::cout << "tu: " << tu.t() << std::endl;
+        if (! equal(tu_ref, tu, epsilon)) {
+          return EXIT_FAILURE;
+        }
+      }
+    }
+
+    {
+      std::cout << "** Test vpRxyzVector" << std::endl;
+      vpRxyzVector rxyz_ref(0, M_PI_2, M_PI);
+      std::cout << "rxyz_ref: " << rxyz_ref.t() << std::endl;
+      {
+        vpRxyzVector rxyz = {0, M_PI_2, M_PI};
+        std::cout << "rxyz: " << rxyz.t() << std::endl;
+        if (! equal(rxyz_ref, rxyz, epsilon)) {
+          return EXIT_FAILURE;
+        }
+      }
+    }
+
+    {
+      std::cout << "** Test vpRzyxVector" << std::endl;
+      vpRzyxVector rzyx_ref(0, M_PI_2, M_PI);
+      std::cout << "rzyx_ref: " << rzyx_ref.t() << std::endl;
+      {
+        vpRzyxVector rzyx = {0, M_PI_2, M_PI};
+        std::cout << "rzyx: " << rzyx.t() << std::endl;
+        if (! equal(rzyx_ref, rzyx, epsilon)) {
+          return EXIT_FAILURE;
+        }
+      }
+    }
+
+    {
+      std::cout << "** Test vpRzyzVector" << std::endl;
+      vpRzyzVector rzyz_ref(0, M_PI_2, M_PI);
+      std::cout << "rzyz_ref: " << rzyz_ref.t() << std::endl;
+      {
+        vpRzyzVector rzyz = {0, M_PI_2, M_PI};
+        std::cout << "rzyz: " << rzyz.t() << std::endl;
+        if (! equal(rzyz_ref, rzyz, epsilon)) {
+          return EXIT_FAILURE;
+        }
+      }
+    }
+
+    {
+      std::cout << "** Test vpQuaternionVector" << std::endl;
+      vpThetaUVector tu_ref(0, M_PI_2, M_PI);
+      vpQuaternionVector q_ref(tu_ref);
+      std::cout << "q_ref: " << q_ref.t() << std::endl;
+      {
+        vpQuaternionVector q = {q_ref[0], q_ref[1], q_ref[2], q_ref[3]};
+        std::cout << "q: " << q.t() << std::endl;
+        if (! equal(q_ref, q, epsilon)) {
+          return EXIT_FAILURE;
+        }
+      }
+    }
   }
 #endif
 
@@ -162,7 +340,99 @@ int main()
     std::cout << "c: (" << c.getRows() << ", " << c.getCols() << "):\n" << c << std::endl;
     std::cout << "dynamic_cast<vpColVector *>(ptr_array):\n" << *dynamic_cast<vpColVector *>(ptr_array) << std::endl;
     std::cout << "ptr_array:\n" << *ptr_array << std::endl;
+
+    {
+      std::cout << "** Test vpThetaUVector" << std::endl;
+      vpThetaUVector tu_ref(0, M_PI_2, M_PI);
+      std::cout << "tu_ref: " << tu_ref.t() << std::endl;
+      vpThetaUVector tu;
+      tu << 0, M_PI_2, M_PI;
+      std::cout << "tu: " << tu.t() << std::endl;
+      if (! equal(tu_ref, tu, epsilon)) {
+        return EXIT_FAILURE;
+      }
+      // Do it twice
+      tu << 0, M_PI_2, M_PI;
+      std::cout << "tu: " << tu.t() << std::endl;
+      if (! equal(tu_ref, tu, epsilon)) {
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      std::cout << "** Test vpRxyzVector" << std::endl;
+      vpRxyzVector rxyz_ref(0, M_PI_2, M_PI);
+      std::cout << "rxyz_ref: " << rxyz_ref.t() << std::endl;
+      vpRxyzVector rxyz;
+      rxyz << 0, M_PI_2, M_PI;
+      std::cout << "rxyz: " << rxyz.t() << std::endl;
+      if (! equal(rxyz_ref, rxyz, epsilon)) {
+        return EXIT_FAILURE;
+      }
+      // Do it twice
+      rxyz << 0, M_PI_2, M_PI;
+      std::cout << "rxyz: " << rxyz.t() << std::endl;
+      if (! equal(rxyz_ref, rxyz, epsilon)) {
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      std::cout << "** Test vpRzyxVector" << std::endl;
+      vpRzyxVector rzyx_ref(0, M_PI_2, M_PI);
+      std::cout << "rzyx_ref: " << rzyx_ref.t() << std::endl;
+      vpRzyxVector rzyx;
+      rzyx << 0, M_PI_2, M_PI;
+      std::cout << "rzyx: " << rzyx.t() << std::endl;
+      if (! equal(rzyx_ref, rzyx, epsilon)) {
+        return EXIT_FAILURE;
+      }
+      // Do it twice
+      rzyx << 0, M_PI_2, M_PI;
+      std::cout << "rzyx: " << rzyx.t() << std::endl;
+      if (! equal(rzyx_ref, rzyx, epsilon)) {
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      std::cout << "** Test vpRzyzVector" << std::endl;
+      vpRzyzVector rzyz_ref(0, M_PI_2, M_PI);
+      std::cout << "rzyz_ref: " << rzyz_ref.t() << std::endl;
+      vpRzyzVector rzyz;
+      rzyz << 0, M_PI_2, M_PI;
+      std::cout << "rzyz: " << rzyz.t() << std::endl;
+      if (! equal(rzyz_ref, rzyz, epsilon)) {
+        return EXIT_FAILURE;
+      }
+      // Do it twice
+      rzyz << 0, M_PI_2, M_PI;
+      std::cout << "rzyz: " << rzyz.t() << std::endl;
+      if (! equal(rzyz_ref, rzyz, epsilon)) {
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      std::cout << "** Test vpQuaternionVector" << std::endl;
+      vpThetaUVector tu_ref(0, M_PI_2, M_PI);
+      vpQuaternionVector q_ref(tu_ref);
+      std::cout << "q_ref: " << q_ref.t() << std::endl;
+      vpQuaternionVector q;
+      q << q_ref[0], q_ref[1], q_ref[2], q_ref[3];
+      std::cout << "q: " << q.t() << std::endl;
+      if (! equal(q_ref, q, epsilon)) {
+        return EXIT_FAILURE;
+      }
+      // Do it twice
+      q << q_ref[0], q_ref[1], q_ref[2], q_ref[3];
+      std::cout << "q: " << q.t() << std::endl;
+      if (! equal(q_ref, q, epsilon)) {
+        return EXIT_FAILURE;
+      }
+    }
   }
 
+  std::cout << "Test succeed" << std::endl;
   return  EXIT_SUCCESS;
 }
