4e5f8c9899284167ca70878167fa89974e41f809
Fabien Spindler <Fabien.Spindler@inria.fr>
2019-05-03 17:26:18 +0200

    - Create plot.gp file when logs are created - move PI controller after filtering

--- modules/robot/src/real-robot/franka/vpForceTorqueGenerator_impl.cpp
+++ modules/robot/src/real-robot/franka/vpForceTorqueGenerator_impl.cpp
@@ -96,8 +96,44 @@ void vpForceTorqueGenerator::control_thread(franka::Robot *robot, std::atomic_bo
   tau_error_integral.setZero();
 
   if (! activate_pi_controller) {
-    k_p = 0;
-    k_i = 0;
+    k_p = 0.;
+    k_i = 0.;
+  }
+
+  if (! log_folder.empty()) {
+    std::cout << "Save franka logs in \"" << log_folder << "\" folder" << std::endl;
+    std::cout << "Use gnuplot tool to visualize logs:" << std::endl;
+    std::cout << "$ cd " << log_folder << std::endl;
+    std::cout << "$ gnuplot plot.gp" << std::endl;
+    std::cout << "<press return key in the terminal to view next plot>" << std::endl;
+
+    std::ofstream gnuplot;
+    gnuplot.open(log_folder + "/plot.gp");
+    gnuplot << "set st data li\n" << std::endl;
+    gnuplot << "plot ";
+    for (size_t i = 0; i < 7; i++) {
+      gnuplot << "'tau_cmd.log' u " << i+1 << " title \"tau_cmd" << i+1 << "\", ";
+    }
+    gnuplot << "\npause -1\n" << std::endl;
+    gnuplot << "plot ";
+    for (size_t i = 0; i < 7; i++) {
+      gnuplot << "'tau_d.log' u " << i+1 << " title \"tau_d" << i+1 << "\", ";
+    }
+    gnuplot << "\npause -1\n" << std::endl;
+    gnuplot << "plot ";
+    for (size_t i = 0; i < 7; i++) {
+      gnuplot << "'tau_mes.log' u " << i+1 << " title \"tau_mes" << i+1 << "\", ";
+    }
+    gnuplot << "\npause -1\n" << std::endl;
+
+    if (frame == vpRobot::CAMERA_FRAME || frame == vpRobot::REFERENCE_FRAME || frame == vpRobot::END_EFFECTOR_FRAME) {
+      for (size_t i = 0; i < 7; i++) {
+        gnuplot << "plot 'tau_d.log' u " << i+1 << " title \"tau_d" << i+1 << "\", 'tau_mes.log' u " << i+1 << " title \"tau_mes" << i+1 << "\"" << std::endl;
+        gnuplot << "\npause -1\n" << std::endl;
+      }
+    }
+
+    gnuplot.close();
   }
 
   std::ofstream log_time;
@@ -115,7 +151,6 @@ void vpForceTorqueGenerator::control_thread(franka::Robot *robot, std::atomic_bo
 
     if (time == 0.0) {
       if (! log_folder.empty()) {
-        std::cout << "Save franka logs in \"" << log_folder << "\" folder" << std::endl;
         log_time.open(log_folder + "/time.log");
         log_tau_cmd.open(log_folder + "/tau_cmd.log");
         log_tau_d.open(log_folder + "/tau_d.log");
@@ -135,13 +170,14 @@ void vpForceTorqueGenerator::control_thread(franka::Robot *robot, std::atomic_bo
     tau_ext << tau_measured - gravity - initial_tau_ext;
 
     tau_error_integral += period.toSec() * (tau_joint_d - tau_ext);
-    // FF + PI control
-    tau_cmd << tau_joint_d + k_p * (tau_joint_d - tau_ext) + k_i * tau_error_integral;
 
     // Smoothly update the mass to reach the desired target value
     for(size_t i = 0; i < 7; i++) {
       tau_joint_d[i] = filter_gain * tau_J_des[i] + (1 - filter_gain) * tau_joint_d[i];
     }
+
+    // FF + PI control
+    tau_cmd << tau_joint_d + k_p * (tau_joint_d - tau_ext) + k_i * tau_error_integral;
 
     std::array<double, 7> tau_d_array{};
     Eigen::VectorXd::Map(&tau_d_array[0], 7) = tau_cmd;
@@ -172,9 +208,13 @@ void vpForceTorqueGenerator::control_thread(franka::Robot *robot, std::atomic_bo
       (const franka::RobotState& state, franka::Duration period) -> franka::Torques {
     time += period.toSec();
 
+    Eigen::VectorXd tau_d(7), tau_cmd(7), tau_ext(7), desired_tau(7);
+
     if (time == 0.0) {
+      tau_d << 0, 0, 0, 0, 0, 0, 0;
+      tau_ext << 0, 0, 0, 0, 0, 0, 0;
+
       if (! log_folder.empty()) {
-        std::cout << "Save franka logs in \"" << log_folder << "\" folder" << std::endl;
         log_time.open(log_folder + "/time.log");
         log_tau_cmd.open(log_folder + "/tau_cmd.log");
         log_tau_d.open(log_folder + "/tau_d.log");
@@ -196,13 +236,9 @@ void vpForceTorqueGenerator::control_thread(franka::Robot *robot, std::atomic_bo
     Eigen::Map<const Eigen::Matrix<double, 7, 1> > tau_measured(state.tau_J.data());
     Eigen::Map<const Eigen::Matrix<double, 7, 1> > gravity(gravity_array.data());
 
-    Eigen::VectorXd tau_d(7), tau_cmd(7), tau_ext(7), desired_tau(7);
     tau_ext << tau_measured - gravity - initial_tau_ext;
 
     tau_error_integral += period.toSec() * (tau_d - tau_ext);
-
-    // FF + PI control
-    tau_cmd << tau_d + k_p * (tau_d - tau_ext) + k_i * tau_error_integral;
 
     // Apply force with gradually increasing the force
     for(size_t i = 0; i < 7; i++) {
@@ -210,6 +246,9 @@ void vpForceTorqueGenerator::control_thread(franka::Robot *robot, std::atomic_bo
     }
 
     tau_d << jacobian.transpose() * tau_cart_d;
+
+    // FF + PI control
+    tau_cmd << tau_d + k_p * (tau_d - tau_ext) + k_i * tau_error_integral;
 
     std::array<double, 7> tau_d_array{};
     Eigen::VectorXd::Map(&tau_d_array[0], 7) = tau_cmd;
