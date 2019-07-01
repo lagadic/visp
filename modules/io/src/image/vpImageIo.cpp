8f99fb21af609f2399ffb033db8d9fb96f0cef9f
Fabien Spindler <Fabien.Spindler@inria.fr>
2019-07-01 10:48:30 +0200

    Move stb_image files in 3rdparty

--- modules/io/src/image/vpImageIo.cpp
+++ modules/io/src/image/vpImageIo.cpp
@@ -65,10 +65,10 @@
 #if !defined(VISP_HAVE_OPENCV)
 #if !defined(VISP_HAVE_JPEG) || !defined(VISP_HAVE_PNG)
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb_image.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include <stb_image_write.h>
 #endif
 #endif
 
