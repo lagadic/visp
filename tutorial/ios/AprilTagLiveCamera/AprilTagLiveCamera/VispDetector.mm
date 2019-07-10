5523fbaddaadcc3c30f3c2fa932083a5e5523ee2
Yasutomo Shirahama <yasutomo@aplix.co.jp>
2019-07-10 13:56:10 +0900

    removed ViSP GPL notice
    It may be strange if I add visp notice.
    It might be better if ViSP add this.

--- tutorial/ios/AprilTagLiveCamera/AprilTagLiveCamera/VispDetector.mm
+++ tutorial/ios/AprilTagLiveCamera/AprilTagLiveCamera/VispDetector.mm
@@ -1,34 +1,3 @@
-/****************************************************************************
- *
- * This file is part of the ViSP software.
- * Copyright (C) 2005 - 2017 by Inria. All rights reserved.
- *
- * This software is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * See the file LICENSE.txt at the root directory of this source
- * distribution for additional information about the GNU GPL.
- *
- * For using ViSP with software that can not be combined with the GNU
- * GPL, please contact Inria about acquiring a ViSP Professional
- * Edition License.
- *
- * See http://visp.inria.fr for more information.
- *
- * This software was developed at:
- * Inria Rennes - Bretagne Atlantique
- * Campus Universitaire de Beaulieu
- * 35042 Rennes Cedex
- * France
- *
- * If you have questions regarding the use of this file, please contact
- * Inria at visp@inria.fr
- *
- * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
- * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- *
- *****************************************************************************/
 // This file is copied and modified from original file, which is distributed under
 // GNU General Public License v2.0.
 //  VispDetector.mm
