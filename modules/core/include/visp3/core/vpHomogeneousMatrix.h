1a67b9d40dfda7dea9059a86eafd1d2827507c62
Fabien Spindler <Fabien.Spindler@inria.fr>
2020-04-14 15:56:50 +0200

    Add vpHomogeneousMatrix contruction from list of double

--- modules/core/include/visp3/core/vpHomogeneousMatrix.h
+++ modules/core/include/visp3/core/vpHomogeneousMatrix.h
@@ -139,8 +139,16 @@ int main()
 int main()
 {
 #if (VISP_CXX_STANDARD >= VISP_CXX_STANDARD_11)
-  vpHomogeneousMatrix M( vpTranslationVector(0.1, 0.2, 0.3), vpRotationMatrix( {0, 0, -1, 0, -1, 0, -1, 0, 0} ) );
-  std::cout << "M:\n" << M << std::endl;
+  {
+    vpHomogeneousMatrix M( vpTranslationVector(0.1, 0.2, 0.3), vpRotationMatrix( {0, 0, -1, 0, -1, 0, -1, 0, 0} ) );
+    std::cout << "M:\n" << M << std::endl;
+  }
+  {
+    vpHomogeneousMatrix M { 0,  0, -1, 0.1,
+                            0, -1,  0, 0.2,
+                           -1,  0,  0, 0.3 };
+    std::cout << "M:\n" << M << std::endl;
+  }
 #endif
 }
   \endcode
@@ -158,6 +166,7 @@ public:
   explicit vpHomogeneousMatrix(const std::vector<float> &v);
   explicit vpHomogeneousMatrix(const std::vector<double> &v);
   vpHomogeneousMatrix(double tx, double ty, double tz, double tux, double tuy, double tuz);
+  vpHomogeneousMatrix(const std::initializer_list<double> &list);
   /*!
     Destructor.
   */
@@ -215,6 +224,9 @@ public:
   // Multiply by a point
   vpPoint operator*(const vpPoint &bP) const;
 
+  vpHomogeneousMatrix& operator<<(double val);
+  vpHomogeneousMatrix& operator,(double val);
+
   void print() const;
 
   /*!
@@ -248,6 +260,9 @@ public:
   vp_deprecated void setIdentity();
 //@}
 #endif
+
+protected:
+  unsigned int m_index;
 };
 
 #endif
