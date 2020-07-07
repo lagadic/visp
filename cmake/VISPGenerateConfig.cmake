lagadic/visp

commit 33847f5f6c68fed299f73fbd0a2ca359fb27b297

When building with Ogre, fix boost dependency on Ubuntu 20.04. Closes #780.

CMakeLists.txt
@@ -120,8 +120,6 @@ list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake")
 include(cmake/VISPUtils.cmake)
 include(cmake/VISPDetectCXXStandard.cmake) # Set cxx standard to 11 by default
 
-vp_clear_vars(VISPModules_TARGETS)
-
 #-----------------------------------------------------------------------------
 # VISP version number.  An even minor number corresponds to releases.
 set(VISP_VERSION_MAJOR "3")

cmake/VISPGenerateConfig.cmake
@@ -102,7 +102,7 @@ endif()
 # -------------------------------------------------------------------------------------------
 
 # Export the library
-export(TARGETS ${VISPModules_TARGETS} FILE "${PROJECT_BINARY_DIR}/VISPModules.cmake")
+export(EXPORT VISPModules FILE "${PROJECT_BINARY_DIR}/VISPModules.cmake")
 
 ## Update include dirs
 set(VISP_INCLUDE_DIRS_CONFIGCMAKE "${VISP_INCLUDE_DIR}")

modules/ar/CMakeLists.txt
@@ -41,6 +41,10 @@ set(opt_incs "")
 set(opt_libs "")
 
 if(USE_OGRE)
+  # With Ubuntu 20.04, OGRE_LIBRARIES contains Boost::thread that cannot
+  # be exported as is on pain of producing a build issue when ViSP is used
+  # as a 3rd party. That's why here we get its imported location.
+  vp_filter_libraries_with_imported_location(OGRE_LIBRARIES)
   mark_as_advanced(OGRE_SAMPLES_INCLUDEPATH)
   #message("OGRE_SAMPLES_INCLUDEPATH: ${OGRE_SAMPLES_INCLUDEPATH}")
   if(OGRE_SAMPLES_INCLUDEPATH)

cmake/VISPUtils.cmake
@@ -1610,3 +1610,22 @@ macro(vp_get_all_libs _modules _extra_opt _extra_dbg _3rdparty)
     vp_list_reverse(${lst})
   endforeach()
 endmacro()
+
+# Filter libraries.
+# When a library is a target like Boost::thread or Boost::date_time
+# replace the target by its imported location /usr/lib/x86_64-linux-gnu/libboost_thread.so.1.71.0
+macro(vp_filter_libraries_with_imported_location libs)
+  set(__libs)
+  foreach(lib_ ${${libs}})
+    if(TARGET ${lib_})
+      get_target_property(imported_libs_ ${lib_} INPORTED_LOCATION)
+      if(imported_libs_)
+        list(APPEND __libs ${imported_libs_})
+      endif()
+    else()
+      list(APPEND __libs ${lib_})
+    endif()
+  endforeach()
+  set(${libs} ${__libs})
+endmacro()
+

commit d6d0092ab362c324eb441f6f9180827d5e95320f

Fix compat with cmake < 3.0.0 that was broken in commit 33847f5f6c6

CMakeLists.txt
@@ -120,6 +120,10 @@ list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake")
 include(cmake/VISPUtils.cmake)
 include(cmake/VISPDetectCXXStandard.cmake) # Set cxx standard to 11 by default
 
+if (CMAKE_VERSION VERSION_LESS 3.0.0)
+  vp_clear_vars(VISPModules_TARGETS)
+endif()
+
 #-----------------------------------------------------------------------------
 # VISP version number.  An even minor number corresponds to releases.
 set(VISP_VERSION_MAJOR "3")

cmake/VISPGenerateConfig.cmake
@@ -102,7 +102,11 @@ endif()
 # -------------------------------------------------------------------------------------------
 
 # Export the library
-export(EXPORT VISPModules FILE "${PROJECT_BINARY_DIR}/VISPModules.cmake")
+if (CMAKE_VERSION VERSION_LESS 3.0.0)
+  export(TARGETS ${VISPModules_TARGETS} FILE "${PROJECT_BINARY_DIR}/VISPModules.cmake")
+else()
+  export(EXPORT VISPModules FILE "${PROJECT_BINARY_DIR}/VISPModules.cmake")
+endif()
 
 ## Update include dirs
 set(VISP_INCLUDE_DIRS_CONFIGCMAKE "${VISP_INCLUDE_DIR}")