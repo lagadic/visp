lagadic/visp

commit 96cc870c05f9ee994752777c2c4c258f556c3098

Fix inconsistant handling of Exif Orientation between libjpeg and opencv. Closes #798

modules/core/test/image/testConversion.cpp
@@ -429,7 +429,7 @@ int main(int argc, const char **argv)
     cv::Mat imageMat;
     filename = vpIoTools::createFilePath(ipath, "Klimt/Klimt.ppm");
     std::cout << "   Reading the color image with c++ interface of opencv: " << filename << std::endl;
-    imageMat = cv::imread(filename, 1); // force to a three channel color image.
+    imageMat = cv::imread(filename, cv::IMREAD_COLOR); // Force to a three channel BGR color image.
     if (imageMat.data == NULL) {
       std::cout << "   Cannot read image: " << filename << std::endl;
       return -1;
@@ -443,7 +443,7 @@ int main(int argc, const char **argv)
     /* Read the pgm image */
 
     std::cout << "   Reading the greyscale image with opencv: " << filename << std::endl;
-    imageMat = cv::imread(filename, 0); // forced to grayscale.
+    imageMat = cv::imread(filename, cv::IMREAD_GRAYSCALE); // Forced to grayscale.
     if (imageMat.data == NULL) {
       std::cout << "   Cannot read image: " << filename << std::endl;
       return (-1);
@@ -462,7 +462,7 @@ int main(int argc, const char **argv)
     /* Read the color image */
 
     std::cout << "   Reading the color image with opencv: " << filename << std::endl;
-    imageMat = cv::imread(filename, 1); // force to a three channel color image.
+    imageMat = cv::imread(filename, cv::IMREAD_COLOR); // Force to a three channel BGR color image.
     if (imageMat.data == NULL) {
       std::cout << "   Cannot read image: " << filename << std::endl;
       return -1;
@@ -477,7 +477,7 @@ int main(int argc, const char **argv)
     /* Read the pgm image */
 
     std::cout << "   Reading the greyscale image with opencv: " << filename << std::endl;
-    imageMat = cv::imread(filename, 0);
+    imageMat = cv::imread(filename, cv::IMREAD_GRAYSCALE);
     if (imageMat.data == NULL) {
       std::cout << "   Cannot read image: " << filename << std::endl;
       return (-1);

modules/core/src/image/vpImageConvert.cpp
@@ -644,7 +644,7 @@ int main()
   cv::Mat Ip;
 
   // Read an image on a disk with openCV library
-  Ip = cv::imread("image.pgm", 1);// second parameter > 0 for a RGB encoding.
+  Ip = cv::imread("image.pgm", cv::IMREAD_COLOR); // Second parameter for a BGR encoding.
   // Convert the grayscale cv::Mat into vpImage<vpRGBa>
   vpImageConvert::convert(Ip, Ic);
 
@@ -745,7 +745,7 @@ int main()
   cv::Mat Ip;
 
   // Read an image on a disk with openCV library
-  Ip = cv::imread("image.pgm", 0);// second parameter = 0 for a gray level.
+  Ip = cv::imread("image.pgm", cv::IMREAD_GRAYSCALE); // Second parameter for a gray level.
   // Convert the grayscale cv::Mat into vpImage<unsigned char>
   vpImageConvert::convert(Ip, Ig);
 

modules/io/src/image/vpImageIo.cpp
@@ -230,12 +230,13 @@ std::string vpImageIo::getExtension(const std::string &filename)
   only if the new image size is different, else we re-use the same
   memory space.
 
-  Always supported formats are *.pgm and *.ppm.
+  Always supported formats are `*.pgm` and `*.ppm`.
   JPEG and PNG formats are supported through the stb_image public domain image loader.
-  If \c libjpeg 3rd party is used, we support also *.jpg and *.jpeg files.
-  If \c libpng 3rd party is used, we support also *.png files.
-  If OpenCV 3rd party is used, we support *.jpg, *.jpeg, *.jp2, *.rs, *.ras,
-  *.tiff, *.tif, *.png, *.bmp, *.pbm files.
+  - If libjpeg 3rd party is used, we support also `*.jpg` and `*.jpeg` files.
+  - If libpng 3rd party is used, we support also `*.png` files.
+  - If OpenCV 3rd party is used, we support `*.jpg`, `*.jpeg`, `*.jp2`, `*.rs`, `*.ras`,
+  `*.tiff`, `*.tif`, `*.png`, `*.bmp`, `*.pbm` files.
+  - If EXIF information is embedded in the image file, the EXIF orientation is ignored.
 
   \param I : Image to set with the \e filename content.
   \param filename : Name of the file containing the image.
@@ -288,7 +289,7 @@ void vpImageIo::read(vpImage<unsigned char> &I, const std::string &filename)
   if (try_opencv_reader) {
 #if VISP_HAVE_OPENCV_VERSION >= 0x030000
     // std::cout << "Use opencv to read the image" << std::endl;
-    cv::Mat cvI = cv::imread(final_filename, cv::IMREAD_GRAYSCALE);
+    cv::Mat cvI = cv::imread(final_filename, cv::IMREAD_GRAYSCALE | cv::IMREAD_IGNORE_ORIENTATION);
     if (cvI.cols == 0 && cvI.rows == 0) {
       std::string message = "Cannot read file \"" + std::string(final_filename) + "\": Image format not supported";
       throw(vpImageException(vpImageException::ioError, message));
@@ -334,12 +335,13 @@ void vpImageIo::read(vpImage<unsigned char> &I, const std::string &filename)
   only if the new image size is different, else we re-use the same
   memory space.
 
-  Always supported formats are *.pgm and *.ppm.
+  Always supported formats are `*.pgm` and `*.ppm`.
   JPEG and PNG formats are supported through the stb_image public domain image loader.
-  If \c libjpeg 3rd party is used, we support also *.jpg and *.jpeg files.
-  If \c libpng 3rd party is used, we support also *.png files.
-  If OpenCV 3rd party is used, we support *.jpg, *.jpeg, *.jp2, *.rs, *.ras,
-  *.tiff, *.tif, *.png, *.bmp, *.pbm files.
+  - If libjpeg 3rd party is used, we support also `*.jpg` and `*.jpeg` files.
+  - If libpng 3rd party is used, we support also `*.png` files.
+  - If OpenCV 3rd party is used, we support `*.jpg`, `*.jpeg`, `*.jp2`, `*.rs`, `*.ras`,
+  `*.tiff`, `*.tif`, `*.png`, `*.bmp`, `*.pbm` files.
+  - If EXIF information is embedded in the image file, the EXIF orientation is ignored.
 
   \param I : Image to set with the \e filename content.
   \param filename : Name of the file containing the image.
@@ -391,7 +393,7 @@ void vpImageIo::read(vpImage<vpRGBa> &I, const std::string &filename)
   if (try_opencv_reader) {
 #if VISP_HAVE_OPENCV_VERSION >= 0x030000
     // std::cout << "Use opencv to read the image" << std::endl;
-    cv::Mat cvI = cv::imread(final_filename, cv::IMREAD_COLOR);
+    cv::Mat cvI = cv::imread(final_filename, cv::IMREAD_COLOR | cv::IMREAD_IGNORE_ORIENTATION);
     if (cvI.cols == 0 && cvI.rows == 0) {
       std::string message = "Cannot read file \"" + std::string(final_filename) + "\": Image format not supported";
       throw(vpImageException(vpImageException::ioError, message));
@@ -1385,6 +1387,8 @@ void vpImageIo::writeJPEG(const vpImage<vpRGBa> &I, const std::string &filename)
   only if the new image size is different, else we re-use the same
   memory space.
 
+  If EXIF information is embedded in the image file, the EXIF orientation is ignored.
+
   \param I : Image to set with the \e filename content.
   \param filename : Name of the file containing the image.
 
@@ -1392,7 +1396,7 @@ void vpImageIo::writeJPEG(const vpImage<vpRGBa> &I, const std::string &filename)
 void vpImageIo::readJPEG(vpImage<unsigned char> &I, const std::string &filename)
 {
 #if (VISP_HAVE_OPENCV_VERSION >= 0x030000)
-  cv::Mat Ip = cv::imread(filename.c_str(), cv::IMREAD_GRAYSCALE);
+  cv::Mat Ip = cv::imread(filename.c_str(), cv::IMREAD_GRAYSCALE | cv::IMREAD_IGNORE_ORIENTATION);
   if (!Ip.empty())
     vpImageConvert::convert(Ip, I);
   else
@@ -1429,13 +1433,15 @@ void vpImageIo::readJPEG(vpImage<unsigned char> &I, const std::string &filename)
   If the file corresponds to a grayscaled image, a conversion is done to deal
   with \e I which is a color image.
 
+  If EXIF information is embedded in the image file, the EXIF orientation is ignored.
+
   \param I : Color image to set with the \e filename content.
   \param filename : Name of the file containing the image.
 */
 void vpImageIo::readJPEG(vpImage<vpRGBa> &I, const std::string &filename)
 {
 #if (VISP_HAVE_OPENCV_VERSION >= 0x030000)
-  cv::Mat Ip = cv::imread(filename.c_str(), cv::IMREAD_GRAYSCALE);
+  cv::Mat Ip = cv::imread(filename.c_str(), cv::IMREAD_GRAYSCALE | cv::IMREAD_IGNORE_ORIENTATION);
   if (!Ip.empty())
     vpImageConvert::convert(Ip, I);
   else
@@ -2113,6 +2119,8 @@ void vpImageIo::writePNG(const vpImage<vpRGBa> &I, const std::string &filename)
   only if the new image size is different, else we re-use the same
   memory space.
 
+  If EXIF information is embedded in the image file, the EXIF orientation is ignored.
+
   \param I : Image to set with the \e filename content.
   \param filename : Name of the file containing the image.
 
@@ -2120,7 +2128,7 @@ void vpImageIo::writePNG(const vpImage<vpRGBa> &I, const std::string &filename)
 void vpImageIo::readPNG(vpImage<unsigned char> &I, const std::string &filename)
 {
 #if (VISP_HAVE_OPENCV_VERSION >= 0x030000)
-  cv::Mat Ip = cv::imread(filename.c_str(), cv::IMREAD_GRAYSCALE);
+  cv::Mat Ip = cv::imread(filename.c_str(), cv::IMREAD_GRAYSCALE | cv::IMREAD_IGNORE_ORIENTATION);
   if (!Ip.empty())
     vpImageConvert::convert(Ip, I);
   else
@@ -2157,13 +2165,15 @@ void vpImageIo::readPNG(vpImage<unsigned char> &I, const std::string &filename)
   If the file corresponds to a grayscaled image, a conversion is done to deal
   with \e I which is a color image.
 
+  If EXIF information is embedded in the image file, the EXIF orientation is ignored.
+
   \param I : Color image to set with the \e filename content.
   \param filename : Name of the file containing the image.
 */
 void vpImageIo::readPNG(vpImage<vpRGBa> &I, const std::string &filename)
 {
 #if (VISP_HAVE_OPENCV_VERSION >= 0x030000)
-  cv::Mat Ip = cv::imread(filename.c_str(), cv::IMREAD_GRAYSCALE);
+  cv::Mat Ip = cv::imread(filename.c_str(), cv::IMREAD_GRAYSCALE | cv::IMREAD_IGNORE_ORIENTATION);
   if (!Ip.empty())
     vpImageConvert::convert(Ip, I);
   else

tutorial/image/tutorial-image-converter.cpp
@@ -8,7 +8,7 @@ int main()
   try {
     cv::Mat A;
 #if (VISP_HAVE_OPENCV_VERSION >= 0x030000)
-    A = cv::imread("monkey.bmp", cv::IMREAD_GRAYSCALE);
+    A = cv::imread("monkey.bmp", cv::IMREAD_GRAYSCALE | cv::IMREAD_IGNORE_ORIENTATION);
 #else
     A = cv::imread("monkey.bmp", CV_LOAD_IMAGE_GRAYSCALE);
 #endif

tutorial/tracking/model-based/generic-rgbd-blender/tutorial-mb-generic-tracker-rgbd-blender.cpp
@@ -35,7 +35,7 @@ bool read_data(unsigned int cpt, const std::string &input_directory, vpImage<uns
   sprintf(buffer, ss.str().c_str(), cpt);
   std::string filename_depth = buffer;
 
-  cv::Mat depth_raw = cv::imread(filename_depth, cv::IMREAD_ANYDEPTH | cv::IMREAD_ANYCOLOR);
+  cv::Mat depth_raw = cv::imread(filename_depth, cv::IMREAD_ANYDEPTH | cv::IMREAD_ANYCOLOR | cv::IMREAD_IGNORE_ORIENTATION);
   if (depth_raw.empty()) {
     std::cerr << "Cannot read: " << filename_depth << std::endl;
     return false;

commit 28735568fabba91a56b13b6d0457963f94460bde

Fix regression introduced in commit 96cc870c05f by keeping compat with OpenCV < 3.0.0

modules/core/test/image/testConversion.cpp
@@ -429,7 +429,12 @@ int main(int argc, const char **argv)
     cv::Mat imageMat;
     filename = vpIoTools::createFilePath(ipath, "Klimt/Klimt.ppm");
     std::cout << "   Reading the color image with c++ interface of opencv: " << filename << std::endl;
-    imageMat = cv::imread(filename, cv::IMREAD_COLOR); // Force to a three channel BGR color image.
+#if VISP_HAVE_OPENCV_VERSION >= 0x030000
+    int flags = cv::IMREAD_COLOR;
+#elif VISP_HAVE_OPENCV_VERSION >= 0x020100
+    int flags = CV_LOAD_IMAGE_COLOR;
+#endif
+    imageMat = cv::imread(filename, flags); // Force to a three channel BGR color image.
     if (imageMat.data == NULL) {
       std::cout << "   Cannot read image: " << filename << std::endl;
       return -1;
@@ -443,7 +448,12 @@ int main(int argc, const char **argv)
     /* Read the pgm image */
 
     std::cout << "   Reading the greyscale image with opencv: " << filename << std::endl;
-    imageMat = cv::imread(filename, cv::IMREAD_GRAYSCALE); // Forced to grayscale.
+#if VISP_HAVE_OPENCV_VERSION >= 0x030000
+    flags = cv::IMREAD_GRAYSCALE;
+#elif VISP_HAVE_OPENCV_VERSION >= 0x020100
+    flags = CV_LOAD_IMAGE_GRAYSCALE;
+#endif
+    imageMat = cv::imread(filename, flags); // Forced to grayscale.
     if (imageMat.data == NULL) {
       std::cout << "   Cannot read image: " << filename << std::endl;
       return (-1);
@@ -462,7 +472,12 @@ int main(int argc, const char **argv)
     /* Read the color image */
 
     std::cout << "   Reading the color image with opencv: " << filename << std::endl;
-    imageMat = cv::imread(filename, cv::IMREAD_COLOR); // Force to a three channel BGR color image.
+#if VISP_HAVE_OPENCV_VERSION >= 0x030000
+    flags = cv::IMREAD_COLOR;
+#elif VISP_HAVE_OPENCV_VERSION >= 0x020100
+    flags = CV_LOAD_IMAGE_COLOR;
+#endif
+    imageMat = cv::imread(filename, flags); // Force to a three channel BGR color image.
     if (imageMat.data == NULL) {
       std::cout << "   Cannot read image: " << filename << std::endl;
       return -1;
@@ -477,7 +492,12 @@ int main(int argc, const char **argv)
     /* Read the pgm image */
 
     std::cout << "   Reading the greyscale image with opencv: " << filename << std::endl;
-    imageMat = cv::imread(filename, cv::IMREAD_GRAYSCALE);
+#if VISP_HAVE_OPENCV_VERSION >= 0x030000
+    flags = cv::IMREAD_GRAYSCALE;
+#elif VISP_HAVE_OPENCV_VERSION >= 0x020100
+    flags = CV_LOAD_IMAGE_GRAYSCALE;
+#endif
+    imageMat = cv::imread(filename, flags);
     if (imageMat.data == NULL) {
       std::cout << "   Cannot read image: " << filename << std::endl;
       return (-1);