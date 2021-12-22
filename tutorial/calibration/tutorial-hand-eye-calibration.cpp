b8ee6baa7c556896635f43f090680438ac8d203f
Fabien Spindler <Fabien.Spindler@inria.fr>
2021-12-22 19:30:38 +0100

    Improve options usage in calibration tools

--- tutorial/calibration/tutorial-hand-eye-calibration.cpp
+++ tutorial/calibration/tutorial-hand-eye-calibration.cpp
@@ -1,46 +1,94 @@
 //! \example tutorial-hand-eye-calibration.cpp
+#include <map>
+
+#include <visp3/core/vpIoTools.h>
 #include <visp3/vision/vpHandEyeCalibration.h>
 
-int main(int argc, char *argv[])
+void usage(const char *argv[], int error)
 {
-  unsigned int ndata = 0;
+  std::cout << "Synopsis" << std::endl
+            << "  " << argv[0] << " [--data-path <path>] [--help] [-h]" << std::endl << std::endl;
+  std::cout << "Description" << std::endl
+            << "  --data-path <path>  Path to the folder containing" << std::endl
+            << "    pose_fPe_\%d.yaml and pose_cPo_\%d.yaml data files." << std::endl
+            << "    Default: \"./\"" << std::endl << std::endl
+            << "  --help, -h  Print this helper message." << std::endl << std::endl;
+  if (error) {
+    std::cout << "Error" << std::endl
+              << "  " << "Unsupported parameter " << argv[error] << std::endl;
+  }
+}
+
+int main(int argc, const char *argv[])
+{
+  std::string data_path = "./";
   for (int i = 1; i < argc; i++) {
-    if (std::string(argv[i]) == "--ndata" && i+1 < argc) {
-      ndata = atoi(argv[i+1]);
-    } else if (std::string(argv[i]) == "--help" || std::string(argv[i]) == "-h") {
-      std::cout << argv[0] << " [--ndata <number of data to process>] "
-                              "[--help] [-h]" << std::endl;
+    if (std::string(argv[i]) == "--data-path" && i+1 < argc) {
+      data_path = std::string(argv[i+1]);
+      i ++;
+    }
+    else if (std::string(argv[i]) == "--help" || std::string(argv[i]) == "-h") {
+      usage(argv, 0);
       return EXIT_SUCCESS;
     }
+    else {
+      usage(argv, i);
+      return EXIT_FAILURE;
+    }
   }
-  if (ndata == 0) {
-    std::cout << "Number of data to process not specified" << std::endl;
-    std::cout << argv[0] << " --help" << std::endl;
-    return EXIT_SUCCESS;
-  }
-  std::vector<vpHomogeneousMatrix> cMo(ndata);
-  std::vector<vpHomogeneousMatrix> wMe(ndata);
+
+  std::vector<vpHomogeneousMatrix> cMo;
+  std::vector<vpHomogeneousMatrix> wMe;
   vpHomogeneousMatrix eMc;
 
-  for (unsigned int i = 1; i <= ndata; i++) {
-    std::ostringstream ss_fPe, ss_cPo;
-    ss_fPe << "pose_fPe_" << i << ".yaml";
-    ss_cPo << "pose_cPo_" << i << ".yaml";
-    std::cout << "Use fPe=" << ss_fPe.str() << ", cPo=" << ss_cPo.str() << std::endl;
-
-    vpPoseVector wPe;
-    if (wPe.loadYAML(ss_fPe.str(), wPe) == false) {
-      std::cout << "Unable to read data from: " << ss_fPe.str() << std::endl;
-      return EXIT_FAILURE;
+  std::map<long, std::string> map_fPe_files;
+  std::map<long, std::string> map_cPo_files;
+  std::vector<std::string> files = vpIoTools::getDirFiles(data_path);
+  for (unsigned int i = 0; i < files.size(); i++) {
+    long index_fPe = vpIoTools::getIndex(files[i], "pose_fPe_%d.yaml");
+    long index_cPo = vpIoTools::getIndex(files[i], "pose_cPo_%d.yaml");
+    if (index_fPe != -1) {
+      map_fPe_files[index_fPe] = files[i];
     }
-    wMe[i - 1] = vpHomogeneousMatrix(wPe);
-
-    vpPoseVector cPo;
-    if (cPo.loadYAML(ss_cPo.str(), cPo)  == false) {
-      std::cout << "Unable to read data from: " << ss_cPo.str() << std::endl;
-      return EXIT_FAILURE;
+    if (index_cPo != -1) {
+      map_cPo_files[index_cPo] = files[i];
     }
-    cMo[i-1] = vpHomogeneousMatrix(cPo);
+  }
+
+  if (map_fPe_files.size() == 0) {
+    std::cout << "No pose_fPe_\%d.yaml files found. Use --data-path <path> option to modify data path." << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (map_cPo_files.size() == 0) {
+    std::cout << "No pose_cPo_\%d.yaml files found. Use --data-path <path> option to modify data path." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  for (std::map<long, std::string>::const_iterator it_fPe = map_fPe_files.begin(); it_fPe != map_fPe_files.end(); ++ it_fPe) {
+    std::string file_fPe = it_fPe->second;
+    std::map<long, std::string>::const_iterator it_cPo = map_cPo_files.find(it_fPe->first);
+    if (it_cPo != map_cPo_files.end()) {
+      std::string file_cPo = it_cPo->second;
+      vpPoseVector wPe;
+      if (wPe.loadYAML(file_fPe, wPe) == false) {
+        std::cout << "Unable to read data from " << data_path << "/" << file_fPe << ". Skip data" << std::endl;
+        continue;
+      }
+
+      vpPoseVector cPo;
+      if (cPo.loadYAML(file_cPo, cPo)  == false) {
+        std::cout << "Unable to read data from " << data_path << "/" << file_cPo << ". Skip data" << std::endl;
+        continue;
+      }
+      std::cout << "Use data from " << data_path << "/" << file_fPe << " and from " << data_path << "/" << file_cPo << std::endl;
+      wMe.push_back(vpHomogeneousMatrix(wPe));
+      cMo.push_back(vpHomogeneousMatrix(cPo));
+    }
+  }
+
+  if (wMe.size() < 3) {
+    std::cout << "Not enough data pairs found." << std::endl;
+    return EXIT_FAILURE;
   }
 
   int ret = vpHandEyeCalibration::calibrate(cMo, wMe, eMc);
