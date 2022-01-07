4d5ca8a5e4c63aa7c5e5a3807a376dd7923fa7ea
Fabien Spindler <Fabien.Spindler@inria.fr>
2022-01-07 19:20:50 +0100

    Turn test with SIFT off when OpenCV 4.5.4. Closes #970

--- modules/vision/test/key-point/testKeyPoint-5.cpp
+++ modules/vision/test/key-point/testKeyPoint-5.cpp
@@ -211,7 +211,9 @@ void run_test(const std::string &env_ipath, bool opt_click_allowed, bool opt_dis
 #if defined(VISP_HAVE_OPENCV_NONFREE) || defined(VISP_HAVE_OPENCV_XFEATURES2D) || \
     (VISP_HAVE_OPENCV_VERSION >= 0x030411 && CV_MAJOR_VERSION < 4) || (VISP_HAVE_OPENCV_VERSION >= 0x040400)
   detectorNames.push_back("PyramidSIFT");
-  detectorNames.push_back("SIFT");
+#if (VISP_HAVE_OPENCV_VERSION != 0x040504)
+  detectorNames.push_back("SIFT"); // SIFT is known unstable with OpenCV 4.5.4
+#endif
 #endif
 #if defined(VISP_HAVE_OPENCV_NONFREE) || defined(VISP_HAVE_OPENCV_XFEATURES2D)
   detectorNames.push_back("PyramidSURF");
