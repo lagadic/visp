38d41ad9c52fb4c157ee4b2e7bf29d8f8258d7ba
Fabien Spindler <Fabien.Spindler@inria.fr>
2022-01-19 15:06:14 +0100

    Fix vpFont.cpp compat with cxx98

--- modules/core/src/image/vpFont.cpp
+++ modules/core/src/image/vpFont.cpp
@@ -60,9 +60,9 @@
 #include <visp3/core/vpFont.h>
 #include <visp3/core/vpIoException.h>
 #include <visp3/core/vpIoTools.h>
+#include <visp3/core/vpMath.h>
 #include "private/Font.hpp"
-#include <cstdint>
-#include <iterator>
+
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "private/stb_truetype.h"
 
@@ -109,7 +109,6 @@ public:
         throw(vpException(vpException::fatalError, "True type font file doesn't exist in %s", ttfFilename.c_str()));
       }
 
-      std::cout << "Load font from file: " << ttf_file << std::endl;
       LoadTTF(ttf_file);
 
       /* calculate font scaling */
@@ -118,8 +117,8 @@ public:
       int lineGap = 0;
       stbtt_GetFontVMetrics(&_info, &_fontAscent, &_fontDescent, &lineGap);
 
-      _fontAscent = std::roundf(_fontAscent * _fontScale);
-      _fontDescent = std::roundf(_fontDescent * _fontScale);
+      _fontAscent = vpMath::round(_fontAscent * _fontScale);
+      _fontDescent = vpMath::round(_fontDescent * _fontScale);
 
       break;
     }
@@ -237,7 +236,7 @@ public:
         int y = _fontAscent + c_y1;
         _y_vec.push_back(y);
 
-        int d_x = x + std::roundf(lsb * _fontScale);
+        int d_x = x + vpMath::round(lsb * _fontScale);
         int d_y = y;
         int d_w = (c_x2 - c_x1);
         int d_h = (c_y2 - c_y1);
@@ -248,13 +247,13 @@ public:
         _bb.setBottom(_bb.getBottom() < d_y+d_h ? d_y+d_h : _bb.getBottom());
 
         /* advance x */
-        x += std::roundf(ax * _fontScale);
+        x += vpMath::round(ax * _fontScale);
 
         /* add kerning */
         int kern = 0;
         if (i < _wordUTF32.size()-1) {
           kern = stbtt_GetCodepointKernAdvance(&_info, _wordUTF32[i], _wordUTF32[i + 1]);
-          x += std::roundf(kern * _fontScale);
+          x += vpMath::round(kern * _fontScale);
         }
       }
 
@@ -322,11 +321,11 @@ public:
 
       for (size_t i = 0, x = 0; i < _wordUTF32.size(); i++) {
         /* render character (stride and offset is important here) */
-        int byteOffset = x + std::roundf(_lsb_vec[i] * _fontScale) + (_y_vec[i] * _fontBuffer.getWidth());
+        int byteOffset = x + vpMath::round(_lsb_vec[i] * _fontScale) + (_y_vec[i] * _fontBuffer.getWidth());
         stbtt_MakeCodepointBitmap(&_info, _fontBuffer.bitmap + byteOffset, _bb_vec[i].getWidth(), _bb_vec[i].getHeight(),
                                   _fontBuffer.getWidth(), _fontScale, _fontScale, _wordUTF32[i]);
 
-        int d_x = x + std::roundf(_lsb_vec[i] * _fontScale);
+        int d_x = x + vpMath::round(_lsb_vec[i] * _fontScale);
         int d_y = _y_vec[i];
         int d_w = _bb_vec[i].getWidth();
         int d_h = _bb_vec[i].getHeight();
@@ -344,12 +343,12 @@ public:
         }
 
         /* advance x */
-        x += std::roundf(_ax_vec[i] * _fontScale);
+        x += vpMath::round(_ax_vec[i] * _fontScale);
 
         /* add kerning */
         if (i < _wordUTF32.size()-1) {
           int kern = stbtt_GetCodepointKernAdvance(&_info, _wordUTF32[i], _wordUTF32[i + 1]);
-          x += std::roundf(kern * _fontScale);
+          x += vpMath::round(kern * _fontScale);
         }
       }
 
@@ -428,11 +427,11 @@ public:
 
       for (size_t i = 0, x = 0; i < _wordUTF32.size(); i++) {
         /* render character (stride and offset is important here) */
-        int byteOffset = x + std::roundf(_lsb_vec[i] * _fontScale) + (_y_vec[i] * _fontBuffer.getWidth());
+        int byteOffset = x + vpMath::round(_lsb_vec[i] * _fontScale) + (_y_vec[i] * _fontBuffer.getWidth());
         stbtt_MakeCodepointBitmap(&_info, _fontBuffer.bitmap + byteOffset, _bb_vec[i].getWidth(), _bb_vec[i].getHeight(),
                                   _fontBuffer.getWidth(), _fontScale, _fontScale, _wordUTF32[i]);
 
-        int d_x = x + std::roundf(_lsb_vec[i] * _fontScale);
+        int d_x = x + vpMath::round(_lsb_vec[i] * _fontScale);
         int d_y = _y_vec[i];
         int d_w = _bb_vec[i].getWidth();
         int d_h = _bb_vec[i].getHeight();
@@ -453,12 +452,12 @@ public:
         }
 
         /* advance x */
-        x += std::roundf(_ax_vec[i] * _fontScale);
+        x += vpMath::round(_ax_vec[i] * _fontScale);
 
         /* add kerning */
         if (i < _wordUTF32.size()-1) {
           int kern = stbtt_GetCodepointKernAdvance(&_info, _wordUTF32[i], _wordUTF32[i + 1]);
-          x += std::roundf(kern * _fontScale);
+          x += vpMath::round(kern * _fontScale);
         }
       }
 
