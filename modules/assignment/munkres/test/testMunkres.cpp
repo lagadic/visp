232a15b87e2c6988e8845e0bde7cea77aac21fdc
jdufour <julien.dufour@inria.fr>
2022-02-02 16:59:06 +0000

    ✅ [Munkres] Catch tests

--- modules/assignment/munkres/test/testMunkres.cpp
+++ modules/assignment/munkres/test/testMunkres.cpp
@@ -38,11 +38,88 @@
   \brief Test Munkres assignment algorithm.
 */
 
-// System
-#include <iostream>
-
-// Internal
 #include <visp3/munkres/vpMunkres.h>
+
+#ifdef VISP_HAVE_CATCH2
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+
+TEST_CASE("Check Munkres-based assignment", "[visp_munkres]")
+{
+  auto testMunkres = [](const std::vector<std::vector<double> > &cost_matrix,
+                        const std::vector<std::pair<uint, uint> > &expected_pairs) {
+    const auto pairs = munkres::vpMunkres::run(cost_matrix);
+    CHECK(pairs.size() == expected_pairs.size());
+    for (auto i = 0u; i < pairs.size(); ++i) {
+      CHECK(expected_pairs.at(i) == pairs.at(i));
+    }
+  };
+
+  SECTION("Square cost matrix")
+  {
+    std::vector<std::vector<double> > costs{};
+    costs.push_back(std::vector<double>{3, 1, 2});
+    costs.push_back(std::vector<double>{2, 3, 1});
+    costs.push_back(std::vector<double>{1, 2, 3});
+
+    std::vector<std::pair<uint, uint> > pairs{};
+    pairs.emplace_back(0, 1);
+    pairs.emplace_back(1, 2);
+    pairs.emplace_back(2, 0);
+
+    testMunkres(costs, pairs);
+  }
+
+  SECTION("Horizontal cost matrix")
+  {
+    std::vector<std::vector<double> > costs{};
+    costs.push_back(std::vector<double>{4, 1, 2, 3});
+    costs.push_back(std::vector<double>{3, 4, 1, 2});
+    costs.push_back(std::vector<double>{2, 3, 4, 1});
+
+    std::vector<std::pair<uint, uint> > pairs{};
+    pairs.emplace_back(0, 1);
+    pairs.emplace_back(1, 2);
+    pairs.emplace_back(2, 3);
+
+    testMunkres(costs, pairs);
+  }
+
+  SECTION("Vertical cost matrix")
+  {
+    std::vector<std::vector<double> > costs{};
+    costs.push_back(std::vector<double>{4, 1, 2});
+    costs.push_back(std::vector<double>{3, 4, 1});
+    costs.push_back(std::vector<double>{2, 3, 4});
+    costs.push_back(std::vector<double>{1, 2, 3});
+
+    std::vector<std::pair<uint, uint> > pairs{};
+    pairs.emplace_back(0, 1);
+    pairs.emplace_back(1, 2);
+    pairs.emplace_back(3, 0);
+
+    testMunkres(costs, pairs);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  Catch::Session session; // There must be exactly one instance
+
+  // Let Catch (using Clara) parse the command line
+  session.applyCommandLine(argc, argv);
+
+  int numFailed = session.run();
+
+  // numFailed is clamped to 255 as some unices only use the lower 8 bits.
+  // This clamping has already been applied, so just return it here
+  // You can also do any post run clean-up here
+  return numFailed;
+}
+
+#else
+// Fallback to classic tests
+#include <iostream>
 
 bool testMunkres(const std::vector<std::vector<double> > &costs_matrix,
                  const std::vector<std::pair<uint, uint> > &expected_pairs)
@@ -155,3 +232,4 @@ int main()
 
   return EXIT_SUCCESS;
 }
+#endif
