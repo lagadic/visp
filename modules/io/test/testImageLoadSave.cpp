919d7e501d9b2d5fe303b951d6a1002787160fe3
Souriya Trinh <souriya.trinh+github@gmail.com>
2022-09-18 22:57:20 +0200

    Check image I/O with the different backends using the Pearson correlation coefficient.

--- modules/io/test/testImageLoadSave.cpp
+++ modules/io/test/testImageLoadSave.cpp
@@ -46,6 +46,8 @@
 
 static const std::string ipath = vpIoTools::getViSPImagesDataPath();
 static const std::string path = ipath + "/Solvay/Solvay_conference_1927_Version2_640x440";
+static const double ccThreshPNG = 1.0;
+static const double ccThreshJPG = 0.99;
 
 static const std::vector<vpImageIo::vpImageIoBackendType> backends
 {
@@ -53,9 +55,9 @@ static const std::vector<vpImageIo::vpImageIoBackendType> backends
   vpImageIo::IO_SYSTEM_LIB_BACKEND,
 #endif
 #if defined(VISP_HAVE_OPENCV)
-      vpImageIo::IO_OPENCV_BACKEND,
+  vpImageIo::IO_OPENCV_BACKEND,
 #endif
-      vpImageIo::IO_SIMDLIB_BACKEND, vpImageIo::IO_STB_IMAGE_BACKEND
+  vpImageIo::IO_SIMDLIB_BACKEND, vpImageIo::IO_STB_IMAGE_BACKEND
 };
 static const std::vector<std::string> backendNamesJpeg
 {
@@ -81,63 +83,128 @@ static std::vector<std::string> backendNamesPng
 static const unsigned int imgWidth = 640;
 static const unsigned int imgHeight = 440;
 
-TEST_CASE("Benchmark grayscale JPEG image loading - Solvay (640x440)", "[benchmark]")
+namespace
 {
-  for (size_t j = 0; j < backends.size(); j++) {
-    vpImage<vpRGBa> I;
+double computePearsonCC(const vpImage<unsigned char>& I1, const vpImage<unsigned char>& I2)
+{
+  double m1 = I1.getSum() / I1.getSize();
+  double m2 = I2.getSum() / I2.getSize();
 
-    SECTION(backendNamesJpeg[j] + " backend")
-    {
-      CHECK_NOTHROW(vpImageIo::read(I, path + ".jpg", backends[j]));
-      CHECK(I.getWidth() == imgWidth);
-      CHECK(I.getHeight() == imgHeight);
-    };
+  double num = 0, den1 = 0, den2 = 0;
+  for (unsigned int i = 0; i < I1.getSize(); i++) {
+    double x1 = I1.bitmap[i];
+    double x2 = I2.bitmap[i];
+    double x1_m = x1 - m1;
+    double x2_m = x2 - m2;
+    num += x1_m * x2_m;
+    den1 += x1_m * x1_m;
+    den2 += x2_m * x2_m;
   }
+
+  return num / (std::sqrt(den1) * std::sqrt(den2));
 }
 
-TEST_CASE("Benchmark RGBA JPEG image loading - Solvay (640x440)", "[benchmark]")
+double computePearsonCC(const vpImage<vpRGBa>& I1, const vpImage<vpRGBa>& I2)
 {
+  double m1 = I1.getSum() / (3*I1.getSize());
+  double m2 = I2.getSum() / (3*I2.getSize());
+
+  double num = 0, den1 = 0, den2 = 0;
+  for (unsigned int i = 0; i < I1.getSize(); i++) {
+    double x1 = I1.bitmap[i].R + I1.bitmap[i].G + I1.bitmap[i].B;
+    double x2 = I2.bitmap[i].R + I2.bitmap[i].G + I2.bitmap[i].B;
+    double x1_m = x1 - m1;
+    double x2_m = x2 - m2;
+    num += x1_m * x2_m;
+    den1 += x1_m * x1_m;
+    den2 += x2_m * x2_m;
+  }
+
+  return num / (std::sqrt(den1) * std::sqrt(den2));
+}
+}
+
+TEST_CASE("Test grayscale JPEG image loading", "[image_I/O]")
+{
+  vpImage<unsigned char> I_ref;
+  vpImageIo::read(I_ref, path + ".jpg");
+
   for (size_t j = 0; j < backends.size(); j++) {
     vpImage<unsigned char> I;
 
     SECTION(backendNamesJpeg[j] + " backend")
     {
       CHECK_NOTHROW(vpImageIo::read(I, path + ".jpg", backends[j]));
+      double cc = computePearsonCC(I_ref, I);
+      std::cout << backendNamesJpeg[j] << " backend Pearson correlation coefficient: " << cc << std::endl;
+      CHECK(cc >= Approx(ccThreshJPG));
       CHECK(I.getWidth() == imgWidth);
       CHECK(I.getHeight() == imgHeight);
     };
   }
 }
 
-TEST_CASE("Benchmark grayscale PNG image loading - Solvay (640x440)", "[benchmark]")
+TEST_CASE("Test RGBA JPEG image loading", "[image_I/O]")
 {
+  vpImage<vpRGBa> I_ref;
+  vpImageIo::read(I_ref, path + ".jpg");
+
   for (size_t j = 0; j < backends.size(); j++) {
     vpImage<vpRGBa> I;
 
-    SECTION(backendNamesPng[j] + " backend")
+    SECTION(backendNamesJpeg[j] + " backend")
     {
-      CHECK_NOTHROW(vpImageIo::read(I, path + ".png", backends[j]));
+      CHECK_NOTHROW(vpImageIo::read(I, path + ".jpg", backends[j]));
+      double cc = computePearsonCC(I_ref, I);
+      std::cout << backendNamesJpeg[j] << " backend Pearson correlation coefficient: " << cc << std::endl;
+      CHECK(cc >= Approx(ccThreshJPG));
       CHECK(I.getWidth() == imgWidth);
       CHECK(I.getHeight() == imgHeight);
     };
   }
 }
 
-TEST_CASE("Benchmark RGBA PNG image loading - Solvay (640x440)", "[benchmark]")
+TEST_CASE("Test grayscale PNG image loading", "[image_I/O]")
 {
+  vpImage<unsigned char> I_ref;
+  vpImageIo::read(I_ref, path + ".png");
+
   for (size_t j = 0; j < backends.size(); j++) {
     vpImage<unsigned char> I;
 
     SECTION(backendNamesPng[j] + " backend")
     {
       CHECK_NOTHROW(vpImageIo::read(I, path + ".png", backends[j]));
+      double cc = computePearsonCC(I_ref, I);
+      std::cout << backendNamesPng[j] << " backend Pearson correlation coefficient: " << cc << std::endl;
+      CHECK(cc == Approx(ccThreshPNG));
       CHECK(I.getWidth() == imgWidth);
       CHECK(I.getHeight() == imgHeight);
     };
   }
 }
 
-TEST_CASE("Benchmark grayscale JPEG image saving - Solvay (640x440)", "[benchmark]")
+TEST_CASE("Test RGBA PNG image loading", "[image_I/O]")
+{
+  vpImage<vpRGBa> I_ref;
+  vpImageIo::read(I_ref, path + ".png");
+
+  for (size_t j = 0; j < backends.size(); j++) {
+    vpImage<vpRGBa> I;
+
+    SECTION(backendNamesPng[j] + " backend")
+    {
+      CHECK_NOTHROW(vpImageIo::read(I, path + ".png", backends[j]));
+      double cc = computePearsonCC(I_ref, I);
+      std::cout << backendNamesPng[j] << " backend Pearson correlation coefficient: " << cc << std::endl;
+      CHECK(cc == Approx(ccThreshPNG));
+      CHECK(I.getWidth() == imgWidth);
+      CHECK(I.getHeight() == imgHeight);
+    };
+  }
+}
+
+TEST_CASE("Test grayscale JPEG image saving", "[image_I/O]")
 {
   std::string tmp_dir = vpIoTools::makeTempDirectory(vpIoTools::getTempPath());
   std::string directory_filename_tmp =
@@ -152,13 +219,18 @@ TEST_CASE("Benchmark grayscale JPEG image saving - Solvay (640x440)", "[benchmar
     SECTION(backendNamesJpeg[j] + " backend")
     {
       CHECK_NOTHROW(vpImageIo::write(I, directory_filename_tmp + "/ViSP_tmp_perf_write.jpg", backends[j]));
+      vpImage<unsigned char> I_read;
+      vpImageIo::read(I_read, directory_filename_tmp + "/ViSP_tmp_perf_write.jpg");
+      double cc = computePearsonCC(I_read, I);
+      std::cout << backendNamesPng[j] << " backend Pearson correlation coefficient: " << cc << std::endl;
+      CHECK(cc >= Approx(ccThreshJPG));
     };
   }
 
   REQUIRE(vpIoTools::remove(directory_filename_tmp));
 }
 
-TEST_CASE("Benchmark RGBA JPEG image saving - Solvay (640x440)", "[benchmark]")
+TEST_CASE("Test RGBA JPEG image saving", "[image_I/O]")
 {
   std::string tmp_dir = vpIoTools::makeTempDirectory(vpIoTools::getTempPath());
   std::string directory_filename_tmp =
@@ -173,13 +245,18 @@ TEST_CASE("Benchmark RGBA JPEG image saving - Solvay (640x440)", "[benchmark]")
     SECTION(backendNamesJpeg[j] + " backend")
     {
       CHECK_NOTHROW(vpImageIo::write(I, directory_filename_tmp + "/ViSP_tmp_perf_write.jpg", backends[j]));
+      vpImage<vpRGBa> I_read;
+      vpImageIo::read(I_read, directory_filename_tmp + "/ViSP_tmp_perf_write.jpg");
+      double cc = computePearsonCC(I_read, I);
+      std::cout << backendNamesPng[j] << " backend Pearson correlation coefficient: " << cc << std::endl;
+      CHECK(cc >= Approx(ccThreshJPG));
     };
   }
 
   REQUIRE(vpIoTools::remove(directory_filename_tmp));
 }
 
-TEST_CASE("Benchmark grayscale PNG image saving - Solvay (640x440)", "[benchmark]")
+TEST_CASE("Test grayscale PNG image saving", "[image_I/O]")
 {
   std::string tmp_dir = vpIoTools::makeTempDirectory(vpIoTools::getTempPath());
   std::string directory_filename_tmp =
@@ -194,13 +271,18 @@ TEST_CASE("Benchmark grayscale PNG image saving - Solvay (640x440)", "[benchmark
     SECTION(backendNamesPng[j] + " backend")
     {
       CHECK_NOTHROW(vpImageIo::write(I, directory_filename_tmp + "/ViSP_tmp_perf_write.png", backends[j]));
+      vpImage<unsigned char> I_read;
+      vpImageIo::read(I_read, directory_filename_tmp + "/ViSP_tmp_perf_write.png");
+      double cc = computePearsonCC(I_read, I);
+      std::cout << backendNamesPng[j] << " backend Pearson correlation coefficient: " << cc << std::endl;
+      CHECK(cc == Approx(ccThreshPNG));
     };
   }
 
   REQUIRE(vpIoTools::remove(directory_filename_tmp));
 }
 
-TEST_CASE("Benchmark RGBA PNG image saving - Solvay (640x440)", "[benchmark]")
+TEST_CASE("Test RGBA PNG image saving", "[image_I/O]")
 {
   std::string tmp_dir = vpIoTools::makeTempDirectory(vpIoTools::getTempPath());
   std::string directory_filename_tmp =
@@ -215,6 +297,11 @@ TEST_CASE("Benchmark RGBA PNG image saving - Solvay (640x440)", "[benchmark]")
     SECTION(backendNamesPng[j] + " backend")
     {
       CHECK_NOTHROW(vpImageIo::write(I, directory_filename_tmp + "/ViSP_tmp_perf_write.png", backends[j]));
+      vpImage<vpRGBa> I_read;
+      vpImageIo::read(I_read, directory_filename_tmp + "/ViSP_tmp_perf_write.png");
+      double cc = computePearsonCC(I_read, I);
+      std::cout << backendNamesPng[j] << " backend Pearson correlation coefficient: " << cc << std::endl;
+      CHECK(cc == Approx(ccThreshPNG));
     };
   }
 
