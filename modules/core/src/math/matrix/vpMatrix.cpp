lagadic/visp

commit a3b12ebaae3f58620c882d020c1e2f58b292a6bf

Remove sprintf usage for security concerns (#1112)
* Fix vpMatrix::print()
The following matrix:
    vpMatrix M(2, 3);
    M[0][0] = -1;
    M[0][1] = -2;
    M[0][2] = -3;
    M[1][0] = 4;
    M[1][1] = 5.5;
    M[1][2] = 6.0f;
Was miss printed:
- instead of 5.5, 5 was printed

* Remove sprintf usage
- On macOS Monterey 12.6 with Apple clang version 14.0.0, there were a lot of warnings like:
  Due to security concerns inherent in the design of sprintf(3), it is highly recommended that you use snprintf(3) instead.
- sprintf() was replaced using either std::stringstream when possible, or snprintf()

* Fix remaining build errors

* Revert last changes in vpXmlParser
- replace sprintf with snprintf
- using std::stringstream lead to the following ustk tests that were not working
 	116 - testUsSequenceIOXml (Failed)
	117 - testUsImagePostScan2DXmlParser (Failed)
	118 - testUsImagePreScan2DXmlParser (Failed)

* Fix build under windows

* Remove unused var

* Turn off warning related to 3rdparties (PCL used by librealsense) that are using sprintf() in their header files
- introduce -Wno-deprecated-declarations compiler option where it is needed
- validated on apple M1

modules/core/src/math/matrix/vpMatrix.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
  *
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2019 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2022 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,9 +30,6 @@
  *
  * Description:
  * Matrix manipulation.
- *
- * Authors:
- * Eric Marchand
  *
  *****************************************************************************/
 /*!
@@ -323,11 +320,11 @@ int main()
       M[i][j] = val++;
     }
   }
-  M.print (std::cout, 4, "M ");
+  M.print(std::cout, 4, "M ");
 
   vpMatrix N;
   N.init(M, 0, 1, 2, 3);
-  N.print (std::cout, 4, "N ");
+  N.print(std::cout, 4, "N ");
 }
 \endcode
   It produces the following output:
@@ -385,9 +382,9 @@ int main()
       M[i][j] = val++;
     }
   }
-  M.print (std::cout, 4, "M ");
+  M.print(std::cout, 4, "M ");
   vpMatrix N = M.extract(0, 1, 2, 3);
-  N.print (std::cout, 4, "N ");
+  N.print(std::cout, 4, "N ");
 }
   \endcode
   It produces the following output:
@@ -5574,12 +5571,12 @@ void vpMatrix::juxtaposeMatrices(const vpMatrix &A, const vpMatrix &B, vpMatrix 
   The common widths before and after the decimal point
   are set with respect to the parameter maxlen.
 
-  \param s Stream used for the printing.
+  \param s : Stream used for the printing.
 
-  \param length The suggested width of each matrix element.
+  \param length : The suggested width of each matrix element.
   The actual width grows in order to accomodate the whole integral part,
   and shrinks if the whole extent is not needed for all the numbers.
-  \param intro The introduction which is printed before the matrix.
+  \param intro : The introduction which is printed before the matrix.
   Can be set to zero (or omitted), in which case
   the introduction is not printed.
 
@@ -5634,11 +5631,6 @@ int vpMatrix::print(std::ostream &s, unsigned int length, const std::string &int
   totalLength = vpMath::maximum(totalLength, maxBefore);
   // decrease maxAfter according to totalLength
   maxAfter = (std::min)(maxAfter, totalLength - maxBefore);
-  if (maxAfter == 1)
-    maxAfter = 0;
-
-  // the following line is useful for debugging
-  // std::cerr <<totalLength <<" " <<maxBefore <<" " <<maxAfter <<"\n";
 
   if (!intro.empty())
     s << intro;
@@ -5655,8 +5647,8 @@ int vpMatrix::print(std::ostream &s, unsigned int length, const std::string &int
       if (maxAfter > 0) {
         s.setf(std::ios::left, std::ios::adjustfield);
         if (p != std::string::npos) {
-          s.width((std::streamsize)maxAfter);
-          s << values[i * n + j].substr(p, maxAfter).c_str();
+          s.width((std::streamsize)maxAfter + 1);
+          s << values[i * n + j].substr(p, maxAfter + 1).c_str();
         } else {
           assert(maxAfter > 1);
           s.width((std::streamsize)maxAfter);

commit 619f678e41ffbee53528cbf68c1bd5d5529d70ed

Fix vpMatrix::print() after wrong changes introduced in PR #1112

modules/core/src/math/matrix/vpMatrix.cpp
@@ -5569,18 +5569,18 @@ void vpMatrix::juxtaposeMatrices(const vpMatrix &A, const vpMatrix &B, vpMatrix 
 
   Pretty print a matrix. The data are tabulated.
   The common widths before and after the decimal point
-  are set with respect to the parameter maxlen.
+  are set with respect to the parameter `length`.
 
   \param s : Stream used for the printing.
 
   \param length : The suggested width of each matrix element.
-  The actual width grows in order to accomodate the whole integral part,
-  and shrinks if the whole extent is not needed for all the numbers.
+  If needed, the used `length` grows in order to accomodate the whole integral part,
+  and shrinks the decimal part to print only `length` digits.
   \param intro : The introduction which is printed before the matrix.
   Can be set to zero (or omitted), in which case
   the introduction is not printed.
 
-  \return Returns the common total width for all matrix elements
+  \return Returns the common total width for all matrix elements.
 
   \sa std::ostream &operator<<(std::ostream &s, const vpArray2D<Type> &A)
 */
@@ -5596,7 +5596,6 @@ int vpMatrix::print(std::ostream &s, unsigned int length, const std::string &int
   std::ostringstream ossFixed;
   std::ios_base::fmtflags original_flags = oss.flags();
 
-  // ossFixed <<std::fixed;
   ossFixed.setf(std::ios::fixed, std::ios::floatfield);
 
   size_type maxBefore = 0; // the length of the integral part
@@ -5621,7 +5620,7 @@ int vpMatrix::print(std::ostream &s, unsigned int length, const std::string &int
         // maxAfter remains the same
       } else {
         maxBefore = vpMath::maximum(maxBefore, p);
-        maxAfter = vpMath::maximum(maxAfter, thislen - p - 1);
+        maxAfter = vpMath::maximum(maxAfter, thislen - p);
       }
     }
   }
@@ -5647,10 +5646,9 @@ int vpMatrix::print(std::ostream &s, unsigned int length, const std::string &int
       if (maxAfter > 0) {
         s.setf(std::ios::left, std::ios::adjustfield);
         if (p != std::string::npos) {
-          s.width((std::streamsize)maxAfter + 1);
-          s << values[i * n + j].substr(p, maxAfter + 1).c_str();
+          s.width((std::streamsize)maxAfter);
+          s << values[i * n + j].substr(p, maxAfter).c_str();
         } else {
-          assert(maxAfter > 1);
           s.width((std::streamsize)maxAfter);
           s << ".0";
         }

modules/core/test/math/testMatrix.cpp
@@ -271,7 +271,7 @@ int main(int argc, char *argv[])
       M[1][1] = 12.345;
       M[1][2] = .12345;
       std::cout << "call std::cout << M;" << std::endl;
-      std::cout << M;
+      std::cout << M << std::endl;
       std::cout << "call M.print (std::cout, 6);" << std::endl;
       M.print(std::cout, 6);
       std::cout << std::endl;
@@ -294,18 +294,31 @@ int main(int argc, char *argv[])
       std::cout << "------------------------" << std::endl;
       M.resize(3, 3);
       M.eye(3);
-      M[0][2] = -0.0000000876;
+      M[0][2] = -0.0000876;
       std::cout << "call std::cout << M;" << std::endl;
       std::cout << M << std::endl;
 
       std::cout << "call M.print (std::cout, 4);" << std::endl;
       M.print(std::cout, 4);
       std::cout << std::endl;
+      std::cout << "call M.print (std::cout, 6, \"M\");" << std::endl;
+      M.print(std::cout, 6, "M");
+      std::cout << std::endl;
       std::cout << "call M.print (std::cout, 10, \"M\");" << std::endl;
       M.print(std::cout, 10, "M");
       std::cout << std::endl;
-      std::cout << "call M.print (std::cout, 20, \"M\");" << std::endl;
-      M.print(std::cout, 20, "M");
+
+      M.resize(2, 3);
+      M[0][0] = -1;
+      M[0][1] = -2;
+      M[0][2] = -3;
+      M[1][0] = 4;
+      M[1][1] = 5.5;
+      M[1][2] = 6.0f;
+      std::cout << "call std::cout << M;" << std::endl;
+      std::cout << M << std::endl;
+      std::cout << "call M.print (std::cout, 5, \"M\");" << std::endl;
+      M.print(std::cout, 5, "M");
       std::cout << std::endl;
 
       std::cout << "------------------------" << std::endl;