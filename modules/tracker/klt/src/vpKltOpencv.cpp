lagadic/visp

commit a3b12ebaae3f58620c882d020c1e2f58b292a6bf

Remove sprintf usage for security concerns (#1112)
* Fix vpMatrix::print()
The following matrix:
    vpMatrix M(2, 3);
    M[0][0] = -1;
    M[0][1] = -2;
    M[0][2] = -3;
    M[1][0] = 4;
    M[1][1] = 5.5;
    M[1][2] = 6.0f;
Was miss printed:
- instead of 5.5, 5 was printed

* Remove sprintf usage
- On macOS Monterey 12.6 with Apple clang version 14.0.0, there were a lot of warnings like:
  Due to security concerns inherent in the design of sprintf(3), it is highly recommended that you use snprintf(3) instead.
- sprintf() was replaced using either std::stringstream when possible, or snprintf()

* Fix remaining build errors

* Revert last changes in vpXmlParser
- replace sprintf with snprintf
- using std::stringstream lead to the following ustk tests that were not working
 	116 - testUsSequenceIOXml (Failed)
	117 - testUsImagePostScan2DXmlParser (Failed)
	118 - testUsImagePreScan2DXmlParser (Failed)

* Fix build under windows

* Remove unused var

* Turn off warning related to 3rdparties (PCL used by librealsense) that are using sprintf() in their header files
- introduce -Wno-deprecated-declarations compiler option where it is needed
- validated on apple M1

modules/tracker/klt/src/vpKltOpencv.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
  *
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2019 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2022 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,10 +31,6 @@
  * Description:
  * Wrapper for the KLT (Kanade-Lucas-Tomasi) feature tracker implemented
  * with opencv.
- *
- * Authors:
- * Fabien Servant
- * Fabien Spindler
  *
  *****************************************************************************/
 
@@ -1274,10 +1270,10 @@ void vpKltOpencv::display(const vpImage<unsigned char> &I, const CvPoint2D32f *f
     ip.set_v(vpMath::round(features_list[i].y));
     vpDisplay::displayCross(I, ip, 10 + thickness, color, thickness);
 
-    char id[10];
-    sprintf(id, "%ld", featuresid_list[i]);
+    std::stringstream id;
+    id << featuresid_list[i]);
     ip.set_u(vpMath::round(features_list[i].x + 5));
-    vpDisplay::displayText(I, ip, id, color);
+    vpDisplay::displayText(I, ip, id.str(), color);
   }
 }
 
@@ -1306,10 +1302,10 @@ void vpKltOpencv::display(const vpImage<vpRGBa> &I, const CvPoint2D32f *features
     ip.set_v(vpMath::round(features_list[i].y));
     vpDisplay::displayCross(I, ip, 10, color, thickness);
 
-    char id[10];
-    sprintf(id, "%ld", featuresid_list[i]);
+    std::stringstream id;
+    id << featuresid_list[i]);
     ip.set_u(vpMath::round(features_list[i].x + 5));
-    vpDisplay::displayText(I, ip, id, color);
+    vpDisplay::displayText(I, ip, id.str(), color);
   }
 }
 #else

commit 4763350bc067361c5bc80f013c7791445e86c7f9

Fix build issue introduced in PR #1112 when OpenCV < 2.4.8

modules/tracker/klt/src/vpKltOpencv.cpp
@@ -1271,7 +1271,7 @@ void vpKltOpencv::display(const vpImage<unsigned char> &I, const CvPoint2D32f *f
     vpDisplay::displayCross(I, ip, 10 + thickness, color, thickness);
 
     std::stringstream id;
-    id << featuresid_list[i]);
+    id << featuresid_list[i];
     ip.set_u(vpMath::round(features_list[i].x + 5));
     vpDisplay::displayText(I, ip, id.str(), color);
   }
@@ -1303,7 +1303,7 @@ void vpKltOpencv::display(const vpImage<vpRGBa> &I, const CvPoint2D32f *features
     vpDisplay::displayCross(I, ip, 10, color, thickness);
 
     std::stringstream id;
-    id << featuresid_list[i]);
+    id << featuresid_list[i];
     ip.set_u(vpMath::round(features_list[i].x + 5));
     vpDisplay::displayText(I, ip, id.str(), color);
   }