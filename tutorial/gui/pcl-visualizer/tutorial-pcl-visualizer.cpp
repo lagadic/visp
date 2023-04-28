c724bbf7ef0971a2237dba8e89b79b414b76d134
rlagneau <romain.lagneau@inria.fr>
2023-04-28 15:58:40 +0200

    [CORPS] Added possibility to choose between blocking display mode and/or threaded mode

--- tutorial/gui/pcl-visualizer/tutorial-pcl-visualizer.cpp
+++ tutorial/gui/pcl-visualizer/tutorial-pcl-visualizer.cpp
@@ -5,22 +5,76 @@
 #if defined(VISP_HAVE_PCL)
 #include "ClassUsingPclVisualizer.h"
 
+typedef enum DisplayMode
+{
+  BLOCKING   = 0,
+  THREADED   = 1,
+  BOTH       = 2,
+  MODE_COUNT = 3
+}DisplayMode;
+
+std::string displayModeToString(const DisplayMode& mode)
+{
+  switch(mode)
+  {
+    case BLOCKING:
+      return "blocking";
+    case THREADED:
+      return "threaded";
+    case BOTH:
+      return "both";
+    default:
+      break;
+  }
+  return "unknown";
+}
+
+DisplayMode displayModeFromString(const std::string &name)
+{
+  DisplayMode res = DisplayMode::MODE_COUNT;
+  bool wasFound = false;
+  std::string lowerCaseName = name; // vpIoTools::toLowerCase(name);
+  for(unsigned int i = 0; i < DisplayMode::MODE_COUNT && !wasFound; i++)
+  {
+    DisplayMode candidate = (DisplayMode) i;
+    if(lowerCaseName == lowerCaseName)
+    {
+      res = candidate;
+      wasFound = true; 
+    }
+  }
+  return res;
+}
+
+std::string getAvailableDisplayMode(const std::string &prefix = "< ", const std::string &sep = " , ", const std::string &suffix = " >" )
+{
+  std::string modes(prefix);
+  for(unsigned int i = 0; i < DisplayMode::MODE_COUNT - 1; i++)
+  {
+    DisplayMode candidate = (DisplayMode) i;
+    modes += displayModeToString(candidate) + sep;
+  }
+  DisplayMode candidate = (DisplayMode) (DisplayMode::MODE_COUNT - 1);
+  modes += displayModeToString(candidate) + suffix;
+  return modes;
+}
+
+
 int main (int argc, char *argv[])
 {
-  const bool def_addNoise = false;
-  const unsigned int def_order = 2;
+  const bool def_addNoise                              = false;
+  const unsigned int def_order                         = 2;
   const std::pair<double, double> def_xlim             = std::pair<double, double>(-2.5,2.5);
   const std::pair<double, double> def_ylim             = std::pair<double, double>(-2.5,2.5);
   const std::pair<unsigned int, unsigned int> def_reso = std::pair<unsigned int, unsigned int>(50,50);
-  
+  const DisplayMode def_mode                           = DisplayMode::THREADED;  
 
-  bool opt_addNoise = def_addNoise;
-  unsigned int opt_order = def_order;
+  bool opt_addNoise                              = def_addNoise;
+  unsigned int opt_order                         = def_order;
   std::pair<double, double> opt_xlim             = def_xlim;
   std::pair<double, double> opt_ylim             = def_ylim;
   std::pair<unsigned int, unsigned int> opt_reso = def_reso;
-  unsigned int opt_nbIn = 5;
-  unsigned int opt_nbOut = 2;
+  DisplayMode opt_mode                           = def_mode;
 
   for(unsigned int i = 1; i < argc; i++)
   {
@@ -55,7 +109,11 @@ int main (int argc, char *argv[])
       opt_reso.second = atoi(argv[i + 2]);
       i+=2;
     }
-    
+    else if (std::string(argv[i]) == "--display-mode" && i + 1 < argc)
+    {
+      opt_mode = displayModeFromString(std::string(argv[i + 1]));
+      i++;
+    }
     else if (std::string(argv[i]) == "--help" || std::string(argv[i]) == "-h")
     {
       std::cout << "NAME" << std::endl;
@@ -63,19 +121,29 @@ int main (int argc, char *argv[])
                 << std::endl;
       std::cout << "SYNOPSIS" << std::endl;
       std::cout << "\t" << argv[0]
-                << " [--no-noise] || [--add-noise] (default: " + (def_addNoise ? std::string("true") : std::string("false")) << ")\n"
-                << " [--order <surface-order>](default: " + std::to_string(def_order) << ")\n"
-                << " [--x-lim <xmin xmax>](default: [" + std::to_string(def_xlim.first) + ";" + std::to_string(def_xlim.second) << "])\n"
-                << " [--y-lim <ymin ymax>](default: [" + std::to_string(def_ylim.first) + ";" + std::to_string(def_ylim.second) << "])\n"
-                << " [--reso <x_resolution y_resolution>](default: [" + std::to_string(def_reso.first) + ";" + std::to_string(def_reso.second) << "])\n"
-                << " [--help] [-h]" << std::endl
+                << "\t[--no-noise] || [--add-noise] (default: " + (def_addNoise ? std::string("true") : std::string("false")) << ")\n"
+                << "\t[--order <surface-order>](default: " + std::to_string(def_order) << ")\n"
+                << "\t[--x-lim <xmin xmax>](default: [" + std::to_string(def_xlim.first) + ";" + std::to_string(def_xlim.second) << "])\n"
+                << "\t[--y-lim <ymin ymax>](default: [" + std::to_string(def_ylim.first) + ";" + std::to_string(def_ylim.second) << "])\n"
+                << "\t[--reso <x_resolution y_resolution>](default: [" + std::to_string(def_reso.first) + ";" + std::to_string(def_reso.second) << "])\n"
+                << "\t[--display-mode " << getAvailableDisplayMode() <<"](default: " << displayModeToString(def_mode) << ")\n"
+                << "\t[--help] [-h]" << std::endl
                 << std::endl;
 
       return EXIT_SUCCESS;
     }
   }
   ClassUsingPclVisualizer demo(opt_xlim, opt_ylim, opt_reso);
-  demo.threadedMode(opt_addNoise, opt_order);
+
+  if(opt_mode == DisplayMode::BLOCKING || opt_mode == DisplayMode::BOTH)
+  {
+    std::cerr << "Blocking mode has not been implemented yet !" << std::endl;
+  }
+
+  if(opt_mode == DisplayMode::THREADED || opt_mode == DisplayMode::BOTH)
+  {
+    demo.threadedMode(opt_addNoise, opt_order);
+  }
   return 0;
 }
 #else
