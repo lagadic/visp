9c311048c23b0d6943b4378efe71e1db42f3b451
Samuel Felton <samuel.felton@irisa.fr>
2023-05-05 17:16:00 +0200

    testing vpCameraParameters serialization, some more testing on tracker

--- modules/tracker/mbt/test/generic-with-dataset/testMbtJsonSettings.cpp
+++ modules/tracker/mbt/test/generic-with-dataset/testMbtJsonSettings.cpp
@@ -66,13 +66,36 @@ vpMbGenericTracker baseTrackerConstructor() {
   cams[names[0]] = cam1;
   cams[names[1]] = cam2;
   t.setCameraParameters(cams);
+
+  t.setLod(false);
+
+  t.setAngleAppear(vpMath::rad(60));
+  t.setAngleDisappear(vpMath::rad(90));
+  
   return t;
 }
+
+
+template<typename T, typename C>
+void checkProperties(const T& t1, const T& t2, C fn, const std::string& message) {
+  THEN(message) {
+    REQUIRE( (t1.*fn)() == (t2.*fn)() );
+  }
+}
+
+
+template<typename T, typename C, typename... Fns>
+void checkProperties(const T& t1, const T& t2, C fn, const std::string& message, Fns... fns) {
+  checkProperties(t1, t2, fn, message);
+  checkProperties(t1, t2, fns...);
+}
+
 
 void compareNamesAndTypes(const vpMbGenericTracker& t1, const vpMbGenericTracker& t2) {
   REQUIRE( t1.getCameraNames() == t2.getCameraNames() );
   REQUIRE( t1.getCameraTrackerTypes() == t2.getCameraTrackerTypes() );
 }
+
 
 void compareCameraParameters(const vpMbGenericTracker& t1, const vpMbGenericTracker& t2) {
   std::map<std::string, vpCameraParameters> c1, c2;
@@ -92,7 +115,6 @@ json loadJson(const std::string& path) {
   return j;
 }
 
-
 void saveJson(const json& j, const std::string& path) {
   std::ofstream json_file(path);
   if(!json_file.good()) {
@@ -102,60 +124,12 @@ void saveJson(const json& j, const std::string& path) {
   json_file.close();
 }
 
-void test_json(const std::string& dir, const std::string& test_name, const std::function<vpMbGenericTracker()> setup, const std::function<void(json&)> modifyJson, const std::function<void(const vpMbGenericTracker&, const vpMbGenericTracker&)> compare) {
-  const std::string json_path = dir + "/" + test_name + ".json";
-  vpMbGenericTracker t1 = setup();
-  t1.saveConfigFile(json_path);
-
-  json j = loadJson(json_path);
-  modifyJson(j);
-  saveJson(j, json_path);
-  
-
-  vpMbGenericTracker t2;
-  t2.loadConfigFile(json_path);
-
-  compare(t1, t2);
-}
-
-
-//   // // Reference camera name not found
-//   // try {
-//   //   test_json(tmp_dir, "reference_cam_not_found", baseTrackerConstructor, [](json& j) -> void { j["referenceCameraName"] = "C3"; }, compareNamesAndTypes);
-//   //   return EXIT_FAILURE;
-//   // } catch(vpException& e) {
-//   //   if(e.getCode() != vpException::badValue) {
-//   //     return EXIT_FAILURE;
-//   //   }
-//   // } catch(...) {
-//   //   std::cerr << "Unexpected exception was caught" << std::endl;
-//   //   return EXIT_FAILURE;
-//   // }
-
-//   // // Camera to ref transformations test
-  
-//   // if(!test_json(tmp_dir, "camTref_removed_for_ref", baseTrackerConstructor, [](json& j) -> void { j["trackers"][j["referenceCameraName"].get<std::string>()].erase("camTref"); }, compareNamesAndTypes)) {
-//   //   std::cerr << "Removing the camera transformations should be allowed for the reference camera" << std::endl;
-//   //   return EXIT_FAILURE;
-//   // }
-
-SCENARIO("MBT JSON Parsing", "[json]") {
+SCENARIO("MBT JSON Serialization", "[json]") {
   // setup test dir
   // Get the user login name
-#if defined(_WIN32)
-  std::string tmp_dir = "C:/temp/";
-#else
-  std::string tmp_dir = "/tmp/";
-#endif
-  std::string username;
-  vpIoTools::getUserName(username);
 
-  tmp_dir += username + "/visp_test_json_parsing_mbt/";
-  vpIoTools::remove(tmp_dir);
-  std::cout << "Create: " << tmp_dir << std::endl;
-  vpIoTools::makeDirectory(tmp_dir);
-
-
+  std::string tmp_dir = vpIoTools::makeTempDirectory(vpIoTools::getTempPath() + vpIoTools::path("/") + "visp_test_json_parsing_mbt");
+  
 
   GIVEN("A generic tracker with two cameras, one with edge and KLT features, the other with depth features") {
     vpMbGenericTracker t1 = baseTrackerConstructor();
@@ -175,7 +149,87 @@ SCENARIO("MBT JSON Parsing", "[json]") {
         compareNamesAndTypes(t1, t2);
         compareCameraParameters(t1, t2);
       }
-      WHEN("Modifying JSON file") {
+
+      THEN("Reloading this tracker has the same basic properties") {
+        vpMbGenericTracker t2;
+        REQUIRE_NOTHROW(t2.loadConfigFile(jsonPath));
+        checkProperties(t1, t2,
+                &vpMbGenericTracker::getAngleAppear, "Angle appear should be the same",
+                &vpMbGenericTracker::getAngleDisappear, "Angle appear should be the same"
+        );
+      }
+
+      THEN("Reloaded edge tracker parameters should be the same") {
+        std::map<std::string, vpMe> oldvpMe, newvpMe;
+        t1.getMovingEdge(oldvpMe);
+        vpMbGenericTracker t2;
+        t2.loadConfigFile(jsonPath);
+        t2.getMovingEdge(newvpMe);
+        for(const auto& it: oldvpMe) {
+          vpMe o = it.second;
+          vpMe n;
+          REQUIRE_NOTHROW( n = newvpMe[it.first] );
+          checkProperties(o, n,
+            &vpMe::getAngleStep, "Angle step should be equal",
+            &vpMe::getMaskNumber, "Mask number should be equal",
+            &vpMe::getMaskSign, "Mask sign should be equal",
+            &vpMe::getMinSampleStep, "Min sample step should be equal",
+            &vpMe::getMu1, "Mu 1 should be equal",
+            &vpMe::getMu2, "Mu 2 should be equal",
+            &vpMe::getNbTotalSample, "Nb total sample should be equal",
+            &vpMe::getPointsToTrack, "Number of points to track should be equal",
+            &vpMe::getRange, "Range should be equal",
+            &vpMe::getStrip, "Strip should be equal"
+          );
+        }
+      }
+      
+      THEN("Reloaded KLT tracker parameters should be the same") {
+        std::map<std::string, vpKltOpencv> oldvpKlt, newvpKlt;
+        t1.getKltOpencv(oldvpKlt);
+        vpMbGenericTracker t2;
+        t2.loadConfigFile(jsonPath);
+        t2.getKltOpencv(newvpKlt);
+        for(const auto& it: oldvpKlt) {
+          vpKltOpencv o = it.second;
+          vpKltOpencv n;
+          REQUIRE_NOTHROW( n = newvpKlt[it.first] );
+          checkProperties(o, n,
+            &vpKltOpencv::getBlockSize, "Block size should be equal",
+            &vpKltOpencv::getHarrisFreeParameter, "Harris parameter should be equal",
+            &vpKltOpencv::getMaxFeatures, "Max number of features should be equal",
+            &vpKltOpencv::getMinDistance, "Minimum distance should be equal",
+            &vpKltOpencv::getPyramidLevels, "Pyramid levels should be equal",
+            &vpKltOpencv::getQuality, "Quality should be equal",
+            &vpKltOpencv::getWindowSize, "Window size should be equal"
+          );
+        }
+      }
+
+      THEN("Clipping properties should be the same") {
+        vpMbGenericTracker t2 = baseTrackerConstructor();
+        t2.setNearClippingDistance(0.1);
+        t2.setFarClippingDistance(2.0);
+        t2.setClipping(vpPolygon3D::ALL_CLIPPING);
+        t2.loadConfigFile(jsonPath);
+        std::map<std::string, unsigned int> oldFlags, newFlags;
+        t1.getClipping(oldFlags);
+        t2.getClipping(newFlags);
+        for(const auto it: oldFlags) {
+          unsigned int o = it.second;
+          unsigned int n;
+          REQUIRE_NOTHROW( n = newFlags[it.first] );
+          THEN("Clipping flags for camera " + it.first + " should be the same") {
+            REQUIRE( o == n );
+          }
+        }
+        checkProperties(t1, t2,
+          &vpMbGenericTracker::getNearClippingDistance, "Near clipping distance should be the same",
+          &vpMbGenericTracker::getFarClippingDistance, "Far clipping distance should be the same"
+        );
+      }
+
+      WHEN("Modifying JSON file/Using a custom JSON file") {
         THEN("Removing version from file generates an error on load") {
           modifyJson([](json& j) -> void {
             j.erase("version");
@@ -211,12 +265,71 @@ SCENARIO("MBT JSON Parsing", "[json]") {
           });
           REQUIRE_THROWS(t1.loadConfigFile(jsonPath));
         }
+
+        THEN("The full clipping config is optional") {
+          vpMbGenericTracker t2 = baseTrackerConstructor();
+          const double near = 0.21;
+          const double far = 5.2;
+          const int clipping = vpPolygon3D::LEFT_CLIPPING;
+          t2.setNearClippingDistance(near);
+          t2.setFarClippingDistance(far);
+          t2.setClipping(clipping);
+          modifyJson([&t1](json &j) -> void {
+            for(const auto& c: t1.getCameraNames()) {
+              j["trackers"][c].erase("clipping");
+            }
+          });
+          REQUIRE_NOTHROW(t2.loadConfigFile(jsonPath, false));
+          REQUIRE( t2.getClipping() == clipping);
+          REQUIRE( t2.getNearClippingDistance() == near );
+          REQUIRE( t2.getFarClippingDistance() == far );
+        }
+
+        THEN("Each clipping param is optional on its own") {
+          vpMbGenericTracker t2 = baseTrackerConstructor();
+          const double near = 0.21;
+          const double far = 5.2;
+          const int clipping = vpPolygon3D::LEFT_CLIPPING;
+          t2.setNearClippingDistance(near);
+          t2.setFarClippingDistance(far);
+          t2.setClipping(clipping);
+          THEN("Near clipping is optional") {
+            modifyJson([&t1](json &j) -> void {
+              for(const auto& c: t1.getCameraNames()) {
+                j["trackers"][c]["clipping"].erase("near");
+              }
+            });
+            t2.loadConfigFile(jsonPath);
+            REQUIRE(t2.getNearClippingDistance() == near);
+            REQUIRE(t2.getFarClippingDistance() == t1.getFarClippingDistance());
+            REQUIRE(t2.getClipping() == t1.getClipping());
+          }
+          THEN("Far clipping is optional") {
+            modifyJson([&t1](json &j) -> void {
+              for(const auto& c: t1.getCameraNames()) {
+                j["trackers"][c]["clipping"].erase("far");
+              }
+            });
+            t2.loadConfigFile(jsonPath);
+            REQUIRE(t2.getNearClippingDistance() == t1.getNearClippingDistance());
+            REQUIRE(t2.getFarClippingDistance() == far);
+            REQUIRE(t2.getClipping() == t1.getClipping());
+          }
+          THEN("Clipping flags are optional") {
+            modifyJson([&t1](json &j) -> void {
+              for(const auto& c: t1.getCameraNames()) {
+                j["trackers"][c]["clipping"].erase("flags");
+              }
+            });
+            t2.loadConfigFile(jsonPath);
+            REQUIRE(t2.getNearClippingDistance() == t1.getNearClippingDistance());
+            REQUIRE(t2.getFarClippingDistance() == t1.getFarClippingDistance());
+            REQUIRE(t2.getClipping() & clipping);
+          }
+        }
       }
     }
   }
-
-
-  
 }
 int main(int argc, char *argv[])
 {
@@ -227,7 +340,7 @@ int main(int argc, char *argv[])
   return numFailed;
 }
 
-#else 
+#else
 
 int main() {
   return EXIT_SUCCESS;
