82897c661261a061ef06dadfab4c22a4a3855ade
Samuel Felton <samuel.felton@irisa.fr>
2023-05-24 01:28:39 +0200

    vpMegaposeTracker tutorial: parsing, start

--- modules/tracker/dnn/include/visp3/dnn_tracker/vpMegaPoseTracker.h
+++ modules/tracker/dnn/include/visp3/dnn_tracker/vpMegaPoseTracker.h
@@ -1,13 +1,16 @@
 #ifndef VPMEGAPOSETRACKER_HPP
 #define VPMEGAPOSETRACKER_HPP
 
-#include <future>
+#include <visp3/core/vpConfig.h>
+#if defined(VISP_HAVE_NLOHMANN_JSON)
 
+#include <future>
+#include<memory>
 #include <visp3/dnn_tracker/vpMegaPose.h>
 
 class vpMegaPoseTracker {
 public:
-  vpMegaPoseTracker(vpMegaPose& megapose, const std::string& objectLabel, const int refinerIterations) :
+  vpMegaPoseTracker(std::shared_ptr<vpMegaPose> megapose, const std::string& objectLabel, const int refinerIterations) :
     megapose(megapose), objectLabel(objectLabel), refinerIterations(refinerIterations)
   {
   }
@@ -15,9 +18,12 @@ public:
   std::future<vpMegaPoseEstimate> track(const vpImage<vpRGBa>& I);
 
 private:
-  vpMegaPose& megapose;
+  std::shared_ptr<vpMegaPose> megapose;
   vpMegaPoseEstimate poseEstimate;
   std::string objectLabel;
   int refinerIterations;
 };
+
+#endif // VISP_HAVE_NLOHMANN_JSON
+
 #endif
--- modules/tracker/dnn/src/vpMegaPoseTracker.cpp
+++ modules/tracker/dnn/src/vpMegaPoseTracker.cpp
@@ -5,15 +5,16 @@ std::future<vpMegaPoseEstimate> vpMegaPoseTracker::init(const vpImage<vpRGBa>& I
 {
   return std::async(std::launch::async, [&I, &bb, this]() -> vpMegaPoseEstimate {
     std::vector<vpRect> bbs = { bb };
-    this->poseEstimate = megapose.estimatePoses(I, { this->objectLabel }, nullptr, 0.0, &bbs, nullptr)[0];
+    this->poseEstimate = megapose->estimatePoses(I, { this->objectLabel }, nullptr, 0.0, &bbs, nullptr)[0];
     return this->poseEstimate;
   });
 }
+
 std::future<vpMegaPoseEstimate>  vpMegaPoseTracker::track(const vpImage<vpRGBa>& I)
 {
   return std::async(std::launch::async, [&I, this]() -> vpMegaPoseEstimate {
     std::vector<vpHomogeneousMatrix> poses = { this->poseEstimate.cTo };
-    this->poseEstimate = megapose.estimatePoses(I, { this->objectLabel }, nullptr, 0.0, nullptr, &poses, refinerIterations)[0];
+    this->poseEstimate = megapose->estimatePoses(I, { this->objectLabel }, nullptr, 0.0, nullptr, &poses, refinerIterations)[0];
     return this->poseEstimate;
   });
 }
