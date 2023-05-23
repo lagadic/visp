03b3e3474408fe9b30b7f3190f677965eebaf319
Fabien Spindler <Fabien.Spindler@inria.fr>
2023-05-23 09:55:05 +0200

    Fix typo instanciate -> instantiate

--- modules/tracker/tt/include/visp3/tt/vpTemplateTracker.h
+++ modules/tracker/tt/include/visp3/tt/vpTemplateTracker.h
@@ -56,7 +56,7 @@
   \class vpTemplateTracker
   \ingroup group_tt_tracker
 
-  This class allows to instanciate a template tracker using image registration
+  This class allows to instantiate a template tracker using image registration
   algorithms \cite Dame10c \cite Dame11c.
 */
 class VISP_EXPORT vpTemplateTracker
