b449b1da6d89d67efa8b3d3689977b3a7eda8f10
Samuel Felton <samuel.felton@irisa.fr>
2023-06-20 01:56:54 +0200

    Script to generate coverage locally, change coverage CI to exclude tests and other directories that should not be reported

---
+++ script/make-coverage-report.sh
@@ -0,0 +1,16 @@
+# usage: $ script/make-coverage-report.sh path/to/build
+if test "$#" -ne 1; then
+  echo "Script should take the path to the build directory as input, and should be started from the root of the visp source directory"
+fi
+
+source_dir=`pwd`
+cd $1
+lcov --zerocounters --directory .
+cmake --build . --target all -j$(nproc)
+cmake --build . --target test
+lcov --directory . --capture --output-file visp-coverage.info
+lcov --remove visp-coverage.info '/usr/*' "$source_dir/3rdparty/*" "$source_dir/demo/*" "$source_dir/samples/*" "$source_dir/example/*" '*/test/*' --output-file visp-coverage.cleaned
+genhtml visp-coverage.cleaned -o coverage
+
+echo "Detected source directory: $source_dir"
+echo "Coverage report generated in $1/coverage"
