ececa6b494ddcb43ca3beaa39f4cd7d58dbe1c8d
Fabien Spindler <Fabien.Spindler@inria.fr>
2023-07-03 16:34:33 +0200

    Fix doxygen warning around snippet usage

--- tutorial/tracking/moving-edges/tutorial-me-line-tracker.cpp
+++ tutorial/tracking/moving-edges/tutorial-me-line-tracker.cpp
@@ -41,16 +41,16 @@ int main()
       std::cout << "Failed to open the camera" << std::endl;
       return EXIT_FAILURE;
     }
-    cv::Mat frame;
-    g >> frame; // get a new frame from camera
 #endif
-
+    //! [grabber container]
+    //! [first image acquisition]
 #if defined(VISP_HAVE_DC1394) || defined(VISP_HAVE_V4L2) || defined(VISP_HAVE_CMU1394)
     g.acquire(I);
 #elif defined(VISP_HAVE_VIDEOIO)
     g >> frame; // get a new frame from camera
     vpImageConvert::convert(frame, I);
 #endif
+    //! [first image acquisition]
 
     //! [display container]
 #if defined(VISP_HAVE_X11)
