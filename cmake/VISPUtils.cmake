3bbec4c2317bb1b407ca363a42e2e8e7db6aebf6
Fabien Spindler <Fabien.Spindler@inria.fr>
2023-07-06 09:49:35 +0200

    Add feedback for OpenCV to give the explicit list of modules that are found

--- cmake/VISPUtils.cmake
+++ cmake/VISPUtils.cmake
@@ -279,13 +279,11 @@ set(${list_var}_DST_${__id} \"${${list_var}_DST_${__id}}\")
   endforeach()
 endmacro()
 
-
 macro(vp_copyfiles_make_config_file filename_var list_var)
   vp_copyfiles_make_config_string(${list_var}_CONFIG ${list_var})
   set(${filename_var} "${CMAKE_CURRENT_BINARY_DIR}/copyfiles-${list_var}.cmake")
   file(WRITE "${${filename_var}}" "${${list_var}_CONFIG}")
 endmacro()
-
 
 macro(vp_copyfiles_add_forced_target target list_var comment_str)
   vp_copyfiles_make_config_file(CONFIG_FILE ${list_var})
@@ -337,6 +335,15 @@ endmacro()
 macro(vp_debug_message)
   #string(REPLACE ";" " " __msg "${ARGN}")
   #message(STATUS "${__msg}")
+endmacro()
+
+# remove all matching elements from the list
+macro(vp_list_filterout lst regex)
+  foreach(item ${${lst}})
+    if(item MATCHES "${regex}")
+      list(REMOVE_ITEM ${lst} "${item}")
+    endif()
+  endforeach()
 endmacro()
 
 # remove all matching elements from the list
@@ -429,7 +436,7 @@ macro(vp_list_remove_separator __lst)
   endif()
 endmacro()
 
-# remove cmake ; list separator
+# replace cmake ; list separator
 macro(vp_list_replace_separator __lst __separator)
   if(${__lst})
     list(GET ${__lst} 0 __lst_reformated)
@@ -458,7 +465,6 @@ endmacro()
 # Example:
 #   VP_OPTION(USE_VTK "VTK" "QUIET" "Include vtk support" "" ON)
 #   VP_OPTION(USE_VTK "VTK;COMPONENTS;vtkCommonCore;vtkFiltersSources" "" "Include vtk support" "" ON)
-
 macro(VP_OPTION variable package quiet description advanced value)
   set(__option TRUE)
   set(__value ${value})
@@ -1915,4 +1921,27 @@ macro(vp_system_information NUMBER_OF_LOGICAL_CORES NUMBER_OF_PHYSICAL_CORES TOT
   set(${IS_64BIT} ${__IS_64BIT})
   set(${HAS_FPU} ${__HAS_FPU})
   set(${CPU_OPTIM} ${__CPU_OPTIM})
+endmacro()
+
+# Replace regular expression in a var
+macro(vp_replace_string var_in var_out regular_expression replacement_expression)
+  set(__var_out ${var_out})
+  if(${var_in} MATCHES "${regular_expression}")
+    string(REGEX REPLACE "${regular_expression}" "${replacement_expression}" ${var_out} ${${var_in}})
+  else()
+    set(${__var_out} ${${var_in}})
+  endif()
+endmacro()
+
+macro(vp_list_replace_string list_in list_out regular_expression replacement_expression)
+  set(__list_out ${var_out})
+  foreach(item ${${list_in}})
+    if(item MATCHES "${regular_expression}")
+      string(REGEX REPLACE "${regular_expression}" "${replacement_expression}" var_out ${item})
+    else()
+      set(var_out ${item})
+    endif()
+    list(APPEND __list_out ${var_out})
+  endforeach()
+  set(${list_out} ${__list_out})
 endmacro()
