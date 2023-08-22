ac70d68eee7fbff634d5c10ee5b12014b2685d15
Samuel Felton <samuel.felton@irisa.fr>
2023-08-22 13:53:40 +0200

    Better type expression, creating header environment for name resolution

--- modules/python/generator/generator.py
+++ modules/python/generator/generator.py
@@ -1,15 +1,47 @@
-from typing import List, Optional, Set, Tuple
+from typing import List, Optional, Set, Tuple, Dict, Union
 from cxxheaderparser.parserstate import ClassBlockState, State
 import pcpp
 import cxxheaderparser
 from cxxheaderparser.visitor import CxxVisitor
 from cxxheaderparser import types
-from cxxheaderparser.simple import parse_string, ParsedData
+from cxxheaderparser.simple import parse_string, ParsedData, NamespaceScope, ClassScope
 from pathlib import Path
 import json
 
-def get_type(param: types.DecoratedType, owner_specs: List[str]) -> str:
-  pass
+
+def get_type(param: Union[types.FunctionType, types.DecoratedType, types.Value], owner_specs: Dict[str, str]) -> Optional[str]:
+  def segment_repr(segment: types.PQNameSegment) -> str:
+    spec_str = ''
+    if isinstance(segment, types.FundamentalSpecifier):
+      return segment.name
+    if segment.name in owner_specs:
+      segment.name = owner_specs[segment.name]
+
+    if segment.specialization is not None:
+      template_strs = []
+      for arg in segment.specialization.args:
+        template_strs.append(get_type(arg.arg, owner_specs))
+
+      spec_str = f'<{",".join(template_strs)}>'
+
+    return segment.name + spec_str
+  if isinstance(param, types.Value):
+    return ''.join([token.value for token in param.tokens])
+  if isinstance(param, types.FunctionType):
+    return 'FUNCTION'
+  if isinstance(param, types.Type):
+    repr_str = '::'.join(list(map(segment_repr, param.typename.segments)))
+    if param.const:
+      repr_str = 'const ' + repr_str
+    return repr_str
+  elif isinstance(param, types.Reference):
+    repr_str = get_type(param.ref_to, owner_specs)
+    if repr_str is not None:
+      return repr_str + '&'
+    else:
+      return None
+  else:
+    return None
 
 def filter_includes(include_names: Set[str]) -> List[str]:
   result = []
@@ -111,6 +143,42 @@ def get_submodules(include_path: Path, generate_path: Path) -> List[Submodule]:
     # Submodule('vs', include_path / 'vs', generate_path / 'vs.cpp')
 
   ]
+class HeaderEnvironment():
+  def __init__(self, data: ParsedData):
+    self.mapping = self.build_mapping(data.namespace)
+  def build_mapping(self, data: Union[NamespaceScope, ClassScope], mapping={}, scope: str = ''):
+    if isinstance(data, NamespaceScope):
+      for alias in data.using_alias:
+        mapping[alias.alias] = get_type(alias.type)
+      for enum in data.enums:
+        print(enum)
+        enum_name = '::'.join([seg.name for seg in enum.base.segments])
+        mapping[enum_name] = scope + enum_name
+      for cls in data.classes:
+        cls_name = '::'.join([seg.name for seg in cls.class_decl.typename.segments])
+        mapping[cls_name] = scope + cls_name
+        mapping.update(self.build_mapping(cls, mapping=mapping, scope=f'{scope}{cls_name}::'))
+      for namespace in data.namespaces:
+        mapping.update(self.build_mapping(data.namespaces[namespace], mapping=mapping, scope=f'{scope}{namespace}::'))
+
+    elif isinstance(data, ClassScope):
+      for alias in data.using_alias:
+        mapping[alias.alias] = get_type(alias.type)
+      for typedef in data.typedefs:
+        mapping[typedef.name] = scope + typedef.name
+      for enum in data.enums:
+        print(enum)
+
+        enum_name = '::'.join([seg.name for seg in enum.typename.segments if not isinstance(seg, types.AnonymousName)])
+        if enum_name == 'vpFontFamily':
+          print('AAAAAAAAA' * 1000)
+        mapping[enum_name] = scope + enum_name
+      for cls in data.classes:
+        cls_name = '::'.join([seg.name for seg in cls.class_decl.typename.segments])
+        mapping[cls_name] = scope + cls_name
+        mapping.update(self.build_mapping(cls, mapping=mapping, scope=f'{scope}{cls_name}::'))
+
+    return mapping
 
 class HeaderFile():
   def __init__(self, path: Path, submodule: Submodule):
@@ -121,7 +189,7 @@ class HeaderFile():
     self.binding_code = None
     self.generate_binding_code(content)
 
-  def run_preprocessor(self):
+  def run_preprocessor(self): # TODO: run without generating a new file
     tmp_file_path = self.submodule.submodule_file_path.parent / "tmp" / self.path.name
     argv = [
       '',
@@ -140,6 +208,7 @@ class HeaderFile():
     with open(tmp_file_path, 'r') as header_file:
       preprocessed_header_content = '\n'.join(header_file.readlines())
     return preprocessed_header_content
+
   def generate_binding_code(self, content: str) -> None:
     parsed_data = parse_string(content)
     self.binding_code = self.parse_data(parsed_data)
@@ -147,7 +216,11 @@ class HeaderFile():
 
   def parse_data(self, data: ParsedData):
     result = ''
-
+    header_env = HeaderEnvironment(data)
+    if 'vpFont' in self.path.name:
+      print(header_env.mapping)
+      import time
+      # time.sleep(1000)
     print(f'Parsing namespace "{data.namespace.name}"')
     for cls in data.namespace.classes:
       print(f'Parsing class "{cls.class_decl.typename}"')
@@ -175,23 +248,21 @@ class HeaderFile():
           params_strs = []
           skip_constructor = False
           for param in method.parameters:
-
-            if isinstance(param.type, types.Type):
-              print(param.type.typename)
-              params_strs.append('::'.join([seg.name for seg in param.type.typename.segments]))
-            elif isinstance(param.type, types.Reference):
-              param_str = ''
-              print(param)
-              print(param.type.ref_to.typename.segments[0].specialization)
-              if param.type.ref_to.const:
-                param_str += 'const '
-              param_str += '::'.join([seg.name for seg in param.type.ref_to.typename.segments])
-
-              param_str += '&'
-              params_strs.append(param_str)
-            else:
+            param_str = get_type(param.type, {})
+            if 'vpFont' in self.path.name:
+              print(param_str)
+            if param_str in header_env.mapping: # TODO: replace param str with raw type: no const or ref
+              param_str = header_env.mapping[param_str]
+            if 'vpFont' in self.path.name:
+              print(param_str)
+              time.sleep(1)
+            print(param_str)
+            if param_str is None:
+              print('Skipping constructor', method)
               skip_constructor = True
               break
+            else:
+              params_strs.append(param_str)
           if not skip_constructor:
             argument_types = ', '.join(params_strs)
             print(argument_types)
