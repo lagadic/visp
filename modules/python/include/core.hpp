ca5c68c5c29dbe0dc460d37a1319b132412ca5bf
Samuel Felton <samuel.felton@irisa.fr>
2023-09-08 17:14:26 +0200

    more work on numpy conversion

--- modules/python/include/core.hpp
+++ modules/python/include/core.hpp
@@ -1,13 +1,21 @@
 #ifndef VISP_PYTHON_CORE_HPP
 #define VISP_PYTHON_CORE_HPP
+#include <sstream>
+#include <cstring>
+
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 
 #include <visp3/core/vpArray2D.h>
 #include <visp3/core/vpColVector.h>
+#include <visp3/core/vpHomogeneousMatrix.h>
+
 
 namespace py = pybind11;
+
+template<typename Item>
+using np_array_cf = py::array_t<Item, py::array::c_style | py::array::forcecast>;
 
 /*
 Create a buffer info for a row major array
@@ -34,36 +42,97 @@ py::buffer_info make_array_buffer(T *data, std::array<unsigned, N> dims, bool re
   );
 }
 
+std::string shape_to_string(const std::vector<ssize_t> &shape)
+{
+  std::stringstream ss;
+  ss << "(";
+  for (int i = 0; i < shape.size() - 1; ++i) {
+    ss << shape[i] << ",";
+  }
+  if (shape.size() > 0) {
+    ss << shape[shape.size() - 1];
+  }
+  ss << ")";
+  return ss.str();
+}
+
+template<typename Item>
+void verify_array_shape_and_dims(np_array_cf<Item> np_array, unsigned dims, const char *class_name)
+{
+  py::buffer_info buffer = np_array.request();
+  std::vector<ssize_t> shape = buffer.shape;
+  if (shape.size() != dims) {
+    std::stringstream ss;
+    ss << "Tried to instanciate " << class_name
+      << " that expects a " << dims << "D array but got a numpy array of shape "
+      << shape_to_string(shape);
+
+    throw std::runtime_error(ss.str());
+  }
+}
+template<typename Item>
+void verify_array_shape_and_dims(np_array_cf<Item> np_array, std::vector<ssize_t> expected_dims, const char *class_name)
+{
+  verify_array_shape_and_dims(np_array, expected_dims.size());
+  py::buffer_info buffer = np_array.request();
+  std::vector<ssize_t> shape = buffer.shape;
+  bool invalid_shape = false;
+  for (unsigned int i = 0; i < expected_dims.size(); ++i) {
+    if (shape[i] != expected_dims[i]) {
+      invalid_shape = true;
+      break;
+    }
+  }
+  if (invalid_shape) {
+    std::stringstream ss;
+    ss << "Tried to instanciate " << class_name
+      << " that expects an array of dimensions " << shape_to_string(expected_dims)
+      << " but got a numpy array of shape " << shape_to_string(shape);
+
+    throw std::runtime_error(ss.str());
+  }
+}
+template<typename Item>
+void copy_data_from_np(np_array_cf<Item> src, Item *dest)
+{
+  py::buffer_info buffer = src.request();
+  std::vector<ssize_t> shape = buffer.shape;
+  unsigned int elements = 1;
+  for (ssize_t dim : shape) {
+    elements *= dim;
+  }
+  const Item *data = (Item *)buffer.ptr;
+  std::memcpy(dest, data, elements * sizeof(Item));
+
+}
+
 template<typename T>
 void bindings_vpArray2D(py::class_<vpArray2D<T>> &pyArray2D)
 {
   pyArray2D.def_buffer([](vpArray2D<T> &array) -> py::buffer_info {
     return make_array_buffer<T, 2>(array.data, { array.getRows(), array.getCols() }, false);
-    // py::buffer_info(
-    //     array.data,                             /* Pointer to buffer */
-    //     sizeof(T),                              /* Size of one scalar */
-    //     py::format_descriptor<T>::format(),     /* Python struct-style format descriptor */
-    //     2,                                      /* Number of dimensions */
-    //     py::detail::any_container<ssize_t>({ array.getRows(), array.getCols() }),   /* Buffer dimensions */
-    //     py::detail::any_container<ssize_t>({ sizeof(T) * array.getCols(), sizeof(T) }),  /* Strides (in bytes) for each index */
-    //     false /* Readonly? */
-    // );
   });
+  pyArray2D.def(py::init([](np_array_cf<T> np_array) {
+    verify_array_shape_and_dims(np_array, 2, "ViSP 2D array");
+    const std::vector<ssize_t> shape = np_array.request().shape;
+    vpArray2D<T> result(shape[0], shape[1]);
+    copy_data_from_np(np_array, result.data);
+    return result;
+  }));
 }
 
 
 void bindings_vpColVector(py::class_<vpColVector, vpArray2D<double>> &pyColVector)
 {
   pyColVector.def_buffer([](vpColVector &array) -> py::buffer_info {
-    return py::buffer_info(
-        array.data,                             /* Pointer to buffer */
-        sizeof(double),                              /* Size of one scalar */
-        py::format_descriptor<double>::format(),     /* Python struct-style format descriptor */
-        1,                                      /* Number of dimensions */
-        py::detail::any_container<ssize_t>({ array.getRows() }),   /* Buffer dimensions */
-        py::detail::any_container<ssize_t>({ sizeof(double) }),  /* Strides (in bytes) for each index */
-        false /* Readonly? */
-    );
+    return make_array_buffer<double, 1>(array.data, { array.getRows() }, false);
+  });
+}
+
+void bindings_vpRowVector(py::class_<vpRowVector, vpArray2D<double>> &pyRowVector)
+{
+  pyRowVector.def_buffer([](vpRowVector &array) -> py::buffer_info {
+    return make_array_buffer<double, 1>(array.data, { array.getCols() }, false);
   });
 }
 
@@ -71,15 +140,14 @@ void bindings_vpColVector(py::class_<vpColVector, vpArray2D<double>> &pyColVecto
 void bindings_vpRotationMatrix(py::class_<vpRotationMatrix, vpArray2D<double>> &pyRotationMatrix)
 {
   pyRotationMatrix.def_buffer([](vpRotationMatrix &array) -> py::buffer_info {
-    return py::buffer_info(
-        array.data,                             /* Pointer to buffer */
-        sizeof(double),                              /* Size of one scalar */
-        py::format_descriptor<double>::format(),     /* Python struct-style format descriptor */
-        2,                                      /* Number of dimensions */
-        py::detail::any_container<ssize_t>({ array.getRows(), array.getCols() }),   /* Buffer dimensions */
-        py::detail::any_container<ssize_t>({ 3 * sizeof(double), sizeof(double) }),  /* Strides (in bytes) for each index */
-        true /* Readonly? */
-    );
+    return make_array_buffer<double, 2>(array.data, { array.getRows(), array.getCols() }, true);
+  });
+}
+
+void bindings_vpHomogeneousMatrix(py::class_<vpHomogeneousMatrix, vpArray2D<double>> &pyHomogeneousMatrix)
+{
+  pyHomogeneousMatrix.def_buffer([](vpHomogeneousMatrix &array) -> py::buffer_info {
+    return make_array_buffer<double, 2>(array.data, { array.getRows(), array.getCols() }, true);
   });
 }
 
