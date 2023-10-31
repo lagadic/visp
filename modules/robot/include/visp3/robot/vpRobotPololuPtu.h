49a71acfbb2ab9f79d89874f7f7d754441b76209
Fabien Spindler <Fabien.Spindler@inria.fr>
2023-10-31 17:23:57 +0100

    Refactor Pololu related classes and introduce an IBVS example

---
+++ modules/robot/include/visp3/robot/vpRobotPololuPtu.h
@@ -0,0 +1,264 @@
+/*
+ * ViSP, open source Visual Servoing Platform software.
+ * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact Inria about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See https://visp.inria.fr for more information.
+ *
+ * This software was developed at:
+ * Inria Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Inria at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Description:
+ * Common features for Pololu Maestro PanTiltUnit.
+ */
+
+#ifndef _vpRobotPololuPtu_h_
+#define _vpRobotPololuPtu_h_
+
+#include <visp3/core/vpConfig.h>
+
+#ifdef VISP_HAVE_POLOLU
+
+#include <visp3/robot/vpRobot.h>
+#include <visp3/robot/vpPololu.h>
+
+using namespace std;
+
+/*!
+ * \class vpRobotPololuPtu
+ * \ingroup group_robot_real_arm
+ *
+ * \brief Interface for the Pololu Maestro pan-tilt unit using two servo motors.
+ *
+ * See https://www.pololu.com/category/102/maestro-usb-servo-controllers for more details.
+ *
+ * This class handle the vpPololu class in a higher level and allows to control
+ * the pan-tilt unit using position or velocity commands.
+ *
+ * The corresponding Denavit-Hartenberg representations of the PTU is the following:
+ *
+ * | Joint | \f$a_i\f$ | \f$d_i\f$ | \f$\alpha_i\f$ | \f$\theta_i\f$    |
+ * | :---: | :-------: | :-------: | -------------: | ----------------: |
+ * |     1 |         0 |         0 |   \f$ \pi/2\f$ |         \f$q_1\f$ |
+ * |     2 |         0 |         0 |   \f$-\pi/2\f$ | \f$q_2 - \pi/2\f$ |
+ */
+class VISP_EXPORT vpRobotPololuPtu : public vpRobot
+{
+public:
+  /*!
+   * Default constructor.
+   *
+   * \param[in] device : Name of the serial interface used for communication.
+   * \param[in] baudrate : Baudrate used for the serial communication.
+   * \param[in] verbose : When true, enable verbose mode.
+   */
+  vpRobotPololuPtu(const std::string &device = "/dev/ttyACM0", int baudrate = 9600, bool verbose = false);
+
+  /*!
+   * Destructor that stops the movements.
+   */
+  ~vpRobotPololuPtu() override;
+
+  /*!
+   * Get the robot jacobian expressed in the end-effector frame.
+   *
+   * \warning End-effector frame is not the embedded camera frame. It corresponds to the frame
+   * associated to the tilt axis (see also get_cMe).
+   *
+   * \param[out] eJe : Jacobian between end effector frame and end effector frame (on
+   * tilt axis).
+   */
+  void get_eJe(vpMatrix &eJe) override;
+
+  /*!
+   * Get the robot jacobian expressed in the end-effector frame.
+   *
+   * \warning End-effector frame is not the embedded camera frame. It corresponds to the frame
+   * associated to the tilt axis (see also get_cMe).
+   *
+   * \param[in] q : Joint positions to consider [rad].
+   *
+   * \param[out] eJe : Jacobian between end effector frame and end effector frame (on
+   * tilt axis).
+   */
+  void get_eJe(const vpColVector &q, vpMatrix &eJe) const;
+
+  /*!
+   * Get the robot jacobian expressed in the robot reference frame.
+   *
+   * \param[out] fJe : Jacobian between reference frame (or fix frame) and end
+   * effector frame (on tilt axis).
+   */
+  void get_fJe(vpMatrix &fJe) override;
+
+  /*!
+   * Get the robot jacobian expressed in the robot reference frame.
+   *
+   * \param[in] q : Joint positions to consider [rad].
+   *
+   * \param[out] fJe : Jacobian between reference frame (or fix frame) and end
+   * effector frame (on tilt axis).
+   */
+  void get_fJe(const vpColVector &q, vpMatrix &fJe) const;
+
+  /*!
+   * Return the minimul angular velocity in rad/s that could be applied to move the motors.
+   * It corresponds to 1 pwm converted in rad/s.
+   */
+  float getAngularVelocityResolution() const;
+
+  /*!
+   * Return the position of each joint.
+   *
+   * \param[in] frame : Control frame. This PTU can only be controlled in
+   * joint state.
+   *
+   * \param[out] q : The position of the joints in radians.
+   *
+   * \exception vpRobotException::wrongStateError : If a not supported frame type
+   * is given.
+   */
+  void getPosition(const vpRobot::vpControlFrameType frame, vpColVector &q) override;
+
+  /*!
+   * Get the percentage of the maximum velocity applied to move the PTU in position.
+   *
+   * \return Positioning velocity percentage in [0, 100.0]. The
+   * maximum positioning velocity is given vpRobot::getMaxRotationVelocity().
+   *
+   * \sa setPositioningVelocityPercentage()
+   */
+  float getPositioningVelocityPercentage() const
+  {
+    return m_positioning_velocity_percentage;
+  }
+
+  /*!
+   * Move the robot to a given joint position.
+   *
+   * \warning This method is blocking. That mean that it waits the end of the
+   * positioning.
+   *
+   * \param[in] frame : Control frame. This PTU can only be controlled in
+   * joint state.
+   *
+   * \param[in] q : The joint position to set for each axis in radians.
+   *
+   * \exception vpRobotException::wrongStateError : If a not supported frame
+   * type is given.
+   */
+  void setPosition(const vpRobot::vpControlFrameType frame, const vpColVector &q) override;
+
+  /*!
+   * Set the percentage of the maximum velocity applied to move the PTU in position.
+   *
+   * \param[in] positioning_velocity_percentage : Percentage between [0,100] of the maximum velocity. The
+   * maximum positioning velocity is given vpRobot::getMaxRotationVelocity().
+   *
+   * \sa getPositioningVelocityPercentage()
+   */
+  void setPositioningVelocityPercentage(float positioning_velocity_percentage)
+  {
+    m_positioning_velocity_percentage = positioning_velocity_percentage;
+  }
+
+  /*!
+   * Send a velocity on each axis.
+   *
+   * \param[in] frame : Control frame. This Biclops head can only be controlled in
+   * joint state. Be aware, the camera frame (vpRobot::CAMERA_FRAME), the reference
+   * frame (vpRobot::REFERENCE_FRAME), end-effector frame (vpRobot::END_EFFECTOR_FRAME)
+   * and the mixt frame (vpRobot::MIXT_FRAME) are not implemented.
+   *
+   * \param[in] q_dot : The desired joint velocities for each axis in rad/s. \f$ \dot
+   * {r} = [\dot{q}_1, \dot{q}_2]^t \f$ with \f$ \dot{q}_1 \f$ the pan of the
+   * camera and \f$ \dot{q}_2\f$ the tilt of the camera.
+   *
+   * \exception vpRobotException::wrongStateError : If a the robot is not
+   * configured to handle a velocity. The robot can handle a velocity only if the
+   * velocity control mode is set. For that, call setRobotState(
+   * vpRobot::STATE_VELOCITY_CONTROL) before setVelocity().
+   *
+   * \exception vpRobotException::wrongStateError : If a not supported frame type
+   * (vpRobot::CAMERA_FRAME, vpRobot::REFERENCE_FRAME, vpRobot::END_EFFECTOR_FRAME
+   * or vpRobot::MIXT_FRAME) is given.
+   *
+   * \warning Velocities could be saturated if one of them exceed the maximal
+   * authorized speed (see vpRobot::maxRotationVelocity).
+   */
+  void setVelocity(const vpRobot::vpControlFrameType frame, const vpColVector &q_dot) override;
+
+  /*!
+   * Stop the velocity command.
+   */
+  void stopVelocity();
+
+  /*!
+   * Change the state of the robot either to stop them, or to set position or
+   * speed control.
+   */
+  vpRobot::vpRobotStateType setRobotState(const vpRobot::vpRobotStateType newState) override;
+
+private:
+  /*!
+   * Initialize the robot.
+   *
+   * \exception vpRobotException::constructionError If the config file cannot be
+   * opened.
+   */
+  void init() override { };
+
+  /*!
+   * Get the robot displacement since the last call of this method.
+   *
+   * \warning The first call of this method gives not a good value for the
+   * displacement.
+   *
+   * \param[in] frame The frame in which the measured displacement is expressed.
+   *
+   * \param[out] d The displacement:
+   *
+   * - In joint state, the dimension of q is 2  (the number of axis of the robot)
+   *   with respectively d[0] (pan displacement), d[1] (tilt displacement).
+   *
+   * - In camera frame, the dimension of d is 6 (tx, ty, ty, tux, tuy, tuz).
+   *   Translations are expressed in meters, rotations in radians with the theta U
+   *   representation.
+   *
+   * \exception vpRobotException::wrongStateError If a not supported frame type
+   * is given.
+   */
+  void getDisplacement(const vpRobot::vpControlFrameType frame, vpColVector &d) override
+  {
+    (void)frame;
+    (void)d;
+  };
+
+  vpPololu m_pan;
+  vpPololu m_tilt;
+  float m_positioning_velocity_percentage;
+
+  bool m_verbose;
+};
+
+#endif
+#endif
---
+++ modules/robot/test/servo-pololu/testPololuVelocity.cpp
@@ -0,0 +1,223 @@
+/*
+ * ViSP, open source Visual Servoing Platform software.
+ * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact Inria about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See https://visp.inria.fr for more information.
+ *
+ * This software was developed at:
+ * Inria Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Inria at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Description:
+ * Common test for Pololu velocity control of one servo connected to a given channel.
+ */
+
+/*!
+ * \example testPololuVelocity.cpp
+ */
+#include <iostream>
+
+#include <visp3/core/vpConfig.h>
+
+#ifdef VISP_HAVE_POLOLU
+
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <visp3/core/vpMath.h>
+#include <visp3/robot/vpPololu.h>
+
+void usage(const char **argv, int error, const std::string &device, int baudrate, int channel,
+           unsigned short pwm_min, unsigned short pwm_max, float angle_min, float angle_max)
+{
+  std::cout << "Synopsis" << std::endl
+    << "  " << argv[0] << " [--device <name>] [--channel <number>] [--calibrate] [--range-pwm <min max> ] [--verbose, -v] [--help, -h]" << std::endl
+    << std::endl;
+  std::cout << "Description" << std::endl
+    << "  --device <name>  Device name." << std::endl
+    << "    Default: " << device << std::endl
+    << std::endl
+    << "  --baud <rate>  Serial link baud rate." << std::endl
+    << "    Default: " << baudrate << std::endl
+    << std::endl
+    << "  --channel <number>  Channel to dial with." << std::endl
+    << "    Default: " << channel << std::endl
+    << std::endl
+    << "  --range-pwm <min max>  Set PWM min and max values." << std::endl
+    << "    You can use \"--calibrate\" to retrieve min and max pwm values."
+    << "    Default: " << pwm_min << " " << pwm_max << std::endl
+    << std::endl
+    << "  --range-angles <min max>  Set angle min and max values (deg)." << std::endl
+    << "    Default: " << vpMath::deg(angle_min) << " " << vpMath::deg(angle_max) << std::endl
+    << std::endl
+    << "  --verbose, -v  Enable verbosity." << std::endl
+    << std::endl
+    << "  --calibrate  Start pwm calibration determining min and max admissible values." << std::endl
+    << "    Once calibration done you can use \"--range-pwm <min max>\" option to set" << std::endl
+    << "    the corresponding values" << std::endl
+    << std::endl
+    << "  --help, -h  Print this helper message." << std::endl
+    << std::endl;
+  if (error) {
+    std::cout << "Error" << std::endl
+      << "  "
+      << "Unsupported parameter " << argv[error] << std::endl;
+  }
+}
+
+int main(int argc, const char **argv)
+{
+#ifdef _WIN32
+  std::string opt_device = "COM4";
+#else
+  std::string opt_device = "/dev/ttyACM0";
+  // Example for Mac OS, the Maestro creates two devices, use the one with the lowest number (the command port)
+  //std::string opt_device = "/dev/cu.usbmodem00031501";
+#endif
+  int opt_channel = 0;
+  int opt_baudrate = 38400;
+  bool opt_verbose = false;
+  bool opt_calibrate = false;
+  unsigned short opt_pwm_min = 4000;
+  unsigned short opt_pwm_max = 8000;
+  float opt_angle_min = vpMath::rad(-45);
+  float opt_angle_max = vpMath::rad(45);
+  float opt_velocity = vpMath::rad(5);
+  float last_angle = 0;
+
+  for (int i = 1; i < argc; i++) {
+    if (std::string(argv[i]) == "--device" && i + 1 < argc) {
+      opt_device = std::string(argv[i + 1]);
+      i++;
+    }
+    else if (std::string(argv[i]) == "--baud" && i + 1 < argc) {
+      opt_baudrate = std::atoi(argv[i + 1]);
+      i++;
+    }
+    else if (std::string(argv[i]) == "--channel" && i + 1 < argc) {
+      opt_channel = std::atoi(argv[i + 1]);
+      i++;
+    }
+    else if (std::string(argv[i]) == "--range-pwm" && i + 2 < argc) {
+      opt_pwm_min = static_cast<unsigned short>(vpMath::rad(std::atoi(argv[i + 1])));
+      opt_pwm_max = static_cast<unsigned short>(vpMath::rad(std::atoi(argv[i + 2])));
+      i += 2;
+    }
+    else if (std::string(argv[i]) == "--range-angles" && i + 2 < argc) {
+      opt_angle_min = static_cast<float>(std::atof(argv[i + 1]));
+      opt_angle_max = static_cast<float>(std::atof(argv[i + 2]));
+      i += 2;
+    }
+    else if (std::string(argv[i]) == "--calibrate") {
+      opt_calibrate = true;
+    }
+    else if (std::string(argv[i]) == "--verbose" || std::string(argv[i]) == "-v") {
+      opt_verbose = true;
+    }
+    else if (std::string(argv[i]) == "--help" || std::string(argv[i]) == "-h") {
+      usage(argv, 0, opt_device, opt_baudrate, opt_channel, opt_pwm_min, opt_pwm_max, opt_angle_min, opt_angle_max);
+      return EXIT_SUCCESS;
+    }
+    else {
+      usage(argv, i, opt_device, opt_baudrate, opt_channel, opt_pwm_min, opt_pwm_max, opt_angle_min, opt_angle_max);
+      return EXIT_FAILURE;
+    }
+  }
+
+  std::chrono::seconds sec(1);
+
+  try {
+    // Creating the servo object on channel 0
+    vpPololu servo(opt_verbose);
+
+    servo.connect(opt_device, opt_baudrate, opt_channel);
+
+    if (opt_calibrate) {
+      std::cout << "Proceed to calibration to determine pwm min and max values..." << std::endl;
+      std::cout << "WARNING: Calibration will move the servo at channel " << opt_channel << "!" << std::endl;
+      std::cout << "Press Enter to move to min and max pwm positions..." << std::endl;
+      std::cin.ignore();
+
+      unsigned short pwm_min, pwm_max;
+      servo.calibrate(pwm_min, pwm_max);
+      std::cout << "Servo on channel " << opt_channel << " has pwm range [" << pwm_min << ", " << pwm_max << "]" << std::endl;
+      return EXIT_SUCCESS;
+    }
+
+    servo.setPwmRange(opt_pwm_min, opt_pwm_max);
+    servo.setAngularRange(opt_angle_min, opt_angle_max);
+
+    // Servo will move to 0 angle at a max velocity in rad/s
+    std::cout << "Move to zero position (deg): " << vpMath::deg(0) << " at max velocity" << std::endl;
+    servo.setAngularPosition(0, 0);
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    last_angle = servo.getAngularPosition();
+    std::cout << "Servo reached position (deg): " << vpMath::deg(last_angle) << std::endl;
+
+    if (1) {
+        // Servo will first move in one direction at a velocity of 10 for 3 sec and move back in the other direction for 3 sec
+      short vel_pwm = 10;
+      std::cout << "Move at velocity (pwm): " << vel_pwm << " for 3 sec" << std::endl;
+      servo.setPwmVelocity(vel_pwm);
+      std::this_thread::sleep_for(3 * sec);
+      std::cout << "Servo reached position (pwm): " << servo.getPwmPosition() << std::endl;
+
+      vel_pwm = -10;
+      std::cout << "Move at velocity (pwm): " << vel_pwm << " for 3 sec" << std::endl;
+      servo.setPwmVelocity(vel_pwm);
+      std::this_thread::sleep_for(3 * sec);
+      std::cout << "Servo reached position (pwm): " << servo.getPwmPosition() << std::endl;
+      std::cout << "End of velocity motion" << std::endl;
+    }
+
+    // Servo will first move in one direction at a velocity of 10 for 3 sec and move back in the other direction for 3 sec
+    std::cout << "Move at velocity (deg/s): " << vpMath::deg(opt_velocity) << " for 3 sec" << std::endl;
+    servo.setAngularVelocity(opt_velocity);
+    std::this_thread::sleep_for(3 * sec);
+    std::cout << "Servo reached position (deg): " << servo.getPwmPosition() << std::endl;
+
+    std::cout << "Move at velocity (deg/s): " << vpMath::deg(-opt_velocity) << " for 3 sec" << std::endl;
+    servo.setAngularVelocity(-opt_velocity);
+    std::this_thread::sleep_for(3 * sec);
+    std::cout << "Servo reached position (deg): " << servo.getPwmPosition() << std::endl;
+
+    // Stopping the velocity command.
+    servo.stopVelocityCmd();
+
+    std::cout << "The end" << std::endl;
+
+    return EXIT_SUCCESS;
+  }
+  catch (const vpException &e) {
+    std::cout << e.getMessage() << std::endl;
+    return EXIT_FAILURE;
+  }
+}
+
+#else
+int main()
+{
+  std::cout << "ViSP doesn't support Pololu 3rd party library" << std::endl;
+}
+#endif
