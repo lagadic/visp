effd41b1c07f5f1100f7ab75c0f60b7ccf09c426
Fabien Spindler <Fabien.Spindler@inria.fr>
2023-12-08 11:07:52 +0100

    Sonarqube fixes

--- modules/vision/src/homography-estimation/vpHomography.cpp
+++ modules/vision/src/homography-estimation/vpHomography.cpp
@@ -99,7 +99,8 @@ void vpHomography::buildFrom(const vpRotationMatrix &aRb, const vpTranslationVec
 
 void vpHomography::buildFrom(const vpPoseVector &arb, const vpPlane &p)
 {
-  m_aMb.buildFrom(arb[0], arb[1], arb[2], arb[3], arb[4], arb[5]);
+  double tx = arb[0], ty = arb[1], tz = arb[2], tux = arb[3], tuy = arb[4], tuz = arb[5];
+  m_aMb.buildFrom(tx, ty, tz, tux, tuy, tuz);
   insert(p);
   build();
 }
@@ -130,11 +131,13 @@ vpHomography vpHomography::inverse(double sv_threshold, unsigned int *rank) cons
   }
 
   vpHomography H;
-
-  for (unsigned int i = 0; i < 3; i++)
-    for (unsigned int j = 0; j < 3; j++)
+  unsigned int nbRows = H.getRows();
+  unsigned int nbCols = H.getCols();
+  for (unsigned int i = 0; i < nbRows; ++i) {
+    for (unsigned int j = 0; j < nbCols; ++j) {
       H[i][j] = Minv[i][j];
-
+    }
+  }
   return H;
 }
 
@@ -153,10 +156,11 @@ void vpHomography::save(std::ofstream &f) const
 vpHomography vpHomography::operator*(const vpHomography &H) const
 {
   vpHomography Hp;
-  for (unsigned int i = 0; i < 3; i++) {
-    for (unsigned int j = 0; j < 3; j++) {
+  const unsigned int nbCols = 3, nbRows = 3;
+  for (unsigned int i = 0; i < nbRows; ++i) {
+    for (unsigned int j = 0; j < nbCols; ++j) {
       double s = 0.;
-      for (unsigned int k = 0; k < 3; k++) {
+      for (unsigned int k = 0; k < nbCols; ++k) {
         s += (*this)[i][k] * H[k][j];
       }
       Hp[i][j] = s;
@@ -167,15 +171,18 @@ vpHomography vpHomography::operator*(const vpHomography &H) const
 
 vpColVector vpHomography::operator*(const vpColVector &b) const
 {
-  if (b.size() != 3)
+  const unsigned int requiredSize = 3;
+  if (b.size() != requiredSize) {
     throw(vpException(vpException::dimensionError, "Cannot multiply an homography by a vector of dimension %d",
                       b.size()));
+  }
 
-  vpColVector a(3);
-  for (unsigned int i = 0; i < 3; i++) {
+  vpColVector a(requiredSize);
+  for (unsigned int i = 0; i < requiredSize; ++i) {
     a[i] = 0.;
-    for (unsigned int j = 0; j < 3; j++)
+    for (unsigned int j = 0; j < requiredSize; ++j) {
       a[i] += (*this)[i][j] * b[j];
+    }
   }
 
   return a;
@@ -183,9 +190,10 @@ vpColVector vpHomography::operator*(const vpColVector &b) const
 
 vpHomography vpHomography::operator*(const double &v) const
 {
+  const unsigned int nbData = 9; // cols x rows = 3 x 3
   vpHomography H;
 
-  for (unsigned int i = 0; i < 9; i++) {
+  for (unsigned int i = 0; i < nbData; ++i) {
     H.data[i] = this->data[i] * v;
   }
 
@@ -201,11 +209,11 @@ vpPoint vpHomography::operator*(const vpPoint &b_P) const
   v[1] = b_P.get_y();
   v[2] = b_P.get_w();
 
-  v1[0] = (*this)[0][0] * v[0] + (*this)[0][1] * v[1] + (*this)[0][2] * v[2];
-  v1[1] = (*this)[1][0] * v[0] + (*this)[1][1] * v[1] + (*this)[1][2] * v[2];
-  v1[2] = (*this)[2][0] * v[0] + (*this)[2][1] * v[1] + (*this)[2][2] * v[2];
+  v1[0] = ((*this)[0][0] * v[0]) + ((*this)[0][1] * v[1]) + ((*this)[0][2] * v[2]);
+  v1[1] = ((*this)[1][0] * v[0]) + ((*this)[1][1] * v[1]) + ((*this)[1][2] * v[2]);
+  v1[2] = ((*this)[2][0] * v[0]) + ((*this)[2][1] * v[1]) + ((*this)[2][2] * v[2]);
 
-  //  v1 = M*v ;
+  //  v1 is equal to M v ;
   a_P.set_x(v1[0]);
   a_P.set_y(v1[1]);
   a_P.set_w(v1[2]);
@@ -216,12 +224,14 @@ vpPoint vpHomography::operator*(const vpPoint &b_P) const
 vpHomography vpHomography::operator/(const double &v) const
 {
   vpHomography H;
-  if (std::fabs(v) <= std::numeric_limits<double>::epsilon())
+  if (std::fabs(v) <= std::numeric_limits<double>::epsilon()) {
     throw vpMatrixException(vpMatrixException::divideByZeroError, "Divide by zero in method /=(double v)");
+  }
 
   double vinv = 1 / v;
 
-  for (unsigned int i = 0; i < 9; i++) {
+  const unsigned int nbData = 9; // cols x rows = 3 x 3
+  for (unsigned int i = 0; i < nbData; ++i) {
     H.data[i] = this->data[i] * vinv;
   }
 
@@ -230,23 +240,28 @@ vpHomography vpHomography::operator/(const double &v) const
 
 vpHomography &vpHomography::operator/=(double v)
 {
-  // if (x == 0)
-  if (std::fabs(v) <= std::numeric_limits<double>::epsilon())
+  if (std::fabs(v) <= std::numeric_limits<double>::epsilon()) {
     throw vpMatrixException(vpMatrixException::divideByZeroError, "Divide by zero in method /=(double v)");
+  }
 
   double vinv = 1 / v;
 
-  for (unsigned int i = 0; i < 9; i++)
+  const unsigned int nbData = 9; // cols x rows = 3 x 3
+  for (unsigned int i = 0; i < nbData; ++i) {
     data[i] *= vinv;
+  }
 
   return *this;
 }
 
 vpHomography &vpHomography::operator=(const vpHomography &H)
 {
-  for (unsigned int i = 0; i < 3; i++)
-    for (unsigned int j = 0; j < 3; j++)
+  const unsigned int nbCols = 3, nbRows = 3;
+  for (unsigned int i = 0; i < nbRows; ++i){
+    for (unsigned int j = 0; j < nbCols; ++j) {
       (*this)[i][j] = H[i][j];
+    }
+  }
 
   m_aMb = H.m_aMb;
   m_bP = H.m_bP;
@@ -255,23 +270,29 @@ vpHomography &vpHomography::operator=(const vpHomography &H)
 
 vpHomography &vpHomography::operator=(const vpMatrix &H)
 {
-  if (H.getRows() != 3 || H.getCols() != 3)
+  const unsigned int nbCols = 3, nbRows = 3;
+  if ((H.getRows() != nbRows) || (H.getCols() != nbCols)) {
     throw(vpException(vpException::dimensionError, "The matrix is not an homography"));
+  }
 
-  for (unsigned int i = 0; i < 3; i++)
-    for (unsigned int j = 0; j < 3; j++)
+  for (unsigned int i = 0; i < nbRows; ++i) {
+    for (unsigned int j = 0; j < nbCols; ++j) {
       (*this)[i][j] = H[i][j];
+    }
+  }
 
   return *this;
 }
 
 void vpHomography::load(std::ifstream &f)
 {
+  const unsigned int nbCols = 3, nbRows = 3;
   if (!f.fail()) {
-    for (unsigned int i = 0; i < 3; i++)
-      for (unsigned int j = 0; j < 3; j++) {
+    for (unsigned int i = 0; i < nbRows; ++i) {
+      for (unsigned int j = 0; j < nbCols; ++j) {
         f >> (*this)[i][j];
       }
+    }
   }
   else {
     throw(vpException(vpException::ioError, "Cannot read the homography from the input stream"));
@@ -287,25 +308,29 @@ void vpHomography::load(std::ifstream &f)
 */
 void vpHomography::build()
 {
-  vpColVector n(3);
-  vpColVector atb(3);
-  vpMatrix aRb(3, 3);
-  for (unsigned int i = 0; i < 3; i++) {
+  const unsigned int nbCols = 3, nbRows = 3;
+  vpColVector n(nbRows);
+  vpColVector atb(nbRows);
+  vpMatrix aRb(nbRows, nbCols);
+  for (unsigned int i = 0; i < nbRows; ++i) {
     atb[i] = m_aMb[i][3];
-    for (unsigned int j = 0; j < 3; j++)
+    for (unsigned int j = 0; j < nbCols; ++j) {
       aRb[i][j] = m_aMb[i][j];
+    }
   }
 
   m_bP.getNormal(n);
 
   double d = m_bP.getD();
-  vpMatrix aHb = aRb - atb * n.t() / d; // the d used in the equation is such as nX=d is the
+  vpMatrix aHb = aRb - ((atb * n.t()) / d); // the d used in the equation is such as nX=d is the
                                         // plane equation. So if the plane is described by
                                         // Ax+By+Cz+D=0, d=-D
 
-  for (unsigned int i = 0; i < 3; i++)
-    for (unsigned int j = 0; j < 3; j++)
+  for (unsigned int i = 0; i < nbRows; ++i) {
+    for (unsigned int j = 0; j < nbCols; ++j) {
       (*this)[i][j] = aHb[i][j];
+    }
+  }
 }
 
 /*!
@@ -318,42 +343,51 @@ void vpHomography::build()
 */
 void vpHomography::build(vpHomography &aHb, const vpHomogeneousMatrix &aMb, const vpPlane &bP)
 {
-  vpColVector n(3);
-  vpColVector atb(3);
-  vpMatrix aRb(3, 3);
-  for (unsigned int i = 0; i < 3; i++) {
+  const unsigned int nbCols = 3, nbRows = 3;
+  vpColVector n(nbRows);
+  vpColVector atb(nbRows);
+  vpMatrix aRb(nbRows, nbCols);
+  for (unsigned int i = 0; i < nbRows; ++i) {
     atb[i] = aMb[i][3];
-    for (unsigned int j = 0; j < 3; j++)
+    for (unsigned int j = 0; j < nbCols; ++j) {
       aRb[i][j] = aMb[i][j];
+    }
   }
 
   bP.getNormal(n);
 
   double d = bP.getD();
-  vpMatrix aHb_ = aRb - atb * n.t() / d; // the d used in the equation is such as nX=d is the
+  vpMatrix aHb_ = aRb - ((atb * n.t()) / d); // the d used in the equation is such as nX=d is the
                                          // plane equation. So if the plane is described by
                                          // Ax+By+Cz+D=0, d=-D
 
-  for (unsigned int i = 0; i < 3; i++)
-    for (unsigned int j = 0; j < 3; j++)
+  for (unsigned int i = 0; i < nbRows; ++i) {
+    for (unsigned int j = 0; j < nbCols; ++j) {
       aHb[i][j] = aHb_[i][j];
+    }
+  }
 }
 
 double vpHomography::det() const
 {
-  return ((*this)[0][0] * ((*this)[1][1] * (*this)[2][2] - (*this)[1][2] * (*this)[2][1]) -
-          (*this)[0][1] * ((*this)[1][0] * (*this)[2][2] - (*this)[1][2] * (*this)[2][0]) +
-          (*this)[0][2] * ((*this)[1][0] * (*this)[2][1] - (*this)[1][1] * (*this)[2][0]));
+  return ((*this)[0][0] * (((*this)[1][1] * (*this)[2][2]) - ((*this)[1][2] * (*this)[2][1])) -
+          (*this)[0][1] * (((*this)[1][0] * (*this)[2][2]) - ((*this)[1][2] * (*this)[2][0])) +
+          (*this)[0][2] * (((*this)[1][0] * (*this)[2][1]) - ((*this)[1][1] * (*this)[2][0])));
 }
 
 void vpHomography::eye()
 {
-  for (unsigned int i = 0; i < 3; i++)
-    for (unsigned int j = 0; j < 3; j++)
-      if (i == j)
+  const unsigned int nbCols = 3, nbRows = 3;
+  for (unsigned int i = 0; i < nbRows; ++i) {
+    for (unsigned int j = 0; j < nbCols; ++j) {
+      if (i == j) {
         (*this)[i][j] = 1.0;
-      else
+      }
+      else {
         (*this)[i][j] = 0.0;
+      }
+    }
+  }
 }
 
 vpImagePoint vpHomography::project(const vpCameraParameters &cam, const vpHomography &bHa, const vpImagePoint &iPa)
@@ -361,9 +395,9 @@ vpImagePoint vpHomography::project(const vpCameraParameters &cam, const vpHomogr
   double xa = iPa.get_u();
   double ya = iPa.get_v();
   vpHomography bGa = bHa.homography2collineation(cam);
-  double z = xa * bGa[2][0] + ya * bGa[2][1] + bGa[2][2];
-  double xb = (xa * bGa[0][0] + ya * bGa[0][1] + bGa[0][2]) / z;
-  double yb = (xa * bGa[1][0] + ya * bGa[1][1] + bGa[1][2]) / z;
+  double z = (xa * bGa[2][0]) + (ya * bGa[2][1]) + bGa[2][2];
+  double xb = ((xa * bGa[0][0]) + (ya * bGa[0][1]) + bGa[0][2]) / z;
+  double yb = ((xa * bGa[1][0]) + (ya * bGa[1][1]) + bGa[1][2]) / z;
 
   vpImagePoint iPb(yb, xb);
 
@@ -374,9 +408,9 @@ vpPoint vpHomography::project(const vpHomography &bHa, const vpPoint &Pa)
 {
   double xa = Pa.get_x();
   double ya = Pa.get_y();
-  double z = xa * bHa[2][0] + ya * bHa[2][1] + bHa[2][2];
-  double xb = (xa * bHa[0][0] + ya * bHa[0][1] + bHa[0][2]) / z;
-  double yb = (xa * bHa[1][0] + ya * bHa[1][1] + bHa[1][2]) / z;
+  double z = (xa * bHa[2][0]) + (ya * bHa[2][1]) + bHa[2][2];
+  double xb = ((xa * bHa[0][0]) + (ya * bHa[0][1]) + bHa[0][2]) / z;
+  double yb = ((xa * bHa[1][0]) + (ya * bHa[1][1]) + bHa[1][2]) / z;
 
   vpPoint Pb;
   Pb.set_x(xb);
@@ -390,13 +424,16 @@ void vpHomography::robust(const std::vector<double> &xb, const std::vector<doubl
                           const std::vector<double> &ya, vpHomography &aHb, std::vector<bool> &inliers,
                           double &residual, double weights_threshold, unsigned int niter, bool normalization)
 {
-  unsigned int n = (unsigned int)xb.size();
-  if (yb.size() != n || xa.size() != n || ya.size() != n)
+  unsigned int n = static_cast<unsigned int>(xb.size());
+  if ((yb.size() != n) || (xa.size() != n) || (ya.size() != n)) {
     throw(vpException(vpException::dimensionError, "Bad dimension for robust homography estimation"));
+  }
 
   // 4 point are required
-  if (n < 4)
+  const unsigned int nbRequiredPoints = 4;
+  if (n < nbRequiredPoints) {
     throw(vpException(vpException::fatalError, "There must be at least 4 matched points"));
+  }
 
   try {
     std::vector<double> xan, yan, xbn, ybn;
@@ -406,8 +443,8 @@ void vpHomography::robust(const std::vector<double> &xb, const std::vector<doubl
     vpHomography aHbn;
 
     if (normalization) {
-      vpHomography::HartleyNormalization(xb, yb, xbn, ybn, xg1, yg1, coef1);
-      vpHomography::HartleyNormalization(xa, ya, xan, yan, xg2, yg2, coef2);
+      vpHomography::hartleyNormalization(xb, yb, xbn, ybn, xg1, yg1, coef1);
+      vpHomography::hartleyNormalization(xa, ya, xan, yan, xg2, yg2, coef2);
     }
     else {
       xbn = xb;
@@ -428,12 +465,12 @@ void vpHomography::robust(const std::vector<double> &xb, const std::vector<doubl
     // square scheme
     w = 1;
     // Update the square matrix associated to the weights
-    for (unsigned int i = 0; i < nbLinesA * n; i++) {
+    for (unsigned int i = 0; i < (nbLinesA * n); ++i) {
       W[i][i] = w[i];
     }
 
     // build matrix A
-    for (unsigned int i = 0; i < n; i++) {
+    for (unsigned int i = 0; i < n; ++i) {
       A[nbLinesA * i][0] = xbn[i];
       A[nbLinesA * i][1] = ybn[i];
       A[nbLinesA * i][2] = 1;
@@ -443,17 +480,17 @@ void vpHomography::robust(const std::vector<double> &xb, const std::vector<doubl
       A[nbLinesA * i][6] = -xbn[i] * xan[i];
       A[nbLinesA * i][7] = -ybn[i] * xan[i];
 
-      A[nbLinesA * i + 1][0] = 0;
-      A[nbLinesA * i + 1][1] = 0;
-      A[nbLinesA * i + 1][2] = 0;
-      A[nbLinesA * i + 1][3] = xbn[i];
-      A[nbLinesA * i + 1][4] = ybn[i];
-      A[nbLinesA * i + 1][5] = 1;
-      A[nbLinesA * i + 1][6] = -xbn[i] * yan[i];
-      A[nbLinesA * i + 1][7] = -ybn[i] * yan[i];
+      A[(nbLinesA * i) + 1][0] = 0;
+      A[(nbLinesA * i) + 1][1] = 0;
+      A[(nbLinesA * i) + 1][2] = 0;
+      A[(nbLinesA * i) + 1][3] = xbn[i];
+      A[(nbLinesA * i) + 1][4] = ybn[i];
+      A[(nbLinesA * i) + 1][5] = 1;
+      A[(nbLinesA * i) + 1][6] = -xbn[i] * yan[i];
+      A[(nbLinesA * i) + 1][7] = -ybn[i] * yan[i];
 
       Y[nbLinesA * i] = xan[i];
-      Y[nbLinesA * i + 1] = yan[i];
+      Y[(nbLinesA * i) + 1] = yan[i];
     }
 
     vpMatrix WA;
@@ -466,36 +503,38 @@ void vpHomography::robust(const std::vector<double> &xb, const std::vector<doubl
 
       X = WA.pseudoInverse(1e-26) * W * Y;
       vpColVector residu;
-      residu = Y - A * X;
+      residu = Y - (A * X);
 
       // Compute the weights using the Tukey biweight M-Estimator
       r.MEstimator(vpRobust::TUKEY, residu, w);
 
       // Update the weights matrix
-      for (unsigned int i = 0; i < n * nbLinesA; i++) {
+      for (unsigned int i = 0; i < (n * nbLinesA); ++i) {
         W[i][i] = w[i];
       }
       // Build the homography
-      for (unsigned int i = 0; i < 8; i++)
+      for (unsigned int i = 0; i < 8; ++i) {
         aHbn.data[i] = X[i];
+      }
       aHbn[2][2] = 1;
 
-      iter++;
+      ++iter;
     }
     inliers.resize(n);
     unsigned int nbinliers = 0;
-    for (unsigned int i = 0; i < n; i++) {
-      if (w[i * 2] < weights_threshold && w[i * 2 + 1] < weights_threshold)
+    for (unsigned int i = 0; i < n; ++i) {
+      if ((w[i * 2] < weights_threshold) && (w[(i * 2) + 1] < weights_threshold)) {
         inliers[i] = false;
+      }
       else {
         inliers[i] = true;
-        nbinliers++;
+        ++nbinliers;
       }
     }
 
     if (normalization) {
       // H after denormalization
-      vpHomography::HartleyDenormalization(aHbn, aHb, xg1, yg1, coef1, xg2, yg2, coef2);
+      vpHomography::hartleyDenormalization(aHbn, aHb, xg1, yg1, coef1, xg2, yg2, coef2);
     }
     else {
       aHb = aHbn;
@@ -503,7 +542,7 @@ void vpHomography::robust(const std::vector<double> &xb, const std::vector<doubl
 
     residual = 0;
     vpColVector a(3), b(3), c(3);
-    for (unsigned int i = 0; i < n; i++) {
+    for (unsigned int i = 0; i < n; ++i) {
       if (inliers[i]) {
         a[0] = xa[i];
         a[1] = ya[i];
@@ -531,9 +570,9 @@ vpImagePoint vpHomography::projection(const vpImagePoint &ipb)
   double u = ipb.get_u();
   double v = ipb.get_v();
 
-  double u_a = (*this)[0][0] * u + (*this)[0][1] * v + (*this)[0][2];
-  double v_a = (*this)[1][0] * u + (*this)[1][1] * v + (*this)[1][2];
-  double w_a = (*this)[2][0] * u + (*this)[2][1] * v + (*this)[2][2];
+  double u_a = ((*this)[0][0] * u) + ((*this)[0][1] * v) + (*this)[0][2];
+  double v_a = ((*this)[1][0] * u) + ((*this)[1][1] * v) + (*this)[1][2];
+  double w_a = ((*this)[2][0] * u) + ((*this)[2][1] * v) + (*this)[2][2];
 
   if (std::fabs(w_a) > std::numeric_limits<double>::epsilon()) {
     ipa.set_u(u_a / w_a);
@@ -545,10 +584,13 @@ vpImagePoint vpHomography::projection(const vpImagePoint &ipb)
 
 vpMatrix vpHomography::convert() const
 {
-  vpMatrix M(3, 3);
-  for (unsigned int i = 0; i < 3; i++)
-    for (unsigned int j = 0; j < 3; j++)
+  const unsigned int nbRows = 3, nbCols = 3;
+  vpMatrix M(nbRows, nbCols);
+  for (unsigned int i = 0; i < nbRows; ++i) {
+    for (unsigned int j = 0; j < nbCols; ++j) {
       M[i][j] = (*this)[i][j];
+    }
+  }
 
   return M;
 }
@@ -569,12 +611,12 @@ vpHomography vpHomography::collineation2homography(const vpCameraParameters &cam
   double u0_one_over_px = u0 * one_over_px;
   double v0_one_over_py = v0 * one_over_py;
 
-  double A = h00 * one_over_px - h20 * u0_one_over_px;
-  double B = h01 * one_over_px - h21 * u0_one_over_px;
-  double C = h02 * one_over_px - h22 * u0_one_over_px;
-  double D = h10 * one_over_py - h20 * v0_one_over_py;
-  double E = h11 * one_over_py - h21 * v0_one_over_py;
-  double F = h12 * one_over_py - h22 * v0_one_over_py;
+  double A = (h00 * one_over_px) - (h20 * u0_one_over_px);
+  double B = (h01 * one_over_px) - (h21 * u0_one_over_px);
+  double C = (h02 * one_over_px) - (h22 * u0_one_over_px);
+  double D = (h10 * one_over_py) - (h20 * v0_one_over_py);
+  double E = (h11 * one_over_py) - (h21 * v0_one_over_py);
+  double F = (h12 * one_over_py) - (h22 * v0_one_over_py);
 
   H[0][0] = A * px;
   H[1][0] = D * px;
@@ -584,9 +626,9 @@ vpHomography vpHomography::collineation2homography(const vpCameraParameters &cam
   H[1][1] = E * py;
   H[2][1] = h21 * py;
 
-  H[0][2] = A * u0 + B * v0 + C;
-  H[1][2] = D * u0 + E * v0 + F;
-  H[2][2] = h20 * u0 + h21 * v0 + h22;
+  H[0][2] = (A * u0) + (B * v0) + C;
+  H[1][2] = (D * u0) + (E * v0) + F;
+  H[2][2] = (h20 * u0) + (h21 * v0) + h22;
 
   return H;
 }
@@ -604,12 +646,12 @@ vpHomography vpHomography::homography2collineation(const vpCameraParameters &cam
   double h10 = (*this)[1][0], h11 = (*this)[1][1], h12 = (*this)[1][2];
   double h20 = (*this)[2][0], h21 = (*this)[2][1], h22 = (*this)[2][2];
 
-  double A = h00 * px + u0 * h20;
-  double B = h01 * px + u0 * h21;
-  double C = h02 * px + u0 * h22;
-  double D = h10 * py + v0 * h20;
-  double E = h11 * py + v0 * h21;
-  double F = h12 * py + v0 * h22;
+  double A = (h00 * px) + (u0 * h20);
+  double B = (h01 * px) + (u0 * h21);
+  double C = (h02 * px) + (u0 * h22);
+  double D = (h10 * py) + (v0 * h20);
+  double E = (h11 * py) + (v0 * h21);
+  double F = (h12 * py) + (v0 * h22);
 
   H[0][0] = A * one_over_px;
   H[1][0] = D * one_over_px;
@@ -622,9 +664,9 @@ vpHomography vpHomography::homography2collineation(const vpCameraParameters &cam
   double u0_one_over_px = u0 * one_over_px;
   double v0_one_over_py = v0 * one_over_py;
 
-  H[0][2] = -A * u0_one_over_px - B * v0_one_over_py + C;
-  H[1][2] = -D * u0_one_over_px - E * v0_one_over_py + F;
-  H[2][2] = -h20 * u0_one_over_px - h21 * v0_one_over_py + h22;
+  H[0][2] = ((-A * u0_one_over_px) - (B * v0_one_over_py)) + C;
+  H[1][2] = ((-D * u0_one_over_px) - (E * v0_one_over_py)) + F;
+  H[2][2] = ((-h20 * u0_one_over_px) - (h21 * v0_one_over_py)) + h22;
 
   return H;
 }
