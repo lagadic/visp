b4c373d6c6b1ad9b3037247d5d63b2d2215dd084
Samuel Felton <samuel.felton@irisa.fr>
2024-02-05 16:33:56 +0100

    remove default values in vpDisplayWin32.cpp

--- modules/gui/src/display/windows/vpDisplayWin32.cpp
+++ modules/gui/src/display/windows/vpDisplayWin32.cpp
@@ -61,13 +61,13 @@ void vpCreateWindow(threadParam *param)
 */
 vpDisplayWin32::vpDisplayWin32(vpWin32Renderer *rend) : iStatus(false), window(rend) { }
 
-vpDisplayWin32::vpDisplayWin32(vpImage<vpRGBa> &I, int winx = -1, int winy = -1, const std::string &title = "")
+vpDisplayWin32::vpDisplayWin32(vpImage<vpRGBa> &I, int winx, int winy, const std::string &title)
   : iStatus(false), window(nullptr)
 {
   init(I, winx, winy, title);
 }
 
-vpDisplayWin32::vpDisplayWin32(vpImage<unsigned char> &I, int winx = -1, int winy = -1, const std::string &title = "")
+vpDisplayWin32::vpDisplayWin32(vpImage<unsigned char> &I, int winx, int winy, const std::string &title)
   : iStatus(false), window(nullptr)
 {
   init(I, winx, winy, title);
