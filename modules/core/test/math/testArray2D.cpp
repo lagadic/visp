a7782c5fd8cf97275a07a3bf952aa2bb45d5e08c
Fabien Spindler <Fabien.Spindler@inria.fr>
2024-04-02 16:33:32 +0200

    Introduce vpArray2D contructor from std::vector Refactor testArray2D.cpp tu use catch Introduce new tests

--- modules/core/test/math/testArray2D.cpp
+++ modules/core/test/math/testArray2D.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
  *
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2024 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,6 +39,12 @@
   Test some vpArray2D functionalities.
 */
 
+#include <visp3/core/vpConfig.h>
+
+#if defined(VISP_HAVE_CATCH2)
+#define CATCH_CONFIG_RUNNER
+
+#include <catch.hpp>
 #include <cmath>
 #include <limits>
 #include <vector>
@@ -64,17 +70,37 @@ template <typename Type> bool test(const std::string &s, const vpArray2D<Type> &
   return true;
 }
 
-int main()
+template <typename Type>
+bool test_hadamar(const vpArray2D<Type> &A, const vpArray2D<Type> &B, const vpArray2D<Type> &H)
 {
+  if (A.getCols() != B.getCols() || A.getCols() != H.getCols()) {
+    std::cout << "Test fails: bad columns size" << std::endl;
+    return false;
+  }
+  if (A.getRows() != B.getRows() || A.getRows() != H.getRows()) {
+    std::cout << "Test fails: bad rows size" << std::endl;
+    return false;
+  }
+  for (unsigned int i = 0; i < A.size(); i++) {
+    if (std::fabs((A.data[i] * B.data[i]) - H.data[i]) > std::numeric_limits<double>::epsilon()) {
+      std::cout << "Test fails: bad content" << std::endl;
+      return false;
+    }
+  }
+
+  return true;
+}
+
+TEST_CASE("Test constructors with double", "[constructors]")
+{
+  SECTION("Default constructor")
   {
-    // test default constructor
     vpArray2D<double> A;
     std::vector<double> bench;
-    if (test("A", A, bench) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench));
   }
+  SECTION("Copy constructor")
   {
-    // test copy constructor
     vpArray2D<double> A(3, 4);
 
     std::vector<double> bench(12);
@@ -84,42 +110,79 @@ int main()
         bench[i * 4 + j] = (double)(i + j);
       }
     }
-    if (test("A", A, bench) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench));
 
     vpArray2D<double> B(A);
-    if (test("B", B, bench) == false)
-      return EXIT_FAILURE;
-    std::cout << "Min/Max: " << B.getMinValue() << " " << B.getMaxValue() << std::endl;
+    CHECK(test("B", B, bench));
   }
+  SECTION("Constructor with initial value")
   {
-    // test constructor with initial value
     vpArray2D<double> A(3, 4, 2.);
     std::vector<double> bench1(12, 2);
-    if (test("A", A, bench1) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench1));
 
     A.resize(5, 6);
     std::vector<double> bench2(30, 0);
-    if (test("A", A, bench2) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench2));
 
     A = -2.;
     std::vector<double> bench3(30, -2);
-    if (test("A", A, bench3) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench3));
   }
 
-  // Test with float
+  SECTION("Constructor from std::vector")
   {
-    // test default constructor
+    std::vector<double> bench(12);
+    for (unsigned int i = 0; i < 3; i++) {
+      for (unsigned int j = 0; j < 4; j++) {
+        bench[i * 4 + j] = (double)(i + j);
+      }
+    }
+    SECTION("Keep default size (r=0, c=0)")
+    {
+      vpArray2D<double> A(bench);
+      std::cout << "A with default size (r=0, c=0):\n" << A << std::endl;
+      CHECK(test("A", A, bench));
+      CHECK(A.getRows() == bench.size());
+      CHECK(A.getCols() == 1);
+    }
+    SECTION("Keep row size to 0")
+    {
+      vpArray2D<double> A(bench, 0, bench.size());
+      std::cout << "A with row size to 0:\n" << A << std::endl;
+      CHECK(test("A", A, bench));
+      CHECK(A.getRows() == 1);
+      CHECK(A.getCols() == bench.size());
+    }
+    SECTION("Keep col size to 0")
+    {
+      vpArray2D<double> A(bench, bench.size(), 0);
+      std::cout << "A with col size to 0:\n" << A << std::endl;
+      CHECK(test("A", A, bench));
+      CHECK(A.getRows() == bench.size());
+      CHECK(A.getCols() == 1);
+    }
+    SECTION("Set r=3 and c=4")
+    {
+      vpArray2D<double> A(bench, 3, 4);
+      std::cout << "A with r=3 and c=4:\n" << A << std::endl;
+      CHECK(test("A", A, bench));
+      CHECK(A.getRows() == 3);
+      CHECK(A.getCols() == 4);
+    }
+  }
+}
+
+TEST_CASE("Test constructors with float", "[constructors]")
+{
+  SECTION("Default constructor")
+  {
     vpArray2D<float> A;
     std::vector<float> bench;
-    if (test("A", A, bench) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench));
   }
+  SECTION("Copy constructor")
   {
-    // test copy constructor
     vpArray2D<float> A(3, 4);
 
     std::vector<float> bench(12);
@@ -129,62 +192,117 @@ int main()
         bench[i * 4 + j] = (float)(i + j);
       }
     }
-    if (test("A", A, bench) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench));
 
     vpArray2D<float> B(A);
-    if (test("B", B, bench) == false)
-      return EXIT_FAILURE;
-    std::cout << "Min/Max: " << B.getMinValue() << " " << B.getMaxValue() << std::endl;
+    CHECK(test("B", B, bench));
   }
+  SECTION("Constructor with initial value")
   {
-    // test constructor with initial value
     vpArray2D<float> A(3, 4, 2.);
     std::vector<float> bench1(12, 2);
-    if (test("A", A, bench1) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench1));
 
     A.resize(5, 6);
     std::vector<float> bench2(30, 0);
-    if (test("A", A, bench2) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench2));
 
     A = -2.;
     std::vector<float> bench3(30, -2);
-    if (test("A", A, bench3) == false)
-      return EXIT_FAILURE;
+    CHECK(test("A", A, bench3));
   }
+  SECTION("Constructor from std::vector")
   {
-    // Test Hadamard product
-    std::cout << "\nTest Hadamard product" << std::endl;
-    vpArray2D<int> A1(3, 5), A2(3, 5);
-    vpRowVector R1(15), R2(15);
-    vpColVector C1(15), C2(15);
-
-    for (unsigned int i = 0; i < A1.size(); i++) {
-      A1.data[i] = i;
-      A2.data[i] = i + 2;
-      R1.data[i] = i;
-      R2.data[i] = i + 2;
-      C1.data[i] = i;
-      C2.data[i] = i + 2;
+    std::vector<float> bench(12);
+    for (unsigned int i = 0; i < 3; i++) {
+      for (unsigned int j = 0; j < 4; j++) {
+        bench[i * 4 + j] = (float)(i + j);
+      }
     }
-
-    std::cout << "A1:\n" << A1 << std::endl;
-    std::cout << "\nA2:\n" << A2 << std::endl;
-    A2 = A1.hadamard(A2);
-    std::cout << "\nRes:\n" << A2 << std::endl;
-
-    std::cout << "\nR1:\n" << R1 << std::endl;
-    std::cout << "\nR2:\n" << R2 << std::endl;
-    R2 = R1.hadamard(R2);
-    std::cout << "\nRes:\n" << R2 << std::endl;
-
-    std::cout << "\nC1:\n" << C1 << std::endl;
-    std::cout << "\nC2:\n" << C2 << std::endl;
-    C2 = C1.hadamard(C2);
-    std::cout << "\nRes:\n" << C2 << std::endl;
+    SECTION("Keep default size (r=0, c=0)")
+    {
+      vpArray2D<float> A(bench);
+      std::cout << "A with default size (r=0, c=0):\n" << A << std::endl;
+      CHECK(test("A", A, bench));
+      CHECK(A.getRows() == bench.size());
+      CHECK(A.getCols() == 1);
+    }
+    SECTION("Keep row size to 0")
+    {
+      vpArray2D<float> A(bench, 0, bench.size());
+      std::cout << "A with row size to 0:\n" << A << std::endl;
+      CHECK(test("A", A, bench));
+      CHECK(A.getRows() == 1);
+      CHECK(A.getCols() == bench.size());
+    }
+    SECTION("Keep col size to 0")
+    {
+      vpArray2D<float> A(bench, bench.size(), 0);
+      std::cout << "A with col size to 0:\n" << A << std::endl;
+      CHECK(test("A", A, bench));
+      CHECK(A.getRows() == bench.size());
+      CHECK(A.getCols() == 1);
+    }
+    SECTION("Set r=3 and c=4")
+    {
+      vpArray2D<float> A(bench, 3, 4);
+      std::cout << "A with r=3 and c=4:\n" << A << std::endl;
+      CHECK(test("A", A, bench));
+      CHECK(A.getRows() == 3);
+      CHECK(A.getCols() == 4);
+    }
   }
-  std::cout << "All tests succeed" << std::endl;
-  return EXIT_SUCCESS;
 }
+
+TEST_CASE("Test Hadamar product", "[hadamar]")
+{
+  vpArray2D<int> A1(3, 5), A2(3, 5), A3;
+  vpRowVector R1(15), R2(15), R3;
+  vpColVector C1(15), C2(15), C3;
+
+  for (unsigned int i = 0; i < A1.size(); i++) {
+    A1.data[i] = i;
+    A2.data[i] = i + 2;
+    R1.data[i] = i;
+    R2.data[i] = i + 2;
+    C1.data[i] = i;
+    C2.data[i] = i + 2;
+  }
+
+  std::cout << "A1:\n" << A1 << std::endl;
+  std::cout << "\nA2:\n" << A2 << std::endl;
+  A3 = A1.hadamard(A2);
+  CHECK(test_hadamar(A1, A2, A3));
+  std::cout << "\nRes hadamar(A1, A2):\n" << A3 << std::endl;
+
+  std::cout << "\nR1:\n" << R1 << std::endl;
+  std::cout << "\nR2:\n" << R2 << std::endl;
+  R3 = R1.hadamard(R2);
+  CHECK(test_hadamar(R1, R2, R3));
+  std::cout << "\nRes hadamar(R1, R2):\n" << R3 << std::endl;
+
+  std::cout << "\nC1:\n" << C1 << std::endl;
+  std::cout << "\nC2:\n" << C2 << std::endl;
+  C3 = C1.hadamard(C2);
+  CHECK(test_hadamar(C1, C2, C3));
+  std::cout << "\nRes hadamar(C1, C2):\n" << C3 << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+  Catch::Session session; // There must be exactly one instance
+
+  // Let Catch (using Clara) parse the command line
+  session.applyCommandLine(argc, argv);
+
+  int numFailed = session.run();
+
+  // numFailed is clamped to 255 as some unices only use the lower 8 bits.
+  // This clamping has already been applied, so just return it here
+  // You can also do any post run clean-up here
+  std::cout << (numFailed ? "Test failed" : "Test succeed") << std::endl;
+  return numFailed;
+}
+#else
+int main() { return EXIT_SUCCESS; }
+#endif
