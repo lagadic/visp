7e63f6d5751d085761686093426aa06499d96681
rlagneau <romain.lagneau@inria.fr>
2024-05-06 11:44:30 +0200

    [TEST] Use catch2 instead of plain old-school fashion c++ test

--- modules/core/test/math/testMatrixCholesky.cpp
+++ modules/core/test/math/testMatrixCholesky.cpp
@@ -38,9 +38,16 @@
   \brief Test matrix Cholesky's decomposition.
 */
 
+#include <visp3/core/vpConfig.h>
+
+#ifdef VISP_HAVE_CATCH2
 #include <iostream>
 #include <stdlib.h>
 #include <visp3/core/vpMatrix.h>
+
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+
 
 bool equal(const vpMatrix &A, const vpMatrix &B)
 {
@@ -106,34 +113,9 @@ bool testCholeskyDecomposition(const vpMatrix &M, const vpMatrix &gtResult, cons
   return areEqual;
 }
 
-void printHelp(const std::string &progName)
-{
-  std::cout << "SYNOPSIS: " << std::endl;
-  std::cout << "  " << progName << " [-v, --verbose] [-h, --help]" << std::endl;
-  std::cout << "DETAILS:" << std::endl;
-  std::cout << "  -v, --verbose" << std::endl;
-  std::cout << "    Activate verbose mode to have some logs in the console." << std::endl;
-  std::cout << std::endl;
-  std::cout << "  -h, --help" << std::endl;
-  std::cout << "    Display the help about the program." << std::endl;
-  std::cout << std::endl;
-}
 
-int main(const int argc, const char *argv[])
+TEST_CASE("3 x 3 input", "[cholesky]")
 {
-  bool verbose = false;
-  for (int i = 1; i < argc; ++i) {
-    std::string argName(argv[i]);
-    if ((argName == "-v") || (argName == "--verbose")) {
-      verbose = true;
-    }
-    else if ((argName == "-h") || (argName == "--help")) {
-      printHelp(std::string(argv[0]));
-      return EXIT_FAILURE;
-    }
-  }
-
-#if defined(VISP_HAVE_LAPACK) || defined(VISP_HAVE_EIGEN3)
   vpMatrix M(3, 3);
   M[0][0] = 4; M[0][1] = 12; M[0][2] = -16;
   M[1][0] = 12; M[1][1] = 37; M[1][2] = -43;
@@ -144,54 +126,74 @@ int main(const int argc, const char *argv[])
   gtL[1][0] = 6; gtL[1][1] = 1; // M[1][2] = 0;
   gtL[2][0] = -8; gtL[2][1] = 5; gtL[2][2] = 3;
 
-  vpMatrix M2(4, 4);
-  M2[0][0] = 4.16; M2[0][1] = -3.12; M2[0][2] = 0.56; M2[0][3] = -0.10;
-  M2[1][0] = -3.12; M2[1][1] = 5.03; M2[1][2] = -0.83; M2[1][3] = 1.18;
-  M2[2][0] = 0.56; M2[2][1] = -0.83; M2[2][2] = 0.76; M2[2][3] = 0.34;
-  M2[3][0] = -0.10; M2[3][1] = 1.18; M2[3][2] = 0.34; M2[3][3] = 1.18;
-
-  vpMatrix gtL2(4, 4, 0.);
-  gtL2[0][0] = 2.0396;
-  gtL2[1][0] = -1.5297; gtL2[1][1] = 1.6401;
-  gtL2[2][0] = 0.2746; gtL2[2][1] = -0.2500; gtL2[2][2] = 0.7887;
-  gtL2[3][0] = -0.0490; gtL2[3][1] = 0.6737; gtL2[3][2] = 0.6617; gtL2[3][3] = 0.5347;
-
-  bool allSuccess = true;
-
 #if defined(VISP_HAVE_LAPACK)
-  vpMatrix L = M.choleskyByLapack();
-  bool success = testCholeskyDecomposition(M, gtL, L,
-                               "Test 1 Cholesky's decomposition using Lapack", verbose);
-  allSuccess = allSuccess && success;
-
-  L = M2.choleskyByLapack();
-  success = testCholeskyDecomposition(M2, gtL2, L,
-                               "Test 2 Cholesky's decomposition using Lapack", verbose);
-  allSuccess = allSuccess && success;
+  SECTION("LAPACK")
+  {
+    vpMatrix L = M.choleskyByLapack();
+    CHECK(testCholeskyDecomposition(M, gtL, L, "Test 1 Cholesky's decomposition using Lapack", true));
+  }
 #endif
 
 #if defined(VISP_HAVE_EIGEN3)
-  L = M.choleskyByEigen3();
-  success = testCholeskyDecomposition(M, gtL, L,
-                               "Test Cholesky's decomposition using Eigen3", verbose);
-  allSuccess = allSuccess && success;
-
-  L = M2.choleskyByEigen3();
-  success = testCholeskyDecomposition(M2, gtL2, L,
-                               "Test 2 Cholesky's decomposition using Eigen3", verbose);
-  allSuccess = allSuccess && success;
-#endif
-
-  if (allSuccess) {
-    std::cout << "Test succeeded" << std::endl;
-    return EXIT_SUCCESS;
+  SECTION("EIGEN3")
+  {
+    vpMatrix L = M.choleskyByEigen3();
+    CHECK(testCholeskyDecomposition(M, gtL, L, "Test Cholesky's decomposition using Eigen3", true));
   }
-  else {
-    std::cout << "Test failed" << std::endl;
-    return EXIT_FAILURE;
-  }
-#else
-  std::cout << "Test ignored: install Lapack or Eigen3" << std::endl;
-  return EXIT_SUCCESS;
 #endif
 }
+
+TEST_CASE("4 x 4 input", "[cholesky]")
+{
+  vpMatrix M(4, 4);
+  M[0][0] = 4.16; M[0][1] = -3.12; M[0][2] = 0.56; M[0][3] = -0.10;
+  M[1][0] = -3.12; M[1][1] = 5.03; M[1][2] = -0.83; M[1][3] = 1.18;
+  M[2][0] = 0.56; M[2][1] = -0.83; M[2][2] = 0.76; M[2][3] = 0.34;
+  M[3][0] = -0.10; M[3][1] = 1.18; M[3][2] = 0.34; M[3][3] = 1.18;
+
+  vpMatrix gtL(4, 4, 0.);
+  gtL[0][0] = 2.0396;
+  gtL[1][0] = -1.5297; gtL[1][1] = 1.6401;
+  gtL[2][0] = 0.2746; gtL[2][1] = -0.2500; gtL[2][2] = 0.7887;
+  gtL[3][0] = -0.0490; gtL[3][1] = 0.6737; gtL[3][2] = 0.6617; gtL[3][3] = 0.5347;
+
+#if defined(VISP_HAVE_LAPACK)
+  SECTION("LAPACK")
+  {
+    vpMatrix L = M.choleskyByLapack();
+    CHECK(testCholeskyDecomposition(M, gtL, L, "Test 1 Cholesky's decomposition using Lapack", true));
+  }
+#endif
+
+#if defined(VISP_HAVE_EIGEN3)
+  SECTION("EIGEN3")
+  {
+    vpMatrix L = M.choleskyByEigen3();
+    CHECK(testCholeskyDecomposition(M, gtL, L, "Test Cholesky's decomposition using Eigen3", true));
+  }
+#endif
+}
+
+int main(int argc, char *argv[])
+{
+  Catch::Session session; // There must be exactly one instance
+
+  // Let Catch (using Clara) parse the command line
+  session.applyCommandLine(argc, argv);
+
+  int numFailed = session.run();
+
+  // numFailed is clamped to 255 as some unices only use the lower 8 bits.
+  // This clamping has already been applied, so just return it here
+  // You can also do any post run clean-up here
+  return numFailed;
+}
+#else
+#include <iostream>
+
+int main()
+{
+  std::cout << "Test ignored: Catch2 is not available" << std::endl;
+  return EXIT_SUCCESS;
+}
+#endif
