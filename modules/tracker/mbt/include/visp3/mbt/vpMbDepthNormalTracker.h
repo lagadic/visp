0fa864a6526ab108222d155118e9850b8662990a
Fabien Spindler <Fabien.Spindler@inria.fr>
2024-09-16 11:35:25 +0200

    Update header

--- modules/tracker/mbt/include/visp3/mbt/vpMbDepthNormalTracker.h
+++ modules/tracker/mbt/include/visp3/mbt/vpMbDepthNormalTracker.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2024 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,8 +31,8 @@
  * Model-based tracker using depth normal features.
  */
 
-#ifndef _vpMbDepthNormalTracker_h_
-#define _vpMbDepthNormalTracker_h_
+#ifndef VP_MB_DEPTH_NORMAL_TRACKER_H
+#define VP_MB_DEPTH_NORMAL_TRACKER_H
 
 #include <visp3/core/vpConfig.h>
 #include <visp3/core/vpPlane.h>
@@ -119,7 +119,6 @@ public:
   virtual void track(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &point_cloud);
 #endif
   virtual void track(const std::vector<vpColVector> &point_cloud, unsigned int width, unsigned int height);
-
 
 protected:
   //! Method to estimate the desired features
