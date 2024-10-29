23ab8dffb8990b4ec85a5cc326de7e28ecc968bc
LAGNEAU Romain <romain.lagneau@inria.fr>
2024-10-29 09:02:29 +0100

    [TUTO][CLEAN] Improved display management in Circle Hough Transform tutorial

--- tutorial/imgproc/hough-transform/drawingHelpers.h
+++ tutorial/imgproc/hough-transform/drawingHelpers.h
@@ -2,31 +2,16 @@
 #define DRAWING_HELPERS_H
 
 #include <visp3/core/vpConfig.h>
+#include <visp3/core/vpDisplay.h>
 #include <visp3/core/vpImage.h>
-#include <visp3/gui/vpDisplayGDI.h>
-#include <visp3/gui/vpDisplayOpenCV.h>
-#include <visp3/gui/vpDisplayX.h>
+#include <visp3/core/vpImageConvert.h>
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 namespace drawingHelpers
 {
-#if defined(VISP_HAVE_X11)
-extern VISP_NAMESPACE_ADDRESSING vpDisplayX d;
-#elif defined(VISP_HAVE_OPENCV)
-extern VISP_NAMESPACE_ADDRESSING vpDisplayOpenCV d;
-#elif defined(VISP_HAVE_GTK)
-extern VISP_NAMESPACE_ADDRESSING vpDisplayGTK d;
-#elif defined(VISP_HAVE_GDI)
-extern VISP_NAMESPACE_ADDRESSING vpDisplayGDI d;
-#elif defined(VISP_HAVE_D3D9)
-extern VISP_NAMESPACE_ADDRESSING vpDisplayD3D d;
-#endif
-
-extern VISP_NAMESPACE_ADDRESSING vpImage<VISP_NAMESPACE_ADDRESSING vpRGBa> I_disp;
-
 bool display(VISP_NAMESPACE_ADDRESSING vpImage<VISP_NAMESPACE_ADDRESSING vpRGBa> &I, const std::string &title, const bool &blockingMode);
-bool display(VISP_NAMESPACE_ADDRESSING vpImage<unsigned char> &I, const std::string &title, const bool &blockingMode);
-bool display(VISP_NAMESPACE_ADDRESSING vpImage<double> &D, const std::string &title, const bool &blockingMode);
+bool display(VISP_NAMESPACE_ADDRESSING vpImage<unsigned char> &I, VISP_NAMESPACE_ADDRESSING vpImage<VISP_NAMESPACE_ADDRESSING vpRGBa> &Idisp, const std::string &title, const bool &blockingMode);
+bool display(VISP_NAMESPACE_ADDRESSING vpImage<double> &D, VISP_NAMESPACE_ADDRESSING vpImage<VISP_NAMESPACE_ADDRESSING vpRGBa> &Idisp, const std::string &title, const bool &blockingMode);
 }
 
 #endif
