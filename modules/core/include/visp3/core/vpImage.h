2df7042a73bcb0263770e3ccf82fad75f2c4c025
Fabien Spindler <Fabien.Spindler@inria.fr>
2024-11-27 16:13:42 +0100

    Fix build issue in vpImage::performLut() Issue reported by CI vpImage_lut.h:176:43: error: out-of-line definition of 'performLut' does not match any declaration in 'vpImage<Type>' template <class Type> void vpImage<Type>::performLut(const Type(&)[256], unsigned int)

--- modules/core/include/visp3/core/vpImage.h
+++ modules/core/include/visp3/core/vpImage.h
@@ -330,8 +330,9 @@ public:
   friend std::ostream &operator<<(std::ostream &s, const vpImage<double> &I);
 
   // Perform a look-up table transformation
-  static const unsigned int val_256 = 256;
-  void performLut(const Type(&lut)[val_256], unsigned int nbThreads = 1);
+  // static const unsigned int val_256 = 256;
+  // void performLut(const Type(&lut)[val_256], unsigned int nbThreads = 1); // Doesn't pass CI
+  void performLut(const Type(&lut)[256], unsigned int nbThreads = 1);
 
   // Returns a new image that's a quarter size of the current image
   void quarterSizeImage(vpImage<Type> &res) const;
