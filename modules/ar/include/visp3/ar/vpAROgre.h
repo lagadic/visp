6059bcfe2cbdf4c44427a29c59f0fd6ab2a2544c
LAGNEAU Romain <romain.lagneau@inria.fr>
2025-01-10 09:51:03 +0100

    [FIX] Fixed HelloWorldOgre and HelloWorldOgreAdvanced
    The window did not close when clicking on the [x] button of the window. Now it works

--- modules/ar/include/visp3/ar/vpAROgre.h
+++ modules/ar/include/visp3/ar/vpAROgre.h
@@ -302,7 +302,9 @@ public:
     mWindow->reposition(static_cast<int>(win_x), static_cast<int>(win_y));
   }
 
-  virtual void windowClosed(Ogre::RenderWindow *rw);
+  virtual bool windowClosing(Ogre::RenderWindow *rw) VP_OVERRIDE;
+
+  virtual void windowClosed(Ogre::RenderWindow *rw) VP_OVERRIDE;
 
 protected:
   virtual void init(bool bufferedKeys = false, bool hidden = false);
--- modules/ar/src/ogre-simulator/vpAROgre.cpp
+++ modules/ar/src/ogre-simulator/vpAROgre.cpp
@@ -536,6 +536,7 @@ void vpAROgre::init(bool
 
   // Register as a Window listener
   OgreWindowEventUtilities::addWindowEventListener(mWindow, this);
+  OgreWindowEventUtilities::_addRenderWindow(mWindow);
 
 #ifdef VISP_HAVE_OIS
   // Initialise OIS
@@ -713,6 +714,21 @@ bool vpAROgre::customframeStarted(const Ogre::FrameEvent & /*evt*/)
   \return True if everything went well.
 */
 bool vpAROgre::customframeEnded(const Ogre::FrameEvent & /*evt*/) { return true; }
+
+/**
+ * \brief Check if the window that is currently closing is the one attached to the object.
+ *
+ * \param[in] rw The RenderWindow that is closing.
+ * \return true It corresponds to the vpAROgre object.
+ * \return false It is another window that is closing.
+ */
+bool vpAROgre::windowClosing(Ogre::RenderWindow *rw)
+{
+  if (rw == mWindow) {
+    keepOn = false;
+  }
+  return !keepOn;
+}
 
 /*!
 
