b206a465c1ef0e1465515e8042d0a090a2447b9f
LAGNEAU Romain <romain.lagneau@inria.fr>
2025-02-17 17:58:06 +0100

    [FIX] Fix parsing of methods have VP_EXPLICIT keyword

--- modules/java/generator/hdr_parser.py
+++ modules/java/generator/hdr_parser.py
@@ -398,7 +398,7 @@ class CppHeaderParser(object):
         # INFO: Handle unsigned args/return type too. open-cv didn't have any
         # TODO: I'm removing `unsigned` keyword from function declaration
         decl_str = self.batch_replace(decl_str, [("static inline", ""),("inline", ""),
-                                                 ("VISP_EXPORT", ""), ("friend", ""),('explicit','')
+                                                 ("VISP_EXPORT", ""), ("VP_EXPLICIT", ""), ("friend", ""),('explicit','')
                                       ,("unsigned","")]).strip()
 
         if decl_str.strip().startswith('virtual'):
