9d903b58768cd03c04ac9accfcf7918f7a81bd83
Olivier Roussel <olivier.roussel@inria.fr>
2025-02-19 14:18:31 +0100

    fix visp install paths syntax on windows bash

--- development/scripts/pixi/activation.bat
+++ development/scripts/pixi/activation.bat
@@ -8,4 +8,4 @@ set CMAKE_COLOR_DIAGNOSTICS=1
 
 :: Set OS specific extra command CMake options
 :: We neeed to force installation of Visp to standard paths instead of <arch>/<vc_version>/ ones done by default
-set VISP_EXTRA_CMAKE_ARGS="-DVISP_LIB_INSTALL_PATH:PATH=^"lib^" -DVISP_BIN_INSTALL_PATH:PATH=^"bin^" -DVISP_CONFIG_INSTALL_PATH:PATH=^"cmake^""
+set VISP_EXTRA_CMAKE_ARGS="-DVISP_LIB_INSTALL_PATH:PATH=lib -DVISP_BIN_INSTALL_PATH:PATH=bin -DVISP_CONFIG_INSTALL_PATH:PATH=cmake"
