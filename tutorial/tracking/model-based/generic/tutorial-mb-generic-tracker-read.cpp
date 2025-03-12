lagadic/visp

commit ad9cecf314a7149bbe0d933fd693b423c01d59b0

Fiw warning : null destination pointer [-Wformat-overflow=]    31 |   std::sprintf(&buf[0], fmt, val);       |   ~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~ detected with c++ (GCC) 14.2.1 on Fedora 40

tutorial/tracking/model-based/generic/tutorial-mb-generic-tracker-save.cpp
@@ -27,8 +27,8 @@ std::string toString(const std::string &name, int val)
 {
   auto fmt = name.c_str();
   int sz = std::snprintf(nullptr, 0, fmt, val);
-  std::vector<char> buf(sz + 1);      // note +1 for null terminator
-  std::sprintf(buf.data(), fmt, val);
+  std::vector<char> buf(sz);
+  std::snprintf(&buf[0], sz, fmt, val);
   std::string str(buf.begin(), buf.end());
 
   return str;

commit 81d54c325db254c6eb483ffc59b775f81c8589d9

Fix toString() used to build model_iter_sz string
Issue was introduced in commit ad9cecf

tutorial/tracking/model-based/generic/tutorial-mb-generic-tracker-read.cpp
@@ -14,8 +14,9 @@ std::string toString(const std::string &name, int val)
 {
   auto fmt = name.c_str();
   int sz = std::snprintf(nullptr, 0, fmt, val);
-  std::vector<char> buf(sz + 1);      // note +1 for null terminator
-  std::sprintf(buf.data(), fmt, val);
+  sz += 1; // note +1 for null terminator
+  std::vector<char> buf(sz);
+  std::snprintf(buf.data(), sz, fmt, val);
   std::string str(buf.begin(), buf.end());
 
   return str;

tutorial/tracking/model-based/generic/tutorial-mb-generic-tracker-save.cpp
@@ -27,8 +27,9 @@ std::string toString(const std::string &name, int val)
 {
   auto fmt = name.c_str();
   int sz = std::snprintf(nullptr, 0, fmt, val);
+  sz += 1; // note +1 for null terminator
   std::vector<char> buf(sz);
-  std::snprintf(&buf[0], sz, fmt, val);
+  std::snprintf(buf.data(), sz, fmt, val);
   std::string str(buf.begin(), buf.end());
 
   return str;
@@ -183,6 +184,7 @@ int main(int argc, char **argv)
     std::vector<std::vector<double>> model = mapOfModels[camera_name];
     const std::string model_iter = toString("model_%06zu", static_cast<int>(iter));
     const std::string model_iter_sz = model_iter + "_sz";
+
     const size_t model_size = model.size();
     visp::cnpy::npz_save(npz_filename, model_iter_sz, &model_size, { 1 }, "a");
 