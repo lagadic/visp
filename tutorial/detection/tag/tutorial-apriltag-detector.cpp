17fcc3df23a770e526602ccd19b207a8993b255f
Fabien Spindler <fabien.spindler@inria.fr>
2025-03-24 19:13:05 +0100

    Update apriltag detector tutorial - rename vars associated to tag detection command line options (prefixed with opt_tag_) - introduce usage of getTagsId()

--- tutorial/detection/tag/tutorial-apriltag-detector.cpp
+++ tutorial/detection/tag/tutorial-apriltag-detector.cpp
@@ -142,16 +142,16 @@ int main(int argc, const char **argv)
 #endif
   std::string opt_input_filename = "AprilTag.jpg";
   vpDetectorAprilTag::vpAprilTagFamily opt_tag_family = vpDetectorAprilTag::TAG_36h11;
-  vpDetectorAprilTag::vpPoseEstimationMethod opt_pose_estimation_method = vpDetectorAprilTag::HOMOGRAPHY_VIRTUAL_VS;
+  vpDetectorAprilTag::vpPoseEstimationMethod opt_tag_pose_estimation_method = vpDetectorAprilTag::HOMOGRAPHY_VIRTUAL_VS;
   double opt_tag_size = 0.053;
-  float opt_quad_decimate = 1.0;
-  int opt_nThreads = 1;
+  float opt_tag_quad_decimate = 1.0;
+  int opt_tag_nThreads = 1;
   std::string opt_intrinsic_file = "";
   std::string opt_camera_name = "";
   bool opt_display_tag = false;
   int opt_color_id = -1;
   unsigned int opt_thickness = 2;
-  bool opt_z_aligned = false;
+  bool opt_tag_z_aligned = false;
 
   for (int i = 1; i < argc; ++i) {
     if (std::string(argv[i]) == "--input" && i + 1 < argc) {
@@ -164,16 +164,16 @@ int main(int argc, const char **argv)
       opt_tag_family = static_cast<vpDetectorAprilTag::vpAprilTagFamily>(atoi(argv[++i]));
     }
     else if (std::string(argv[i]) == "--tag-quad-decimate" && i + 1 < argc) {
-      opt_quad_decimate = static_cast<float>(atof(argv[++i]));
+      opt_tag_quad_decimate = static_cast<float>(atof(argv[++i]));
     }
     else if (std::string(argv[i]) == "--tag-n-threads" && i + 1 < argc) {
-      opt_nThreads = atoi(argv[++i]);
+      opt_tag_nThreads = atoi(argv[++i]);
     }
     else if (std::string(argv[i]) == "--tag-z-aligned") {
-      opt_z_aligned = true;
+      opt_tag_z_aligned = true;
     }
     else if (std::string(argv[i]) == "--tag-pose-method" && i + 1 < argc) {
-      opt_pose_estimation_method = static_cast<vpDetectorAprilTag::vpPoseEstimationMethod>(atoi(argv[++i]));
+      opt_tag_pose_estimation_method = static_cast<vpDetectorAprilTag::vpPoseEstimationMethod>(atoi(argv[++i]));
     }
 #if defined(VISP_HAVE_PUGIXML)
     else if (std::string(argv[i]) == "--intrinsic" && i + 1 < argc) {
@@ -204,20 +204,32 @@ int main(int argc, const char **argv)
     }
   }
 
+  std::cout << "Input data" << std::endl;
+  std::cout << "  Image          : " << opt_input_filename << std::endl;
+
   vpCameraParameters cam;
   cam.initPersProjWithoutDistortion(615.1674805, 615.1675415, 312.1889954, 243.4373779);
 #if defined(VISP_HAVE_PUGIXML)
   vpXmlParserCamera parser;
   if (!opt_intrinsic_file.empty() && !opt_camera_name.empty()) {
+    std::cout << "  Intrinsics     : " << opt_intrinsic_file << std::endl << std::endl;
     parser.parse(cam, opt_intrinsic_file, opt_camera_name, vpCameraParameters::perspectiveProjWithoutDistortion);
   }
+  else {
+    std::cout << "  Intrinsics     : default" << std::endl << std::endl;
+  }
+#else
+  std::cout << "  Intrinsics     : default" << std::endl << std::endl;
 #endif
 
   std::cout << cam << std::endl;
-  std::cout << "opt_pose_estimation_method: " << opt_pose_estimation_method << std::endl;
-  std::cout << "opt_tag_family: " << opt_tag_family << std::endl;
-  std::cout << "opt_nThreads : " << opt_nThreads << std::endl;
-  std::cout << "Z aligned: " << opt_z_aligned << std::endl;
+  std::cout << "Tag detector settings" << std::endl;
+  std::cout << "  Tag size [m]   : " << opt_tag_size << std::endl;
+  std::cout << "  Tag family     : " << opt_tag_family << std::endl;
+  std::cout << "  Quad decimate  : " << opt_tag_quad_decimate << std::endl;
+  std::cout << "  Thread numbers : " << opt_tag_nThreads << std::endl;
+  std::cout << "  Z aligned      : " << opt_tag_z_aligned << std::endl;
+  std::cout << "  Pose estimation: " << opt_tag_pose_estimation_method << std::endl;
 
 #if (VISP_CXX_STANDARD >= VISP_CXX_STANDARD_11)
   std::shared_ptr<vpDisplay> display, display2;
@@ -242,11 +254,11 @@ int main(int argc, const char **argv)
     //! [Create AprilTag detector]
 
     //! [AprilTag detector settings]
-    detector.setAprilTagQuadDecimate(opt_quad_decimate);
-    detector.setAprilTagPoseEstimationMethod(opt_pose_estimation_method);
-    detector.setAprilTagNbThreads(opt_nThreads);
+    detector.setAprilTagQuadDecimate(opt_tag_quad_decimate);
+    detector.setAprilTagPoseEstimationMethod(opt_tag_pose_estimation_method);
+    detector.setAprilTagNbThreads(opt_tag_nThreads);
     detector.setDisplayTag(opt_display_tag, opt_color_id < 0 ? vpColor::none : vpColor::getColor(opt_color_id), opt_thickness);
-    detector.setZAlignedWithCameraAxis(opt_z_aligned);
+    detector.setZAlignedWithCameraAxis(opt_tag_z_aligned);
     //! [AprilTag detector settings]
     // TODO:
     detector.setAprilTagDebugOption(true);
@@ -264,7 +276,12 @@ int main(int argc, const char **argv)
     ss << "Detection time: " << t << " ms for " << detector.getNbObjects() << " tags";
     vpDisplay::displayText(I, 40, 20, ss.str(), vpColor::red);
 
+    //! [Get tag ids]
+    std::vector<int> tag_ids = detector.getTagsId();
+    //! [Get tag ids]
+
     //! [Parse detected codes]
+    std::cout << "\nDetected tags" << std::endl;
     for (size_t i = 0; i < detector.getNbObjects(); i++) {
       //! [Parse detected codes]
       //! [Get location]
@@ -274,22 +291,21 @@ int main(int argc, const char **argv)
       vpDisplay::displayRectangle(I, bbox, vpColor::green);
       //! [Get message]
       std::string message = detector.getMessage(i);
+      std::cout << "  " << message << std::endl;
       //! [Get message]
-      //! [Get tag id]
-      std::size_t tag_id_pos = message.find("id: ");
-      if (tag_id_pos != std::string::npos) {
-        int tag_id = atoi(message.substr(tag_id_pos + 4).c_str());
-        ss.str("");
-        ss << "Tag id: " << tag_id;
-        vpDisplay::displayText(I, static_cast<int>(bbox.getTop() - 10), static_cast<int>(bbox.getLeft()), ss.str(), vpColor::red);
-      }
-      //! [Get tag id]
+      //! [Display tag ids]
+      ss.str("");
+      ss << "Tag id: " << tag_ids[i];
+      vpDisplay::displayText(I, static_cast<int>(bbox.getTop()), static_cast<int>(bbox.getLeft()), ss.str(), vpColor::red);
+      //! [Display tag ids]
+      //! [Display corner indexes]
       for (size_t j = 0; j < p.size(); j++) {
         vpDisplay::displayCross(I, p[j], 14, vpColor::red, 3);
         std::ostringstream number;
         number << j;
         vpDisplay::displayText(I, p[j] + vpImagePoint(15, 5), number.str(), vpColor::blue);
       }
+      //! [Display corner indexes]
     }
 
     vpDisplay::displayText(I, 20, 20, "Click to display tag poses", vpColor::red);
