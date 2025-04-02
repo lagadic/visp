d1934b13050a419ce2a2b9e388af825ccdec81d5
Olivier Roussel <olivier.roussel@inria.fr>
2025-04-02 12:49:56 +0200

    remove PANDA_T_VISP and VISP_T_PANDA constants that makes clang-cl fail

--- modules/ar/include/visp3/ar/vpPanda3DBaseRenderer.h
+++ modules/ar/include/visp3/ar/vpPanda3DBaseRenderer.h
@@ -256,8 +256,8 @@ protected:
    */
   virtual void setupRenderTarget() { }
 
-  const static vpHomogeneousMatrix VISP_T_PANDA; //! Homogeneous transformation matrix to convert from the Panda coordinate system (right-handed Z-up) to the ViSP coordinate system (right-handed Y-Down)
-  const static vpHomogeneousMatrix PANDA_T_VISP; //! Inverse of VISP_T_PANDA
+  // const static vpHomogeneousMatrix VISP_T_PANDA; //! Homogeneous transformation matrix to convert from the Panda coordinate system (right-handed Z-up) to the ViSP coordinate system (right-handed Y-Down)
+  // const static vpHomogeneousMatrix PANDA_T_VISP; //! Inverse of VISP_T_PANDA
 
 protected:
   std::string m_name; //! name of the renderer
