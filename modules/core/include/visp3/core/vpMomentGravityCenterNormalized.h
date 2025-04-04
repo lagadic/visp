lagadic/visp

commit bb0da9683f11462df49f3479c932a20c215b82e1

Fix warning : 'compute' overrides a member function but is not marked 'override' [-Wsuggest-override]

modules/core/include/visp3/core/vpMomentGravityCenterNormalized.h
@@ -65,8 +65,8 @@ public:
   vpMomentGravityCenterNormalized();
   void compute();
   //! Moment name.
-  const std::string name() const { return "vpMomentGravityCenterNormalized"; }
-  void printDependencies(std::ostream &os) const;
+  const std::string name() const VP_OVERRIDE { return "vpMomentGravityCenterNormalized"; }
+  void printDependencies(std::ostream &os) cons VP_OVERRIDE t;
   friend VISP_EXPORT std::ostream &operator<<(std::ostream &os, const vpMomentGravityCenterNormalized &v);
 };
 END_VISP_NAMESPACE

modules/core/include/visp3/core/vpMomentAlpha.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -217,7 +217,7 @@ public:
   vpMomentAlpha();
   vpMomentAlpha(const std::vector<double> &mu3_ref, double alpha_ref, double threshold = 1e-6);
 
-  void compute();
+  void compute() VP_OVERRIDE;
   /*!
    * Retrieve the orientation of the object as a single double value.
    */
@@ -225,7 +225,7 @@ public:
   /*!
    * Moment name.
    */
-  const std::string name() const { return "vpMomentAlpha"; }
+  const std::string name() const VP_OVERRIDE { return "vpMomentAlpha"; }
 
   /*!
    * Returns true if the alpha moment was constructed as a reference with values in \f$ [-\pi/2 ; \pi/2] \f$, false
@@ -252,7 +252,7 @@ public:
   }
 
   friend VISP_EXPORT std::ostream &operator<<(std::ostream &os, const vpMomentAlpha &v);
-  void printDependencies(std::ostream &os) const;
+  void printDependencies(std::ostream &os) const VP_OVERRIDE;
 };
 END_VISP_NAMESPACE
 #endif

modules/core/include/visp3/core/vpMomentArea.h
@@ -64,8 +64,8 @@ public:
   //@{
   void compute();
   //! Moment name.
-  const std::string name() const { return "vpMomentArea"; }
-  void printDependencies(std::ostream &os) const;
+  const std::string name() const VP_OVERRIDE { return "vpMomentArea"; }
+  void printDependencies(std::ostream &os) const VP_OVERRIDE;
   //@}
   friend VISP_EXPORT std::ostream &operator<<(std::ostream &os, const vpMomentArea &m);
 };

modules/core/include/visp3/core/vpMomentAreaNormalized.h
@@ -183,9 +183,9 @@ public:
   /*!
    * Moment name.
    */
-  const std::string name() const { return "vpMomentAreaNormalized"; }
+  const std::string name() const VP_OVERRIDE { return "vpMomentAreaNormalized"; }
   friend VISP_EXPORT std::ostream &operator<<(std::ostream &os, const vpMomentAreaNormalized &v);
-  void printDependencies(std::ostream &os) const;
+  void printDependencies(std::ostream &os) const VP_OVERRIDE;
 };
 END_VISP_NAMESPACE
 #endif

modules/core/include/visp3/core/vpMomentBasic.h
@@ -80,9 +80,9 @@ public:
   /*!
     Moment name.
     */
-  const std::string name() const { return "vpMomentBasic"; }
+  const std::string name() const VP_OVERRIDE { return "vpMomentBasic"; }
   friend VISP_EXPORT std::ostream &operator<<(std::ostream &os, const vpMomentBasic &v);
-  void printDependencies(std::ostream &os) const;
+  void printDependencies(std::ostream &os) const VP_OVERRIDE;
 };
 END_VISP_NAMESPACE
 #endif

modules/core/include/visp3/core/vpMomentCInvariant.h
@@ -228,7 +228,7 @@ public:
   /*!
     Moment name.
     */
-  const std::string name() const { return "vpMomentCInvariant"; }
+  const std::string name() const VP_OVERRIDE { return "vpMomentCInvariant"; }
 
   /*!
     Print partial invariant.

modules/core/include/visp3/core/vpMomentCentered.h
@@ -85,11 +85,11 @@ public:
   /*!
      Moment name.
   */
-  inline const std::string name() const { return "vpMomentCentered"; }
+  inline const std::string name() const VP_OVERRIDE { return "vpMomentCentered"; }
 
   friend VISP_EXPORT std::ostream &operator<<(std::ostream &os, const vpMomentCentered &v);
   void printWithIndices(std::ostream &os) const;
-  void printDependencies(std::ostream &os) const;
+  void printDependencies(std::ostream &os) const VP_OVERRIDE;
 
 protected:
   void set(unsigned int i, unsigned int j, double value);

modules/core/include/visp3/core/vpMomentGravityCenter.h
@@ -134,8 +134,8 @@ public:
   /*!
    * The class's string name.
    */
-  const std::string name() const { return "vpMomentGravityCenter"; }
-  void printDependencies(std::ostream &os) const;
+  const std::string name() const VP_OVERRIDE { return "vpMomentGravityCenter"; }
+  void printDependencies(std::ostream &os) const VP_OVERRIDE;
   //@}
   friend VISP_EXPORT std::ostream &operator<<(std::ostream &os, const vpMomentGravityCenter &v);
 };

commit 83e5c856bbd55ec69b9370d68b7084c98e0be2f6

Fix typo introduced in commit bb0da9683

modules/core/include/visp3/core/vpMomentGravityCenterNormalized.h
@@ -66,7 +66,7 @@ public:
   void compute();
   //! Moment name.
   const std::string name() const VP_OVERRIDE { return "vpMomentGravityCenterNormalized"; }
-  void printDependencies(std::ostream &os) cons VP_OVERRIDE t;
+  void printDependencies(std::ostream &os) const VP_OVERRIDE;
   friend VISP_EXPORT std::ostream &operator<<(std::ostream &os, const vpMomentGravityCenterNormalized &v);
 };
 END_VISP_NAMESPACE