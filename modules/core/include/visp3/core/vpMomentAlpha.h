lagadic/visp

commit fbfbf6b86d204a78f4496381d2dc9c1f8b9f27d1

Fix warning : extra ';' after member function definition [-Wextra-semi]

modules/core/include/visp3/core/vpMomentCInvariant.h
@@ -274,7 +274,7 @@ public:
   /*!
    * To know if Sx and Sy were calculated from normalized moments or not
    */
-  bool isSxSyfromNormalizedMoments() const { return flg_sxsynormalization_; };
+  bool isSxSyfromNormalizedMoments() const { return flg_sxsynormalization_; }
 
   /*!
    *  To get all the invariant values as a whole.

modules/io/include/visp3/io/vpImageQueue.h
@@ -65,7 +65,7 @@ template <class Type> class vpImageQueue
 {
 public:
   struct vpCancelled_t
-  { };
+  { }
 
   /*!
    * Queue (FIFO) constructor. By default the max queue size is set to 1024*8.

modules/tracker/mbt/include/visp3/mbt/vpMbtTukeyEstimator.h
@@ -118,7 +118,7 @@ END_VISP_NAMESPACE
 {
 // Check if std:c++14 or higher
 #if ((__cplusplus >= 201402L) || (defined(_MSVC_LANG) && (_MSVC_LANG >= 201402L)))
-auto AbsDiff = [](const auto &a, const auto &b) { return std::fabs(a - b); };
+auto AbsDiff = [](const auto &a, const auto &b) { return std::fabs(a - b); }
 #else
 template <typename T> struct AbsDiff : public std::binary_function<T, T, T>
 {

modules/vision/src/plane-estimation/vpPlaneEstimation.cpp
@@ -211,7 +211,7 @@ vpPlaneEstimation::estimatePlane(const vpImage<uint16_t> &I_depth_raw, double de
 
   // Local helper: Reduce computation (roi.isInside)
   // Default: the img is totally included in the ROI
-  std::function<bool(const vpImagePoint &)> isInside = [](const vpImagePoint &) { return true; };
+  std::function<bool(const vpImagePoint &)> isInside = [](const vpImagePoint &) { return true; }
 
   // If the img is crossed by the ROI, vpPolygon::isInside has to be used
   {
@@ -220,7 +220,7 @@ vpPlaneEstimation::estimatePlane(const vpImage<uint16_t> &I_depth_raw, double de
                            static_cast<double>(I_depth_raw.getHeight()) };
     for (const auto &roi_corner : roi.getCorners()) {
       if (img_bound.isInside(roi_corner)) {
-        isInside = [&roi](const vpImagePoint &ip) { return roi.isInside(ip); };
+        isInside = [&roi](const vpImagePoint &ip) { return roi.isInside(ip); }
         break;
       }
     }
@@ -233,7 +233,7 @@ vpPlaneEstimation::estimatePlane(const vpImage<uint16_t> &I_depth_raw, double de
          !roi.isInside(img_bound.getBottomRight()))
     // clang-format on
     {
-      isInside = [&roi](const vpImagePoint &ip) { return roi.isInside(ip); };
+      isInside = [&roi](const vpImagePoint &ip) { return roi.isInside(ip); }
     }
   }
 

commit dfbb33b4c44b8f651905608183acaa394f5cdeb5

Fix warnings and issues introduced in commit fbfbf6b8 - commit fbfbf6b8 relates to extra ';' - warning: 'compute' overrides a member function but is not marked 'override' [-Winconsistent-missing-override]   122 |   void compute();       |        ^

modules/core/include/visp3/core/vpMomentAlpha.h
@@ -36,8 +36,8 @@
   \brief Alpha moment descriptor for in-plane orientation.
 */
 
-#ifndef _vpMomentAlpha_h_
-#define _vpMomentAlpha_h_
+#ifndef VP_MOMENT_ALPHA_H
+#define VP_MOMENT_ALPHA_H
 
 #include <visp3/core/vpConfig.h>
 #include <visp3/core/vpMoment.h>

modules/core/include/visp3/core/vpMomentArea.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,8 +30,9 @@
  * Description:
  * Just the area m00 = mu00
  */
-#ifndef _vpMomentArea_h_
-#define _vpMomentArea_h_
+
+#ifndef VP_MOMENT_AREA_H
+#define VP_MOMENT_AREA_H
 
 #include <visp3/core/vpConfig.h>
 #include <visp3/core/vpMoment.h>
@@ -62,7 +63,7 @@ public:
 
   /** @name Inherited functionalities from vpMomentArea */
   //@{
-  void compute();
+  void compute() VP_OVERRIDE;
   //! Moment name.
   const std::string name() const VP_OVERRIDE { return "vpMomentArea"; }
   void printDependencies(std::ostream &os) const VP_OVERRIDE;

modules/core/include/visp3/core/vpMomentAreaNormalized.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -34,8 +34,8 @@
  * \file vpMomentAreaNormalized.h
  * \brief 2D normalized surface moment descriptor (usually described as An).
  */
-#ifndef _vpMomentAreaNormalized_h_
-#define _vpMomentAreaNormalized_h_
+#ifndef VP_MOMENT_AREA_NORMALIZED_H
+#define VP_MOMENT_AREA_NORMALIZED_H
 
 #include <visp3/core/vpConfig.h>
 #include <visp3/core/vpMoment.h>
@@ -143,7 +143,7 @@ private:
 
 public:
   vpMomentAreaNormalized(double a_star, double Z_star);
-  void compute();
+  void compute() VP_OVERRIDE;
 
   /*!
    * Retrieves the desired surface \e a* as specified in the constructor.

modules/core/include/visp3/core/vpMomentBasic.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,8 +36,8 @@
   \brief Basic moment descriptor (also called m).
 */
 
-#ifndef _vpMomentBasic_h_
-#define _vpMomentBasic_h_
+#ifndef VP_MOMENT_BASIC_H
+#define VP_MOMENT_BASIC_H
 
 #include <visp3/core/vpConfig.h>
 #include <visp3/core/vpMoment.h>
@@ -74,7 +74,7 @@ class VISP_EXPORT vpMomentBasic : public vpMoment
 public:
   vpMomentBasic();
 
-  void compute();
+  void compute() VP_OVERRIDE;
   const std::vector<double> &get() const;
   double get(unsigned int i, unsigned int j) const;
   /*!

modules/core/include/visp3/core/vpMomentCInvariant.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,13 +31,14 @@
  * Descriptor for various invariants used to drive space rotations around X and
  * Y axis.
  */
+
 /*!
   \file vpMomentCInvariant.h
   \brief Descriptor for various invariants used to drive space rotations around
   X and Y axis.
 */
-#ifndef _vpMomentCInvariant_h_
-#define _vpMomentCInvariant_h_
+#ifndef VP_MOMENT_CINVARIANT_H
+#define VP_MOMENT_CINVARIANT_H
 
 #include <visp3/core/vpConfig.h>
 #include <visp3/core/vpMoment.h>
@@ -187,7 +188,7 @@ public:
     */
   double C10() const { return values[9]; }
 
-  void compute();
+  void compute() VP_OVERRIDE;
 
   /*!
     Gets the desired invariant.

modules/core/include/visp3/core/vpMomentCentered.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,8 +36,8 @@
   \brief Centered moment descriptor (also referred as \f$\mu_{ij}\f$).
 */
 
-#ifndef _vpMomentCentered_h_
-#define _vpMomentCentered_h_
+#ifndef VP_MOMENT_CENTERED_H
+#define VP_MOMENT_CENTERED_H
 
 #include <visp3/core/vpConfig.h>
 #include <visp3/core/vpMoment.h>
@@ -78,7 +78,7 @@ class VISP_EXPORT vpMomentCentered : public vpMoment
 public:
   vpMomentCentered();
 
-  void compute();
+  void compute() VP_OVERRIDE;
   double get(unsigned int i, unsigned int j) const;
 
   inline const std::vector<double> &get() const;

modules/core/include/visp3/core/vpMomentCommon.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,12 +30,13 @@
  * Description:
  * Pre-filled moment database with all commonly used moments.
  */
+
 /*!
   \file vpMomentCommon.h
   \brief Pre-filled moment database with all commonly used moments.
 */
-#ifndef VPCOMMONMOMENTS_H
-#define VPCOMMONMOMENTS_H
+#ifndef VP_MOMENT_COMMON_H
+#define VP_MOMENT_COMMON_H
 
 #include <visp3/core/vpMomentAlpha.h>
 #include <visp3/core/vpMomentArea.h>

modules/core/include/visp3/core/vpMomentDatabase.h
@@ -35,8 +35,8 @@
  * \file vpMomentDatabase.h
  * \brief Pseudo-database used to handle dependencies between moments.
  */
-#ifndef _vpMomentDatabase_h_
-#define _vpMomentDatabase_h_
+#ifndef VP_MOMENT_DATABASE_H
+#define VP_MOMENT_DATABASE_H
 
 #include <visp3/core/vpImage.h>
 

modules/core/include/visp3/core/vpMomentGravityCenter.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,13 +30,14 @@
  * Description:
  * 2D Gravity Center moment descriptor (usually described by the pair Xg,Yg)
  */
+
 /*!
  * \file vpMomentGravityCenter.h
  * \brief 2D Gravity Center moment descriptor (usually described by the pair
  * Xg,Yg).
  */
-#ifndef _vpMomentGravityCenter_h_
-#define _vpMomentGravityCenter_h_
+#ifndef VP_MOMENT_GRAVITY_CENTER_H
+#define VP_MOMENT_GRAVITY_CENTER_H
 
 #include <visp3/core/vpConfig.h>
 #include <visp3/core/vpMoment.h>
@@ -119,7 +120,7 @@ public:
 
   /** @name Inherited functionalities from vpMomentGravityCenter */
   //@{
-  void compute();
+  void compute() VP_OVERRIDE;
   const std::vector<double> &get() const;
   /*!
    * Shortcut function to retrieve \f$x_g\f$.

modules/core/include/visp3/core/vpMomentGravityCenterNormalized.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,13 +31,14 @@
  * 2D normalized gravity center moment descriptor (usually described by the
  * pair Xn,Yn)
  */
+
 /*!
   \file vpMomentGravityCenterNormalized.h
   \brief 2D normalized gravity center moment descriptor (usually described by
   the pair Xn,Yn)
 */
-#ifndef _vpMomentGravityCenterNormalized_h_
-#define _vpMomentGravityCenterNormalized_h_
+#ifndef VP_MOMENT_GRAVITY_CENTER_NORMALIZED_H
+#define VP_MOMENT_GRAVITY_CENTER_NORMALIZED_H
 
 #include <visp3/core/vpMomentDatabase.h>
 #include <visp3/core/vpMomentGravityCenter.h>
@@ -63,7 +64,7 @@ class VISP_EXPORT vpMomentGravityCenterNormalized : public vpMomentGravityCenter
 {
 public:
   vpMomentGravityCenterNormalized();
-  void compute();
+  void compute() VP_OVERRIDE;
   //! Moment name.
   const std::string name() const VP_OVERRIDE { return "vpMomentGravityCenterNormalized"; }
   void printDependencies(std::ostream &os) const VP_OVERRIDE;

modules/core/include/visp3/core/vpMomentObject.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,12 +30,13 @@
  * Description:
  * Object input structure used by moments.
  */
+
 /*!
   \file vpMomentObject.h
   \brief Object input structure used by moments.
 */
-#ifndef _vpMomentObject_h_
-#define _vpMomentObject_h_
+#ifndef VP_MOMENT_OBJECT_H
+#define VP_MOMENT_OBJECT_H
 
 #include <cstdlib>
 #include <utility>

modules/io/include/visp3/io/vpImageQueue.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,8 +31,8 @@
  * Image queue for storage helper.
  */
 
-#ifndef vpImageQueue_h
-#define vpImageQueue_h
+#ifndef VP_IMAGE_QUEUE_H
+#define VP_IMAGE_QUEUE_H
 
 #include <visp3/core/vpConfig.h>
 
@@ -65,7 +65,7 @@ template <class Type> class vpImageQueue
 {
 public:
   struct vpCancelled_t
-  { }
+  { };
 
   /*!
    * Queue (FIFO) constructor. By default the max queue size is set to 1024*8.

modules/tracker/mbt/include/visp3/mbt/vpMbtTukeyEstimator.h
@@ -118,7 +118,7 @@ END_VISP_NAMESPACE
 {
 // Check if std:c++14 or higher
 #if ((__cplusplus >= 201402L) || (defined(_MSVC_LANG) && (_MSVC_LANG >= 201402L)))
-auto AbsDiff = [](const auto &a, const auto &b) { return std::fabs(a - b); }
+auto AbsDiff = [](const auto &a, const auto &b) { return std::fabs(a - b); };
 #else
 template <typename T> struct AbsDiff : public std::binary_function<T, T, T>
 {

modules/vision/src/plane-estimation/vpPlaneEstimation.cpp
@@ -211,7 +211,7 @@ vpPlaneEstimation::estimatePlane(const vpImage<uint16_t> &I_depth_raw, double de
 
   // Local helper: Reduce computation (roi.isInside)
   // Default: the img is totally included in the ROI
-  std::function<bool(const vpImagePoint &)> isInside = [](const vpImagePoint &) { return true; }
+  std::function<bool(const vpImagePoint &)> isInside = [](const vpImagePoint &) { return true; };
 
   // If the img is crossed by the ROI, vpPolygon::isInside has to be used
   {
@@ -220,7 +220,7 @@ vpPlaneEstimation::estimatePlane(const vpImage<uint16_t> &I_depth_raw, double de
                            static_cast<double>(I_depth_raw.getHeight()) };
     for (const auto &roi_corner : roi.getCorners()) {
       if (img_bound.isInside(roi_corner)) {
-        isInside = [&roi](const vpImagePoint &ip) { return roi.isInside(ip); }
+        isInside = [&roi](const vpImagePoint &ip) { return roi.isInside(ip); };
         break;
       }
     }
@@ -233,7 +233,7 @@ vpPlaneEstimation::estimatePlane(const vpImage<uint16_t> &I_depth_raw, double de
          !roi.isInside(img_bound.getBottomRight()))
     // clang-format on
     {
-      isInside = [&roi](const vpImagePoint &ip) { return roi.isInside(ip); }
+      isInside = [&roi](const vpImagePoint &ip) { return roi.isInside(ip); };
     }
   }
 