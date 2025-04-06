f731cb0f079fc622fae411d6e8e16b9d0b444c33
Fabien Spindler <fabien.spindler@inria.fr>
2025-04-06 19:05:12 +0200

    Fix warning : implicit conversion changes signedness: 'unsigned int' to 'int' [-Wsign-conversion]

--- modules/core/src/display/vpDisplay_rgba.cpp
+++ modules/core/src/display/vpDisplay_rgba.cpp
@@ -481,13 +481,15 @@ void vpDisplay::displayLine(const vpImage<vpRGBa> &I, int i1, int j1, int i2, in
     // line equation in image: i = a * j + b
     double delta_j = static_cast<double>(j2) - static_cast<double>(j1);
     double delta_i = static_cast<double>(i2) - static_cast<double>(i1);
+    int w = static_cast<int>(I.getWidth());
+    int h = static_cast<int>(I.getHeight());
     // Test if horizontal line
     if (std::fabs(delta_i) <= std::numeric_limits<double>::epsilon()) {
-      vp_display_display_line(I, i1, 0, i1, (I.getWidth() - 1), color, thickness);
+      vp_display_display_line(I, i1, 0, i1, (w - 1), color, thickness);
     }
     // Test if vertical line
     else if (std::fabs(delta_j) <= std::numeric_limits<double>::epsilon()) {
-      vp_display_display_line(I, 0, j1, (I.getHeight() - 1), j1, color, thickness);
+      vp_display_display_line(I, 0, j1, (h - 1), j1, color, thickness);
     }
     else {
       double a = delta_i / delta_j;
@@ -495,12 +497,12 @@ void vpDisplay::displayLine(const vpImage<vpRGBa> &I, int i1, int j1, int i2, in
       std::vector<vpImagePoint> vip; // Image points that intersect image borders
       // Test intersection with vertical line j=0
       vpImagePoint ip_left(b, 0);
-      if ((ip_left.get_i() >= 0.) && (ip_left.get_i() <= (I.getHeight() - 1.))) {
+      if ((ip_left.get_i() >= 0.) && (ip_left.get_i() <= (h - 1.))) {
         vip.push_back(ip_left);
       }
       // Test intersection with vertical line j=width-1
-      vpImagePoint ip_right((a * (I.getWidth() - 1)) + b, I.getWidth() - 1.);
-      if ((ip_right.get_i() >= 0.) && (ip_right.get_i() <= (I.getHeight() - 1.))) {
+      vpImagePoint ip_right((a * (w - 1)) + b, w - 1.);
+      if ((ip_right.get_i() >= 0.) && (ip_right.get_i() <= (h - 1.))) {
         vip.push_back(ip_right);
       }
       if (vip.size() == 2) {
@@ -509,7 +511,7 @@ void vpDisplay::displayLine(const vpImage<vpRGBa> &I, int i1, int j1, int i2, in
       }
       // Test intersection with horizontal line i=0
       vpImagePoint ip_top(0, -b / a);
-      if ((ip_top.get_j() >= 0.) && (ip_top.get_j() <= (I.getWidth() - 1.))) {
+      if ((ip_top.get_j() >= 0.) && (ip_top.get_j() <= (w - 1.))) {
         vip.push_back(ip_top);
       }
       if (vip.size() == 2) {
@@ -517,8 +519,8 @@ void vpDisplay::displayLine(const vpImage<vpRGBa> &I, int i1, int j1, int i2, in
         return;
       }
       // Test intersection with horizontal line i=height-1
-      vpImagePoint ip_bottom(I.getHeight() - 1., (I.getHeight() - 1. - b) / a);
-      if ((ip_bottom.get_j() >= 0.) && (ip_bottom.get_j() <= (I.getWidth() - 1.))) {
+      vpImagePoint ip_bottom(h - 1., (h - 1. - b) / a);
+      if ((ip_bottom.get_j() >= 0.) && (ip_bottom.get_j() <= (w - 1.))) {
         vip.push_back(ip_bottom);
       }
       if (vip.size() == 2) {
