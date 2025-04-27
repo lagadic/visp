ca381bfd403589d7523b047035da59744bd76ba8
Fabien Spindler <fabien.spindler@inria.fr>
2025-04-27 10:45:43 +0200

    Fix warning : definition of implicit copy assignment operator for 'vpRequest' is deprecated because it has a user-provided destructor [-Wdeprecated-copy-with-user-provided-dtor]

--- modules/core/include/visp3/core/vpRequest.h
+++ modules/core/include/visp3/core/vpRequest.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -132,6 +132,15 @@ protected:
 
 public:
   vpRequest();
+  vpRequest(const vpRequest &req) : request_id(req.request_id), listOfParams(req.listOfParams) { }
+  virtual ~vpRequest() { }
+
+  vpRequest &operator=(const vpRequest &req)
+  {
+    request_id = req.request_id;
+    listOfParams = req.listOfParams;
+    return *this;
+  }
 
   void addParameter(const char *params);
   void addParameter(const std::string &params);
