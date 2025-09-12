1a44c166820b18240043c0b9cddf168955a25aa7
Fabien Spindler <Fabien.Spindler@inria.fr>
2025-09-12 10:44:09 +0200

    Add missing copyright header and BEGIN/END_VISP_NAMESPACE macros

--- modules/tracker/rbt/src/core/vpTemporalWeighting.cpp
+++ modules/tracker/rbt/src/core/vpTemporalWeighting.cpp
@@ -1,9 +1,40 @@
+/*
+ * ViSP, open source Visual Servoing Platform software.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact Inria about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See https://visp.inria.fr for more information.
+ *
+ * This software was developed at:
+ * Inria Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Inria at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #include <visp3/rbt/vpTemporalWeighting.h>
 
 #if defined(VISP_HAVE_NLOHMANN_JSON)
 #include VISP_NLOHMANN_JSON(json.hpp)
 #endif
 
+BEGIN_VISP_NAMESPACE
 
 #if defined(VISP_HAVE_NLOHMANN_JSON)
 std::shared_ptr<vpTemporalWeighting> vpTemporalWeighting::parseTemporalWeighting(const nlohmann::json &j)
@@ -24,12 +55,10 @@ std::shared_ptr<vpTemporalWeighting> vpTemporalWeighting::parseTemporalWeighting
       j.value("midpointLocation", 0.5),
       slopePower
     );
-
   }
   else {
     throw vpException(vpException::badValue, "Wrong weighting type");
   }
-
 }
 std::shared_ptr<vpTemporalWeighting> vpTemporalWeighting::parseTemporalWeightingRawJson(const std::string &s)
 {
@@ -37,9 +66,7 @@ std::shared_ptr<vpTemporalWeighting> vpTemporalWeighting::parseTemporalWeighting
   return parseTemporalWeighting(j);
 }
 
-
 #endif
-
 
 double vpFixedTemporalWeighting::weight(const double /*progress*/) const
 {
@@ -48,7 +75,6 @@ double vpFixedTemporalWeighting::weight(const double /*progress*/) const
 
 double vpSigmoidTemporalWeighting::weight(const double progress) const
 {
-
   double w = 1.0;
   if (progress == 0.0 || m_location == 1.0) {
     if (m_power > 0.0) {
@@ -66,3 +92,5 @@ double vpSigmoidTemporalWeighting::weight(const double progress) const
   }
   return m_minWeight + w * (m_maxWeight - m_minWeight);
 }
+
+END_VISP_NAMESPACE
