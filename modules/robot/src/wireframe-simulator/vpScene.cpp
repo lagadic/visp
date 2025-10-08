55f3949b81e978b1e3e4927fc87dac23907dbd52
Fabien Spindler <Fabien.Spindler@inria.fr>
2025-10-08 09:40:20 +0200

    Fix warning: variable 'nbFaces' set but not used [-Wunused-but-set-variable]

--- modules/robot/src/wireframe-simulator/vpScene.cpp
+++ modules/robot/src/wireframe-simulator/vpScene.cpp
@@ -1,7 +1,6 @@
-/****************************************************************************
- *
+/*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,13 +26,7 @@
  *
  * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
  * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Description:
- *
- * Authors:
- * Jean-Luc CORRE
- *
-*****************************************************************************/
+ */
 
 #include <visp3/core/vpConfig.h>
 
@@ -161,7 +154,6 @@ void set_scene_wrl(const char *str, Bound_scene *sc, float factor)
   for (int i = 0; i < nbShapes; i++) {
     child = sceneGraphVRML2->getChild(i);
     if (child->getTypeId() == SoVRMLShape::getClassTypeId()) {
-      int nbFaces = 0;
       std::list<indexFaceSet *> ifs_list;
       SoChildList *child2list = child->getChildren();
       for (int j = 0; j < child2list->getLength(); j++) {
@@ -171,7 +163,6 @@ void set_scene_wrl(const char *str, Bound_scene *sc, float factor)
           face_set = (SoVRMLIndexedFaceSet *)child2list->get(j);
           extractFaces(face_set, ifs);
           ifs_list.push_back(ifs);
-          nbFaces++;
         }
         //         if (((SoNode*)child2list->get(j))->getTypeId() ==
         //         SoVRMLIndexedLineSet::getClassTypeId())
