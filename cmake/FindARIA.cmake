b32252e05db82fa2b887c364372b68bf441bed74
Fabien Spindler <Fabien.Spindler@inria.fr>
2025-10-10 09:27:34 +0200

    Update Copyright years to 2005 - 2025

--- cmake/FindARIA.cmake
+++ cmake/FindARIA.cmake
@@ -1,7 +1,7 @@
 #############################################################################
 #
 # ViSP, open source Visual Servoing Platform software.
-# Copyright (C) 2005 - 2023 by Inria. All rights reserved.
+# Copyright (C) 2005 - 2025 by Inria. All rights reserved.
 #
 # This software is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -129,4 +129,3 @@ mark_as_advanced(
   ARIA_LIBRARY_RELEASE
   ARIA_LIB_SEARCH_PATH
 )
-
