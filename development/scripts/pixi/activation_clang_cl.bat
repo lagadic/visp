816891c6da3c00aee0d4cc7694bd642eed82bcbe
Olivier Roussel <olivier.roussel@inria.fr>
2025-10-27 17:00:58 +0100

    debugging

--- development/scripts/pixi/activation_clang_cl.bat
+++ development/scripts/pixi/activation_clang_cl.bat
@@ -1,4 +1,4 @@
 :: Setup clang-cl compiler
 :: Explicit path is necessary to avoid getting the clang-cl compiler installed on system
-set CC="%CONDA_PREFIX%\\Library\\bin\\clang-cl"
-set CXX="%CONDA_PREFIX%\\Library\\bin\\clang-cl"
+@REM set CC="%CONDA_PREFIX%\\Library\\bin\\clang-cl"
+@REM set CXX="%CONDA_PREFIX%\\Library\\bin\\clang-cl"
