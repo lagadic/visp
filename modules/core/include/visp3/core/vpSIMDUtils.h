c4bc02aa143749b7ccc07570aeba2c51c7fa4b2e
Fabien Spindler <Fabien.Spindler@inria.fr>
2025-12-07 10:09:55 +0100

    Update copyright headers

--- modules/core/include/visp3/core/vpSIMDUtils.h
+++ modules/core/include/visp3/core/vpSIMDUtils.h
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2024 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2025 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@
  * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
  * Description:
- * Rotation matrix.
+ * SIMD utilities.
  */
 
 /*!
