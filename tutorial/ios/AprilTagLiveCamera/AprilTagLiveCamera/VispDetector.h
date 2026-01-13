afb91ed5e446925d88d8cbe77fce12add93cc23f
Fabien Spindler <Fabien.Spindler@inria.fr>
2026-01-13 18:36:33 +0100

    Update Xcode projects and remove references to opencv2.framework and visp3.framework

--- tutorial/ios/AprilTagLiveCamera/AprilTagLiveCamera/VispDetector.h
+++ tutorial/ios/AprilTagLiveCamera/AprilTagLiveCamera/VispDetector.h
@@ -1,5 +1,9 @@
 #import <UIKit/UIKit.h>
 
+#ifdef __cplusplus
+#import <visp3/visp.h>
+#endif
+
 @interface VispDetector : NSObject
 - (UIImage *)detectAprilTag: (UIImage*)image px:(float)px py:(float)py;
 @end
