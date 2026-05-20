542ea64db3c241a2a4661bbdffd8dae98221112f
Fabien Spindler <fabien.spindler@inria.fr>
2026-05-20 09:58:09 +0200

    Fix visp/example/servo-viper850/servoViper850FourPoints2DCamVelocityLs_cur.cpp:246:23: warning: declaration of ‘p’ shadows a previous local [-Wshadow]   246 |       vpColVector cP, p;       |                       ^ visp/example/servo-viper850/servoViper850FourPoints2DCamVelocityLs_cur.cpp:223:20: note: shadowed declaration is here   223 |     vpFeaturePoint p[4];       |                    ^

--- example/servo-viper850/servoViper850FourPoints2DCamVelocityLs_cur.cpp
+++ example/servo-viper850/servoViper850FourPoints2DCamVelocityLs_cur.cpp
@@ -243,12 +243,12 @@ int main()
     vpFeaturePoint pd[4];
     // Compute the desired position of the features from the desired pose
     for (int i = 0; i < 4; ++i) {
-      vpColVector cP, p;
+      vpColVector cP, pp;
       point[i].changeFrame(cMo, cP);
-      point[i].projection(cP, p);
+      point[i].projection(cP, pp);
 
-      pd[i].set_x(p[0]);
-      pd[i].set_y(p[1]);
+      pd[i].set_x(pp[0]);
+      pd[i].set_y(pp[1]);
       pd[i].set_Z(cP[2]);
     }
 
