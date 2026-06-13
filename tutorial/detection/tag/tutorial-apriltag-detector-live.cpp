4c9ce4bdc7677f2bc56fb28b99cbd87394b9b870
Fabien Spindler <Fabien.Spindler@inria.fr>
2026-06-13 09:47:29 +0200

    Fix typo introduced in previous changes

--- tutorial/detection/tag/tutorial-apriltag-detector-live.cpp
+++ tutorial/detection/tag/tutorial-apriltag-detector-live.cpp
@@ -227,7 +227,7 @@ int main(int argc, const char **argv)
   std::string camera_name = "";
   bool opt_display_tag = false;
   int opt_color_id = -1;
-  unsigned int thickness = 2;
+  unsigned int opt_thickness = 2;
   bool opt_tag_z_align_frame = false;
   bool opt_verbose = false;
 
@@ -287,7 +287,7 @@ int main(int argc, const char **argv)
       opt_color_id = atoi(argv[++i]);
     }
     else if (std::string(argv[i]) == "--thickness" && i + 1 < argc) {
-      thickness = static_cast<unsigned int>(atoi(argv[++i]));
+      opt_thickness = static_cast<unsigned int>(atoi(argv[++i]));
     }
 #endif
     else if (std::string(argv[i]) == "--verbose" || std::string(argv[i]) == "-v") {
