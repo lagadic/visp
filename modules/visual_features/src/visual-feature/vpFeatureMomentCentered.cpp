a8b8740c9d2c02d84e771ba24ab707e28a1170e9
Fabien Spindler <fabien.spindler@inria.fr>
2026-06-26 15:57:27 +0200

    Fix warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:139:6: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:139:6: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:139:6: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:139:6: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:139:6: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:285:21: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      285 |   for (int j = 1; j < static_cast<int>(order) - 1; j++) {
          |                   ~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:139:6: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      139 | void vpFeatureMomentCentered::compute_interaction()
          |      ^~~~~~~~~~~~~~~~~~~~~~~
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:289:23: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      289 |     for (int i = 1; i < static_cast<int>(order) - j - 1; i++) {
          |                     ~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:139:6: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      139 | void vpFeatureMomentCentered::compute_interaction()
          |      ^~~~~~~~~~~~~~~~~~~~~~~
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:262:21: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      262 |   for (int j = 1; j < static_cast<int>(order) - 1; j++) {
          |                   ~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:139:6: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      139 | void vpFeatureMomentCentered::compute_interaction()
          |      ^~~~~~~~~~~~~~~~~~~~~~~
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:239:21: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      239 |   for (int i = 1; i < static_cast<int>(order) - 1; i++) {
          |                   ~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    visp/modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp:139:6: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      139 | void vpFeatureMomentCentered::compute_interaction()
          |      ^~~~~~~~~~~~~~~~~~~~~~~

--- modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp
+++ modules/visual_features/src/visual-feature/vpFeatureMomentCentered.cpp
@@ -1,6 +1,6 @@
 /*
  * ViSP, open source Visual Servoing Platform software.
- * Copyright (C) 2005 - 2024 by Inria. All rights reserved.
+ * Copyright (C) 2005 - 2026 by Inria. All rights reserved.
  *
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -56,7 +56,7 @@ BEGIN_VISP_NAMESPACE
   vpFeatureMomentCentered::vpFeatureMomentCentered(vpMomentDatabase &moments_, double A_, double B_, double C_,
                                                    vpFeatureMomentDatabase *featureMoments)
   : vpFeatureMoment(moments_, A_, B_, C_, featureMoments), order(0)
-{ }
+{}
 
 /*!
  * Interaction matrix corresponding to \f$ \mu_{ij} \f$ moment.
@@ -194,8 +194,8 @@ void vpFeatureMomentCentered::compute_interaction()
   if (!found_moment_gravity)
     throw vpException(vpException::notInitialized, "vpMomentGravityCenter not found");
 
-  int delta;
-  int epsilon;
+  unsigned int delta;
+  unsigned int epsilon;
   const vpMomentObject &momentObject = moment->getObject();
   order = momentObject.getOrder() + 1;
   interaction_matrices.resize(order * order);
@@ -236,82 +236,79 @@ void vpFeatureMomentCentered::compute_interaction()
     -A * interaction_matrices[0][0][WY] + B * interaction_matrices[0][0][WX] + (2 * delta) * C * mu00;
   interaction_matrices[0][0][WZ] = 0.;
 
-  for (int i = 1; i < static_cast<int>(order) - 1; i++) {
-    unsigned int i_ = static_cast<unsigned int>(i);
-    unsigned int im1_ = i_ - 1;
-    unsigned int ip1_ = i_ + 1;
+  const unsigned int order_u = static_cast<unsigned int>(order);
+  for (unsigned int i = 1; i < order_u - 1; i++) {
+    unsigned int im1 = i - 1;
+    unsigned int ip1 = i + 1;
 
-    double mu_im10 = momentCentered.get(im1_, 0);
-    double mu_ip10 = momentCentered.get(ip1_, 0);
-    double mu_im11 = momentCentered.get(im1_, 1);
-    double mu_i0 = momentCentered.get(i_, 0);
-    double mu_i1 = momentCentered.get(i_, 1);
+    double mu_im10 = momentCentered.get(im1, 0);
+    double mu_ip10 = momentCentered.get(ip1, 0);
+    double mu_im11 = momentCentered.get(im1, 1);
+    double mu_i0 = momentCentered.get(i, 0);
+    double mu_i1 = momentCentered.get(i, 1);
 
-    interaction_matrices[i_][0][VX] = -(i + delta) * A * mu_i0 - (i * B * mu_im11);
-    interaction_matrices[i_][0][VY] = -(delta)*B * mu_i0;
+    interaction_matrices[i][0][VX] = -(i + delta) * A * mu_i0 - (i * B * mu_im11);
+    interaction_matrices[i][0][VY] = -(delta)*B * mu_i0;
 
-    interaction_matrices[i_][0][WX] =
+    interaction_matrices[i][0][WX] =
       (i + 3 * delta) * mu_i1 + (i + 3 * delta) * Yg * mu_i0 + i * Xg * mu_im11 - i * epsilon * n11 * mu_im10;
-    interaction_matrices[i_][0][WY] =
+    interaction_matrices[i][0][WY] =
       -(i + 3 * delta) * mu_ip10 - (2 * i + 3 * delta) * Xg * mu_i0 + i * epsilon * n20 * mu_im10;
-    interaction_matrices[i_][0][VZ] =
-      -A * interaction_matrices[i_][0][WY] + B * interaction_matrices[i_][0][WX] + (i + 2 * delta) * C * mu_i0;
-    interaction_matrices[i_][0][WZ] = i * mu_im11;
+    interaction_matrices[i][0][VZ] =
+      -A * interaction_matrices[i][0][WY] + B * interaction_matrices[i][0][WX] + (i + 2 * delta) * C * mu_i0;
+    interaction_matrices[i][0][WZ] = i * mu_im11;
   }
 
-  for (int j = 1; j < static_cast<int>(order) - 1; j++) {
-    unsigned int j_ = static_cast<unsigned int>(j);
-    unsigned int jm1_ = j_ - 1;
-    unsigned int jp1_ = j_ + 1;
+  for (unsigned int j = 1; j < order_u - 1; j++) {
+    unsigned int jm1 = j - 1;
+    unsigned int jp1 = j + 1;
 
-    double mu_0jm1 = momentCentered.get(0, jm1_);
-    double mu_0jp1 = momentCentered.get(0, jp1_);
-    double mu_1jm1 = momentCentered.get(1, jm1_);
-    double mu_0j = momentCentered.get(0, j_);
-    double mu_1j = momentCentered.get(1, j_);
+    double mu_0jm1 = momentCentered.get(0, jm1);
+    double mu_0jp1 = momentCentered.get(0, jp1);
+    double mu_1jm1 = momentCentered.get(1, jm1);
+    double mu_0j = momentCentered.get(0, j);
+    double mu_1j = momentCentered.get(1, j);
 
-    interaction_matrices[j_ * order][0][VX] = -(delta)*A * mu_0j;
-    interaction_matrices[j_ * order][0][VY] = -j * A * mu_1jm1 - (j + delta) * B * mu_0j;
+    interaction_matrices[j * order][0][VX] = -(delta)*A * mu_0j;
+    interaction_matrices[j * order][0][VY] = -j * A * mu_1jm1 - (j + delta) * B * mu_0j;
 
-    interaction_matrices[j_ * order][0][WX] =
+    interaction_matrices[j * order][0][WX] =
       (j + 3 * delta) * mu_0jp1 + (2 * j + 3 * delta) * Yg * mu_0j - j * epsilon * n02 * mu_0jm1;
-    interaction_matrices[j_ * order][0][WY] =
+    interaction_matrices[j * order][0][WY] =
       -(j + 3 * delta) * mu_1j - (j + 3 * delta) * Xg * mu_0j - j * Yg * mu_1jm1 + j * epsilon * n11 * mu_0jm1;
-    interaction_matrices[j_ * order][0][VZ] = -A * interaction_matrices[j_ * order][0][WY] +
-      B * interaction_matrices[j_ * order][0][WX] + (j + 2 * delta) * C * mu_0j;
-    interaction_matrices[j_ * order][0][WZ] = -j * mu_1jm1;
+    interaction_matrices[j * order][0][VZ] = -A * interaction_matrices[j * order][0][WY] +
+      B * interaction_matrices[j * order][0][WX] + (j + 2 * delta) * C * mu_0j;
+    interaction_matrices[j * order][0][WZ] = -j * mu_1jm1;
   }
 
-  for (int j = 1; j < static_cast<int>(order) - 1; j++) {
-    unsigned int j_ = static_cast<unsigned int>(j);
-    unsigned int jm1_ = j_ - 1;
-    unsigned int jp1_ = j_ + 1;
-    for (int i = 1; i < static_cast<int>(order) - j - 1; i++) {
-      unsigned int i_ = static_cast<unsigned int>(i);
-      unsigned int im1_ = i_ - 1;
-      unsigned int ip1_ = i_ + 1;
+  for (unsigned int j = 1; j < order_u - 1; j++) {
+    unsigned int jm1_ = j - 1;
+    unsigned int jp1_ = j + 1;
+    for (unsigned int i = 1; i < order_u - j - 1; i++) {
+      unsigned int im1 = i - 1;
+      unsigned int ip1 = i + 1;
 
-      double mu_ijm1 = momentCentered.get(i_, jm1_);
-      double mu_ij = momentCentered.get(i_, j_);
-      double mu_ijp1 = momentCentered.get(i_, jp1_);
-      double mu_im1j = momentCentered.get(im1_, j_);
-      double mu_im1jp1 = momentCentered.get(im1_, jp1_);
-      double mu_ip1jm1 = momentCentered.get(ip1_, jm1_);
-      double mu_ip1j = momentCentered.get(ip1_, j_);
+      double mu_ijm1 = momentCentered.get(i, jm1_);
+      double mu_ij = momentCentered.get(i, j);
+      double mu_ijp1 = momentCentered.get(i, jp1_);
+      double mu_im1j = momentCentered.get(im1, j);
+      double mu_im1jp1 = momentCentered.get(im1, jp1_);
+      double mu_ip1jm1 = momentCentered.get(ip1, jm1_);
+      double mu_ip1j = momentCentered.get(ip1, j);
 
-      interaction_matrices[j_ * order + i_][0][VX] = -(i + delta) * A * mu_ij - i * B * mu_im1jp1;
-      interaction_matrices[j_ * order + i_][0][VY] = -j * A * mu_ip1jm1 - (j + delta) * B * mu_ij;
+      interaction_matrices[j * order + i][0][VX] = -(i + delta) * A * mu_ij - i * B * mu_im1jp1;
+      interaction_matrices[j * order + i][0][VY] = -j * A * mu_ip1jm1 - (j + delta) * B * mu_ij;
 
-      interaction_matrices[j_ * order + i_][0][WX] = (i + j + 3 * delta) * mu_ijp1 +
+      interaction_matrices[j * order + i][0][WX] = (i + j + 3 * delta) * mu_ijp1 +
         (i + 2 * j + 3 * delta) * Yg * mu_ij + i * Xg * mu_im1jp1 -
         i * epsilon * n11 * mu_im1j - j * epsilon * n02 * mu_ijm1;
-      interaction_matrices[j_ * order + i_][0][WY] = -(i + j + 3 * delta) * mu_ip1j -
+      interaction_matrices[j * order + i][0][WY] = -(i + j + 3 * delta) * mu_ip1j -
         (2 * i + j + 3 * delta) * Xg * mu_ij - j * Yg * mu_ip1jm1 +
         i * epsilon * n20 * mu_im1j + j * epsilon * n11 * mu_ijm1;
-      interaction_matrices[j_ * order + i_][0][VZ] = -A * interaction_matrices[j_ * order + i_][0][WY] +
-        B * interaction_matrices[j_ * order + i_][0][WX] +
+      interaction_matrices[j * order + i][0][VZ] = -A * interaction_matrices[j * order + i][0][WY] +
+        B * interaction_matrices[j * order + i][0][WX] +
         (i + j + 2 * delta) * C * mu_ij;
-      interaction_matrices[j_ * order + i_][0][WZ] = i * mu_im1jp1 - j * mu_ip1jm1;
+      interaction_matrices[j * order + i][0][WZ] = i * mu_im1jp1 - j * mu_ip1jm1;
     }
   }
 #endif // #ifdef VISP_MOMENTS_COMBINE_MATRICES
