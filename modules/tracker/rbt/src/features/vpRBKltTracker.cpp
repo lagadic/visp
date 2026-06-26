887318cffc1b3c3d7b87be55cc7cc36038d570af
Fabien Spindler <fabien.spindler@inria.fr>
2026-06-26 14:54:26 +0200

    Fix warning: assuming signed overflow does not occur when reducing constant in comparison [-Wstrict-overflow]
    visp/modules/tracker/rbt/src/mask/vpColorHistogram.cpp: In member function ‘void visp::vpColorHistogram::computeProbas(const visp::vpImage<visp::vpRGBa>&, visp::vpImage<float>&, const visp::vpRect&) const’:
    visp/modules/tracker/rbt/src/mask/vpColorHistogram.cpp:146:3: warning: assuming signed overflow does not occur when reducing constant in comparison [-Wstrict-overflow]
      146 |   for (int i = top; i <= bottom; ++i) {
          |   ^~~
    [ 59%] Building CXX object modules/rbt/CMakeFiles/visp_rbt.dir/src/mask/vpColorHistogramMask.cpp.o
    In file included from visp/modules/tracker/rbt/include/visp3/rbt/vpRBKltTracker.h:38,
                     from visp/modules/tracker/rbt/src/features/vpRBKltTracker.cpp:31:
    visp/modules/tracker/rbt/src/features/vpRBKltTracker.cpp: In member function ‘virtual void visp::vpRBKltTracker::trackFeatures(const visp::vpRBFeatureTrackerInput&, const visp::vpRBFeatureTrackerInput&, const visp::vpHomogeneousMatrix&)’:
    visp-build-warn-extra/include/visp3/core/vpConfig.h:119:28: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      119 | #define END_VISP_NAMESPACE }
          |                            ^
    visp/modules/tracker/rbt/src/features/vpRBKltTracker.cpp:317:1: note: in expansion of macro ‘END_VISP_NAMESPACE’
      317 | END_VISP_NAMESPACE
          | ^~~~~~~~~~~~~~~~~~
    visp/modules/tracker/rbt/src/features/vpRBKltTracker.cpp: In member function ‘virtual void visp::vpRBKltTracker::extractFeatures(const visp::vpRBFeatureTrackerInput&, const visp::vpRBFeatureTrackerInput&, const visp::vpHomogeneousMatrix&)’:
    visp-build-warn-extra/include/visp3/core/vpConfig.h:119:28: warning: assuming signed overflow does not occur when changing X +- C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]
      119 | #define END_VISP_NAMESPACE }
          |                            ^
    visp/modules/tracker/rbt/src/features/vpRBKltTracker.cpp:317:1: note: in expansion of macro ‘END_VISP_NAMESPACE’
      317 | END_VISP_NAMESPACE
          | ^~~~~~~~~~~~~~~~~~

--- modules/tracker/rbt/src/features/vpRBKltTracker.cpp
+++ modules/tracker/rbt/src/features/vpRBKltTracker.cpp
@@ -124,7 +124,7 @@ void vpRBKltTracker::extractFeatures(const vpRBFeatureTrackerInput &frame, const
       }
     }
     // Remove tracking from klt: iterate in reverse order to invalidate iterator i (shifting in the klt list)
-    for (int i = static_cast<int>(kltIndicesToRemove.size()) - 1; i >= 0; --i) {
+    for (size_t i = kltIndicesToRemove.size() - 1; i-- > 0;) {
       m_klt.suppressFeature(static_cast<int>(kltIndicesToRemove[static_cast<unsigned int>(i)]));
     }
   }
@@ -210,7 +210,8 @@ void vpRBKltTracker::extractFeatures(const vpRBFeatureTrackerInput &frame, const
 
 void vpRBKltTracker::trackFeatures(const vpRBFeatureTrackerInput &frame, const vpRBFeatureTrackerInput &/*previousFrame*/, const vpHomogeneousMatrix &cMo)
 {
-  int nbKltFeatures = m_klt.getNbFeatures();
+  const unsigned int nbKltFeatures = static_cast<unsigned int>(m_klt.getNbFeatures());
+
   if (nbKltFeatures > 0) {
     m_klt.track(m_I);
   }
@@ -218,13 +219,12 @@ void vpRBKltTracker::trackFeatures(const vpRBFeatureTrackerInput &frame, const v
   const vpHomogeneousMatrix oMc = cMo.inverse();
 
   bool testMask = m_useMask && frame.hasMask();
-  nbKltFeatures = m_klt.getNbFeatures();
   std::vector<unsigned> kltIndicesToRemove;
-  for (int i = 0; i < nbKltFeatures; ++i) {
+  for (unsigned int i = 0; i < nbKltFeatures; ++i) {
     long id = 0;
     float u = 0.f, v = 0.f;
     double x = 0.0, y = 0.0;
-    m_klt.getFeature(i, id, u, v);
+    m_klt.getFeature(static_cast<int>(i), id, u, v);
     unsigned int uu = static_cast<unsigned int>(round(u)), uv = static_cast<unsigned int>(round(v));
     // Filter points that are too close to image borders and cannot be reliably tracked
     if (isTooCloseToBorder(uv, uu, frame.renders.depth.getRows(), frame.renders.depth.getCols(), m_border)) {
@@ -254,7 +254,7 @@ void vpRBKltTracker::trackFeatures(const vpRBFeatureTrackerInput &frame, const v
   }
 
   // Remove tracking from klt: iterate in reverse order to invalidate iterator i (shifting in the klt list)
-  for (int i = static_cast<int>(kltIndicesToRemove.size()) - 1; i >= 0; --i) {
+  for (size_t i = kltIndicesToRemove.size() - 1; i-- > 0;) {
     m_klt.suppressFeature(static_cast<int>(kltIndicesToRemove[static_cast<unsigned int>(i)]));
   }
 
