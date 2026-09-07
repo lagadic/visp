6a9f4d5e957bc6cb4bb95bab3e39a68b2a558192
sjourdro <simon.jourdrouin@inria.fr>
2026-09-07 17:48:55 +0200

    minor adjustments on matplotlib display code

--- modules/python/examples/image/tutorial-image-display-matplotlib.py
+++ modules/python/examples/image/tutorial-image-display-matplotlib.py
@@ -1,11 +1,9 @@
 import sys
-import numpy as np
 import matplotlib.pyplot as plt
 
 # ViSP Python bindings
 from visp.core import ImageRGBa
 from visp.io import ImageIo
-
 
 # Image path
 path = sys.path[0] + "/monkey.jpeg"
