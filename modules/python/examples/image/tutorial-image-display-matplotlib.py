b13020b54a645a5464c1f47298ea867ca0e01e80
sjourdro <simon.jourdrouin@inria.fr>
2026-09-08 17:43:45 +0200

    changed code snippets to literalinclude

--- modules/python/examples/image/tutorial-image-display-matplotlib.py
+++ modules/python/examples/image/tutorial-image-display-matplotlib.py
@@ -1,5 +1,8 @@
 import sys
+
+# [imports]
 import matplotlib.pyplot as plt
+# [end-imports]
 
 # ViSP Python bindings
 from visp.core import ImageRGBa
@@ -16,8 +19,10 @@ except:
   print(f"Cannot read image {path}")
   sys.exit()
 
+# [display-image]
 # Display using Matplotlib
 plt.imshow(I)
 plt.axis('off')
 plt.title("monkey.jpeg")
 plt.show()
+# [end-display-image]
--- modules/python/examples/image/tutorial-image-display.py
+++ modules/python/examples/image/tutorial-image-display.py
@@ -1,29 +1,45 @@
 import sys
 
+# [imports]
 # ViSP Python bindings
 from visp.core import ImageRGBa
 from visp.io import ImageIo
+
 from visp.core import Display
 from visp.python.display_utils import get_display
+# [end-imports]
 
+
+# [read-image]
 # Image path
 path = sys.path[0] + "/monkey.jpeg"
 
-# Read the image 
+# Read the image
 I = ImageRGBa()
 try:
   ImageIo.read(I, path)
 except:
   print(f"Cannot read image {path}")
   sys.exit()
+# [end-read-image]
 
+
+# [initialize-display]
 # Display the image
 d = get_display()
 d.init(I)
 Display.setTitle(I, "monkey.jpeg")
+# [end-initialize-display]
+
+
+# [display-image]
 Display.display(I)
 Display.flush(I)
+# [end-display-image]
 
+
+# [wait-for-click]
 # Wait for user input
 print("A click to quit...")
 Display.getClick(I)
+# [end-wait-for-click]
