1fb3ed80322186f179a57b7cdaf132ea5e4c3e9f
LAGNEAU Romain <romain.lagneau@inria.fr>
2026-09-09 12:01:44 +0200

    [SKIP-CI ALL][ENABLE-CI PIXI] Restrict the CMake research to the Pixi environment for all CIs except the MSVC one for which we cannot because MSVC is not installed by Pixi

--- development/scripts/pixi/activation_clang_cl.bat
+++ development/scripts/pixi/activation_clang_cl.bat
@@ -2,3 +2,4 @@
 :: Explicit path is necessary to avoid getting the clang-cl compiler installed on system
 set CC="%CONDA_PREFIX%\\Library\\bin\\clang-cl"
 set CXX="%CONDA_PREFIX%\\Library\\bin\\clang-cl"
+set VISP_EXTRA_CMAKE_ARGS="%VISP_EXTRA_CMAKE_ARGS% -DCMAKE_FIND_ROOT_PATH=%CONDA_PREFIX% -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY"
